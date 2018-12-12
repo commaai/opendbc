c68dc7dcfeef102ea3f57146cbed75bdec93b71b
loop-pair <prpl@pillartechnology.com>
2018-12-12 10:10:52 -0500

    [rhuston] Got tesla_can.dbc test implemented.

--- LOOP/python/integration-tests/deviceclient/DeviceClientIntegration-Test.py
+++ LOOP/python/integration-tests/deviceclient/DeviceClientIntegration-Test.py
@@ -6,29 +6,39 @@ import time
 from loop.deviceclient.DeviceClient import DeviceClient
 from loop.loopserver.LoopServer import LoopServer
 
+dbc_files = [
+    'gm_global_a_chassis.dbc',
+    'tesla_can.dbc',
+    'subaru_outback_2016_eyesight.dbc'
+]
 
 class DeviceClientIntegration(unittest.TestCase):
 
     def setUp(self):
         serverIPaddr = os.getenv('LOOPSERVERIPADDR', default='localhost')
         serverUrl = "http://" + serverIPaddr
-        self.device_client = DeviceClient(serverUrl)
+
         self.loop_server = LoopServer(serverUrl)
         self.loop_server.reset_soft()
 
-        catalog_fullname = os.path.join(os.path.dirname(__file__), '../../catalogs/IntegrationTestCatalog.json')
-        with open(catalog_fullname, 'r') as file:
-            self.TestCatalog = file.read().replace('\n', '').replace(' ', '')
-
+        self.device_client = DeviceClient(serverUrl)
         self.device_client.catalogs.delete_all()
-        self.device_client.catalogs.put("TestCatalog",self.TestCatalog)
         self.device_client.catalogs.reload()
 
+        self.test_catalog = None
+
     def tearDown(self):
+        self.device_client.catalogs.delete_all()
         self.device_client.catalogs.load_default()
 
-    def test_catalog_is_setup(self):
-        # this silly test is so there will be some test that runs on MacOS
+    def test_tesla_can_dbc_catalog(self):
+        catalog_fullname = os.path.join(os.path.dirname(__file__), '../../../../tesla_can.dbc')
+        with open(catalog_fullname, 'r') as file:
+            self.test_catalog = file.read().replace('\n', '').replace(' ', '')
+        self.assertIsNotNone(self.test_catalog)
+
+        self.device_client.catalogs.add('dbc', "TestCatalog", self.test_catalog)
+        self.device_client.catalogs.reload()
         loaded_catalogs = [ x['name'] for x in self.device_client.catalogs.catalog_info()['loadStatus']['loaded'] ]
         self.assertIn("TestCatalog", loaded_catalogs, "TestCatalog is loaded")
 
