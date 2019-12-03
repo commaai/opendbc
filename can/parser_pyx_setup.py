571c9d01df8759201f1ff236cbdaf54a8ba1c428
Willem Melching <willem.melching@gmail.com>
2019-12-03 19:45:33 +0100

    Azure pipelines ci (#202)
    * add docker file and sconstruct
    
    * this should work in ci
    
    * remove branch name

--- can/parser_pyx_setup.py
+++ can/parser_pyx_setup.py
@@ -1,11 +1,35 @@
 import os
 import subprocess
+import sysconfig
 from distutils.core import Extension, setup  # pylint: disable=import-error,no-name-in-module
 
 from Cython.Build import cythonize
+from Cython.Distutils import build_ext
 
-from common.basedir import BASEDIR
-from common.cython_hacks import BuildExtWithoutPlatformSuffix
+BASEDIR = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../../"))
+
+
+def get_ext_filename_without_platform_suffix(filename):
+  name, ext = os.path.splitext(filename)
+  ext_suffix = sysconfig.get_config_var('EXT_SUFFIX')
+
+  if ext_suffix == ext:
+    return filename
+
+  ext_suffix = ext_suffix.replace(ext, '')
+  idx = name.find(ext_suffix)
+
+  if idx == -1:
+    return filename
+  else:
+    return name[:idx] + ext
+
+
+class BuildExtWithoutPlatformSuffix(build_ext):
+  def get_ext_filename(self, ext_name):
+    filename = super().get_ext_filename(ext_name)
+    return get_ext_filename_without_platform_suffix(filename)
+
 
 sourcefiles = ['parser_pyx.pyx']
 extra_compile_args = ["-std=c++11"]
