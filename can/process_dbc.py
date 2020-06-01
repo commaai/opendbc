55afc9ae5b785a455bc08c50a6760e849d792493
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2020-06-01 02:03:03 -0700

    enable almost all flake8 checks

--- can/process_dbc.py
+++ can/process_dbc.py
@@ -10,7 +10,7 @@ from opendbc.can.dbc import dbc
 
 def process(in_fn, out_fn):
   dbc_name = os.path.split(out_fn)[-1].replace('.cc', '')
-  #print("processing %s: %s -> %s" % (dbc_name, in_fn, out_fn))
+  # print("processing %s: %s -> %s" % (dbc_name, in_fn, out_fn))
 
   template_fn = os.path.join(os.path.dirname(__file__), "dbc_template.cc")
 
@@ -121,6 +121,7 @@ def main():
   in_fn = os.path.join(dbc_dir, dbc_name + '.dbc')
 
   process(in_fn, out_fn)
+
 
 if __name__ == '__main__':
   main()
