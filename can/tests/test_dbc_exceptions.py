37bacfb912b729839ff1d5179896c18d32d7b2d1
Gregor Kikelj <gregor1234567890@gmail.com>
2020-09-10 13:33:03 +0200

    added exception tests

---
+++ can/tests/test_dbc_exceptions.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import unittest
+
+from opendbc.can.parser import CANParser, CANDefine
+from opendbc.can.packer import CANPacker
+
+class TestCanParserPackerExceptions(unittest.TestCase):
+  def test_civic_exceptions(self):
+    dbc_file = "honda_civic_touring_2016_can_generated"
+    signals = [
+      ("STEER_TORQUE", "STEERING_CONTROL", 0),
+      ("STEER_TORQUE_REQUEST", "STEERING_CONTROL", 0),
+    ]
+    checks=[]
+    try:
+      CANParser(dbc_file+"abcdefgh", signals, checks, 0)
+      self.assertTrue(False)#Should't get to here, due to the new added exception
+    except:
+      pass
+    try:
+      CANPacker(dbc_file+"abcdefgh")
+      self.assertTrue(False)#Should't get to here, due to the new added exception
+    except:
+      pass
+    try:
+      CANDefine(dbc_file+"abcdefgh")
+      self.assertTrue(False)#Should't get to here, due to the new added exception
+    except:
+      pass
+
+    #This should all work
+    CANParser(dbc_file, signals, checks, 0)
+    CANPacker(dbc_file)
+    CANDefine(dbc_file)
+
+ 
+    
+    
+
+if __name__ == "__main__":
+  unittest.main()
