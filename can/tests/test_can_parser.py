404fa00fdd314cebc764fe4fbcc672af21e81c24
Shane Smiskol <shane@smiskol.com>
2022-05-09 13:23:25 -0700

    test ms per line

--- can/tests/test_can_parser.py
+++ can/tests/test_can_parser.py
@@ -5,49 +5,26 @@ import time
 import unittest
 
 from opendbc import DBC_PATH
-from opendbc.can.packer import CANPacker
 from opendbc.can.parser import CANParser
-from opendbc.can.tests.test_packer_parser import can_list_to_can_capnp
-from opendbc.generator.generator import generated_suffix
 
 
 class TestCANParser(unittest.TestCase):
   @classmethod
   def setUpClass(cls):
-    cls.dbcs = []
-    for dbc in glob.glob(f"{DBC_PATH}/*{generated_suffix}"):
-      cls.dbcs.append(os.path.basename(dbc).split('.')[0])
-
-  def test_detecting_dbcs(self):
-    self.assertGreaterEqual(len(self.dbcs), 20)
+    cls.dbcs = {}
+    for dbc in glob.glob(f"{DBC_PATH}/*.dbc"):
+      dbc_name = os.path.basename(dbc).split('.')[0]
+      with open(dbc, 'r') as f:
+        cls.dbcs[dbc_name] = len(f.readlines())
 
   def test_dbc_parsing_speed(self):
-    start_time = time.time()
-    for dbc in self.dbcs:
+    dbc_times = []
+    for dbc, lines in self.dbcs.items():
+      start_time = time.monotonic()
       CANParser(dbc, [], [], 0)
+      dbc_times.append((time.monotonic() - start_time) / lines * 1000)
 
-    elapsed = time.time() - start_time
-    self.assertLess(elapsed, 0.15, "Took too long to parse {} DBCs".format(len(self.dbcs)))
-
-  def test_can_parsing_speed(self):
-    signals = [
-      ("CR_VSM_DecCmd", "SCC12"),
-      ("aReqValue", "SCC12"),
-      ("aReqRaw", "SCC12"),
-    ]
-    checks = [("SCC12", 50)]
-    parser = CANParser("hyundai_kia_generic", signals, checks, 0)
-    packer = CANPacker("hyundai_kia_generic")
-
-    start_time = time.time()
-    for _ in range(10000):
-      msg = packer.make_can_msg("SCC12", 0, {"CR_VSM_DecCmd": 2.55,  # max values
-                                             "aReqValue": 10.24,
-                                             "aReqRaw": 10.24})
-      parser.update_strings([can_list_to_can_capnp([msg])] * 10)
-
-    elapsed = time.time() - start_time
-    self.assertLess(elapsed, 0.7, "Took too long parsing messages")
+    self.assertTrue(max(dbc_times) < 0.15, f'Max ms/line time too high: {max(dbc_times)} > 0.1')
 
 
 if __name__ == "__main__":
