567601da9a8461e5a0b8067dd56a2ac0c6dfc5b4
Shane Smiskol <shane@smiskol.com>
2022-06-13 12:11:56 -0700

    DBC parser: add back checksum start bit and length checks (#636)
    * Revert "remove checksum and counter alignment checks (#634)"
    
    This reverts commit f3232cc32bd276195b3e6eb1555fd436b0922f65.
    
    * add logic to ignore checksum check if -1
    
    * think we need size too

--- can/dbc.cc
+++ can/dbc.cc
@@ -79,11 +79,13 @@ ChecksumState* get_checksum(const std::string& dbc_name) {
 void set_signal_type(Signal& s, ChecksumState* chk, const std::string& dbc_name, int line_num) {
   if (chk) {
     if (s.name == "CHECKSUM") {
-      DBC_ASSERT(s.size == chk->checksum_size, "CHECKSUM is not " << chk->checksum_size << " bits long");
+      DBC_ASSERT(chk->checksum_size == -1 || s.size == chk->checksum_size, "CHECKSUM is not " << chk->checksum_size << " bits long");
+      DBC_ASSERT(chk->checksum_start_bit == -1 || (s.start_bit % 8) == chk->checksum_start_bit, " CHECKSUM starts at wrong bit");
       DBC_ASSERT(s.is_little_endian == chk->little_endian, "CHECKSUM has wrong endianness");
       s.type = chk->checksum_type;
     } else if (s.name == "COUNTER") {
       DBC_ASSERT(chk->counter_size == -1 || s.size == chk->counter_size, "COUNTER is not " << chk->counter_size << " bits long");
+      DBC_ASSERT(chk->counter_start_bit == -1 || (s.start_bit % 8) == chk->counter_start_bit, "COUNTER starts at wrong bit");
       DBC_ASSERT(chk->little_endian == s.is_little_endian, "COUNTER has wrong endianness");
       s.type = chk->counter_type;
     }
