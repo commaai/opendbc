03b2d75df17a69473ef5913a72bd005e4c20aede
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2022-08-20 21:55:35 -0700

    fix toyota

--- can/parser.cc
+++ can/parser.cc
@@ -227,12 +227,12 @@ void CANParser::UpdateCans(uint64_t sec, const capnp::List<cereal::CanData>::Rea
     auto dat = cmsg.getDat();
 
     if (dat.size() > 64) {
-      DEBUG("got message longer than 64 bytes: 0x%X %zu\n", cmsg.getAddress(), dat.size());
+      LOGE("got message longer than 64 bytes: 0x%X %zu\n", cmsg.getAddress(), dat.size());
       continue;
     }
 
     if (dat.size() != state_it->second.size) {
-      DEBUG("got message with unexpected length: expected %d, got %zu for %d", state_it->second.size, dat.size(), cmsg.getAddress());
+      LOGE("got message with unexpected length: expected %d, got %zu for %d", state_it->second.size, dat.size(), cmsg.getAddress());
       continue;
     }
 
