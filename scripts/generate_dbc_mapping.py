d58b3902b2436a78ef5d63652edec4beecc59109
Cameron Clough <cameronjclough@gmail.com>
2022-08-25 13:07:41 -0700

    fix types and path

--- scripts/generate_dbc_mapping.py
+++ scripts/generate_dbc_mapping.py
@@ -1,24 +1,22 @@
 import argparse
 import json
 import os
-from typing import Dict
 
-from common.basedir import BASEDIR
-from selfdrive.car import dbc_dict
 from selfdrive.car.car_helpers import get_interface_attr
+from opendbc import DBC_PATH
 
 
-DBC_JSON_OUT = os.path.join(BASEDIR, "opendbc", "scripts", "dbc.json")
+DBC_JSON_OUT = os.path.join(DBC_PATH, "scripts", "dbc.json")
 
 
 def generate_dbc_json() -> str:
-  all_car_dbc: Dict[str, dbc_dict] = get_interface_attr("DBC", combine_brands=True)
+  all_car_dbc = get_interface_attr("DBC", combine_brands=True)
   dbc_map = {car: d['pt'] for car, d in all_car_dbc.items()}
   return json.dumps(dbc_map, indent=2)
 
 
 if __name__ == "__main__":
-  parser = argparse.ArgumentParser(description="Auto generates car DBC mapping",
+  parser = argparse.ArgumentParser(description="Generate mapping for all cars to DBC names and outputs json file",
                                    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
   parser.add_argument("--out", default=DBC_JSON_OUT, help="Override default generated filename")
