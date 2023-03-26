1bcee0928100f820a35d23994384a1c85d48a1d1
deanlee <deanlee3@gmail.com>
2023-03-26 14:12:31 +0800

    reduce the const of copying signal values in CANParser::query_latest

--- can/parser.cc
+++ can/parser.cc
@@ -33,7 +33,7 @@ int64_t get_raw_value(const std::vector<uint8_t> &msg, const Signal &sig) {
 
 bool MessageState::parse(uint64_t sec, const std::vector<uint8_t> &dat) {
   for (int i = 0; i < parse_sigs.size(); i++) {
-    auto &sig = parse_sigs[i];
+    const auto &sig = parse_sigs[i];
 
     int64_t tmp = get_raw_value(dat, sig);
     if (sig.is_signed) {
@@ -308,14 +308,13 @@ std::vector<SignalValue> CANParser::query_latest() {
 
     for (int i = 0; i < state.parse_sigs.size(); i++) {
       const Signal &sig = state.parse_sigs[i];
-      ret.push_back((SignalValue){
-        .address = state.address,
-        .ts_nanos = state.last_seen_nanos,
-        .name = sig.name,
-        .value = state.vals[i],
-        .all_values = state.all_vals[i],
-      });
-      state.all_vals[i].clear();
+      SignalValue &v = ret.emplace_back();
+      v.address = state.address;
+      v.ts_nanos = state.last_seen_nanos;
+      v.name = sig.name;
+      v.value = state.vals[i];
+      v.all_values = std::move(state.all_vals[i]);
+      state.all_vals[i] = {};
     }
   }
 
