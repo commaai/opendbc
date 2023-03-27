23c05e37cbb82d9f6e52f8def1f397fc6f71e90c
deanlee <deanlee3@gmail.com>
2023-03-27 18:12:10 +0800

    add benchmarks

---
+++ can/tests/benchmark.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+import time
+import cereal.messaging as messaging
+from opendbc.can.parser import CANParser
+from opendbc.can.packer import CANPacker
+
+
+def can_list_to_can_capnp(can_msgs, logMonoTime):
+  dat = messaging.new_message()
+  dat.init('can', len(can_msgs))
+  dat.logMonoTime = logMonoTime
+  for i, can_msg in enumerate(can_msgs):
+    dat.can[i].address = can_msg[0]
+    dat.can[i].busTime = can_msg[1]
+    dat.can[i].dat = bytes(can_msg[2])
+    dat.can[i].src = can_msg[3]
+  return dat.to_bytes()
+
+
+def benchmark(signals, checks, msg):
+  parser = CANParser('toyota_new_mc_pt_generated', signals, checks, 0, False)
+  packer = CANPacker('toyota_new_mc_pt_generated')
+  can_msgs = []
+  for i in range(10000):
+    values = {"ACC_CONTROL": {"ACC_TYPE": 1, "ALLOW_LONG_PRESS": 3}}
+    msgs = [packer.make_can_msg(k, 0, v) for k, v in values.items()]
+    bts = can_list_to_can_capnp(msgs, int(0.01 * i * 1e9))
+    can_msgs.append(bts)
+
+  t1 = time.time()
+  for m in can_msgs:
+    parser.update_string(m)
+  ts = (time.time() - t1) * 1000
+  print('%s: %.3f ms total: %s avg: %f' % (msg, ts, len(can_msgs), ts / len(can_msgs)))
+
+
+def benchmark_parser_one_signal():
+  benchmark([("ACC_TYPE", "ACC_CONTROL")], [('ACC_CONTROL', 10)], 'One Signal')
+
+def benchmark_parser_all_signals():
+  signals = [
+      ("ACCEL_CMD", "ACC_CONTROL"),
+      ("ALLOW_LONG_PRESS", "ACC_CONTROL"),
+      ("ACC_MALFUNCTION", "ACC_CONTROL"),
+      ("RADAR_DIRTY", "ACC_CONTROL"),
+      ("DISTANCE", "ACC_CONTROL"),
+      ("MINI_CAR", "ACC_CONTROL"),
+      ("CANCEL_REQ", "ACC_CONTROL"),
+      ("ACC_CUT_IN", "ACC_CONTROL"),
+      ("PERMIT_BRAKING", "ACC_CONTROL"),
+      ("RELEASE_STANDSTILL", "ACC_CONTROL"),
+      ("ITS_CONNECT_LEAD", "ACC_CONTROL"),
+      ("ACCEL_CMD_ALT", "ACC_CONTROL"),
+      ("CHECKSUM", "ACC_CONTROL"),
+  ]
+  benchmark(signals, [('ACC_CONTROL', 10)], 'All Signals')
+
+
+if __name__ == "__main__":
+  benchmark_parser_all_signals()
+  benchmark_parser_one_signal()
