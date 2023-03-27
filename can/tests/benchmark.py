9cf47bfa446be95e4bbbfa25101db7d2f903aa3d
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2023-03-27 10:40:28 -0700

    cleanup

--- can/tests/benchmark.py
+++ can/tests/benchmark.py
@@ -1,20 +1,9 @@
 #!/usr/bin/env python3
 import time
-import cereal.messaging as messaging
+
 from opendbc.can.parser import CANParser
 from opendbc.can.packer import CANPacker
-
-
-def can_list_to_can_capnp(can_msgs, logMonoTime):
-  dat = messaging.new_message()
-  dat.init('can', len(can_msgs))
-  dat.logMonoTime = logMonoTime
-  for i, can_msg in enumerate(can_msgs):
-    dat.can[i].address = can_msg[0]
-    dat.can[i].busTime = can_msg[1]
-    dat.can[i].dat = bytes(can_msg[2])
-    dat.can[i].src = can_msg[3]
-  return dat.to_bytes()
+from opendbc.can.tests.test_packer_parser import can_list_to_can_capnp
 
 
 def benchmark(signals, checks, msg):
@@ -24,13 +13,13 @@ def benchmark(signals, checks, msg):
   for i in range(10000):
     values = {"ACC_CONTROL": {"ACC_TYPE": 1, "ALLOW_LONG_PRESS": 3}}
     msgs = [packer.make_can_msg(k, 0, v) for k, v in values.items()]
-    bts = can_list_to_can_capnp(msgs, int(0.01 * i * 1e9))
+    bts = can_list_to_can_capnp(msgs, logMonoTime=int(0.01 * i * 1e9))
     can_msgs.append(bts)
 
-  t1 = time.time()
+  t1 = time.process_time()
   for m in can_msgs:
     parser.update_string(m)
-  ts = (time.time() - t1) * 1000
+  ts = (time.process_time() - t1) * 1000
   print('%s: %.3f ms total: %s avg: %f' % (msg, ts, len(can_msgs), ts / len(can_msgs)))
 
 
