409685ddbd3e1f527462c2b7d1c7b61c8abf0527
Shane Smiskol <shane@smiskol.com>
2023-07-07 01:09:24 -0700

    CANDefine: test all DBCs (#888)
    * change addr for ioniq 6
    
    * add new msg
    
    * a test for this
    
    * add to test_dbc_exceptions
    
    * cmt
    
    * fix

--- can/tests/test_dbc_exceptions.py
+++ can/tests/test_dbc_exceptions.py
@@ -4,6 +4,7 @@ import unittest
 
 from opendbc.can.parser import CANParser, CANDefine
 from opendbc.can.packer import CANPacker
+from opendbc.can.tests import TEST_DBC
 
 
 class TestCanParserPackerExceptions(unittest.TestCase):
@@ -21,6 +22,8 @@ class TestCanParserPackerExceptions(unittest.TestCase):
       CANPacker(dbc_invalid)
     with self.assertRaises(RuntimeError):
       CANDefine(dbc_invalid)
+    with self.assertRaises(KeyError):
+      CANDefine(TEST_DBC)
 
     with self.assertRaises(RuntimeError):
       CANParser(dbc_file, signals, [], 0)
