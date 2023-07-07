422bbefb70694995e8d27d8d5120ddd22bcfc32e
Shane Smiskol <shane@smiskol.com>
2023-07-07 00:55:51 -0700

    add to test_dbc_exceptions

--- can/tests/__init__.py
+++ can/tests/__init__.py
@@ -5,3 +5,4 @@ from opendbc import DBC_PATH
 
 ALL_DBCS = [os.path.basename(dbc).split('.')[0] for dbc in
             glob.glob(f"{DBC_PATH}/*.dbc")]
+TEST_DBC = os.path.abspath(os.path.join(os.path.dirname(__file__), "test.dbc"))
