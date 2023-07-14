7f72bdf12c5acac9b2c7c9f95b6779ea8a6d6295
Shane Smiskol <shane@smiskol.com>
2023-07-14 04:19:31 -0700

    add

--- can/packer.cc
+++ can/packer.cc
@@ -43,6 +43,18 @@ CANPacker::CANPacker(const std::string& dbc_name) {
 std::vector<uint8_t> CANPacker::pack(uint32_t address, const std::vector<SignalPackValue> &signals) {
   std::vector<uint8_t> ret(message_lookup[address].size, 0);
 
+  auto msg_it = message_lookup.find(address);
+
+  // Check all signals with non-zero offsets have explicit values
+  for (const auto& dbc_signal : msg_it->second.sigs) {
+    if (dbc_signal.offset != 0) {
+      auto sig_it = std::find_if(signals.begin(), signals.end(), [&dbc_signal](const SignalPackValue& spv) { return spv.name == dbc_signal.name; });
+      if (sig_it == signals.end()) {
+        throw std::runtime_error("CanPacker::pack(): missing non-zero offset signal " + dbc_signal.name + " in address " + std::to_string(address));
+      }
+    }
+  }
+
   // set all values for all given signal/value pairs
   bool counter_set = false;
   for (const auto& sigval : signals) {
@@ -53,7 +65,6 @@ std::vector<uint8_t> CANPacker::pack(uint32_t address, const std::vector<SignalP
       continue;
     }
     const auto &sig = sig_it->second;
-
     int64_t ival = (int64_t)(round((sigval.value - sig.offset) / sig.factor));
     if (ival < 0) {
       ival = (1ULL << sig.size) + ival;
