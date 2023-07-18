f675112eaceb7d3d343ac4e938934e3da09b2530
Shane Smiskol <shane@smiskol.com>
2023-07-17 22:17:11 -0700

    Revert "CANPacker: refactor to avoid undefined signals (#891)" (#899)
    This reverts commit 4b63235fd8faf7fb9a1b869d2a2a52172a30d17d.

--- can/packer_pyx.pyx
+++ can/packer_pyx.pyx
@@ -1,21 +1,30 @@
 # distutils: language = c++
 # cython: c_string_encoding=ascii, language_level=3
 
-from libc.stdint cimport uint8_t, uint32_t
+from libc.stdint cimport uint8_t
 from libcpp.vector cimport vector
+from libcpp.map cimport map
+from libcpp.string cimport string
 
 from .common cimport CANPacker as cpp_CANPacker
-from .common cimport dbc_lookup, SignalPackValue
+from .common cimport dbc_lookup, SignalPackValue, DBC
 
 
 cdef class CANPacker:
-  cdef cpp_CANPacker *packer
+  cdef:
+    cpp_CANPacker *packer
+    const DBC *dbc
+    map[string, int] name_to_address
 
   def __init__(self, dbc_name):
-    if not dbc_lookup(dbc_name):
+    self.dbc = dbc_lookup(dbc_name)
+    if not self.dbc:
       raise RuntimeError(f"Can't lookup {dbc_name}")
 
     self.packer = new cpp_CANPacker(dbc_name)
+    for i in range(self.dbc[0].msgs.size()):
+      msg = self.dbc[0].msgs[i]
+      self.name_to_address[string(msg.name)] = msg.address
 
   cdef vector[uint8_t] pack(self, addr, values):
     cdef vector[SignalPackValue] values_thing
@@ -29,12 +38,12 @@ cdef class CANPacker:
 
     return self.packer.pack(addr, values_thing)
 
-  cpdef make_can_msg(self, name_or_addr, bus, values) except +RuntimeError:
-    cdef uint32_t addr
+  cpdef make_can_msg(self, name_or_addr, bus, values):
+    cdef int addr
     if type(name_or_addr) == int:
       addr = name_or_addr
     else:
-      addr = self.packer.address_from_name(name_or_addr.encode("utf8"))
+      addr = self.name_to_address[name_or_addr.encode("utf8")]
 
     cdef vector[uint8_t] val = self.pack(addr, values)
     return [addr, 0, (<char *>&val[0])[:val.size()], bus]
