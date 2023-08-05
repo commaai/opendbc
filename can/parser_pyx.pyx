164f7a6c378922205b95520657529f4b3ea73bff
Shane Smiskol <shane@smiskol.com>
2023-08-04 23:48:54 -0700

    can combine this loop

--- can/parser_pyx.pyx
+++ can/parser_pyx.pyx
@@ -55,16 +55,13 @@ cdef class CANParser:
       self.ts_nanos[name] = self.ts_nanos[msg.address]
 
     # Convert message names into addresses and check existence in DBC
+    cdef vector[pair[uint32_t, int]] message_v
     for i in range(len(messages)):
       c = messages[i]
       address = c[0] if isinstance(c[0], numbers.Number) else msg_name_to_address.get(c[0])
       if address not in address_to_msg_name:
         raise RuntimeError(f"could not find message {repr(c[0])} in DBC {self.dbc_name}")
-      messages[i] = (address, c[1])
-
-    cdef vector[pair[uint32_t, int]] message_v
-    for msg_address, freq in messages:
-      message_v.push_back((msg_address, freq))
+      message_v.push_back((address, c[1]))
 
     self.can = new cpp_CANParser(bus, dbc_name, message_v)
     self.update_strings([])
