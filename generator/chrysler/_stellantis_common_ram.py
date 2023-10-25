f3cf7010278e6157053e7b3a7aa5d74c8d6d5362
Shane Smiskol <shane@smiskol.com>
2023-10-25 13:47:08 -0700

    draft way to make it work

--- generator/chrysler/_stellantis_common_ram.py
+++ generator/chrysler/_stellantis_common_ram.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import os
 
-chrysler_to_ram = {
+chrysler_to_ram: dict[str, dict[int, int | tuple[int] | tuple[int, int]]] = {
   "_stellantis_common_ram_dt_generated.dbc": {
     258: 35,
     264: 37,
@@ -22,7 +22,7 @@ chrysler_to_ram = {
     820: 657,
   },
   "_stellantis_common_ram_hd_generated.dbc": {
-    571: 570,
+    571: (570, 571),
     678: 629,
   },
 }
@@ -36,15 +36,50 @@ if __name__ == "__main__":
       out_f.write(f'CM_ "Generated from {src}"\n\n')
 
       wrote_addrs = set()
+      cur_msg = []
       for line in in_f.readlines():
-        if line.startswith(('BO_', 'VAL_')):
-          sl = line.split(' ')
-          addr = int(sl[1])
-          wrote_addrs.add(addr)
+        cur_msg.append(line)
 
-          sl[1] = str(addr_lookup.get(addr, addr))
-          line = ' '.join(sl)
-        out_f.write(line)
+        # if line.startswith(('BO_', 'VAL_')):
+        #   cur_msg.append(line)
+        if line.strip() == '':
+          if not len(cur_msg):
+            continue
+
+          if not cur_msg[0].startswith(('BO_', 'VAL_')):
+            continue
+
+          # msg done
+          sl = cur_msg[0].split(' ')
+          addr = int(sl[1])
+
+          new_addrs = addr_lookup.get(addr, addr)
+          if not isinstance(new_addrs, tuple):
+            new_addrs = (new_addrs,)
+
+          for idx, new_addr in enumerate(new_addrs):
+            if idx > 0:
+              sl[2] = sl[2][:-1] + '_ALT:'
+            sl[1] = str(new_addr)
+            cur_msg[0] = ' '.join(sl)
+            out_f.write(''.join(cur_msg) + '\n')
+            wrote_addrs.add(new_addr)
+
+          cur_msg = []
+
+
+        #   new_addrs = addr_lookup.get
+        #   sl = line.split(' ')
+        #   addr = int(sl[1])
+        #   wrote_addrs.add(addr)
+        #
+        #   sl[1] = str(addr_lookup.get(addr, addr))
+        #   print(sl[1])
+        #   # if isinstance(sl[1], tuple):
+        #     # print(sl[1])
+        #   line = ' '.join(sl)
+        #   print(line)
+        # out_f.write(line)
 
       missing_addrs = set(addr_lookup.keys()) - wrote_addrs
-      assert len(missing_addrs) == 0, f"Missing addrs from {src}: {missing_addrs}"
+      # assert len(missing_addrs) == 0, f"Missing addrs from {src}: {missing_addrs}"
