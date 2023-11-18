13024a15635d27e8b7f03d69abb88e70b7a93aaf
Shane Smiskol <shane@smiskol.com>
2023-11-17 23:06:23 -0800

    also can update at bottom since we don't break early

--- can/parser.cc
+++ can/parser.cc
@@ -74,14 +74,13 @@ bool MessageState::parse(uint64_t nanos, const std::vector<uint8_t> &dat) {
 
 
 bool MessageState::update_counter_generic(int64_t v, int cnt_size) {
-  uint8_t old_counter = counter;
-  counter = v;
-  if (((old_counter+1) & ((1 << cnt_size) -1)) != v) {
+  if (((counter + 1) & ((1 << cnt_size) -1)) != v) {
     counter_fail = std::min(counter_fail + 1, MAX_BAD_COUNTER);
-    LOGE("0x%X COUNTER FAIL #%d -- %d -> %d\n", address, counter_fail, old_counter, (int)v);
+    LOGE("0x%X COUNTER FAIL #%d -- %d -> %d\n", address, counter_fail, counter, (int)v);
   } else if (counter_fail > 0) {
     counter_fail--;
   }
+  counter = v;
   return counter_fail < MAX_BAD_COUNTER;
 }
 
