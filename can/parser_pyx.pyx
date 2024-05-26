328bf4fc6ffd6bfd5bd8a28d45350503ca6afc8c
deanlee <deanlee3@gmail.com>
2024-05-26 21:56:04 +0800

    refactor CanParser

--- can/parser_pyx.pyx
+++ can/parser_pyx.pyx
@@ -1,25 +1,63 @@
 # distutils: language = c++
 # cython: c_string_encoding=ascii, language_level=3
 
-from cython.operator cimport dereference as deref, preincrement as preinc
 from libcpp.pair cimport pair
 from libcpp.string cimport string
 from libcpp.vector cimport vector
 from libc.stdint cimport uint32_t
 
 from .common cimport CANParser as cpp_CANParser
-from .common cimport dbc_lookup, SignalValue, DBC
+from .common cimport MessageState as cpp_MessageState
+from .common cimport dbc_lookup, DBC
 
 import numbers
 from collections import defaultdict
+from collections.abc import Mapping
+
+cdef class MessageState:
+  cdef cpp_MessageState *state
+  cdef list signal_names
+
+  @property
+  def names(self):
+    return self.signal_names
+
+  def value(self, name):
+    return self.state.values.at(name).value
+
+  def all_values(self, name):
+    return self.state.values.at(name).all_values
+
+  def ts_nanos(self, name):
+    return self.state.values.at(name).ts_nanos
+
+  @staticmethod
+  cdef create(cpp_MessageState *s):
+    state = MessageState()
+    state.state = s
+    state.signal_names = [it.first.decode("utf-8") for it in s.values]
+    return state
+
+
+class ValueDict(Mapping):
+  def __init__(self, MessageState state, fetch_func):
+    self.state = state
+    self.fetch_func = fetch_func
+
+  def __getitem__(self, key):
+    return self.fetch_func(self.state, key)
+
+  def __iter__(self):
+    return iter(self.state.names)
+
+  def __len__(self):
+    return len(self.state.names)
 
 
 cdef class CANParser:
   cdef:
     cpp_CANParser *can
     const DBC *dbc
-    vector[SignalValue] can_values
-    vector[uint32_t] addresses
 
   cdef readonly:
     dict vl
@@ -48,56 +86,22 @@ cdef class CANParser:
 
       address = m.address
       message_v.push_back((address, c[1]))
-      self.addresses.push_back(address)
-
-      name = m.name.decode("utf8")
-      self.vl[address] = {}
-      self.vl[name] = self.vl[address]
-      self.vl_all[address] = defaultdict(list)
-      self.vl_all[name] = self.vl_all[address]
-      self.ts_nanos[address] = {}
-      self.ts_nanos[name] = self.ts_nanos[address]
 
     self.can = new cpp_CANParser(bus, dbc_name, message_v)
-    self.update_strings([])
+
+    for address, _ in message_v:
+      name = address_to_msg_name[address]
+      state = MessageState.create(self.can.messageState(address))
+      self.vl[name] = self.vl[address] = ValueDict(state, MessageState.value)
+      self.vl_all[name] = self.vl_all[address] = ValueDict(state, MessageState.all_values)
+      self.ts_nanos[name] = self.ts_nanos[address] = ValueDict(state, MessageState.ts_nanos)
 
   def __dealloc__(self):
     if self.can:
       del self.can
 
   def update_strings(self, strings, sendcan=False):
-    for address in self.addresses:
-      self.vl_all[address].clear()
-
-    cdef vector[SignalValue] new_vals
-    cur_address = -1
-    vl = {}
-    vl_all = {}
-    ts_nanos = {}
-    updated_addrs = set()
-
-    self.can.update_strings(strings, new_vals, sendcan)
-    cdef vector[SignalValue].iterator it = new_vals.begin()
-    cdef SignalValue* cv
-    while it != new_vals.end():
-      cv = &deref(it)
-
-      # Check if the address has changed
-      if cv.address != cur_address:
-        cur_address = cv.address
-        vl = self.vl[cur_address]
-        vl_all = self.vl_all[cur_address]
-        ts_nanos = self.ts_nanos[cur_address]
-        updated_addrs.add(cur_address)
-
-      # Cast char * directly to unicode
-      cv_name = <unicode>cv.name
-      vl[cv_name] = cv.value
-      vl_all[cv_name] = cv.all_values
-      ts_nanos[cv_name] = cv.ts_nanos
-      preinc(it)
-
-    return updated_addrs
+    return self.can.update_strings(strings, sendcan)
 
   @property
   def can_valid(self):
