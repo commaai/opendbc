781fe75b672df95b75a435978ab644c5b63d5e8e
Shane Smiskol <shane@smiskol.com>
2024-06-07 17:38:18 -0700

    no -

--- can/packer.cc
+++ can/packer.cc
@@ -44,7 +44,7 @@ CANPacker::CANPacker(const std::string& dbc_name) {
 std::vector<uint8_t> CANPacker::pack(uint32_t address, const std::vector<SignalPackValue> &signals) {
   auto msg_it = message_lookup.find(address);
   if (msg_it == message_lookup.end()) {
-    LOGE("undefined address - %d", address);
+    LOGE("undefined address %d", address);
     return {};
   }
 
