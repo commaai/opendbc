5433e24bff934c0f08a7368ef6d5cd10f00a88bd
AlexandreSato <sato_alexandre@hotmail.com>
2024-08-18 17:03:17 -0300

    move GWM to opendbc

---
+++ opendbc/car/gwm/interface.py
@@ -0,0 +1,25 @@
+from opendbc.car import get_safety_config, structs
+from opendbc.car.interfaces import CarInterfaceBase
+from opendbc.car.gwm.values import CAR
+
+class CarInterface(CarInterfaceBase):
+  @staticmethod
+  def _get_params(ret: structs.CarParams, candidate: CAR, fingerprint, car_fw, experimental_long, docs) -> structs.CarParams:
+    ret.carName = "gwm"
+    ret.radarUnavailable = True
+
+    # Set global parameters
+
+    ret.safetyConfigs = [get_safety_config(car.CarParams.SafetyModel.allOutput,1)]
+
+    # Global lateral tuning defaults, can be overriden per-vehicle
+
+    ret.steerLimitTimer = 0.4
+    ret.steerActuatorDelay = 0.2
+    CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
+
+    # Global longitudinal tuning defaults, can be overridden per-vehicle
+
+    ret.pcmCruise = not ret.openpilotLongitudinalControl
+
+    return ret
