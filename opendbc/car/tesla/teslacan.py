8c9488c1f6dfe941e04a0da260417b8a81d426f5
lukasloetkolben <61192133+lukasloetkolben@users.noreply.github.com>
2024-08-18 19:59:44 -0700

    Tesla Model 3 / Y

---
+++ opendbc/car/tesla/teslacan.py
@@ -0,0 +1,64 @@
+import crcmod
+
+from opendbc.car.common.conversions import Conversions as CV
+from opendbc.car.tesla.values import CANBUS, CarControllerParams
+
+
+class TeslaCAN:
+  def __init__(self, packer, pt_packer):
+    self.packer = packer
+    self.pt_packer = pt_packer
+    self.crc = crcmod.mkCrcFun(0x11d, initCrc=0x00, rev=False, xorOut=0xff)
+
+  @staticmethod
+  def checksum(msg_id, dat):
+    # TODO: get message ID from name instead
+    ret = (msg_id & 0xFF) + ((msg_id >> 8) & 0xFF)
+    ret += sum(dat)
+    return ret & 0xFF
+
+  def create_steering_control(self, angle, enabled, counter):
+    values = {
+      "DAS_steeringAngleRequest": -angle,
+      "DAS_steeringHapticRequest": 0,
+      "DAS_steeringControlType": 1 if enabled else 0,
+      "DAS_steeringControlCounter": counter,
+    }
+
+    data = self.packer.make_can_msg("DAS_steeringControl", CANBUS.party, values)[2]
+    values["DAS_steeringControlChecksum"] = self.checksum(0x488, data[:3])
+    return self.packer.make_can_msg("DAS_steeringControl", CANBUS.party, values)
+
+  def create_longitudinal_commands(self, acc_state, speed, min_accel, max_accel, cnt):
+    values = {
+      "DAS_setSpeed": speed * CV.MS_TO_KPH,
+      "DAS_accState": acc_state,
+      "DAS_aebEvent": 0,
+      "DAS_jerkMin": CarControllerParams.JERK_LIMIT_MIN,
+      "DAS_jerkMax": CarControllerParams.JERK_LIMIT_MAX,
+      "DAS_accelMin": min_accel,
+      "DAS_accelMax": max_accel,
+      "DAS_controlCounter": cnt,
+      "DAS_controlChecksum": 0,
+    }
+    data = self.packer.make_can_msg("DAS_control", CANBUS.party, values)[2]
+    values["DAS_controlChecksum"] = self.checksum(0x2b9, data[:7])
+    return self.packer.make_can_msg("DAS_control", CANBUS.party, values)
+
+  def right_stalk_press(self, counter, position):
+    # TODO: Implement CRC checksum instead of lookup table.
+    if position == 1:  # half up
+      crc_lookup = [166, 164, 178, 141, 163, 161, 61, 25, 172, 69, 22, 108, 169, 207, 209, 219]
+    else:  # neutral position
+      position = 0
+      crc_lookup = [70, 68, 82, 109, 67, 65, 221, 249, 76, 165, 246, 140, 73, 47, 49, 59]
+
+    values = {"SCCM_rightStalkCounter": counter,
+              "SCCM_rightStalkCrc": crc_lookup[counter],
+              "SCCM_rightStalkReserved1": 0,
+              "SCCM_parkButtonStatus": 0,
+              "SCCM_rightStalkReserved2": 0,
+              "SCCM_rightStalkStatus": position,
+              }
+
+    return self.pt_packer.make_can_msg("SCCM_rightStalk", CANBUS.vehicle, values)
--- opendbc/car/tests/test_fw_fingerprint.py
+++ opendbc/car/tests/test_fw_fingerprint.py
@@ -265,7 +265,7 @@ class TestFwFingerprintTiming:
         print(f'get_vin {name} case, query time={self.total_time / self.N} seconds')
 
   def test_fw_query_timing(self, subtests, mocker):
-    total_ref_time = {1: 6.9, 2: 7.5}
+    total_ref_time = {1: 7.1, 2: 7.7}
     brand_ref_times = {
       1: {
         'gm': 1.0,
@@ -278,11 +278,13 @@ class TestFwFingerprintTiming:
         'nissan': 0.8,
         'subaru': 0.65,
         'toyota': 0.7,
+        'tesla': 0.2,
         'volkswagen': 0.65,
       },
       2: {
         'ford': 1.6,
         'hyundai': 1.15,
+        'tesla': 0.2,
       }
     }
 
