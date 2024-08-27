b51f388c2abfd378bc32b3983bed9c8fe4499b96
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2024-08-27 17:43:52 +0200

    Update interface.py

--- opendbc/car/volkswagen/interface.py
+++ opendbc/car/volkswagen/interface.py
@@ -38,7 +38,6 @@ class CarInterface(CarInterfaceBase):
       ret.safetyConfigs = [get_safety_config(structs.CarParams.SafetyModel.volkswagenMeb)]
       ret.enableBsm        = True
       ret.transmissionType = TransmissionType.direct
-      ret.networkLocation  = NetworkLocation.fwdCamera # TODO signal sources: I am connected at gateway/ICAS 1 right now
       ret.steerControlType = structs.CarParams.SteerControlType.angle
       ret.radarUnavailable = False
       #ret.flags |= VolkswagenFlags.STOCK_HCA_PRESENT.value
