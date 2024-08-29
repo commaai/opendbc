b0398c00ee925c1554b28cdaa70c091ce73a609c
Shane Smiskol <shane@smiskol.com>
2024-08-29 12:29:20 -0700

    adjust wind_brake: we definitely achieve 0.3-0.4 m/s^2 deceleration at only 20 mph, and this was a slight down hill!

--- opendbc/car/honda/carcontroller.py
+++ opendbc/car/honda/carcontroller.py
@@ -170,7 +170,7 @@ class CarController(CarControllerBase):
                                                       CS.CP.openpilotLongitudinalControl))
 
     # wind brake from air resistance decel at high speed
-    wind_brake = interp(CS.out.vEgo, [0.0, 2.3, 35.0], [0.001, 0.002, 0.15])
+    wind_brake = interp(CS.out.vEgo, [0.0, 2.3, 10.0, 35.0], [0.001, 0.005, 0.1, 0.15])
     # all of this is only relevant for HONDA NIDEC
     max_accel = interp(CS.out.vEgo, self.params.NIDEC_MAX_ACCEL_BP, self.params.NIDEC_MAX_ACCEL_V)
     # TODO this 1.44 is just to maintain previous behavior
