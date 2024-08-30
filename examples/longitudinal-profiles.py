7cb4b47da350218233d4e31989a4c3854c0c94c5
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2024-08-29 19:14:24 -0700

    start with creep

--- examples/longitudinal-profiles.py
+++ examples/longitudinal-profiles.py
@@ -3,150 +3,175 @@ import io
 import time
 import base64
 import argparse
+import numpy as np
 import matplotlib.pyplot as plt
 from enum import Enum
 from collections import defaultdict
 from dataclasses import dataclass, asdict
 from pathlib import Path
 
-from opendbc.car.structs import CarControl, CarState
+from opendbc.car.structs import CarControl
 from opendbc.car.can_definitions import CanData
 from opendbc.car.panda_runner import PandaRunner
 
 DT = 0.01  # step time (s)
-OUTPUT_PATH = Path(__file__).resolve().parent / "longitudinal_reports"
 
 class Setup(Enum):
   STOPPED = 0
   STEADY_STATE_SPEED = 1
 
 @dataclass
+class Action:
+  accel: float      # m/s^2
+  duration: float   # seconds
+  longControlState: CarControl.Actuators.LongControlState
+
+  def get_msgs(self):
+    return [
+      (t, CarControl(
+        enabled=True,
+        longActive=True,
+        actuators=CarControl.Actuators(
+          accel=self.accel,
+          longControlState=self.longControlState,
+        ),
+      ))
+      for t in np.linspace(0, self.duration, int(self.duration/DT))
+    ]
+
+@dataclass
 class Maneuver:
   description: str
   setup: Setup         # initial state
-
-  _log = defaultdict(list)
-
-  def get_cc(self, t: float) -> CarControl:
-    CC = CarControl(
-      enabled=True,
-      latActive=False,
-      longActive=True,
-
-      actuators=CarControl.Actuators(
-        gas=0.,
-        brake=0.,
-        speed=0.,
-        accel=0.,
-        longControlState=CarControl.Actuators.LongControlState.off,
-      ),
-
-      cruiseControl=CarControl.CruiseControl(
-        cancel=False,
-        resume=False,
-        override=False,
-      ),
-    )
-    return CC
+  actions: list[Action]
 
   def get_msgs(self):
-    for t in range(0, int(1./DT)):
-      yield t, self.get_cc(t)
-
-  def log(self, t, cc: CarControl, cs: CarState) -> None:
-    self._log["t"].append(t)
-    to_log = {"carControl": cc, "carState": cs, "carControl.actuators": cc.actuators, "carControl.cruiseControl": cc.cruiseControl, "carState.cruiseState": cs.cruiseState}
-    for k, v in to_log.items():
-      for k2, v2 in asdict(v).items():
-        self._log[f"{k}.{k2}"].append(v2)
+    t = 0
+    for action in self.actions:
+      for lt, msg in action.get_msgs():
+        t += lt
+        yield t, msg
 
 MANEUVERS = [
   Maneuver(
-    "start from stop",
-    Setup.STOPPED,
-  ),
-  Maneuver(
-    "brake step response: 20mph steady state -> -1m/ss",
-    Setup.STEADY_STATE_SPEED,
-  ),
-  Maneuver(
-    "brake step response: 20mph steady state -> max brake",
-    Setup.STEADY_STATE_SPEED,
-  ),
-  Maneuver(
-    "gas step response: 20mph steady state -> +1m/ss",
-    Setup.STEADY_STATE_SPEED,
-  ),
-  Maneuver(
-    "gas step response: 20mph steady state -> max gas",
-    Setup.STEADY_STATE_SPEED,
-  ),
-  Maneuver(
     "creeping: alternate between +1m/ss and -1m/ss",
     Setup.STOPPED,
+    [
+      Action(1, 1, CarControl.Actuators.LongControlState.pid),
+      Action(-1, 1, CarControl.Actuators.LongControlState.pid),
+      Action(1, 1, CarControl.Actuators.LongControlState.pid),
+      Action(-1, 1, CarControl.Actuators.LongControlState.pid),
+      Action(1, 1, CarControl.Actuators.LongControlState.pid),
+      Action(-1, 1, CarControl.Actuators.LongControlState.pid),
+    ],
   ),
+  # Maneuver(
+  #   "brake step response: 20mph steady state -> -1m/ss",
+  #   Setup.STEADY_STATE_SPEED,
+  # ),
+  # Maneuver(
+  #   "brake step response: 20mph steady state -> max brake",
+  #   Setup.STEADY_STATE_SPEED,
+  # ),
+  # Maneuver(
+  #   "gas step response: 20mph steady state -> +1m/ss",
+  #   Setup.STEADY_STATE_SPEED,
+  # ),
+  # Maneuver(
+  #   "gas step response: 20mph steady state -> max gas",
+  #   Setup.STEADY_STATE_SPEED,
+  # ),
 ]
 
 def main(args):
   with PandaRunner() as (p, CI):
     print("\n\n")
 
+    logs = {}
     for i, m in enumerate(MANEUVERS):
       print(f"Running {i+1}/{len(MANEUVERS)} '{m.description}'")
 
+      log = defaultdict(list)
+      logs[m.description] = log
+
       # cleanup and get into a good state
       print("- setting up")
-      cs = None
-      for _ in range(int(3./DT)):
+      good_cnt = 0
+      for _ in range(int(30./DT)):
         cd = [CanData(addr, dat, bus) for addr, dat, bus in p.can_recv()]
         cs = CI.update([0, cd])
+
+        cc = CarControl(enabled=True)
+        if m.setup == Setup.STOPPED:
+          cc.longActive = True
+          cc.actuators.accel = -1.5
+          cc.actuators.longControlState = CarControl.Actuators.LongControlState.stopping
+          good_cnt = (good_cnt+1) if cs.vEgo < 0.1 else 0
+        elif m.setup == Setup.STEADY_STATE_SPEED:
+          cc.longActive = True
+          cc.actuators.accel = -1.5
+          cc.actuators.longControlState = CarControl.Actuators.LongControlState.pid
+          good_cnt = (good_cnt+1) if 9 < cs.vEgo < 11 else 0
+
+        break
+        if good_cnt > (2./DT):
+          break
+
         _, can_sends = CI.apply(CarControl(enabled=False))
         p.can_send_many(can_sends, timeout=1000)
         time.sleep(DT)
-      #assert not cs.cruiseState.enabled, "Cruise control not disabled"
+      else:
+        print("ERROR: failed to setup ***********")
+        continue
 
       # run the maneuver
       print("- executing maneuver")
-      for t, msg in m.get_msgs():
+      for t, cc in m.get_msgs():
         cd = [CanData(addr, dat, bus) for addr, dat, bus in p.can_recv()]
         cs = CI.update([0, cd])
         #assert cs.canValid, f"CAN went invalid, check connections"
 
-        _, can_sends = CI.apply(msg)
+        _, can_sends = CI.apply(cc)
         #p.can_send_many(can_sends, timeout=20)
 
-        m.log(t, msg, cs)
         time.sleep(DT)
 
-        if len(m._log["t"]) > 100:
-          break
+        log["t"].append(t)
+        to_log = {"carControl": cc, "carState": cs, "carControl.actuators": cc.actuators,
+                  "carControl.cruiseControl": cc.cruiseControl, "carState.cruiseState": cs.cruiseState}
+        for k, v in to_log.items():
+          for k2, v2 in asdict(v).items():
+            log[f"{k}.{k2}"].append(v2)
 
   # ***** write out report *****
 
   def plt2html():
     plt.legend()
     plt.tight_layout(pad=0)
-
     buffer = io.BytesIO()
     plt.savefig(buffer, format='png')
     buffer.seek(0)
     return f"<img src='data:image/png;base64,{base64.b64encode(buffer.getvalue()).decode()}' style='width:100%; max-width:800px;'>\n"
 
-  output_fn = OUTPUT_PATH / f"{CI.CP.carFingerprint}_{time.strftime('%Y%m%d-%H_%M_%S')}.html"
-  OUTPUT_PATH.mkdir(exist_ok=True)
+  output_path = Path(__file__).resolve().parent / "longitudinal_reports"
+  #output_fn = output_path / f"{CI.CP.carFingerprint}_{time.strftime('%Y%m%d-%H_%M_%S')}.html"
+  output_fn = output_path / f"{CI.CP.carFingerprint}.html"
+  output_path.mkdir(exist_ok=True)
   with open(output_fn, "w") as f:
-    f.write(f"<h1>Longitudinal maneuver report</h1>\n")
+    f.write("<h1>Longitudinal maneuver report</h1>\n")
     f.write(f"<h3>{CI.CP.carFingerprint}</h3>\n")
     if args.desc:
       f.write(f"<h3>{args.desc}</h3>")
     for m in MANEUVERS:
-      f.write(f"<div style='border-top: 1px solid #000; margin: 20px 0;'></div>\n")
+      f.write("<div style='border-top: 1px solid #000; margin: 20px 0;'></div>\n")
       f.write(f"<h2>{m.description}</h2>\n")
+
+      log = logs[m.description]
 
       # accel plot
       plt.figure(figsize=(12, 4))
-      plt.plot(m._log["t"], m._log["carState.aEgo"], label='aEgo')
-      plt.plot(m._log["t"], m._log["carControl.actuators.accel"], label='actuators.accel')
+      plt.plot(log["t"], log["carState.aEgo"], label='aEgo')
+      plt.plot(log["t"], log["carControl.actuators.accel"], label='actuators.accel')
       plt.xlabel('Time (s)')
       plt.ylabel('Acceleration (m/s^2)')
       plt.ylim(-2.2, 2.2)
@@ -159,7 +184,7 @@ def main(args):
       for k in ("carControl.enabled", "carState.cruiseState.enabled"):
         plt.rcParams['lines.linewidth'] = 2
         plt.figure(figsize=(12, 1))
-        plt.plot(m._log["t"], m._log[k], label=k)
+        plt.plot(log["t"], log[k], label=k)
         plt.ylim(0.1, 1.1)
         # plt.grid(False)
         # plt.axis('off')
