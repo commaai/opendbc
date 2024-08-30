7403c60fdb338f489b7d8bd95fd279344f10b145
Günther <96002277+DerGue69@users.noreply.github.com>
2024-08-30 12:30:58 +0200

    Update hyundaican.py
    Some cars show an FCA fault when forwarding CF_Lkas_FusionState. Hide FCA Fault

--- opendbc/car/hyundai/hyundaican.py
+++ opendbc/car/hyundai/hyundaican.py
@@ -41,7 +41,7 @@ def create_lkas11(packer, frame, CP, apply_steer, steer_req,
                            CAR.HYUNDAI_AZERA_6TH_GEN, CAR.HYUNDAI_AZERA_HEV_6TH_GEN, CAR.HYUNDAI_CUSTIN_1ST_GEN):
     values["CF_Lkas_LdwsActivemode"] = int(left_lane) + (int(right_lane) << 1)
     values["CF_Lkas_LdwsOpt_USM"] = 2
-
+                             
     # Some cars show an FCA fault when forwarding CF_Lkas_FusionState. Hide FCA Fault:
     values["CF_Lkas_FusionState"] = 0
                              
