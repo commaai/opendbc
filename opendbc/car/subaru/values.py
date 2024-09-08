857eb9941479dcbc89a605a7761497d99a3aaba2
Martin Lillepuu <martin@mlp.ee>
2024-09-08 20:20:55 +0300

    subaru: update driver toruque scaling factor to use nm

--- opendbc/car/subaru/values.py
+++ opendbc/car/subaru/values.py
@@ -17,7 +17,7 @@ class CarControllerParams:
     self.STEER_DELTA_DOWN = 70         # torque decrease per refresh
     self.STEER_DRIVER_ALLOWANCE = 60   # allowed driver torque before start limiting
     self.STEER_DRIVER_MULTIPLIER = 50  # weight driver torque heavily
-    self.STEER_DRIVER_FACTOR = 1       # from dbc
+    self.STEER_DRIVER_FACTOR = 0.012   # from dbc
 
     if CP.flags & SubaruFlags.GLOBAL_GEN2:
       self.STEER_MAX = 1000
