95279b1561f613dad2ad7d0716c609106351107e
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2024-09-12 12:15:21 +0200

    Update carcontroller.py

--- opendbc/car/volkswagen/carcontroller.py
+++ opendbc/car/volkswagen/carcontroller.py
@@ -203,9 +203,12 @@ class CarController(CarControllerBase):
         elif self.long_heartbeat == 221:
           self.long_heartbeat = 360
 
+        distance = 50 # TODO get distance from model
+        desired_gap = 50 # TODO get desired gap from OP
+
         acc_hud_status = self.CCS.acc_hud_status_value(CS.out.cruiseState.available, CS.out.accFaulted, CC.enabled and CS.out.cruiseState.enabled, CC.cruiseControl.override)
         can_sends.append(self.CCS.create_acc_hud_control(self.packer_pt, CANBUS.pt, acc_hud_status, hud_control.setSpeed * CV.MS_TO_KPH, hud_control.leadVisible,
-                                                         hud_control.leadDistanceBars, self.long_heartbeat, CS.esp_hold_confirmation, CS.meb_acc_01_values))
+                                                         hud_control.leadDistanceBars, desired_gap, distance, self.long_heartbeat, CS.esp_hold_confirmation, CS.meb_acc_01_values))
 
       else:
         lead_distance = 0
