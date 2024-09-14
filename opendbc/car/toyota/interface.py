68dc46cf4f867777a58f80c218968d820efc6e71
Shane Smiskol <shane@smiskol.com>
2024-09-13 19:35:39 -0700

    this is fine now, match stock system rate

--- opendbc/car/toyota/interface.py
+++ opendbc/car/toyota/interface.py
@@ -124,10 +124,10 @@ class CarInterface(CarInterfaceBase):
     tune = ret.longitudinalTuning
     if candidate in TSS2_CAR:
       tune.kiBP = [5., 35.]
-      tune.kiV = [1.5, 0.5]
+      tune.kiV = [0.5, 0.5]
       ret.vEgoStopping = 0.25
       ret.vEgoStarting = 0.25
-      ret.stoppingDecelRate = 0.3  # reach stopping target smoothly
+      ret.stoppingDecelRate = 0.6  # reach stopping target smoothly
     else:
       tune.kiBP = [0., 5., 35.]
       tune.kiV = [3.6, 2.4, 1.5]
