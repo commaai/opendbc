9af2e25c2eb9c43a51b7d93b6848cd298d56fe9f
Jason Wen <haibin.wen3@gmail.com>
2024-09-26 13:55:40 -0400

    Apply suggestions from code review
    Co-authored-by: Jason Young <46612682+jyoung8607@users.noreply.github.com>

--- opendbc/car/hyundai/values.py
+++ opendbc/car/hyundai/values.py
@@ -527,14 +527,11 @@ class CAR(Platforms):
     CarSpecs(mass=1950, wheelbase=2.87, steerRatio=14.6),
     flags=HyundaiFlags.RADAR_SCC,
   )
-  # 2023 GV70 Electrified has Variable Gear Ratio (VGR) rack & pinion with rack mounted EPS.
-  # The overall gear/steering ratio is 13.27.
-  # https://www.genesis.com/content/dam/genesis-p2/au/assets/models/electrified-gv70/specs/Genesis_GV70_EV_August_2022_v0.1.pdf
   GENESIS_GV70_ELECTRIFIED_1ST_GEN = HyundaiCanFDPlatformConfig(
     [
       HyundaiCarDocs("Genesis GV70 Electrified (with HDA II) 2023", "Highway Driving Assist II", car_parts=CarParts.common([CarHarness.hyundai_q])),
     ],
-    CarSpecs(mass=2295, wheelbase=2.87, steerRatio=13.27),
+    CarSpecs(mass=2260, wheelbase=2.87, steerRatio=17.1),
     flags=HyundaiFlags.EV,
   )
   GENESIS_G80 = HyundaiPlatformConfig(
