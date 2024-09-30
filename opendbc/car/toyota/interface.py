4634536b1149f308dd9aa1ceccd1c27f0af892ad
Jason Young <jyoung8607@gmail.com>
2024-09-30 12:48:13 -0400

    remove SecOC key reading from Toyota port

--- opendbc/car/toyota/interface.py
+++ opendbc/car/toyota/interface.py
@@ -134,17 +134,6 @@ class CarInterface(CarInterfaceBase):
     # to a negative value, so it won't matter.
     ret.minEnableSpeed = -1. if stop_and_go else MIN_ACC_SPEED
 
-    # Read SecOC key from param
-    # TODO: read this in card/selfdrived
-    # if ret.flags & ToyotaFlags.SECOC.value:
-    #   # FIXME: have to find a different way after the opendbc refactor
-    #   # key = Params().get("SecOCKey", encoding='utf8')
-    #   key = None
-    #   # TODO: show warning
-    #   if key is None:
-    #    key = "0" * 32
-    #   ret.secOCKey = bytes.fromhex(key.strip())
-
     tune = ret.longitudinalTuning
     if candidate in TSS2_CAR:
       tune.kpV = [0.0]
