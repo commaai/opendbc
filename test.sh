3b468db557f900bb8c10f68ea93a90465e67988a
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2024-10-01 16:11:07 -0700

    source venv in test.sh

--- test.sh
+++ test.sh
@@ -6,6 +6,8 @@ cd $DIR
 
 # ensure we're up to date
 uv venv --allow-existing
+source .venv/bin/activate
+
 uv pip install -e .[testing,docs]
 
 uv run scons -j8
@@ -15,4 +17,4 @@ uv run pytest -n8
 
 GREEN='\033[0;32m'
 NC='\033[0m'
-printf "\n${GREEN}All good!${NC}"
+printf "\n${GREEN}All good!${NC} Finished in build, lint, and test in ${SECONDS}s\n"
