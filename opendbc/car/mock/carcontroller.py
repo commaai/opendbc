dc601edef0c7b1dec705f5142a3ef0dbebf71683
Shane Smiskol <shane@smiskol.com>
2024-09-30 20:14:16 -0700

    forgot

--- opendbc/car/mock/carcontroller.py
+++ opendbc/car/mock/carcontroller.py
@@ -3,4 +3,4 @@ from opendbc.car.interfaces import CarControllerBase
 
 class CarController(CarControllerBase):
   def update(self, CC, CS, now_nanos):
-    return CC.actuators, []
+    return CC.actuators.as_builder(), []
