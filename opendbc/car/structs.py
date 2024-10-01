9b9b456e902448decd1a219e13cedc365271d221
Jason Young <jyoung8607@gmail.com>
2024-10-01 16:56:46 -0400

    support for SecOC cars

--- opendbc/car/structs.py
+++ opendbc/car/structs.py
@@ -392,6 +392,13 @@ class CarParams:
 
   wheelSpeedFactor: float = auto_field()  # Multiplier on wheels speeds to computer actual speeds
 
+  securityConfig: 'CarParams.SecurityConfig' = field(default_factory=lambda: CarParams.SecurityConfig())
+
+  @auto_dataclass
+  class SecurityConfig:
+    secOcRequired: bool = auto_field()
+    secOcKeyAvailable: bool = auto_field()
+
   @auto_dataclass
   class LongitudinalPIDTuning:
     kpBP: list[float] = auto_field()
