d0df185a58f170c46319e1673715db30bddd2f52
Shane Smiskol <shane@smiskol.com>
2024-10-01 19:34:29 -0700

    reimagine

--- opendbc/car/nissan/carstate.py
+++ opendbc/car/nissan/carstate.py
@@ -109,6 +109,8 @@ class CarState(CarStateBase):
     can_gear = int(cp.vl["GEARBOX"]["GEAR_SHIFTER"])
     ret.gearShifter = self.parse_gear_shifter(self.shifter_values.get(can_gear, None))
 
+    # stock lkas should be off
+    # TODO: is this needed?
     if self.CP.carFingerprint == CAR.NISSAN_ALTIMA:
       ret.invalidLkasSetting = bool(cp.vl["LKAS_SETTINGS"]["LKAS_ENABLED"])
     else:
--- opendbc/car/structs.py
+++ opendbc/car/structs.py
@@ -75,8 +75,7 @@ class CarState:
   steeringPressed: bool = auto_field()  # if the user is using the steering wheel
   steerFaultTemporary: bool = auto_field()  # temporary EPS fault
   steerFaultPermanent: bool = auto_field()  # permanent EPS fault
-  lkasDisabled: bool = auto_field()  # LKAS being enabled is required to control car
-  invalidLkasSetting: bool = auto_field()  # LKAS being disabled is required to control car
+  invalidLkasSetting: bool = auto_field()  # stock LKAS is incorrectly configured (i.e. on or off)
   stockAeb: bool = auto_field()
   stockFcw: bool = auto_field()
   espDisabled: bool = auto_field()
