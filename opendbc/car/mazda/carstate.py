65dd9a6f3c27b62f39ad35195aed4f418af7affc
Shane Smiskol <shane@smiskol.com>
2024-10-01 21:48:56 -0700

    revert. that's not what it did

--- opendbc/car/mazda/carstate.py
+++ opendbc/car/mazda/carstate.py
@@ -103,8 +103,7 @@ class CarState(CarStateBase):
     # Check if LKAS is disabled due to lack of driver torque when all other states indicate
     # it should be enabled (steer lockout). Don't warn until we actually get lkas active
     # and lose it again, i.e, after initial lkas activation
-    if not ret.invalidLkasSetting:
-      ret.steerFaultTemporary = self.lkas_allowed_speed and lkas_blocked
+    ret.steerFaultTemporary = self.lkas_allowed_speed and lkas_blocked
 
     self.acc_active_last = ret.cruiseState.enabled
 
