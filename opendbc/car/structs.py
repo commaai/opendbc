6d07d693ff8dd7239173887d5b94a762abac2c5c
Shane Smiskol <shane@smiskol.com>
2024-10-01 21:29:23 -0700

    ButtonEvent: rename altButton3 to mainCruise

--- opendbc/car/structs.py
+++ opendbc/car/structs.py
@@ -157,7 +157,7 @@ class CarState:
       cancel = auto()
       altButton1 = auto()
       altButton2 = auto()
-      altButton3 = auto()
+      mainCruise = auto()
       setCruise = auto()
       resumeCruise = auto()
       gapAdjustCruise = auto()
