5ab5aa03d1309218acbc9e36784f4975d990dfdd
lukasloetkolben <61192133+lukasloetkolben@users.noreply.github.com>
2024-10-13 17:43:20 -0700

    - improve fault logic - better standstill signal - always allow steering

--- opendbc/car/tesla/teslacan.py
+++ opendbc/car/tesla/teslacan.py
@@ -24,18 +24,28 @@ class TeslaCAN:
     values["DAS_steeringControlChecksum"] = self.checksum(0x488, data[:3])
     return self.packer.make_can_msg("DAS_steeringControl", CANBUS.party, values)
 
-  def create_longitudinal_command(self, acc_state, accel, cntr):
+  def create_longitudinal_command(self, acc_state, accel, cntr, active):
     values = {
-      "DAS_setSpeed": 0 if accel < 0 else V_CRUISE_MAX,
+      "DAS_setSpeed": 0 if (accel < 0 or not active) else V_CRUISE_MAX,
       "DAS_accState": acc_state,
       "DAS_aebEvent": 0,
       "DAS_jerkMin": CarControllerParams.JERK_LIMIT_MIN,
       "DAS_jerkMax": CarControllerParams.JERK_LIMIT_MAX,
-      "DAS_accelMin": accel,
-      "DAS_accelMax": max(accel, 0),
+      "DAS_accelMin": -0.1 if not active else accel,
+      "DAS_accelMax": 0 if not active else max(accel, 0),
       "DAS_controlCounter": cntr,
       "DAS_controlChecksum": 0,
     }
     data = self.packer.make_can_msg("DAS_control", CANBUS.party, values)[1]
     values["DAS_controlChecksum"] = self.checksum(0x2b9, data[:7])
     return self.packer.make_can_msg("DAS_control", CANBUS.party, values)
+
+  def create_steering_allowed(self, counter):
+    values = {
+      "APS_eacAllow": 1,
+      "APS_eacMonitorCounter": counter,
+    }
+
+    data = self.packer.make_can_msg("APS_eacMonitor", CANBUS.party, values)[1]
+    values["APS_eacMonitorChecksum"] = self.checksum(0x27d, data[:2])
+    return self.packer.make_can_msg("APS_eacMonitor", CANBUS.party, values)
