d092764b15188ea01a511a225b5714941970a8ff
Shane Smiskol <shane@smiskol.com>
2024-10-22 15:39:49 -0700

    consistent

--- opendbc/car/ford/fordcan.py
+++ opendbc/car/ford/fordcan.py
@@ -207,7 +207,7 @@ def create_acc_ui_msg(packer, CAN: CanBus, CP, main_on: bool, enabled: bool, fcw
     values.update({
       "AccStopStat_D_Dsply": 2 if standstill else 0,              # Stopping status text
       "AccMsgTxt_D2_Rq": 0,                                       # ACC text
-      "AccTGap_B_Dsply": show_distance_bars,                      # Show time gap control UI
+      "AccTGap_B_Dsply": 1 if show_distance_bars else 0,          # Show time gap control UI
       "AccFllwMde_B_Dsply": 1 if hud_control.leadVisible else 0,  # Lead indicator
       "AccStopMde_B_Dsply": 1 if standstill else 0,
       "AccWarn_D_Dsply": 0,                                       # ACC warning
