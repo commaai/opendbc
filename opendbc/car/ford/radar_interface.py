b71e8ba0048f3a3ed231a658998eea3ebd3e1328
Shane Smiskol <shane@smiskol.com>
2024-10-23 17:51:25 -0700

    possible

--- opendbc/car/ford/radar_interface.py
+++ opendbc/car/ford/radar_interface.py
@@ -149,7 +149,7 @@ class RadarInterface(RadarInterfaceBase):
       # TODO: can we group into 2 groups?
       scanIndex = msg[f"CAN_SCAN_INDEX_2LSB_{ii:02d}"]
       # i = (ii - 1) * 4 + scanIndex
-      i = (ii - 1) * 4 + look_index
+      i = (ii - 1) * 2 + (1 if scanIndex in (0, 2) else 0)
       # print('pt scanIndex', scanIndex)
       if scanIndex != int(scan_index) & 0b11 and self.frame > 10:
         print('doesn\'t match!', scanIndex, int(scan_index) & 0b11, scan_index)
