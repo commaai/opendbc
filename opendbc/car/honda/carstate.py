0a1e42eac3332605a419bb4b97e1affd0f224d82
zhou jielei <zhou-jielei@gmo.jp>
2024-10-26 15:06:16 +0900

    fix main_on location

--- opendbc/car/honda/carstate.py
+++ opendbc/car/honda/carstate.py
@@ -234,7 +234,7 @@ class CarState(CarStateBase):
 
     ret.brake = cp.vl["VSA_STATUS"]["USER_BRAKE"]
     ret.cruiseState.enabled = cp.vl["POWERTRAIN_DATA"]["ACC_STATUS"] != 0
-    ret.cruiseState.available = bool(cp.vl[self.main_on_sig_msg]["MAIN_ON"])
+    ret.cruiseState.available = bool(cp.vl["SCM_BUTTONS"]["MAIN_ON"])
 
     # Gets rid of Pedal Grinding noise when brake is pressed at slow speeds for some models
     if self.CP.carFingerprint in (CAR.HONDA_PILOT, CAR.HONDA_RIDGELINE):
