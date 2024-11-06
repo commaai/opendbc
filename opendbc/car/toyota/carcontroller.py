cd1a331a4fcc975e1d19af28c3b1839323c6b73f
Shane Smiskol <shane@smiskol.com>
2024-11-06 13:55:42 -0800

    we need to brake when stopping always

--- opendbc/car/toyota/carcontroller.py
+++ opendbc/car/toyota/carcontroller.py
@@ -167,7 +167,7 @@ class CarController(CarControllerBase):
 
       # Along with rate limiting positive jerk below, this greatly improves gas response time
       # Consider the net acceleration request that the PCM should be applying (pitch included)
-      if net_acceleration_request < 0.1:
+      if net_acceleration_request < 0.1 or actuators.longControlState == LongCtrlState.stopping:
         self.permit_braking = True
       elif net_acceleration_request > 0.2:
         self.permit_braking = False
