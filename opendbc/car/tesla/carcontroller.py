e38a109c87cbb4e235ff04e4b8af9b506d2cac95
lukasloetkolben <61192133+lukasloetkolben@users.noreply.github.com>
2024-11-06 15:01:02 +0100

    remove double space

--- opendbc/car/tesla/carcontroller.py
+++ opendbc/car/tesla/carcontroller.py
@@ -43,7 +43,7 @@ class CarController(CarControllerBase):
     if self.CP.openpilotLongitudinalControl and self.frame % 4 == 0:
       state = 4 if not hands_on_fault else 13  # 4=ACC_ON, 13=ACC_CANCEL_GENERIC_SILENT
       accel = clip(actuators.accel, CarControllerParams.ACCEL_MIN, CarControllerParams.ACCEL_MAX)
-      cntr =  (self.frame // 4) % 8
+      cntr = (self.frame // 4) % 8
       can_sends.append(self.tesla_can.create_longitudinal_command(state, accel, cntr, CC.longActive))
 
     # Increment counter so cancel is prioritized even without openpilot longitudinal
