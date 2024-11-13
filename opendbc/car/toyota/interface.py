e727d88ed521890c3565e0b22d5266a32a618e7a
Shane Smiskol <shane@smiskol.com>
2024-11-12 17:21:55 -0800

    no ki

--- opendbc/car/toyota/interface.py
+++ opendbc/car/toyota/interface.py
@@ -136,7 +136,7 @@ class CarInterface(CarInterfaceBase):
     tune = ret.longitudinalTuning
     if candidate in TSS2_CAR:
       tune.kpV = [0.0]
-      tune.kiV = [0.5]
+      tune.kiV = [0.0]
       ret.vEgoStopping = 0.25
       ret.vEgoStarting = 0.25
       ret.stoppingDecelRate = 0.3  # reach stopping target smoothly
