eabf7c94d6ed09d94220105c3d6ef4d0c877141f
Shane Smiskol <shane@smiskol.com>
2024-11-13 15:22:02 -0800

    fix

--- opendbc/car/toyota/carcontroller.py
+++ opendbc/car/toyota/carcontroller.py
@@ -16,7 +16,7 @@ SteerControlType = structs.CarParams.SteerControlType
 VisualAlert = structs.CarControl.HUDControl.VisualAlert
 
 # The up limit allows the brakes/gas to unwind quickly leaving a stop,
-# the down limit matches what the rate of ACCEL_NET, reducing PCM compensation windup
+# the down limit matches the rate of ACCEL_NET, reducing PCM compensation windup
 ACCEL_WINDUP_LIMIT = 0.5  # m/s^2 / frame
 ACCEL_WINDDOWN_LIMIT = -5.0 * DT_CTRL * 3  # m/s^2 / frame
 
