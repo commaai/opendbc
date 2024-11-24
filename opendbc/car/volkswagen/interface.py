6908e86cd5782828e83431b0714905c710a11921
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2024-11-24 17:06:15 +0100

    Update interface.py

--- opendbc/car/volkswagen/interface.py
+++ opendbc/car/volkswagen/interface.py
@@ -95,10 +95,10 @@ class CarInterface(CarInterfaceBase):
       if ret.transmissionType == TransmissionType.manual:
         ret.minEnableSpeed = 4.5
 
-    ret.vEgoStarting = 0.1
-    ret.vEgoStopping = 0.5
     ret.pcmCruise = not ret.openpilotLongitudinalControl
     ret.stopAccel = -0.55
+    ret.vEgoStarting = 0.1
+    ret.vEgoStopping = 0.5
     ret.autoResumeSng = ret.minEnableSpeed == -1
 
     return ret
