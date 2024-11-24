7de43313be78a8f59805083dbf485414813c7121
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2024-11-24 16:18:42 +0100

    Update values.py

--- opendbc/car/volkswagen/values.py
+++ opendbc/car/volkswagen/values.py
@@ -81,7 +81,7 @@ class CarControllerParams:
       self.STEERING_POWER_MAX_BY_SPEED     = 20    # HCA_03 speed in m/s^2 where maximum steering power is reached
       self.CURVATURE_MAX                   = 0.195 # HCA_03 maximum curvature in 1/m
       self.CURVATURE_ERROR                 = 0.01
-      self.ANGLE_RATE_LIMIT_UP             = AngleRateLimit(speed_bp=[0., 5., 15.], angle_v=[0.005, 0.0015, 0.00015]) # curvature safety limit up (max real steering angle change ~ 120-130 deg/s)
+      self.ANGLE_RATE_LIMIT_UP             = AngleRateLimit(speed_bp=[0., 5., 15.], angle_v=[0.005, 0.0015, 0.00015]) # curvature safety limit up
       self.ANGLE_RATE_LIMIT_DOWN           = AngleRateLimit(speed_bp=[0., 5., 15.], angle_v=[0.005, 0.0034, 0.00034]) # curvature safety limit down
       self.CURVATURE_POWER_FACTOR          = 4000
 
