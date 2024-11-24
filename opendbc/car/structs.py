a9429870865fe184b57312f0cb754106adf886d0
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2024-11-24 16:56:43 +0100

    Update structs.py

--- opendbc/car/structs.py
+++ opendbc/car/structs.py
@@ -17,4 +17,3 @@ CarParams = car.CarParams
 CarStateT = capnp.lib.capnp._StructModule
 RadarDataT = capnp.lib.capnp._StructModule
 CarControlT = capnp.lib.capnp._StructModule
-CarParamsT = capnp.lib.capnp._StructModule
