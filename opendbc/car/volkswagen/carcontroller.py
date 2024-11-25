514692c13fd626d42bff43e5a428b07a41fa4050
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2024-11-26 00:18:08 +0100

    Update carcontroller.py

--- opendbc/car/volkswagen/carcontroller.py
+++ opendbc/car/volkswagen/carcontroller.py
@@ -149,11 +149,11 @@ class CarController(CarControllerBase):
 
         # 1 frame of long_override_begin is enough, but lower the possibility of panda safety blocking it for now until we adapt panda safety correctly
         long_override = CC.cruiseControl.override or CS.out.gasPressed
-        self.long_override_counter = self.long_override_counter + 1 if long_override else 0
-        long_override_begin = True if long_override and self.long_override_counter < 5 else False
+        self.long_override_counter = (self.long_override_counter + 1) if long_override else 0
+        long_override_begin = long_override and self.long_override_counter < 5
         # prevents radar faults / retesting
-        self.long_disabled_counter = self.long_disabled_counter + 1 if not CC.enabled else 0
-        long_disabling = True if not CC.enabled and self.long_disabled_counter < 5 else False
+        self.long_disabled_counter = (self.long_disabled_counter + 1) if not CC.enabled else 0
+        long_disabling = not CC.enabled and self.long_disabled_counter < 5
         
         acc_control = self.CCS.acc_control_value(CS.out.cruiseState.available, CS.out.accFaulted, CC.enabled,
                                                  CS.esp_hold_confirmation, long_override, long_disabling)          
