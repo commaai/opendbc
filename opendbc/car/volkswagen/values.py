640b1ca9c6f25129ee63241c5cf7e25f8f6856e3
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2024-12-03 20:03:08 +0100

    Update values.py adaption car params cupra born

--- opendbc/car/volkswagen/values.py
+++ opendbc/car/volkswagen/values.py
@@ -455,8 +455,8 @@ class CAR(Platforms):
     [
       VWCarDocs("CUPRA Born 2021"),
     ],
-    # these car parameters are rough approximations and properties for CUPRA BORN 77kWh
-    VolkswagenCarSpecs(mass=1950, wheelbase=2.766, steerRatio=15.9, rotationalInertia=5228, centerToFront=1383, tireStiffnessFront=120000, tireStiffnessRear=100000),
+    # for CUPRA BORN 77kWh 170 kW, tire stiffness is an approximation
+    VolkswagenCarSpecs(mass=1950, wheelbase=2.766, steerRatio=15.9, centerToFront=1383, tireStiffnessFactor=1.2),
     chassis_codes={"K1"},
     wmis={WMI.SEAT},
   )
