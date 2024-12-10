3d19deb866749a811b3ec926780b549bae1c62d4
elkoled <elkoled@gmail.com>
2024-12-10 15:39:46 +0100

    chg: merge dbc files for simplicity

--- opendbc/car/psa/carstate.py
+++ opendbc/car/psa/carstate.py
@@ -16,7 +16,7 @@ class CarState(CarStateBase):
 
   def update(self, can_parsers) -> structs.CarState:
     cp = can_parsers[Bus.pt]
-    cp_adas = can_parsers[Bus.adas]
+    # cp_adas = can_parsers[Bus.adas] # TODO check if needed
     ret = structs.CarState()
 
     # car speed
@@ -26,28 +26,25 @@ class CarState(CarStateBase):
       cp.vl['Dyn4_FRE']['P265_VehV_VPsvValWhlBckL'],
       cp.vl['Dyn4_FRE']['P266_VehV_VPsvValWhlBckR'],
     )
-    ret.vEgoRaw = cp_adas.vl['HS2_DYN_ABR_38D']['VITESSE_VEHICULE_ROUES'] * CV.KPH_TO_MS # HS2
+    ret.vEgoRaw = cp.vl['HS2_DYN_ABR_38D']['VITESSE_VEHICULE_ROUES'] * CV.KPH_TO_MS # HS2
     ret.vEgo, ret.aEgo = self.update_speed_kf(ret.vEgoRaw)
-    ret.yawRate = cp_adas.vl['HS2_DYN_UCF_MDD_32D']['VITESSE_LACET_BRUTE'] * CV.DEG_TO_RAD # HS2
+    ret.yawRate = cp.vl['HS2_DYN_UCF_MDD_32D']['VITESSE_LACET_BRUTE'] * CV.DEG_TO_RAD # HS2
     ret.standstill = False  # TODO
 
     # gas
-    # TODO: prevents tests to pass
-    # ret.gas = cp_adas.vl['Dyn_CMM']['P002_Com_rAPP'] / 99.5 # HS1
+    ret.gas = cp.vl['Dyn_CMM']['P002_Com_rAPP'] / 99.5 # HS1
     ret.gasPressed = ret.gas > 0  # TODO
 
     # brake
-    # ret.brake = cp.vl['HS2_DYN_UCF_2CD']['AUTO_BRAKING_PRESSURE'] / 50.6 # HS2 alternative
-    # TODO: prevents tests to pass
-    # ret.brake = cp.vl['Dyn2_FRE']['P515_Com_pStSpMstCyl'] / 1500.  # HS1
+    #ret.brake = cp.vl['HS2_DYN_UCF_2CD']['AUTO_BRAKING_PRESSURE'] / 50.6 # HS2 alternative
+    ret.brake = cp.vl['Dyn2_FRE']['P515_Com_pStSpMstCyl'] / 1500.  # HS1
     ret.brakePressed = bool(cp.vl['Dat_BSI']['P013_MainBrake']) # HS1
     ret.parkingBrake = False # TODO bool(cp.vl['Dat_BSI']['PARKING_BRAKE']) is wrong signal
 
     # steering wheel
-    # TODO: prevents tests to pass
-    # ret.steeringAngleDeg = cp.vl['IS_DYN_VOL']['ANGLE_VOLANT'] # EPS
-    # ret.steeringRateDeg = cp.vl['IS_DYN_VOL']['VITESSE_ROT_VOL'] * cp.vl['IS_DYN_VOL']['SENS_ROT_VOL']  # EPS: Rotation speed * rotation sign/direction
-    # ret.steeringTorque = cp.vl['IS_DAT_DIRA']['CPLE_VOLANT']  # EPS: Driver torque
+    ret.steeringAngleDeg = cp.vl['IS_DYN_VOL']['ANGLE_VOLANT'] # EPS
+    ret.steeringRateDeg = cp.vl['IS_DYN_VOL']['VITESSE_ROT_VOL'] * cp.vl['IS_DYN_VOL']['SENS_ROT_VOL']  # EPS: Rotation speed * rotation sign/direction
+    ret.steeringTorque = cp.vl['IS_DAT_DIRA']['CPLE_VOLANT']  # EPS: Driver torque
     ret.steeringPressed = self.update_steering_pressed(abs(ret.steeringTorque) > CarControllerParams.STEER_DRIVER_ALLOWANCE, 5)  # TODO: adjust threshold
     ret.steerFaultTemporary = False  # TODO
     ret.steerFaultPermanent = False  # TODO
@@ -55,10 +52,10 @@ class CarState(CarStateBase):
 
     # cruise
     # note: this is just for CC car not ACC right now
-    ret.cruiseState.speed = cp_adas.vl['HS2_DAT_MDD_CMD_452']['CONS_LIM_VITESSE_VEH'] * CV.KPH_TO_MS # HS2, set to 255 when ACC is off
-    ret.cruiseState.enabled = cp_adas.vl['HS2_DAT_MDD_CMD_452']['DDE_ACTIVATION_RVV_ACC'] == 1 # HS2
+    ret.cruiseState.speed = cp.vl['HS2_DAT_MDD_CMD_452']['CONS_LIM_VITESSE_VEH'] * CV.KPH_TO_MS # HS2, set to 255 when ACC is off
+    ret.cruiseState.enabled = cp.vl['HS2_DAT_MDD_CMD_452']['DDE_ACTIVATION_RVV_ACC'] == 1 # HS2
     ret.cruiseState.available = True  # TODO
-    ret.cruiseState.nonAdaptive = False  # cp_adas.vl['HS2_DAT_MDD_CMD_452']['COCKPIT_GO_ACC_REQUEST'] == 0
+    ret.cruiseState.nonAdaptive = False  # cp.vl['HS2_DAT_MDD_CMD_452']['COCKPIT_GO_ACC_REQUEST'] == 0
     ret.cruiseState.standstill = False  # TODO
     ret.accFaulted = False
 
@@ -76,7 +73,7 @@ class CarState(CarStateBase):
     ret.stockAeb = False
 
     # button presses
-    blinker = cp_adas.vl['HS2_DAT7_BSI_612']['CDE_CLG_ET_HDC'] # HS2
+    blinker = cp.vl['HS2_DAT7_BSI_612']['CDE_CLG_ET_HDC'] # HS2
     ret.leftBlinker = blinker == 1
     ret.rightBlinker = blinker == 2
 
@@ -91,21 +88,21 @@ class CarState(CarStateBase):
     pt_messages = [
       ('Dyn4_FRE', 50),
       ('Dat_BSI', 20),
-      # TODO: prevents tests passing
-      # ('IS_DYN_VOL', 100),
-      # ('IS_DAT_DIRA', 100),
+      ('IS_DYN_VOL', 100),
+      ('IS_DAT_DIRA', 10),
+      ('Dyn2_FRE', 100),
       ('Dyn2_CMM', 50),
+      ('Dyn_CMM', 100),
       # ('RESTRAINTS', 10), # TODO, must be identical to seatbeltUnlatched Signal
-      # TODO: Dyn_CMM missing
-    ]
-    adas_messages = [
       ('HS2_DYN_ABR_38D', 25),
       ('HS2_DYN_UCF_MDD_32D', 50),
-      ('HS2_BGE_DYN5_CMM_228', 100), # TODO no signals in route
+      # ('HS2_BGE_DYN5_CMM_228', 100), # TODO relevant? no signals in route
       ('HS2_DAT_MDD_CMD_452', 20),
       ('HS2_DAT7_BSI_612', 10),
     ]
+    # adas_messages = [
+    # ]
     return {
-      Bus.pt: CANParser(DBC[CP.carFingerprint][Bus.pt], pt_messages, 0),
-      Bus.adas: CANParser(DBC[CP.carFingerprint][Bus.adas], adas_messages, 1) #TODO: check CAN number
+      Bus.pt: CANParser(DBC[CP.carFingerprint][Bus.pt], pt_messages, 0)
+      # Bus.adas: CANParser(DBC[CP.carFingerprint][Bus.adas], adas_messages, 1) #TODO: check CAN number
     }
--- opendbc/car/psa/values.py
+++ opendbc/car/psa/values.py
@@ -36,8 +36,8 @@ class PSACarDocs(CarDocs):
 @dataclass
 class PSAPlatformConfig(PlatformConfig):
   dbc_dict: DbcDict = field(default_factory=lambda: {
-    Bus.pt: 'AEE2010_R3_HS1',
-    Bus.adas: 'AEE2010_R3_HS2'
+    Bus.pt: 'AEE2010_R3',
+    #Bus.adas: 'AEE2010_R3_HS2'
   })
 
 class CAR(Platforms):
@@ -47,11 +47,11 @@ class CAR(Platforms):
     PSACarSpecs(mass=1200, wheelbase=2.538, steerRatio=19.0)
   )
 
-# TODO: the offsets were all negative but breaking the tests, removed negation. Also 0x100 exceeded uint8 subaddress. Please check.
+# TODO: redo RX_offsets and extra_ecus. Negative offsets and subaddress >0xFF (0x100) were breaking the tests.
 PSA_RX_OFFSETS = {
-  # 0x100: (Ecu.fwdCamera, Ecu.engine, Ecu.epb, Ecu.transmission, Ecu.hud, Ecu.vsa, Ecu.hvac, Ecu.hybrid, Ecu.unknown, Ecu.combinationMeter, Ecu.adas, Ecu.telematics, Ecu.gateway, Ecu.eps, Ecu.fwdRadar),
-  0x20: (Ecu.dsu, Ecu.abs, Ecu.parkingAdas, Ecu.electricBrakeBooster, Ecu.srs, Ecu.body, Ecu.programmedFuelInjection, Ecu.shiftByWire, Ecu.cornerRadar),
-  0xC0: (Ecu.debug),
+  # -0x100: (Ecu.fwdCamera, Ecu.engine, Ecu.epb, Ecu.transmission, Ecu.hud, Ecu.vsa, Ecu.hvac, Ecu.hybrid, Ecu.unknown, Ecu.combinationMeter, Ecu.adas, Ecu.telematics, Ecu.gateway, Ecu.eps, Ecu.fwdRadar),
+  # -0x20: (Ecu.dsu, Ecu.abs, Ecu.parkingAdas, Ecu.electricBrakeBooster, Ecu.srs, Ecu.body, Ecu.programmedFuelInjection, Ecu.shiftByWire, Ecu.cornerRadar),
+  # -0xC0: (Ecu.debug),
 }
 
 FW_QUERY_CONFIG = FwQueryConfig(
@@ -65,33 +65,33 @@ FW_QUERY_CONFIG = FwQueryConfig(
     ) for rx_offset, whitelist_ecus in PSA_RX_OFFSETS.items()
   ],
   extra_ecus=[
+    # TODO: Negative offsets and subaddress >0xFF (0x100) were breaking the tests.
     # Note: these ecu names are wrong
-    # TODO: the offsets (0x100, 0x20...) were all negative but breaking the tests, also 0x100 exceeds uint8 subaddress. Please check.
-    # (Ecu.eps, 0x75D, 0x100),
-    (Ecu.abs, 0x6AD, 0x20),
-    # (Ecu.fwdRadar, 0x765, 0x100),
-    # (Ecu.fwdCamera, 0x744, 0x100),
-    # (Ecu.engine, 0x75C, 0x100),
-    # (Ecu.unknown, 0x760, 0x100),
-    # (Ecu.transmission, 0x752, 0x100),
-    # (Ecu.hybrid, 0x771, 0x100),
-    (Ecu.srs, 0x731, 0x20),
-    # (Ecu.gateway, 0x76D, 0x100),
-    # (Ecu.hud, 0x75F, 0x100),
-    # (Ecu.combinationMeter, 0x74A, 0x100),
-    (Ecu.electricBrakeBooster, 0x730, 0x20),
-    (Ecu.shiftByWire, 0x6B5, 0x20),
-    # (Ecu.adas, 0x742, 0x100),
-    (Ecu.cornerRadar, 0x6A8, 0x20),
-    # (Ecu.hvac, 0x773, 0x100),
-    (Ecu.parkingAdas, 0x6B8, 0x20),
-    # (Ecu.epb, 0x778, 0x100),
-    # (Ecu.telematics, 0x764, 0x100),
-    (Ecu.body, 0x6AF, 0x20),
-    (Ecu.dsu, 0x6A9, 0x20),
-    # (Ecu.vsa, 0x746, 0x100),
-    (Ecu.programmedFuelInjection, 0x6BD, 0x20),
-    (Ecu.debug, 0x6C4, 0xC0),
+    # (Ecu.eps, 0x75D, -0x100),
+    # (Ecu.abs, 0x6AD, -0x20),
+    # (Ecu.fwdRadar, 0x765, -0x100),
+    # (Ecu.fwdCamera, 0x744, -0x100),
+    # (Ecu.engine, 0x75C, -0x100),
+    # (Ecu.unknown, 0x760, -0x100),
+    # (Ecu.transmission, 0x752, -0x100),
+    # (Ecu.hybrid, 0x771, -0x100),
+    # (Ecu.srs, 0x731, -0x20),
+    # (Ecu.gateway, 0x76D, -0x100),
+    # (Ecu.hud, 0x75F, -0x100),
+    # (Ecu.combinationMeter, 0x74A, -0x100),
+    # (Ecu.electricBrakeBooster, 0x730, -0x20),
+    # (Ecu.shiftByWire, 0x6B5, -0x20),
+    # (Ecu.adas, 0x742, -0x100),
+    # (Ecu.cornerRadar, 0x6A8, -0x20),
+    # (Ecu.hvac, 0x773, -0x100),
+    # (Ecu.parkingAdas, 0x6B8, -0x20),
+    # (Ecu.epb, 0x778, -0x100),
+    # (Ecu.telematics, 0x764, -0x100),
+    # (Ecu.body, 0x6AF, -0x20),
+    # (Ecu.dsu, 0x6A9, -0x20),
+    # (Ecu.vsa, 0x746, -0x100),
+    # (Ecu.programmedFuelInjection, 0x6BD, -0x20),
+    # (Ecu.debug, 0x6C4, -0xC0),
   ],
 )
 
