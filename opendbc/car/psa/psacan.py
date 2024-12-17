5c98af7c31e7c4c1dfdeb4d84364c48176662309
elkoled <elkoled@gmail.com>
2024-12-17 15:42:26 +0100

    chg: send CAN torque

--- opendbc/car/psa/psacan.py
+++ opendbc/car/psa/psacan.py
@@ -28,44 +28,43 @@ def calculate_checksum(dat: bytearray) -> int:
     needed = (11 - checksum) & 0xF
     return needed
 
-def create_lka_msg(packer, CP, original_lka_values):
-    values = original_lka_values.copy()
-    values['CHECKSUM'] = 0
-    msg = packer.make_can_msg('LANE_KEEP_ASSIST', CanBus(CP).main, values)
-    dat = msg[1]
-    if isinstance(dat, int):
-        dat = dat.to_bytes(1, 'big')
-    values['CHECKSUM'] = calculate_checksum(dat)
-    return packer.make_can_msg('LANE_KEEP_ASSIST', CanBus(CP).main, values)
-
-# def create_lka_msg(packer, CP, apply_steer: float, frame: int, lat_active: bool, max_torque: int, reverse: bool):
-#     # TODO: hud control for lane departure, status
-#     # TODO: unknowns could be recuperation mode/drive mode
-#     # TODO find better way to handle this
-#     max_torque = max_torque if lat_active else 0
-#     # ramp up/down
-#     CanBus(CP).ramp_value = max(min(CanBus(CP).ramp_value + (1 if lat_active else -1), 100), 0)
-
-#     values = {
-#         'unknown1': 0 if reverse else 1,
-#         'COUNTER': (frame//5) % 0x10,
-#         'CHECKSUM': 0,
-#         'unknown2': min(frame // 100, 0x0B), # TODO: check, currently ramps up 1/s up to 0x0B
-#         'TORQUE': max_torque if lat_active else 0,
-#         'LANE_DEPARTURE': 2 if max_torque < 0 else 1 if max_torque > 0 else 0, # TODO: check sign
-#         'LKA_DENY': 0 if lat_active else 1,
-#         'STATUS': 2 if lat_active else 1,
-#         'unknown3': 0, # TODO check
-#         'RAMP': CanBus(CP).ramp_value, # TODO check, currently ramps up 100/s
-#         'ANGLE': clip(apply_steer, -90, 90),
-#         'unknown4': 1,
-#     }
-
-#     msg = packer.make_can_msg('LANE_KEEP_ASSIST', 0, values)
+# def create_lka_msg(packer, CP, original_lka_values):
+#     values = original_lka_values.copy()
+#     values['CHECKSUM'] = 0
+#     msg = packer.make_can_msg('LANE_KEEP_ASSIST', CanBus(CP).main, values)
 #     dat = msg[1]
 #     if isinstance(dat, int):
 #         dat = dat.to_bytes(1, 'big')
-
 #     values['CHECKSUM'] = calculate_checksum(dat)
-
 #     return packer.make_can_msg('LANE_KEEP_ASSIST', CanBus(CP).main, values)
+
+def create_lka_msg(packer, CP, apply_steer: float, steering_angle: float, frame: int, lat_active: bool, max_torque: int, reverse: bool):
+    # TODO: hud control for lane departure, status
+    # TODO: unknowns could be recuperation mode/drive mode
+    # TODO find better way to handle this
+    # ramp up/down
+    CanBus(CP).ramp_value = max(min(CanBus(CP).ramp_value + (1 if lat_active else -1), 100), 0)
+
+    values = {
+        'unknown1': 0 if reverse else 1, # TODO: rename to REVERSE
+        'COUNTER': (frame//5) % 0x10,
+        'CHECKSUM': 0,
+        'unknown2': 0x0B, # TODO: check, currently ramps up 1/s up to 0x0B
+        'TORQUE': apply_steer,
+        'LANE_DEPARTURE': 2 if apply_steer < 0 else 1 if apply_steer > 0 else 0, # 2: Departure left, 1: Departure right, 0: No departure, steer < 0: left, steer > 0: right
+        'LKA_DENY': 0 if apply_steer!=0 else 1, # TODO: check to use lka_active or apply_steer
+        'STATUS': 2 if apply_steer!=0 else 1, # TODO: check to use lka_active or apply_steer
+        'unknown3': 0, # TODO check
+        'RAMP': CanBus(CP).ramp_value, # TODO check, currently ramps up 100/s
+        'ANGLE': steering_angle,
+        'unknown4': 1,
+    }
+
+    msg = packer.make_can_msg('LANE_KEEP_ASSIST', 0, values)
+    dat = msg[1]
+    if isinstance(dat, int):
+        dat = dat.to_bytes(1, 'big')
+
+    values['CHECKSUM'] = calculate_checksum(dat)
+
+    return packer.make_can_msg('LANE_KEEP_ASSIST', CanBus(CP).main, values)
