931370d37f24a4c49a7190881135422b0fc029f4
royjr <royjr96@gmail.com>
2024-12-20 11:39:50 -0500

    stop lfa beeps

--- opendbc/car/hyundai/hyundaicanfd.py
+++ opendbc/car/hyundai/hyundaicanfd.py
@@ -151,6 +151,9 @@ def create_ccnc(packer, CAN, frame, CP, CC, CS):
   if msg_161.get("ALERTS_2") == 5:  # CONSIDER_TAKING_A_BREAK
     msg_161.update({"ALERTS_2": 0, "SOUNDS_2": 0, "DAW_ICON": 0})
 
+  if msg_161.get("SOUNDS_4") == 2 and msg_161.get("LFA_ICON") in (3, 0,):  # LFA BEEPS
+    msg_161["SOUNDS_4"] = 0
+
   # ICONS, LANELINES
   msg_161.update({
     "CENTERLINE": 1 if enabled else 0,
