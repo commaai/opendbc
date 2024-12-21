ff4ea3aac08b1a03f37dcc1b899742f4b285ca74
elkoled <elkoled@gmail.com>
2024-12-21 14:49:24 +0100

    enable lkas

--- opendbc/car/psa/psacan.py
+++ opendbc/car/psa/psacan.py
@@ -28,43 +28,43 @@ def calculate_checksum(dat: bytearray) -> int:
     needed = (11 - checksum) & 0xF
     return needed
 
-def create_lka_msg(packer, CP, original_lka_values):
-    # values = original_lka_values.copy()
-    # values['CHECKSUM'] = 0
-    # msg = packer.make_can_msg('LANE_KEEP_ASSIST', CanBus(CP).main, values)
-    # dat = msg[1]
-    # if isinstance(dat, int):
-    #     dat = dat.to_bytes(1, 'big')
-    # values['CHECKSUM'] = calculate_checksum(dat)
+def create_lka_msg_only_chks(packer, CP, original_lka_values):
+    values = original_lka_values.copy()
+    values['CHECKSUM'] = 0
+    msg = packer.make_can_msg('LANE_KEEP_ASSIST', CanBus(CP).main, values)
+    dat = msg[1]
+    if isinstance(dat, int):
+        dat = dat.to_bytes(1, 'big')
+    values['CHECKSUM'] = calculate_checksum(dat)
     return packer.make_can_msg('LANE_KEEP_ASSIST', CanBus(CP).main, original_lka_values)
 
-# def create_lka_msg(packer, CP, apply_steer: float, steering_angle: float, frame: int, lat_active: bool, max_torque: int, reverse: bool):
-#     # TODO: hud control for lane departure, status
-#     # TODO: unknowns could be recuperation mode/drive mode
-#     # TODO find better way to handle this
-#     # ramp up/down
-#     CanBus(CP).ramp_value = max(min(CanBus(CP).ramp_value + (1 if lat_active else -1), 100), 0)
+def create_lka_msg(packer, CP, apply_steer: float, steering_angle: float, frame: int, lat_active: bool, max_torque: int, reverse: bool):
+    # TODO: hud control for lane departure, status
+    # TODO: unknowns could be recuperation mode/drive mode
+    # TODO find better way to handle this
+    # ramp up/down
+    CanBus(CP).ramp_value = max(min(CanBus(CP).ramp_value + (1 if lat_active else -1), 100), 0)
 
-#     values = {
-#         'unknown1': 0 if reverse else 1, # TODO: rename to REVERSE
-#         'COUNTER': (frame//5) % 0x10,
-#         'CHECKSUM': 0,
-#         'unknown2': 0x0B, # TODO: check, currently ramps up 1/s up to 0x0B
-#         'TORQUE': apply_steer,
-#         'LANE_DEPARTURE': 2 if apply_steer < 0 else 1 if apply_steer > 0 else 0, # 2: Departure left, 1: Departure right, 0: No departure, steer < 0: left, steer > 0: right
-#         'LKA_DENY': 0 if apply_steer!=0 else 1, # TODO: check to use lka_active or apply_steer
-#         'STATUS': 2 if apply_steer!=0 else 1, # TODO: check to use lka_active or apply_steer
-#         'unknown3': 0, # TODO check
-#         'RAMP': CanBus(CP).ramp_value, # TODO check, currently ramps up 100/s
-#         'ANGLE': steering_angle,
-#         'unknown4': 1,
-#     }
+    values = {
+        'unknown1': 0 if reverse else 1, # TODO: rename to REVERSE
+        'COUNTER': (frame//5) % 0x10,
+        'CHECKSUM': 0,
+        'unknown2': 0x0B, # TODO: check, currently ramps up 1/s up to 0x0B
+        'TORQUE': apply_steer,
+        'LANE_DEPARTURE': 2 if apply_steer < 0 else 1 if apply_steer > 0 else 0, # 2: Departure left, 1: Departure right, 0: No departure, steer < 0: left, steer > 0: right
+        'LKA_DENY': 0 if apply_steer!=0 else 1, # TODO: check to use lka_active or apply_steer
+        'STATUS': 2 if apply_steer!=0 else 1, # TODO: check to use lka_active or apply_steer
+        'unknown3': 0, # TODO check
+        'RAMP': CanBus(CP).ramp_value, # TODO check, currently ramps up 100/s
+        'ANGLE': steering_angle,
+        'unknown4': 1,
+    }
 
-#     msg = packer.make_can_msg('LANE_KEEP_ASSIST', 0, values)
-#     dat = msg[1]
-#     if isinstance(dat, int):
-#         dat = dat.to_bytes(1, 'big')
+    msg = packer.make_can_msg('LANE_KEEP_ASSIST', 0, values)
+    dat = msg[1]
+    if isinstance(dat, int):
+        dat = dat.to_bytes(1, 'big')
 
-#     values['CHECKSUM'] = calculate_checksum(dat)
+    values['CHECKSUM'] = calculate_checksum(dat)
 
-#     return packer.make_can_msg('LANE_KEEP_ASSIST', CanBus(CP).main, values)
+    return packer.make_can_msg('LANE_KEEP_ASSIST', CanBus(CP).main, values)
