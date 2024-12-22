409b281cffa50c4d770165846846bff938521a30
elkoled <elkoled@gmail.com>
2024-12-22 18:30:57 +0100

    fix psacan values

--- opendbc/car/psa/psacan.py
+++ opendbc/car/psa/psacan.py
@@ -34,11 +34,11 @@ def create_lka_msg(packer, CP, apply_angle: float, frame: int, lat_active: bool,
         'unknown1': 1 if driving else 0, # TODO: rename to DRIVING
         'COUNTER': (frame // 5) % 0x10,
         'CHECKSUM': 0,
-        'unknown2': 0x8B,# original_lkas_values['unknown2'], # TODO: check if forward is ok, currently ramps up 1/s up to 0x0B
-        'TORQUE': 2043 if lat_active else 0, # TODO: maybe better to do with apply-angle_active or something. oscillates around 2043
+        'unknown2': 0x0B,# original_lkas_values['unknown2'], # TODO: check if forward is ok, currently ramps up 1/s up to 0x0B
+        'TORQUE': 0, # TODO: where did I see this: "oscillates around 2043"
         'LANE_DEPARTURE': 0, # not used in HDA # 2 if apply_steer < 0 else 1 if apply_steer > 0 else 0,
         'STATUS': 4 if lat_active else 2,
-        'LXA_ACTIVATION': 0,
+        'LXA_ACTIVATION': 1, # TODO: either status of LKA or HDA
         'TORQUE_FACTOR': ramp_value, #TODO: rename to torque_factor
         'SET_ANGLE': apply_angle, #TODO: rename dbc to APPLY_ANGLE
         'unknown4': 1,
