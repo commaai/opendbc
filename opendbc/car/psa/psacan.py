4b2017bd6ae5334667a7d661c73d6333b5ccd954
elkoled <elkoled@gmail.com>
2024-12-22 21:19:04 +0100

    cleanup

--- opendbc/car/psa/psacan.py
+++ opendbc/car/psa/psacan.py
@@ -23,27 +23,22 @@ def calculate_checksum(dat: bytearray) -> int:
         high_nibble = b >> 4
         low_nibble = b & 0xF
         checksum += high_nibble + low_nibble
-    # find CHK so that (checksum + CHK) % 16 = 11 (0xB)
     needed = (11 - checksum) & 0xF
     return needed
 
 def create_lka_msg(packer, CP, apply_angle: float, frame: int, lat_active: bool, max_torque: int, ramp_value: int, driving: bool):
-    # Construct message
     values = {
-        # 'unknown1': 1 if driving else 0, # TODO: check if required
-        'COUNTER': (frame // 5) % 0x10, # REQUIRED
-        'CHECKSUM': 0, # REQUIRED
-        'STATUS': 2 if lat_active and ((frame % 15) // 5) == 0 else 3 if ((frame % 15) // 5) == 1 else 4 if lat_active else 4 if not lat_active and ((frame % 15) // 5) == 2 else 3 if ((frame % 15) // 5) == 1 else 2, # REQUIRED
-        'LXA_ACTIVATION': lat_active, # REQUIRED
-        'TORQUE_FACTOR': ramp_value, # REQUIRED
+        'COUNTER': (frame // 5) % 0x10,
+        'CHECKSUM': 0,
+        'STATUS': 2 if lat_active and ((frame % 15) // 5) == 0 else 3 if ((frame % 15) // 5) == 1 else 4 if lat_active else 4 if not lat_active and ((frame % 15) // 5) == 2 else 3 if ((frame % 15) // 5) == 1 else 2,
+        'LXA_ACTIVATION': lat_active,
+        'TORQUE_FACTOR': ramp_value,
         'SET_ANGLE': apply_angle,
-        # 'unknown4': 1, # check if required
     }
 
-    msg = packer.make_can_msg('LANE_KEEP_ASSIST', 0, values)
-    dat = msg[1]
-    if isinstance(dat, int):
-        dat = dat.to_bytes(1, 'big')
-    values['CHECKSUM'] = calculate_checksum(dat)
+    msg = packer.make_can_msg('LANE_KEEP_ASSIST', 0, values)[1]
+    if isinstance(msg, int):
+        msg = msg.to_bytes(1, 'big')
+    values['CHECKSUM'] = calculate_checksum(msg)
 
     return packer.make_can_msg('LANE_KEEP_ASSIST', CanBus(CP).camera, values)
