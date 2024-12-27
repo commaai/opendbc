f2facc50ef5315baf15f9818615d4b2894f122cc
elkoled <elkoled@gmail.com>
2024-12-27 12:55:31 +0100

    clean: psacan

--- opendbc/car/psa/psacan.py
+++ opendbc/car/psa/psacan.py
@@ -18,23 +18,23 @@ class CanBus(CanBusBase):
     return self.offset + 2
 
 def calculate_checksum(dat: bytearray) -> int:
-    checksum = sum((b >> 4) + (b & 0xF) for b in dat)
-    return (11 - checksum) & 0xF
+  checksum = sum((b >> 4) + (b & 0xF) for b in dat)
+  return (11 - checksum) & 0xF
 
 def create_lka_msg(packer, CP, apply_angle: float, frame: int, lat_active: bool, ramp_value: int):
-    values = {
-        'COUNTER': (frame // 5) % 0x10,
-        'CHECKSUM': 0,
-        'STATUS': (((frame // 5) % 3) + 2) if lat_active else 2,  # ramp status 2->3->4->2->3->4, 2: READY, 3: AUTHORIZED, 4: ACTIVE
-        'LXA_ACTIVATION': lat_active,
-        'TORQUE_FACTOR': ramp_value,
-        'SET_ANGLE': apply_angle,
-    }
+  values = {
+    'COUNTER': (frame // 5) % 0x10,
+    'CHECKSUM': 0,
+    'STATUS': (((frame // 5) % 3) + 2) if lat_active else 2,  # ramp status 2->3->4->2->3->4, 2: READY, 3: AUTHORIZED, 4: ACTIVE
+    'LXA_ACTIVATION': lat_active,
+    'TORQUE_FACTOR': ramp_value,
+    'SET_ANGLE': apply_angle,
+  }
 
-    msg = packer.make_can_msg('LANE_KEEP_ASSIST', 0, values)[1]
-    if isinstance(msg, int):
-        msg = msg.to_bytes(1, 'big')
-    values['CHECKSUM'] = calculate_checksum(msg)
+  msg = packer.make_can_msg('LANE_KEEP_ASSIST', 0, values)[1]
+  if isinstance(msg, int):
+    msg = msg.to_bytes(1, 'big')
+  values['CHECKSUM'] = calculate_checksum(msg)
 
-    # TODO: swap CAN 0/2 on harness
-    return packer.make_can_msg('LANE_KEEP_ASSIST', CanBus(CP).camera, values)
+  # TODO: swap CAN 0/2 on harness
+  return packer.make_can_msg('LANE_KEEP_ASSIST', CanBus(CP).camera, values)
