b889098caec49d8b455ab85cbd1023030f1a337f
elkoled <elkoled@gmail.com>
2024-12-27 13:23:11 +0100

    fix

--- opendbc/car/psa/values.py
+++ opendbc/car/psa/values.py
@@ -8,14 +8,14 @@ from opendbc.car.fw_query_definitions import FwQueryConfig, Request, StdQueries
 Ecu = CarParams.Ecu
 
 class CarControllerParams:
-  STEER_STEP = 1  # TODO: sync in panda, LANE_KEEP_ASSIST, 100Hz
+  STEER_STEP = 1  # spam at 100 Hz, lkas is at 20 Hz
 
   STEER_MAX = 360.0  # TODO: test >360°  - Max angle for LKA
   ANGLE_RATE_LIMIT_UP = AngleRateLimit(speed_bp=[0., 5., 15.], angle_v=[10., 1.6, .3])
   ANGLE_RATE_LIMIT_DOWN = AngleRateLimit(speed_bp=[0., 5., 15.], angle_v=[10., 7., 0.8])
   LKAS_MAX_TORQUE = 100  # not used in LCA TODO: verify (max seen is 60, signal max is 2047...)
   STEER_THRESHOLD = 25  # TODO: verify
-  STEER_DRIVER_ALLOWANCE = 10  # Driver intervention threshold
+  STEER_DRIVER_ALLOWANCE = 10  # Driver intervention threshold, Nm/10
 
   def __init__(self, CP):
     pass
