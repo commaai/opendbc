1235859b8d7439c74220bc3347ddb2808fb5fceb
Mike Busuttil <mikebusuttil@gmail.com>
2024-12-29 21:54:08 -0500

    raw scons failure fixed by setting PYTHONPATH

--- test.sh
+++ test.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 set -e
 
-DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
-cd $DIR
-
-# TODO: why doesn't uv do this?
-export PYTHONPATH=$DIR
-
 # *** dependencies install ***
 if ! command -v uv &>/dev/null; then
   echo "'uv' is not installed. Installing 'uv'..."
@@ -17,7 +11,7 @@ uv sync --all-extras
 source .venv/bin/activate
 
 # *** build ***
-uv run scons -j8
+uv run scons -j$(nproc)
 
 # *** lint ***
 # TODO: pre-commit is slow; replace it with openpilot's "op lint"
@@ -25,7 +19,7 @@ uv run scons -j8
 uv run ruff check .
 
 # *** test ***
-uv run pytest -n8
+uv run pytest -n$(nproc)
 
 # *** all done ***
 GREEN='\033[0;32m'
