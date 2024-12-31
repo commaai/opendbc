e2f5557f95a5dcb3a42b9ef20a00e6432143fb01
Mike Busuttil <mikebusuttil@gmail.com>
2024-12-30 23:14:40 -0500

    🐚 back in we go

--- list-contents.py
+++
@@ -1,5 +0,0 @@
-import os
-
-for PATH in ["/opt/homebrew/Cellar/re2/", "/opt/homebrew/Cellar/abseil"]:
-    for file_path in [os.path.join(dp, f) for dp, dn, filenames in os.walk(PATH) for f in filenames]:
-        print(file_path)
