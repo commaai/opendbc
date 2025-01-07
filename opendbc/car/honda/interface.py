ead5118df693563c49d17220b4b7deb30d433a43
zhou jielei <zhou-jielei@gmo.jp>
2025-01-07 14:25:01 +0900

    tune

--- opendbc/car/honda/interface.py
+++ opendbc/car/honda/interface.py
@@ -97,7 +97,7 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.8], [0.24]]
     elif candidate in (CAR.HONDA_ODYSSEY_RC5):
       ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 4096], [0, 4096]]  # TODO: determine if there is a dead zone at the top end
-      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.28], [0.08]]
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.24], [0.08]]
     elif candidate == CAR.HONDA_ACCORD:
       ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 4096], [0, 4096]]  # TODO: determine if there is a dead zone at the top end
 
