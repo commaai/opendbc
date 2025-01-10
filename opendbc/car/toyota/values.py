3d0a3d7b3736b014af58eadae91e2d40e44689b4
Jason Young <46612682+jyoung8607@users.noreply.github.com>
2025-01-09 23:25:35 -0500

    Toyota: Standard SecOC platform config (#1619)

--- opendbc/car/toyota/values.py
+++ opendbc/car/toyota/values.py
@@ -86,6 +86,19 @@ class ToyotaTSS2PlatformConfig(PlatformConfig):
     if self.flags & ToyotaFlags.RADAR_ACC:
       self.dbc_dict = {Bus.pt: 'toyota_nodsu_pt_generated'}
 
+@dataclass
+class ToyotaSecOCPlatformConfig(PlatformConfig):
+  dbc_dict: dict = field(default_factory=lambda: dbc_dict('toyota_secoc_pt_generated', 'toyota_tss2_adas'))
+
+  def init(self):
+    # don't expose car docs until SecOC cars can be suppressed from the comma website
+    self.car_docs = []
+
+    self.flags |= ToyotaFlags.TSS2 | ToyotaFlags.NO_STOP_TIMER | ToyotaFlags.NO_DSU | ToyotaFlags.SECOC
+
+    if self.flags & ToyotaFlags.RADAR_ACC:
+      self.dbc_dict = {Bus.pt: 'toyota_secoc_pt_generated'}
+
 
 class CAR(Platforms):
   # Toyota
@@ -250,13 +263,9 @@ class CAR(Platforms):
     TOYOTA_RAV4_TSS2.specs,
     flags=ToyotaFlags.RADAR_ACC | ToyotaFlags.ANGLE_CONTROL,
   )
-  TOYOTA_RAV4_PRIME = PlatformConfig(
-    # TODO: Enable this docs entry when it can be suppressed from openpilot CARS.md
-    # [ToyotaCarDocs("Toyota RAV4 Prime 2021-23", min_enable_speed=MIN_ACC_SPEED)],
-    [],
+  TOYOTA_RAV4_PRIME = ToyotaSecOCPlatformConfig(
+    [ToyotaCarDocs("Toyota RAV4 Prime 2021-23", min_enable_speed=MIN_ACC_SPEED)],
     CarSpecs(mass=4372. * CV.LB_TO_KG, wheelbase=2.68, steerRatio=16.88, tireStiffnessFactor=0.5533),
-    dbc_dict('toyota_secoc_pt_generated', 'toyota_tss2_adas'),
-    flags=ToyotaFlags.TSS2 | ToyotaFlags.NO_STOP_TIMER | ToyotaFlags.NO_DSU | ToyotaFlags.SECOC,
   )
   TOYOTA_MIRAI = ToyotaTSS2PlatformConfig( # TSS 2.5
     [ToyotaCarDocs("Toyota Mirai 2021")],
@@ -268,13 +277,9 @@ class CAR(Platforms):
     dbc_dict('toyota_tnga_k_pt_generated', 'toyota_adas'),
     flags=ToyotaFlags.NO_STOP_TIMER,
   )
-  TOYOTA_SIENNA_4TH_GEN = PlatformConfig(
-    # TODO: Enable this docs entry when it can be suppressed from openpilot CARS.md
-    # [ToyotaCarDocs("Toyota Sienna 2021-23", min_enable_speed=MIN_ACC_SPEED)],
-    [],
+  TOYOTA_SIENNA_4TH_GEN = ToyotaSecOCPlatformConfig(
+    [ToyotaCarDocs("Toyota Sienna 2021-23", min_enable_speed=MIN_ACC_SPEED)],
     CarSpecs(mass=4625. * CV.LB_TO_KG, wheelbase=3.06, steerRatio=17.8, tireStiffnessFactor=0.444),
-    dbc_dict('toyota_secoc_pt_generated', 'toyota_tss2_adas'),
-    flags=ToyotaFlags.TSS2 | ToyotaFlags.NO_STOP_TIMER | ToyotaFlags.NO_DSU | ToyotaFlags.SECOC,
   )
 
   # Lexus
