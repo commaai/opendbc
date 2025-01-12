1431acebe09225f401e31ec814a9e8ceee948043
AlexandreSato <sato_alexandre@hotmail.com>
2025-01-12 13:03:40 -0300

    use the new generic SecOC carPlatform

--- opendbc/car/toyota/values.py
+++ opendbc/car/toyota/values.py
@@ -267,13 +267,10 @@ class CAR(Platforms):
     [ToyotaCarDocs("Toyota RAV4 Prime 2021-23", min_enable_speed=MIN_ACC_SPEED)],
     CarSpecs(mass=4372. * CV.LB_TO_KG, wheelbase=2.68, steerRatio=16.88, tireStiffnessFactor=0.5533),
   )
-  TOYOTA_YARIS = PlatformConfig(
-    # TODO: Enable this docs entry when it can be suppressed from openpilot CARS.md
-    # [ToyotaCarDocs("Toyota Yaris 2023 (Non-US only)", min_enable_speed=MIN_ACC_SPEED)],
-    [],
+  TOYOTA_YARIS = ToyotaSecOCPlatformConfig(
+    [ToyotaCarDocs("Toyota Yaris 2023 (Non-US only)", min_enable_speed=MIN_ACC_SPEED)],
     CarSpecs(mass=1170, wheelbase=2.55, steerRatio=14.80, tireStiffnessFactor=0.5533),
-    {Bus.pt: 'toyota_rav4_prime_generated'},
-    flags=ToyotaFlags.TSS2 | ToyotaFlags.NO_DSU | ToyotaFlags.SECOC | ToyotaFlags.RADAR_ACC,
+    flags=ToyotaFlags.RADAR_ACC,
   )
   TOYOTA_MIRAI = ToyotaTSS2PlatformConfig( # TSS 2.5
     [ToyotaCarDocs("Toyota Mirai 2021")],
