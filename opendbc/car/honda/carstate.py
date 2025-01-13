1f260b3be01ec69d6e4e758865ae2be43576a78a
Kenneth Porter <kennethporter@Kenneths-MacBook-Air.local>
2025-01-13 14:53:06 -0500

    Add fingerprints and other code for 25 civic

--- opendbc/car/honda/carstate.py
+++ opendbc/car/honda/carstate.py
@@ -92,6 +92,8 @@ class CarState(CarStateBase):
     self.gearbox_msg = "GEARBOX"
     if CP.carFingerprint == CAR.HONDA_ACCORD and CP.transmissionType == TransmissionType.cvt:
       self.gearbox_msg = "GEARBOX_15T"
+    elif CP.carFingerprint == CAR.HONDA_CIVIC_2022 and CP.transmissionType == TransmissionType.cvt:
+      self.gearbox_msg = "GEARBOX_ALT"
 
     self.main_on_sig_msg = "SCM_FEEDBACK"
     if CP.carFingerprint in HONDA_NIDEC_ALT_SCM_MESSAGES:
