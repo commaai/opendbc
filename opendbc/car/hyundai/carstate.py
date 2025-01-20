f109409c3e97c4108a263d5f6d1c585ff0366f3a
ccdunder <cdunder@gmail.com>
2025-01-20 04:37:31 +0000

    fixup 6a150da

--- opendbc/car/hyundai/carstate.py
+++ opendbc/car/hyundai/carstate.py
@@ -239,8 +239,8 @@ class CarState(CarStateBase):
       ret.rightBlindspot = cp.vl["BLINDSPOTS_REAR_CORNERS"]["FR_INDICATOR"] != 0
 
     if self.CP.flags & HyundaiFlags.CCNC and not self.CP.flags & HyundaiFlags.CANFD_HDA2:
-      self.msg_161 = copy.copy(cp.vl["MSG_161"])
-      self.msg_162 = copy.copy(cp.vl["MSG_162"])
+      self.msg_161 = copy.copy(cp_cam.vl["MSG_161"])
+      self.msg_162 = copy.copy(cp_cam.vl["MSG_162"])
 
     # cruise state
     # CAN FD cars enable on main button press, set available if no TCS faults preventing engagement
@@ -325,7 +325,7 @@ class CarState(CarStateBase):
         ("SCC_CONTROL", 50),
       ]
 
-    if self.CP.flags & HyundaiFlags.CCNC and not self.CP.flags & HyundaiFlags.CANFD_HDA2:
+    if CP.flags & HyundaiFlags.CCNC and not CP.flags & HyundaiFlags.CANFD_HDA2:
       cam_messages += [
         ("MSG_161", 20),
         ("MSG_162", 20),
