d6ef59bf4c9fe162a04cab5341cd652316a8989c
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2025-01-28 21:01:14 +0100

    Update carcontroller.py fix data type

--- opendbc/car/volkswagen/carcontroller.py
+++ opendbc/car/volkswagen/carcontroller.py
@@ -221,8 +221,8 @@ class CarController(CarControllerBase):
     new_actuators = actuators.as_builder()
     new_actuators.steer = self.apply_steer_last / self.CCP.STEER_MAX
     new_actuators.steerOutputCan = self.apply_steer_last
-    new_actuators.curvature = self.apply_curvature_last
-    new_actuators.accel = self.accel_last
+    new_actuators.curvature = float(self.apply_curvature_last)
+    new_actuators.accel = float(self.accel_last)
 
     self.gra_acc_counter_last = CS.gra_stock_values["COUNTER"]
     self.frame += 1
