ef19950841a334078f082e8c621f792e0d815a70
Shane Smiskol <shane@smiskol.com>
2025-02-03 20:19:54 -0800

    environ

--- opendbc/car/logging.py
+++ opendbc/car/logging.py
@@ -1,8 +1,10 @@
+import os
 import logging
 
 # set up logging
+LOGPRINT = os.environ.get('LOGPRINT', 'INFO').upper()
 carlog = logging.getLogger('carlog')
-carlog.setLevel(logging.INFO)
+carlog.setLevel(LOGPRINT)
 carlog.propagate = False
 
 handler = logging.StreamHandler()
