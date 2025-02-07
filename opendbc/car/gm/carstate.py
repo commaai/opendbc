97c80b06c7799914a3705c54ef1de92c1add9dcc
Shane Smiskol <shane@smiskol.com>
2025-02-06 17:36:18 -0800

    common func with override

--- opendbc/car/gm/carstate.py
+++ opendbc/car/gm/carstate.py
@@ -33,6 +33,15 @@ class CarState(CarStateBase):
 
     self.distance_button = 0
 
+  def update_button_enable(self, buttonEvents: list[structs.CarState.ButtonEvent]):
+    """ The ECM allows enabling on falling edge of set, but only rising edge of resume """
+    if not self.CP.pcmCruise:
+      for b in buttonEvents:
+        if (b.type == ButtonType.accelCruise and b.pressed) or \
+          (b.type == ButtonType.decelCruise and not b.pressed):
+          return True
+    return False
+
   def update(self, can_parsers) -> structs.CarState:
     pt_cp = can_parsers[Bus.pt]
     cam_cp = can_parsers[Bus.cam]
@@ -148,13 +157,6 @@ class CarState(CarStateBase):
         *create_button_events(self.distance_button, prev_distance_button,
                               {1: ButtonType.gapAdjustCruise})
       ]
-
-      if not self.CP.pcmCruise:
-        # The ECM allows enabling on falling edge of set, but only rising edge of resume
-        for b in ret.buttonEvents:
-          if (b.type == ButtonType.accelCruise and b.pressed) or \
-            (b.type == ButtonType.decelCruise and not b.pressed):
-            ret.buttonEnable = True
 
     return ret
 
