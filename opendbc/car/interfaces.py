ae56008207480d562af37ef48e0e02d2538a3bb3
Shane Smiskol <shane@smiskol.com>
2025-02-06 17:37:36 -0800

    Revert "this is common"
    This reverts commit 93ec5f9290a0dec335ee5890846cbeafe0fe8cc9.

--- opendbc/car/interfaces.py
+++ opendbc/car/interfaces.py
@@ -244,8 +244,7 @@ class CarInterfaceBase(ABC):
     if ret.cruiseState.speedCluster == 0:
       ret.cruiseState.speedCluster = ret.cruiseState.speed
 
-    if not self.CP.pcmCruise:
-      ret.buttonEnable = self.CS.update_button_enable(ret.buttonEvents)
+    ret.buttonEnable = self.CS.update_button_enable(ret.buttonEvents)
 
     # save for next iteration
     self.CS.out = ret
@@ -350,10 +349,11 @@ class CarStateBase(ABC):
     return bool(left_blinker_stalk or self.left_blinker_cnt > 0), bool(right_blinker_stalk or self.right_blinker_cnt > 0)
 
   def update_button_enable(self, buttonEvents: list[structs.CarState.ButtonEvent]):
-    for b in buttonEvents:
-      # Enable OP long on falling edge of enable buttons
-      if b.type in (ButtonType.accelCruise, ButtonType.decelCruise) and not b.pressed:
-        return True
+    if not self.CP.pcmCruise:
+      for b in buttonEvents:
+        # Enable OP long on falling edge of enable buttons
+        if b.type in (ButtonType.accelCruise, ButtonType.decelCruise) and not b.pressed:
+          return True
     return False
 
   @staticmethod
