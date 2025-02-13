a8029c3fb5d197766073e1f5ee08315e7d47eb45
Shane Smiskol <shane@smiskol.com>
2025-02-12 22:49:09 -0800

    fix coverage

--- opendbc/safety/tests/test.sh
+++ opendbc/safety/tests/test.sh
@@ -5,7 +5,7 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 cd $DIR
 
 # reset coverage data and generate gcc note file
-rm -f ../libsafety/*.gcda
+rm -f libsafety/*.gcda
 scons -j$(nproc) -D --coverage
 
 # run safety tests and generate coverage data
@@ -13,13 +13,13 @@ pytest test_*.py
 
 # generate and open report
 if [ "$1" == "--report" ]; then
-  geninfo ../libsafety/ -o coverage.info
+  geninfo libsafety/ -o coverage.info
   genhtml coverage.info -o coverage-out
   sensible-browser coverage-out/index.html
 fi
 
 # test coverage
-GCOV_OUTPUT=$(gcov -n ../libsafety/safety.c)
+GCOV_OUTPUT=$(gcov -n libsafety/safety.c)
 INCOMPLETE_COVERAGE=$(echo "$GCOV_OUTPUT" | paste -s -d' \n' | grep -E "File.*(\/safety\/safety_.*)|(safety)\.h" | grep -v "100.00%" || true)
 if [ -n "$INCOMPLETE_COVERAGE" ]; then
   echo "FAILED: Some files have less than 100% coverage:"
