e43ba073125e9ff19fabf2d5031262340255a404
vanillagorillaa <31773928+vanillagorillaa@users.noreply.github.com>
2025-02-16 12:18:12 -0600

    remove

--- opendbc/car/honda/fingerprints.py
+++ opendbc/car/honda/fingerprints.py
@@ -916,17 +916,9 @@ FW_VERSIONS = {
     (Ecu.srs, 0x18da53f1, None): [
       b'77959-3S5-A920\x00\x00',
     ],
-    (Ecu.combinationMeter, 0x18da60f1, None): [
-      b'78108-3S5-AC20\x00\x00',
-      b'78108-3S5-A220\x00\x00',
-    ],
     (Ecu.vsa, 0x18da28f1, None): [
       b'57114-3S5-CA30\x00\x00',
       b'57114-3S5-CB30\x00\x00'
-    ],
-    (Ecu.programmedFuelInjection, 0x18da10f1, None): [
-      b'37805-6LM-A020\x00\x00',
-      b'37805-6LM-A520\x00\x00',
     ],
   },
 }
