f82de4ffcd8e9db13ff57b13d81fb83b3804717d
Shane Smiskol <shane@smiskol.com>
2025-02-16 22:30:01 -0800

    Tesla: lower steering actuator delay (#1735)
    0.1

--- opendbc/car/tesla/interface.py
+++ opendbc/car/tesla/interface.py
@@ -16,7 +16,7 @@ class CarInterface(CarInterfaceBase):
     #ret.safetyConfigs = [get_safety_config(structs.CarParams.SafetyModel.tesla)]
 
     ret.steerLimitTimer = 1.0
-    ret.steerActuatorDelay = 0.25
+    ret.steerActuatorDelay = 0.1
 
     ret.steerControlType = structs.CarParams.SteerControlType.angle
     ret.radarUnavailable = True
