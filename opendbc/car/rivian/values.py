a24b8fe49e5aed386b19d260913cb98e4c75ece7
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2025-02-16 19:36:26 -0800

    add exp long

--- opendbc/car/rivian/values.py
+++ opendbc/car/rivian/values.py
@@ -1,4 +1,3 @@
-from enum import IntFlag
 from dataclasses import dataclass, field
 
 from opendbc.car.structs import CarParams
@@ -17,7 +16,7 @@ class RivianCarDocs(CarDocs):
 
 class CAR(Platforms):
   RIVIAN_R1_GEN1 = PlatformConfig(
-    # TODO: verify this and add harness
+    # TODO: verify this
     [RivianCarDocs("Rivian R1S 2022-24"), RivianCarDocs("Rivian R1T 2022-24")],
     CarSpecs(mass=3206., wheelbase=3.08, steerRatio=15.2),
     {Bus.pt: 'rivian_can'}
@@ -48,9 +47,5 @@ class CarControllerParams:
 
   ACCEL_MIN = -3.48  # m/s^2
   ACCEL_MAX = 2.0  # m/s^2
-
-class RivianFlags(IntFlag):
-  FLAG_RIVIAN_LONG_CONTROL = 1
-
 
 DBC = CAR.create_dbc_map()
