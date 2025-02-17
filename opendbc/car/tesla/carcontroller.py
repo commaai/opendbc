5aa40a8c2af00d0078297af9a35342fd80e49f8e
Shane Smiskol <shane@smiskol.com>
2025-02-16 22:27:21 -0800

    more obvious the if else here

--- opendbc/car/tesla/carcontroller.py
+++ opendbc/car/tesla/carcontroller.py
@@ -40,16 +40,18 @@ class CarController(CarControllerBase):
       can_sends.append(self.tesla_can.create_steering_allowed((self.frame // 10) % 16))
 
     # Longitudinal control
-    if self.CP.openpilotLongitudinalControl and self.frame % 4 == 0:
-      state = 13 if cruise_cancel else 4  # 4=ACC_ON, 13=ACC_CANCEL_GENERIC_SILENT
-      accel = float(np.clip(actuators.accel, CarControllerParams.ACCEL_MIN, CarControllerParams.ACCEL_MAX))
-      cntr = (self.frame // 4) % 8
-      can_sends.append(self.tesla_can.create_longitudinal_command(state, accel, cntr, CC.longActive))
+    if self.CP.openpilotLongitudinalControl:
+      if self.frame % 4 == 0:
+        state = 13 if cruise_cancel else 4  # 4=ACC_ON, 13=ACC_CANCEL_GENERIC_SILENT
+        accel = float(np.clip(actuators.accel, CarControllerParams.ACCEL_MIN, CarControllerParams.ACCEL_MAX))
+        cntr = (self.frame // 4) % 8
+        can_sends.append(self.tesla_can.create_longitudinal_command(state, accel, cntr, CC.longActive))
 
-    # Increment counter so cancel is prioritized even without openpilot longitudinal
-    if cruise_cancel and not self.CP.openpilotLongitudinalControl:
-      cntr = (CS.das_control["DAS_controlCounter"] + 1) % 8
-      can_sends.append(self.tesla_can.create_longitudinal_command(13, 0,  cntr, False))
+    else:
+      # Increment counter so cancel is prioritized even without openpilot longitudinal
+      if cruise_cancel:
+        cntr = (CS.das_control["DAS_controlCounter"] + 1) % 8
+        can_sends.append(self.tesla_can.create_longitudinal_command(13, 0,  cntr, False))
 
     # TODO: HUD control
     new_actuators = actuators.as_builder()
