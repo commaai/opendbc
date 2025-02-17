cb6d627326ee50011c66a3bad8decacc7f816ce7
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2025-02-16 19:38:19 -0800

    Rivian: little fixups (#1728)
    * rename and add r1t
    
    * add harness
    
    * add exp long

--- opendbc/car/docs_definitions.py
+++ opendbc/car/docs_definitions.py
@@ -138,6 +138,7 @@ class CarHarness(EnumBase):
   ford_q3 = BaseCarHarness("Ford Q3 connector")
   ford_q4 = BaseCarHarness("Ford Q4 connector", parts=[Accessory.harness_box, Accessory.comma_power_v2, Cable.rj45_cable_7ft, Cable.long_obdc_cable,
                                                        Cable.usbc_coupler])
+  rivian = BaseCarHarness("Rivian connector")
 
 
 class Device(EnumBase):
