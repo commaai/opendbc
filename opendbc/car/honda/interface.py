84d410e1c872fb5d2c5d189f235ddf32663368a9
Joshua Mack <joshnoodle@gmail.com>
2025-02-17 19:04:13 -0500

    Try without kpBP

--- opendbc/car/honda/interface.py
+++ opendbc/car/honda/interface.py
@@ -100,7 +100,7 @@ class CarInterface(CarInterfaceBase):
 
     elif candidate == CAR.HONDA_CIVIC_2022:
       ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 4096], [0, 4096]]  # TODO: determine if there is a dead zone at the top end
-      ret.lateralTuning.pid.kpBP, ret.lateralTuning.pid.kpV = [[0, 20], [0.4, 0.6]]
+      ret.lateralTuning.pid.kpV = [0.5]
       ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kiV = [[0, 20], [0.05, 0.125]]
 
     elif candidate == CAR.HONDA_ACCORD:
