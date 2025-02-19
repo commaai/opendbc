c49eda2e180c1266f57315fe8a719c44983c49a6
Shane Smiskol <shane@smiskol.com>
2025-02-19 01:21:11 -0800

    Revert "no"
    This reverts commit 39e10a045a4a5411a64de791ae463461f8a5f37b.

--- opendbc/safety/tests/mutation.sh
+++ opendbc/safety/tests/mutation.sh
@@ -11,10 +11,9 @@ GIT_ROOT=$(git rev-parse --show-toplevel)
 MULL_OPS="mutators: [cxx_increment, cxx_decrement, cxx_comparison, cxx_boundary, cxx_bitwise_assignment, cxx_bitwise, cxx_arithmetic_assignment, cxx_arithmetic, cxx_remove_negation]"
 echo -e "$MULL_OPS" > $GIT_ROOT/mull.yml
 scons --mutation -j$(nproc) -D
-#echo -e "timeout: 10000\ngitDiffRef: $GIT_REF\ngitProjectRoot: $GIT_ROOT" >> $GIT_ROOT/mull.yml
+echo -e "timeout: 10000\ngitDiffRef: $GIT_REF\ngitProjectRoot: $GIT_ROOT" >> $GIT_ROOT/mull.yml
 
 SAFETY_MODELS=$(find * | grep "^test_.*\.py")
-echo -e "Safety models found: $SAFETY_MODELS"
 for safety_model in ${SAFETY_MODELS[@]}; do
   echo ""
   echo ""
