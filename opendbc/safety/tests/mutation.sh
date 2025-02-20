da79307124f6a1305cb273082934fb84739e329f
Shane Smiskol <shane@smiskol.com>
2025-02-19 22:33:44 -0800

    add safety mutation test (#1756)
    * safety mutation test
    
    * use pytest
    
    * try this
    
    * debug
    
    * ?
    
    * ooh
    
    * install
    
    * fix?
    
    * test it's running
    
    * need
    
    * faster?
    
    * try everything
    
    * test mutat
    
    * does it fail here?
    
    * try
    
    * 0?
    
    * loadfile
    
    * fix
    
    * try
    
    * try
    
    * ignore misra!
    
    * fix
    
    * what's the default?
    
    * testtt
    
    * try 16
    
    * it's fast now
    
    * no
    
    * 1m
    
    * clean up
    
    * probably doesn't matter now
    
    * no .
    
    * even need this?
    
    * and this still works?
    
    * yep!

--- opendbc/safety/tests/mutation.sh
+++ opendbc/safety/tests/mutation.sh
@@ -13,10 +13,4 @@ echo -e "$MULL_OPS" > $GIT_ROOT/mull.yml
 scons --mutation -j$(nproc) -D
 echo -e "timeout: 10000\ngitDiffRef: $GIT_REF\ngitProjectRoot: $GIT_ROOT" >> $GIT_ROOT/mull.yml
 
-SAFETY_MODELS=$(find * | grep "^test_.*\.py")
-for safety_model in ${SAFETY_MODELS[@]}; do
-  echo ""
-  echo ""
-  echo -e "Testing mutations on : $safety_model"
-  mull-runner-17 --ld-search-path /lib/x86_64-linux-gnu/ ./libsafety/libsafety.so -test-program=./$safety_model
-done
+mull-runner-17 --ld-search-path /lib/x86_64-linux-gnu/ ./libsafety/libsafety.so -test-program=pytest -- -n8
