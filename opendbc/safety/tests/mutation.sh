789874c2d0371111cf6d408f10aad2e088467487
Shane Smiskol <shane@smiskol.com>
2025-02-19 21:22:13 -0800

    faster?

--- opendbc/safety/tests/mutation.sh
+++ opendbc/safety/tests/mutation.sh
@@ -18,5 +18,5 @@ for safety_model in ${SAFETY_MODELS[@]}; do
   echo ""
   echo ""
   echo -e "Testing mutations on: $safety_model"
-  mull-runner-17 --ld-search-path /lib/x86_64-linux-gnu/ ./libsafety/libsafety.so -test-program=$(which pytest) -- -n8 $safety_model -s
+  mull-runner-17 --ld-search-path /lib/x86_64-linux-gnu/ ./libsafety/libsafety.so --workers=8 -test-program=$(which pytest) -- -n8 $safety_model -s
 done
