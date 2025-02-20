5ac9dbe94cd3d5ec88559f4bfe11835f6f07bdd6
Shane Smiskol <shane@smiskol.com>
2025-02-19 21:25:01 -0800

    try everything

--- opendbc/safety/tests/mutation.sh
+++ opendbc/safety/tests/mutation.sh
@@ -14,9 +14,9 @@ scons --mutation -j$(nproc) -D
 echo -e "timeout: 10000\ngitDiffRef: $GIT_REF\ngitProjectRoot: $GIT_ROOT" >> $GIT_ROOT/mull.yml
 
 SAFETY_MODELS=$(find * | grep "^test_.*\.py")
-for safety_model in ${SAFETY_MODELS[@]}; do
-  echo ""
-  echo ""
-  echo -e "Testing mutations on: $safety_model"
-  mull-runner-17 --ld-search-path /lib/x86_64-linux-gnu/ ./libsafety/libsafety.so --workers=8 -test-program=$(which pytest) -- -n8 $safety_model -s
-done
+mull-runner-17 --ld-search-path /lib/x86_64-linux-gnu/ ./libsafety/libsafety.so -test-program=$(which pytest) -- -n8 test_*.py -s
+#for safety_model in ${SAFETY_MODELS[@]}; do
+#  echo ""
+#  echo ""
+#  echo -e "Testing mutations on: $safety_model"
+#done
