0370c8d5bf8549773afeba201ae34df46d945a2f
Isaac <48419673+coffee-cake-isaac@users.noreply.github.com>
2025-02-22 14:35:34 -0500

    Update fingerprints.py

--- opendbc/car/ford/fingerprints.py
+++ opendbc/car/ford/fingerprints.py
@@ -98,7 +98,7 @@ FW_VERSIONS = {
     ],
     (Ecu.fwdCamera, 0x706, None): [
       b'ML3T-14H102-ABT\x00\x00\x00\x00\x00\x00\x00\x00\x00',
-      b'RJ6T-14H102-BBC\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+      b'RJ6T-14H102-BBC\x00\x00\x00\x00\x00\x00\x00\x00\x00',
     ],
     (Ecu.fwdRadar, 0x764, None): [
       b'ML3T-14D049-AL\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00',
