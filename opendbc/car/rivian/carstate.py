c975c8c09770c906481a51a36f43c8d704009be2
Laptop Researcher <laptop@researcher.com>
2025-02-22 23:23:28 -0800

    misra

--- opendbc/car/rivian/carstate.py
+++ opendbc/car/rivian/carstate.py
@@ -12,6 +12,7 @@ class CarState(CarStateBase):
     self.last_speed = 30
 
     self.acm_lka_hba_cmd = None
+    self.sccm_wheel_touch = None
 
   def update(self, can_parsers) -> structs.CarState:
     cp = can_parsers[Bus.pt]
@@ -72,6 +73,7 @@ class CarState(CarStateBase):
 
     # Messages needed by carcontroller
     self.acm_lka_hba_cmd = copy.copy(cp_cam.vl["ACM_lkaHbaCmd"])
+    self.sccm_wheel_touch = copy.copy(cp.vl["SCCM_WheelTouch"])
 
     return ret
 
