8424c8704b22b6ac89aa808f49d22728685ebfa1
Jason Young <jyoung8607@gmail.com>
2025-02-23 02:16:05 -0500

    combine identical mqbcan/mebcan packers

--- opendbc/car/volkswagen/mebcan.py
+++ opendbc/car/volkswagen/mebcan.py
@@ -1,3 +1,11 @@
+import opendbc.car.volkswagen.mqbcan as mqbcan
+
+
+create_eps_update = mqbcan.create_eps_update
+create_lka_hud_control = mqbcan.create_lka_hud_control
+create_acc_buttons_control = mqbcan.create_acc_buttons_control
+
+
 ACC_CTRL_ERROR    = 6
 ACC_CTRL_OVERRIDE = 4
 ACC_CTRL_ACTIVE   = 3
@@ -28,62 +36,6 @@ def create_steering_control(packer, bus, apply_curvature, lkas_enabled, power, p
     "Standby": not lkas_enabled,
   }
   return packer.make_can_msg("HCA_03", bus, values)
-
-
-def create_eps_update(packer, bus, eps_stock_values, ea_simulated_torque):
-  values = {s: eps_stock_values[s] for s in [
-    "COUNTER",                     # Sync counter value to EPS output
-    "EPS_Lenkungstyp",             # EPS rack type
-    "EPS_Berechneter_LW",          # Absolute raw steering angle
-    "EPS_VZ_BLW",                  # Raw steering angle sign
-    "EPS_HCA_Status",              # EPS HCA control status
-  ]}
-
-  values.update({
-    # Absolute driver torque input and sign, with EA inactivity mitigation
-    "EPS_Lenkmoment": abs(ea_simulated_torque),
-    "EPS_VZ_Lenkmoment": 1 if ea_simulated_torque < 0 else 0,
-  })
-
-  return packer.make_can_msg("LH_EPS_03", bus, values)
-
-
-def create_lka_hud_control(packer, bus, ldw_stock_values, lat_active, steering_pressed, hud_alert, hud_control):
-  values = {}
-  if len(ldw_stock_values):
-    values = {s: ldw_stock_values[s] for s in [
-      "LDW_SW_Warnung_links",   # Blind spot in warning mode on left side due to lane departure
-      "LDW_SW_Warnung_rechts",  # Blind spot in warning mode on right side due to lane departure
-      "LDW_Seite_DLCTLC",       # Direction of most likely lane departure (left or right)
-      "LDW_DLC",                # Lane departure, distance to line crossing
-      "LDW_TLC",                # Lane departure, time to line crossing
-    ]}
-
-  values.update({
-    "LDW_Status_LED_gelb": 1 if lat_active and steering_pressed else 0,
-    "LDW_Status_LED_gruen": 1 if lat_active and not steering_pressed else 0,
-    "LDW_Lernmodus_links": 3 if hud_control.leftLaneDepart else 1 + hud_control.leftLaneVisible,
-    "LDW_Lernmodus_rechts": 3 if hud_control.rightLaneDepart else 1 + hud_control.rightLaneVisible,
-    "LDW_Texte": hud_alert,
-  })
-  return packer.make_can_msg("LDW_02", bus, values)
-
-
-def create_acc_buttons_control(packer, bus, gra_stock_values, cancel=False, resume=False):
-  values = {s: gra_stock_values[s] for s in [
-    "GRA_Hauptschalter",           # ACC button, on/off
-    "GRA_Typ_Hauptschalter",       # ACC main button type
-    "GRA_Codierung",               # ACC button configuration/coding
-    "GRA_Tip_Stufe_2",             # unknown related to stalk type
-    "GRA_ButtonTypeInfo",          # unknown related to stalk type
-  ]}
-
-  values.update({
-    "COUNTER": (gra_stock_values["COUNTER"] + 1) % 16,
-    "GRA_Abbrechen": cancel,
-    "GRA_Tip_Wiederaufnahme": resume,
-  })
-  return packer.make_can_msg("GRA_ACC_01", bus, values)
 
 
 def acc_control_value(main_switch_on, acc_faulted, long_active, esp_hold, override):
