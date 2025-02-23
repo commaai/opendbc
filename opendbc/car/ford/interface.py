c0f00b804ca70cd43898d175ea7e78a081e9a0f5
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2025-02-22 18:29:54 -0800

    Ford: enable Q4 platform (#1804)

--- opendbc/car/ford/interface.py
+++ opendbc/car/ford/interface.py
@@ -20,7 +20,6 @@ class CarInterface(CarInterfaceBase):
   @staticmethod
   def _get_params(ret: structs.CarParams, candidate, fingerprint, car_fw, experimental_long, docs) -> structs.CarParams:
     ret.brand = "ford"
-    ret.dashcamOnly = bool(ret.flags & FordFlags.CANFD)
 
     ret.radarUnavailable = Bus.radar not in DBC[candidate]
     ret.steerControlType = structs.CarParams.SteerControlType.angle
