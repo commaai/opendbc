8f86df41100fe4282fa46064ef802e3207828ecf
Jason Young <jyoung8607@gmail.com>
2025-02-25 01:10:47 -0500

    packer fixes corresponding to DBC fixes

--- opendbc/car/volkswagen/mebcan.py
+++ opendbc/car/volkswagen/mebcan.py
@@ -24,16 +24,13 @@ ACC_HUD_ENABLED  = 2
 ACC_HUD_DISABLED = 0
 
 
-def create_steering_control(packer, bus, apply_curvature, lkas_enabled, power, power_boost):
-  # active lateral control deactivates active steering wheel centering
+def create_steering_control(packer, bus, apply_curvature, lkas_enabled, power):
   values = {
     "Curvature": abs(apply_curvature), # in rad/m
-    "VZ": 1 if apply_curvature > 0 and lkas_enabled else 0,
-    "Power": power if lkas_enabled else 0, # maximum working value is 127, but observed max value with VW travel assist is 125
-    "Power_Boost": 1 if power_boost and lkas_enabled else 0, # this bit has not been observed with VW travel assist yet, keep it separated from POWER for now
-    "Active": lkas_enabled,
-    "Request": lkas_enabled,
-    "Standby": not lkas_enabled,
+    "Curvature_VZ": 1 if apply_curvature > 0 and lkas_enabled else 0,
+    "Power": power if lkas_enabled else 0,
+    "RequestStatus": 4 if lkas_enabled else 2,
+    "HighSendRate": lkas_enabled,
   }
   return packer.make_can_msg("HCA_03", bus, values)
 
