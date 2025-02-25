5a008707145fb9c36cec36bc3acd68df0d3159a2
Shane Smiskol <shane@smiskol.com>
2025-02-24 21:35:58 -0800

    better

--- opendbc/car/tesla/values.py
+++ opendbc/car/tesla/values.py
@@ -10,9 +10,10 @@ Ecu = CarParams.Ecu
 
 class Footnote(Enum):
   HW_TYPE = CarFootnote(
-    "To verify which hardware type your vehicle has, refer to " +
-    "<a href=\"https://www.notateslaapp.com/news/2173/how-to-check-if-your-tesla-has-hardware-4-ai4-or-hardware-3\">this documentation</a>.",
-    Column.PACKAGE)
+    "Some 2023 model years have HW4. To check which hardware type your vehicle has, look for " +
+    "<b>Autopilot computer</b> under <b>Software -> Additional Vehicle Information</b> on your vehicle's touchscreen. </br></br>" +
+    "See <a href=\"https://www.notateslaapp.com/news/2173/how-to-check-if-your-tesla-has-hardware-4-ai4-or-hardware-3\">this page</a> for more information.",
+    Column.MODEL)
 
 
 @dataclass
