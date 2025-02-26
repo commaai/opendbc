0ab8543b5892e14bdf93ea0a50ea00e75979f237
Jason Wen <haibin.wen3@gmail.com>
2025-02-26 00:25:17 -0500

    different name

--- opendbc/car/hyundai/hyundaican.py
+++ opendbc/car/hyundai/hyundaican.py
@@ -113,7 +113,7 @@ def create_clu11(packer, frame, clu11, button, CP, CAN):
   values["CF_Clu_CruiseSwState"] = button
   values["CF_Clu_AliveCnt1"] = frame % 0x10
   # send buttons to camera on camera-scc based cars
-  bus = CAN.CAM if CP.flags & HyundaiFlags.CAMERA_SCC else CAN.MAIN
+  bus = CAN.CAMERA if CP.flags & HyundaiFlags.CAMERA_SCC else CAN.MAIN
   return packer.make_can_msg("CLU11", bus, values)
 
 
