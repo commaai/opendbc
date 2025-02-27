f4b0a11ccd4c5a762d924e2a1a667d7dcad03a6f
Shane Smiskol <shane@smiskol.com>
2025-02-27 03:22:43 -0800

    more clear

--- opendbc/safety/tests/test_ford.py
+++ opendbc/safety/tests/test_ford.py
@@ -300,8 +300,8 @@ class TestFordSafetyBase(common.PandaCarSafetyTest):
       up_cases = (self.MAX_CURVATURE_ERROR * 2, [
         (not limit_command, 0, 0),
         (not limit_command, 0, max_delta_up_lower - small_curvature),
-        (True, 1e-6, max_delta_down),  # TODO: safety should not allow down limits at 0
-        (not limit_command, 1e-6, max_delta_up_lower),  # TODO: safety should not allow down limits at 0
+        (True, 1e-9, max_delta_down),  # TODO: safety should not allow down limits at 0
+        (not limit_command, 1e-9, max_delta_up_lower),  # TODO: safety should not allow down limits at 0
         (True, 0, max_delta_up_lower),
         (True, 0, max_delta_up),
         (False, 0, max_delta_up + small_curvature),
