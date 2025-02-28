a985c41d86c56dd501268227c7dde85938c088e4
Shane Smiskol <shane@smiskol.com>
2025-02-28 02:15:05 -0600

    safety: consider max angle for inactive angle (#1876)
    * allow not being out of max desired angle rang
    
    * do toyota & nissan
    
    * do the rest
    
    * stash not working yet
    
    * Revert "stash not working yet"
    
    This reverts commit 01cbeb400533d774450ec962e2cc2f795ad2c2c9.
    
    * we can go above on the measurements now
    
    * EPAS
    
    * misra

--- opendbc/safety/safety.h
+++ opendbc/safety/safety.h
@@ -743,8 +743,10 @@ bool steer_angle_cmd_checks(int desired_angle, bool steer_control_enabled, const
 
   // Angle should either be 0 or same as current angle while not steering
   if (!steer_control_enabled) {
+    const int max_inactive_angle = CLAMP(angle_meas.max, -limits.max_angle, limits.max_angle) + 1;
+    const int min_inactive_angle = CLAMP(angle_meas.min, -limits.max_angle, limits.max_angle) - 1;
     violation |= (limits.inactive_angle_is_zero ? (desired_angle != 0) :
-                  max_limit_check(desired_angle, angle_meas.max + 1, angle_meas.min - 1));
+                  max_limit_check(desired_angle, max_inactive_angle, min_inactive_angle));
   }
 
   // No angle control allowed when controls are not allowed
