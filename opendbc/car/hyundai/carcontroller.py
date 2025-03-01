366219b7a1791d3df5c51443011dfcd4ba2e8d8c
royjr <royjr96@gmail.com>
2025-03-01 16:39:10 -0500

    Revert "dont send this since we use 161"
    This reverts commit fc1c7297320b925c2cdc05a4e84f6d8a04de0794.

--- opendbc/car/hyundai/carcontroller.py
+++ opendbc/car/hyundai/carcontroller.py
@@ -126,11 +126,10 @@ class CarController(CarControllerBase):
         can_sends.extend(hyundaicanfd.create_spas_messages(self.packer, self.CAN, self.frame, CC.leftBlinker, CC.rightBlinker))
 
       if self.CP.openpilotLongitudinalControl:
-        if not self.CP.flags & HyundaiFlags.CCNC:
-          if lka_steering:
-            can_sends.extend(hyundaicanfd.create_adrv_messages(self.packer, self.CAN, self.frame))
-          else:
-            can_sends.extend(hyundaicanfd.create_fca_warning_light(self.packer, self.CAN, self.frame))
+        if lka_steering:
+          can_sends.extend(hyundaicanfd.create_adrv_messages(self.packer, self.CAN, self.frame))
+        else:
+          can_sends.extend(hyundaicanfd.create_fca_warning_light(self.packer, self.CAN, self.frame))
         if self.frame % 2 == 0:
           if self.CP.flags & HyundaiFlags.CCNC:
             can_sends.append(hyundaicanfd.create_ccnc_acc_control(self.packer, self.CAN, CC.enabled, self.accel_last, accel, stopping,
