fc695742460d111e03945ebb7cf37651ce954e8f
Jason Wen <haibin.wen3@gmail.com>
2025-03-03 01:03:47 -0500

    label message names

--- opendbc/car/hyundai/interface.py
+++ opendbc/car/hyundai/interface.py
@@ -32,7 +32,8 @@ class CarInterface(CarInterfaceBase):
 
       ret.enableBsm = 0x1e5 in fingerprint[CAN.ECAN]
 
-      # Check if the car is hybrid. Some ICE cars have 0x105, so check if both 0x105 and 0x130 are on E-CAN
+      # Check if the car is hybrid. Some ICE cars have 0x105 (ACCELERATOR_ALT),
+      # so check if both 0x105 (ACCELERATOR_ALT) and 0x130 (GEAR_SHIFTER) are on E-CAN
       if 0x105 in fingerprint[CAN.ECAN] and 0x130 in fingerprint[CAN.ECAN]:
         ret.flags |= HyundaiFlags.HYBRID.value
 
