d6f067205284cc0dca79e8d5a476a8333b04a74c
Shane Smiskol <shane@smiskol.com>
2025-03-02 18:15:28 -0800

    Update carcontroller.py

--- opendbc/car/rivian/carcontroller.py
+++ opendbc/car/rivian/carcontroller.py
@@ -1,8 +1,11 @@
 from opendbc.can.packer import CANPacker
 from opendbc.car import Bus, apply_driver_steer_torque_limits
 from opendbc.car.interfaces import CarControllerBase
-from opendbc.car.rivian.riviancan import create_lka_steering, create_longitudinal, create_wheel_touch
+from opendbc.car.rivian.riviancan import create_lka_steering, create_longitudinal, create_wheel_touch, common_fault_avoidance
 from opendbc.car.rivian.values import CarControllerParams
+
+MAX_STEER_RATE = 90  # deg/s
+MAX_STEER_RATE_FRAMES = 10  # tx control frames needed before torque can be cut
 
 
 class CarController(CarControllerBase):
@@ -11,10 +14,16 @@ class CarController(CarControllerBase):
     self.apply_torque_last = 0
     self.packer = CANPacker(dbc_names[Bus.pt])
 
+    self.steer_rate_counter = 0
+
   def update(self, CC, CS, now_nanos):
     actuators = CC.actuators
     can_sends = []
 
+    # >100 degree/sec steering fault prevention
+    self.steer_rate_counter, apply_steer_req = common_fault_avoidance(abs(CS.out.steeringRateDeg) >= MAX_STEER_RATE, CC.latActive,
+                                                                      self.steer_rate_counter, MAX_STEER_RATE_FRAMES)
+    
     apply_torque = 0
     if CC.latActive:
       new_torque = int(round(CC.actuators.torque * CarControllerParams.STEER_MAX))
@@ -23,7 +32,7 @@ class CarController(CarControllerBase):
 
     # send steering command
     self.apply_torque_last = apply_torque
-    can_sends.append(create_lka_steering(self.packer, CS.acm_lka_hba_cmd, apply_torque, CC.latActive))
+    can_sends.append(create_lka_steering(self.packer, CS.acm_lka_hba_cmd, apply_torque, apply_steer_req))
 
     if self.frame % 5 == 0:
       can_sends.append(create_wheel_touch(self.packer, CS.sccm_wheel_touch, CC.enabled))
