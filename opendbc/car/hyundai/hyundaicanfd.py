2e9d918c6fbd1949e136e2a217cb5619242a6432
royjr <royjr96@gmail.com>
2025-03-04 00:24:45 -0500

    comment

--- opendbc/car/hyundai/hyundaicanfd.py
+++ opendbc/car/hyundai/hyundaicanfd.py
@@ -229,7 +229,7 @@ def create_acc_control(packer, CAN, enabled, accel_last, accel, stopping, gas_ov
     "DISTANCE_SETTING": hud_control.leadDistanceBars,
   }
 
-  # fixes auto regen stuck on max for hybrids
+  # fixes auto regen stuck on max for hybrids, should probably apply to all cars
   values.update({"ACC_ObjDist": 1} if cruise_info is None else {s: cruise_info[s] for s in ["ACC_ObjDist", "ACC_ObjRelSpd"]})
 
   return packer.make_can_msg("SCC_CONTROL", CAN.ECAN, values)
