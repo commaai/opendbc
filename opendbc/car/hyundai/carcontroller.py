77ef598781f3ce307c1a252a15a299f7226b7edd
royjr <royjr96@gmail.com>
2025-03-04 00:44:59 -0500

    match upstream

--- opendbc/car/hyundai/carcontroller.py
+++ opendbc/car/hyundai/carcontroller.py
@@ -132,8 +132,8 @@ class CarController(CarControllerBase):
           can_sends.extend(hyundaicanfd.create_fca_warning_light(self.packer, self.CAN, self.frame))
         if self.frame % 2 == 0:
           cruise_info = CS.cruise_info if self.CP.flags & HyundaiFlags.CCNC else None
-          can_sends.append(hyundaicanfd.create_acc_control(self.packer, self.CAN, CC.enabled, self.accel_last, accel, stopping,
-                                                           CC.cruiseControl.override, set_speed_in_units, hud_control, cruise_info))
+          can_sends.append(hyundaicanfd.create_acc_control(self.packer, self.CAN, CC.enabled, self.accel_last, accel, stopping, CC.cruiseControl.override,
+                                                           set_speed_in_units, hud_control, cruise_info))
           self.accel_last = accel
       else:
         # button presses
