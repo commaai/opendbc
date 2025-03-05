75b161844e062bf0438620e2e284c01a61a91acd
Cameron Clough <cameronjclough@gmail.com>
2025-03-05 21:29:44 +0000

    all HEV and PHEV Kugas

--- opendbc/car/ford/values.py
+++ opendbc/car/ford/values.py
@@ -131,8 +131,8 @@ class CAR(Platforms):
   FORD_ESCAPE_MK4_5 = FordCANFDPlatformConfig(
     [
       FordCarDocs("Ford Escape 2023-24", hybrid=True, plug_in_hybrid=True),
-      FordCarDocs("Ford Kuga Hybrid 2024", "Adaptive Cruise Control with Lane Centering Assist"),
-      FordCarDocs("Ford Kuga Plug-in Hybrid 2024", "Adaptive Cruise Control with Lane Centering Assist"),
+      FordCarDocs("Ford Kuga Hybrid 2024", "All"),
+      FordCarDocs("Ford Kuga Plug-in Hybrid 2024", "All"),
     ],
     CarSpecs(mass=1750, wheelbase=2.71, steerRatio=16.7),
   )
