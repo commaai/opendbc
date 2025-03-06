9b713cd1fe5bd8ac69ce58e7fff6241536d50e57
Jason Young <jyoung8607@gmail.com>
2025-03-06 13:49:30 -0500

    VW MEB: Reserve safety identifier

--- opendbc/safety/safety.h
+++ opendbc/safety/safety.h
@@ -55,6 +55,7 @@
 #define SAFETY_BODY 27U
 #define SAFETY_HYUNDAI_CANFD 28U
 #define SAFETY_RIVIAN 33U
+#define SAFETY_VOLKSWAGEN_MEB 34U
 
 uint32_t GET_BYTES(const CANPacket_t *msg, int start, int len) {
   uint32_t ret = 0U;
