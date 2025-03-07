bddbbe13533f85f7c9fa19a2175ace353b705b29
royjr <royjr96@gmail.com>
2025-03-07 17:59:00 -0500

    getless

--- opendbc/car/hyundai/hyundaicanfd.py
+++ opendbc/car/hyundai/hyundaicanfd.py
@@ -131,16 +131,16 @@ def create_ccnc(packer, CAN, CP, CC, CS, lat_active):
   for f in {"FAULT_LSS", "FAULT_HDA", "FAULT_DAS", "FAULT_LFA", "FAULT_DAW"}:
     msg_162[f] = 0
 
-  if msg_161.get("ALERTS_2") == 5:  # CONSIDER_TAKING_A_BREAK
+  if msg_161["ALERTS_2"] == 5:  # CONSIDER_TAKING_A_BREAK
     msg_161.update({"ALERTS_2": 0, "SOUNDS_2": 0})
 
-  if msg_161.get("ALERTS_3") == 17:  # DRIVE_CAREFULLY
+  if msg_161["ALERTS_3"] == 17:  # DRIVE_CAREFULLY
     msg_161["ALERTS_3"] = 0
 
-  if msg_161.get("ALERTS_5") in (2, 4, 5):  # WATCH_FOR_SURROUNDING_VEHICLES, SMART_CRUISE_CONTROL_CONDITIONS_NOT_MET, USE_SWITCH_OR_PEDAL_TO_ACCELERATE
+  if msg_161["ALERTS_5"] in (2, 4, 5):  # WATCH_FOR_SURROUNDING_VEHICLES, SMART_CRUISE_CONTROL_CONDITIONS_NOT_MET, USE_SWITCH_OR_PEDAL_TO_ACCELERATE
     msg_161["ALERTS_5"] = 0
 
-  if msg_161.get("SOUNDS_4") == 2 and msg_161.get("LFA_ICON") in (3, 0,):  # LFA BEEPS
+  if msg_161["SOUNDS_4"] == 2 and msg_161["LFA_ICON"] in (3, 0,):  # LFA BEEPS
     msg_161["SOUNDS_4"] = 0
 
   msg_161.update({
@@ -182,10 +182,10 @@ def create_ccnc(packer, CAN, CP, CC, CS, lat_active):
       "TARGET": 0,
     })
 
-    if msg_161.get("ALERTS_3") in (1, 2, 3, 4, 7, 8, 9, 10):  # HIDE ISLA, DISTANCE MESSAGES
+    if msg_161["ALERTS_3"] in (1, 2, 3, 4, 7, 8, 9, 10):  # HIDE ISLA, DISTANCE MESSAGES
       msg_161["ALERTS_3"] = 0
 
-    if msg_161.get("NAV_ICON") in (2, 4):  # DISABLE NAV IF AVAILABLE
+    if msg_161["NAV_ICON"] in (2, 4):  # DISABLE NAV IF AVAILABLE
       msg_161["NAV_ICON"] = 1
 
     msg_162["LEAD"] = 0
