527b3665abb6b035a03a1110d9b5d0b7bf0bf541
royjr <royjr96@gmail.com>
2025-03-06 19:26:47 -0500

    sure

--- opendbc/car/hyundai/carcontroller.py
+++ opendbc/car/hyundai/carcontroller.py
@@ -114,7 +114,7 @@ class CarController(CarControllerBase):
       can_sends.append(hyundaicanfd.create_lfahda_cluster(self.packer, self.CAN, CC.enabled))
 
     # blinkers
-    if lka_steering and self.CP.flags & HyundaiFlags.ENABLE_BLINKERS:
+    if self.CP.flags & HyundaiFlags.ENABLE_BLINKERS and lka_steering:
       can_sends.extend(hyundaicanfd.create_spas_messages(self.packer, self.CAN, self.frame, CC.leftBlinker, CC.rightBlinker))
 
     if self.CP.openpilotLongitudinalControl:
