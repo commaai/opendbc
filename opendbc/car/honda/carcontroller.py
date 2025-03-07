d1a97115a31b444d36a82c940bf9e35126ccfad0
mvl-boston <manuel@alumni.virginia.edu>
2025-03-07 06:44:19 -0500

    MDX

--- opendbc/car/honda/carcontroller.py
+++ opendbc/car/honda/carcontroller.py
@@ -117,33 +117,7 @@ class CarController(CarControllerBase):
     self.gas = 0.0
     self.brake = 0.0
     self.last_torque = 0.0
-    self.apply_steer = 0.0
     self.apply_steer_last = 0.0
-
-  def apply_driver_steer_torque_limits(apply_torque, apply_torque_last, driver_torque, LIMITS, ss=False):
-
-    # limits due to driver torque
-    driver_max_torque = LIMITS.STEER_MAX + (LIMITS.STEER_DRIVER_ALLOWANCE + driver_torque * LIMITS.STEER_DRIVER_FACTOR) * LIMITS.STEER_DRIVER_MULTIPLIER
-    driver_min_torque = -LIMITS.STEER_MAX + (-LIMITS.STEER_DRIVER_ALLOWANCE + driver_torque * LIMITS.STEER_DRIVER_FACTOR) * LIMITS.STEER_DRIVER_MULTIPLIER
-    max_steer_allowed = max(min(LIMITS.STEER_MAX, driver_max_torque), 0)
-    min_steer_allowed = min(max(-LIMITS.STEER_MAX, driver_min_torque), 0)
-
-    if ss:
-      min_torque = min(abs(max_steer_allowed), abs(min_steer_allowed))
-      max_steer_allowed = min_torque
-      min_steer_allowed = -min_torque
-
-    apply_torque = np.clip(apply_torque, min_steer_allowed, max_steer_allowed)
-
-    # slow rate if steer torque increases in magnitude
-    if apply_torque_last > 0:
-      apply_torque = np.clip(apply_torque, max(apply_torque_last - LIMITS.STEER_DELTA_DOWN, -LIMITS.STEER_DELTA_UP),
-                        apply_torque_last + LIMITS.STEER_DELTA_UP)
-    else:
-      apply_torque = np.clip(apply_torque, apply_torque_last - LIMITS.STEER_DELTA_UP,
-                        min(apply_torque_last + LIMITS.STEER_DELTA_DOWN, LIMITS.STEER_DELTA_UP))
-
-    return int(round(float(apply_torque)))
 
   def update(self, CC, CS, now_nanos):
     actuators = CC.actuators
