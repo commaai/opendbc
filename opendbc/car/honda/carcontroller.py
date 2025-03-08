ac5551f494d3d26cc316fa6dfa247d454949831c
mvl-boston <manuel@alumni.virginia.edu>
2025-03-08 10:40:10 -0500

    RDX_2022

--- opendbc/car/honda/carcontroller.py
+++ opendbc/car/honda/carcontroller.py
@@ -160,7 +160,7 @@ class CarController(CarControllerBase):
     if self.CP.carFingerprint in (HONDA_BOSCH - HONDA_BOSCH_RADARLESS) and self.CP.openpilotLongitudinalControl:
       if self.frame % 10 == 0:
         can_sends.append(make_tester_present_msg(0x18DAB0F1, 1, suppress_response=True))
-    
+
     # Send steering command.
     can_sends.append(hondacan.create_steering_control(self.packer, self.CAN, apply_torque, CC.latActive))
 
