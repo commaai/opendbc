649cdd67e5355ddc36df3228355f861a9037bb19
Shane Smiskol <shane@smiskol.com>
2025-03-08 01:51:15 -0800

    Rivian is nice and clean

--- opendbc/safety/safety/safety_rivian.h
+++ opendbc/safety/safety/safety_rivian.h
@@ -91,50 +91,13 @@ static bool rivian_tx_hook(const CANPacket_t *to_send) {
   return tx;
 }
 
-static int rivian_fwd_hook(int bus, int addr) {
-  int bus_fwd = -1;
-  bool block_msg = false;
-
-  if (bus == 0) {
-    // SCCM_WheelTouch: for hiding hold wheel alert
-    if (addr == 0x321) {
-      block_msg = true;
-    }
-
-    // VDM_AdasSts: for canceling stock ACC
-    if ((addr == 0x162) && !rivian_longitudinal) {
-      block_msg = true;
-    }
-
-    if (!block_msg) {
-      bus_fwd = 2;
-    }
-  }
-
-  if (bus == 2) {
-    // ACM_lkaHbaCmd: lateral control message
-    if (addr == 0x120) {
-      block_msg = true;
-    }
-
-    // ACM_longitudinalRequest: longitudinal control message
-    if (rivian_longitudinal && (addr == 0x160)) {
-      block_msg = true;
-    }
-
-    if (!block_msg) {
-      bus_fwd = 0;
-    }
-  }
-
-  return bus_fwd;
-}
-
 static safety_config rivian_init(uint16_t param) {
+  // Block SCCM_WheelTouch: for hiding hold wheel alert
+  // Block VDM_AdasSts: for canceling stock ACC
   // 0x120 = ACM_lkaHbaCmd, 0x321 = SCCM_WheelTouch, 0x162 = VDM_AdasSts
-  static const CanMsg RIVIAN_TX_MSGS[] = {{0x120, 0, 8}, {0x321, 2, 7}, {0x162, 2, 8}};
+  static const CanMsg RIVIAN_TX_MSGS[] = {{0x120, 0, 8, .blocked = true}, {0x321, 2, 7, .blocked = true}, {0x162, 2, 8, .blocked = true}};
   // 0x160 = ACM_longitudinalRequest
-  static const CanMsg RIVIAN_LONG_TX_MSGS[] = {{0x120, 0, 8}, {0x321, 2, 7}, {0x160, 0, 5}};
+  static const CanMsg RIVIAN_LONG_TX_MSGS[] = {{0x120, 0, 8, .blocked = true}, {0x321, 2, 7, .blocked = true}, {0x160, 0, 5, .blocked = true}};
 
   static RxCheck rivian_rx_checks[] = {
     {.msg = {{0x208, 0, 8, .frequency = 50U, .ignore_checksum = true, .ignore_counter = true}, { 0 }, { 0 }}},   // ESP_Status (speed)
@@ -158,5 +121,4 @@ const safety_hooks rivian_hooks = {
   .init = rivian_init,
   .rx = rivian_rx_hook,
   .tx = rivian_tx_hook,
-  .fwd = rivian_fwd_hook,
 };
