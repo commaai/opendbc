5545138d4e8d095f717be69fc366af2f6be4a7d6
royjr <royjr96@gmail.com>
2025-03-09 01:31:47 -0500

    cleaner

--- opendbc/car/hyundai/carcontroller.py
+++ opendbc/car/hyundai/carcontroller.py
@@ -39,11 +39,8 @@ class CarController(CarControllerBase):
     actuators = CC.actuators
     hud_control = CC.hudControl
 
-    (
-      apply_torque, apply_steer_req, torque_fault, accel, stopping,
-      set_speed_in_units, sys_warning, sys_state, left_lane_warning,
-      right_lane_warning, tester_present_msgs
-    ) = self.compute_common_controls(CC, CS)
+    apply_torque, apply_steer_req, torque_fault, accel, stopping, set_speed_in_units, sys_warning, sys_state, left_lane_warning, right_lane_warning,
+     tester_present_msgs = self.compute_common_controls(CC, CS)
 
     can_sends = tester_present_msgs.copy()
 
@@ -88,11 +85,8 @@ class CarController(CarControllerBase):
     actuators = CC.actuators
     hud_control = CC.hudControl
 
-    (
-      apply_torque, apply_steer_req, torque_fault, accel, stopping,
-      set_speed_in_units, sys_warning, sys_state, left_lane_warning,
-      right_lane_warning, tester_present_msgs
-    ) = self.compute_common_controls(CC, CS)
+    apply_torque, apply_steer_req, torque_fault, accel, stopping, set_speed_in_units, sys_warning, sys_state, left_lane_warning, right_lane_warning,
+     tester_present_msgs = self.compute_common_controls(CC, CS)
 
     can_sends = tester_present_msgs.copy()
 
