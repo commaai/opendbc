5593eccb5c7113ea2e8d98a5e8a7803e47fbb552
elkoled <elkoled@gmail.com>
2025-03-10 09:47:09 +0100

    disable radar ECU

--- opendbc/car/psa/interface.py
+++ opendbc/car/psa/interface.py
@@ -1,4 +1,4 @@
-from opendbc.car import structs
+from opendbc.car import structs, uds
 from opendbc.car import get_safety_config
 from opendbc.car.interfaces import CarInterfaceBase
 from opendbc.car.psa.values import CAR
@@ -30,9 +30,9 @@ class CarInterface(CarInterfaceBase):
 
     return ret
 
-  # TODO: find radar ECU address to disable it
-  # @staticmethod
-  # def init(CP, can_recv, can_send):
-
-    # communication_control = bytes([uds.SERVICE_TYPE.COMMUNICATION_CONTROL, uds.CONTROL_TYPE.ENABLE_RX_DISABLE_TX, uds.MESSAGE_TYPE.NORMAL])
-    # disable_ecu(can_recv, can_send, bus=0, addr=0x750, sub_addr=0xf, com_cont_req=communication_control)
+  # TODO: find radar ECU address to disable it, check sub_addr with panda script
+  @staticmethod
+  def init(CP, can_recv, can_send):
+    # ARTIV	ARTIV, RADAR_AV_4, LIDAR, ARTIV_UDS	>6B6:696
+    communication_control = bytes([uds.SERVICE_TYPE.COMMUNICATION_CONTROL, uds.CONTROL_TYPE.ENABLE_RX_DISABLE_TX, uds.MESSAGE_TYPE.NORMAL])
+    disable_ecu(can_recv, can_send, bus=1, addr=0x6B6, sub_addr=0x0, com_cont_req=communication_control)
