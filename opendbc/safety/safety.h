7d056bd340e0dd889ccdfca7419ba4f117518a51
Shane Smiskol <shane@smiskol.com>
2025-03-10 15:40:15 -0700

    fix

--- opendbc/safety/safety.h
+++ opendbc/safety/safety.h
@@ -216,7 +216,7 @@ bool safety_rx_hook(const CANPacket_t *to_push) {
 
     const int bus = GET_BUS(to_push);
     // check all tx msgs for liveness on opposite bus if specified.
-    // could be to detect a relay malfunction for control messages from disabled ECUs (radar)
+    // used to detect a relay malfunction or control messages from disabled ECUs (radar)
     for (int i = 0; i < current_safety_config.tx_msgs_len; i++) {
       const CanMsg *m = &current_safety_config.tx_msgs[i];
       if (m->check_relay) {
