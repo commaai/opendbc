fc3b4f688518b273e46c8201f182dce652aea014
Jason Wen <haibin.wen3@gmail.com>
2025-03-10 16:13:07 -0400

    let's use anon enum

--- opendbc/safety/safety/safety_hyundai_flags.h
+++ opendbc/safety/safety/safety_hyundai_flags.h
@@ -1,5 +1,20 @@
 #pragma once
 
+enum {
+  HYUNDAI_PARAM_EV_GAS = 1,
+  HYUNDAI_PARAM_HYBRID_GAS = 2,
+#ifdef ALLOW_DEBUG
+  HYUNDAI_PARAM_LONGITUDINAL = 4,
+#endif
+  HYUNDAI_PARAM_CAMERA_SCC = 8,
+  HYUNDAI_PARAM_CANFD_LKA_STEERING = 16,  // TODO: rename for support with CAN/CAN FD Blended platforms
+  HYUNDAI_PARAM_CANFD_ALT_BUTTONS = 32,
+  HYUNDAI_PARAM_ALT_LIMITS = 64,
+  HYUNDAI_PARAM_CANFD_LKA_STEERING_ALT = 128,
+  HYUNDAI_PARAM_FCEV_GAS = 256,
+  HYUNDAI_PARAM_ALT_LIMITS_2 = 512,
+};
+
 // common flags
 extern bool hyundai_ev_gas_signal;
 bool hyundai_ev_gas_signal = false;
@@ -34,12 +49,6 @@ extern bool hyundai_canfd_lka_steering_alt;
 bool hyundai_canfd_lka_steering_alt = false;
 
 void hyundai_common_flags(uint16_t param) {
-  const int HYUNDAI_PARAM_EV_GAS = 1;
-  const int HYUNDAI_PARAM_HYBRID_GAS = 2;
-  const int HYUNDAI_PARAM_CAMERA_SCC = 8;
-  const int HYUNDAI_PARAM_CANFD_LKA_STEERING = 16;  // TODO: rename for support with CAN/CAN FD Blended platforms
-  const int HYUNDAI_PARAM_FCEV_GAS = 256;
-
   hyundai_ev_gas_signal = GET_FLAG(param, HYUNDAI_PARAM_EV_GAS);
   hyundai_hybrid_gas_signal = !hyundai_ev_gas_signal && GET_FLAG(param, HYUNDAI_PARAM_HYBRID_GAS);
   hyundai_camera_scc = GET_FLAG(param, HYUNDAI_PARAM_CAMERA_SCC);
@@ -47,7 +56,6 @@ void hyundai_common_flags(uint16_t param) {
   hyundai_fcev_gas_signal = GET_FLAG(param, HYUNDAI_PARAM_FCEV_GAS);
 
 #ifdef ALLOW_DEBUG
-  const int HYUNDAI_PARAM_LONGITUDINAL = 4;
   hyundai_longitudinal = GET_FLAG(param, HYUNDAI_PARAM_LONGITUDINAL);
 #else
   hyundai_longitudinal = false;
@@ -57,18 +65,12 @@ void hyundai_common_flags(uint16_t param) {
 void hyundai_flags(uint16_t param) {
   hyundai_common_flags(param);
 
-  const int HYUNDAI_PARAM_ALT_LIMITS = 64;
-  const int HYUNDAI_PARAM_ALT_LIMITS_2 = 512;
-
   hyundai_alt_limits = GET_FLAG(param, HYUNDAI_PARAM_ALT_LIMITS);
   hyundai_alt_limits_2 = GET_FLAG(param, HYUNDAI_PARAM_ALT_LIMITS_2);
 }
 
 void hyundai_canfd_flags(uint16_t param) {
   hyundai_common_flags(param);
-
-  const int HYUNDAI_PARAM_CANFD_ALT_BUTTONS = 32;
-  const int HYUNDAI_PARAM_CANFD_LKA_STEERING_ALT = 128;
 
   hyundai_canfd_lka_steering_alt = GET_FLAG(param, HYUNDAI_PARAM_CANFD_LKA_STEERING_ALT);
   hyundai_canfd_alt_buttons = GET_FLAG(param, HYUNDAI_PARAM_CANFD_ALT_BUTTONS);
