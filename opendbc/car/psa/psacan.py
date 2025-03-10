009b1371b76312bb44bd7c9e6e71537020141ba1
elkoled <elkoled@gmail.com>
2025-03-10 11:51:59 +0100

    checksum init

--- opendbc/car/psa/psacan.py
+++ opendbc/car/psa/psacan.py
@@ -18,7 +18,8 @@ class CanBus(CanBusBase):
 
 def calculate_checksum(dat: bytearray) -> int:
   checksum = sum((b >> 4) + (b & 0xF) for b in dat)
-  return (11 - checksum) & 0xF
+  # CHK_INI = 0xB
+  return (0xB - checksum) & 0xF
 
 def create_lka_msg(packer, CP, frame: int, lat_active: bool, apply_angle: float):
   values = {
@@ -32,8 +33,6 @@ def create_lka_msg(packer, CP, frame: int, lat_active: bool, apply_angle: float)
   }
 
   msg = packer.make_can_msg('LANE_KEEP_ASSIST', 0, values)[1]
-  if isinstance(msg, int):
-    msg = msg.to_bytes(1, 'big')
   values['CHECKSUM'] = calculate_checksum(msg)
 
   return packer.make_can_msg('LANE_KEEP_ASSIST', CanBus(CP).camera, values)
@@ -60,7 +59,7 @@ def create_cancel_request(packer, CP, frame: int, set_speed: int):
     'ACC_PROGRAM_MODE': 0,
   }
 
-  # Calculate checksum using only SPEED_SETPOINT and FRAME_COUNTER_BSI2
+  # checksum covers only SPEED_SETPOINT and FRAME_COUNTER_BSI2
   data = bytearray([set_speed & 0xFF, values['FRAME_COUNTER_BSI2'] & 0xFF])
   values['CHECKSUM_SPEED_SETPOINT'] = calculate_checksum(data)
 
