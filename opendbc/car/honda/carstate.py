db3238b05a83fec3c3659c01a294b5d79932c5b7
mvl-boston <manuel@alumni.virginia.edu>
2025-03-10 01:15:11 -0400

    RDX 2022 - formatting fix

--- opendbc/car/honda/carstate.py
+++ opendbc/car/honda/carstate.py
@@ -255,7 +255,7 @@ class CarState(CarStateBase):
 
     # Adds low speed warning as some models disable cruise at various speeds, ignore warning under 3mph
     ret.lowSpeedAlert = ret.cruiseState.enabled and ret.vEgo >= 4 * CV.MPH_TO_MS and cp.vl["STEER_STATUS"]["STEER_CONTROL_ACTIVE"] == 0
-    
+
     # Gets rid of Pedal Grinding noise when brake is pressed at slow speeds for some models
     if self.CP.carFingerprint in (CAR.HONDA_PILOT, CAR.HONDA_RIDGELINE):
       if ret.brake > 0.1:
