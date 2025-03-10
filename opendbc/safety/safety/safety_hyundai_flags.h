08341dcc07c192017de7e2750235d74793793cb9
Jason Wen <haibin.wen3@gmail.com>
2025-03-10 16:15:02 -0400

    do we need to be this explicit?

--- opendbc/safety/safety/safety_hyundai_flags.h
+++ opendbc/safety/safety/safety_hyundai_flags.h
@@ -50,7 +50,7 @@ bool hyundai_canfd_lka_steering_alt = false;
 
 void hyundai_common_flags(uint16_t param) {
   hyundai_ev_gas_signal = GET_FLAG(param, HYUNDAI_PARAM_EV_GAS);
-  hyundai_hybrid_gas_signal = !hyundai_ev_gas_signal && GET_FLAG(param, HYUNDAI_PARAM_HYBRID_GAS);
+  hyundai_hybrid_gas_signal = GET_FLAG(param, HYUNDAI_PARAM_HYBRID_GAS);
   hyundai_camera_scc = GET_FLAG(param, HYUNDAI_PARAM_CAMERA_SCC);
   hyundai_canfd_lka_steering = GET_FLAG(param, HYUNDAI_PARAM_CANFD_LKA_STEERING);
   hyundai_fcev_gas_signal = GET_FLAG(param, HYUNDAI_PARAM_FCEV_GAS);
