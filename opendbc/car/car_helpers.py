d597285e794a84c2bbb5379b1d55b39737bf3fb1
mvl-boston <manuel@alumni.virginia.edu>
2025-03-11 00:35:29 -0400

    revert forced fingerprint test

--- opendbc/car/car_helpers.py
+++ opendbc/car/car_helpers.py
@@ -144,8 +144,6 @@ def fingerprint(can_recv: CanRecvCallable, can_send: CanSendCallable, set_obd_mu
     car_fingerprint = fixed_fingerprint
     source = CarParams.FingerprintSource.fixed
 
-  car_fingerprint = "ACURA_MDX_3G_HYBRID"
-
   carlog.error({"event": "fingerprinted", "car_fingerprint": str(car_fingerprint), "source": source, "fuzzy": not exact_match,
                 "cached": cached, "fw_count": len(car_fw), "ecu_responses": list(ecu_rx_addrs), "vin_rx_addr": vin_rx_addr,
                 "vin_rx_bus": vin_rx_bus, "fingerprints": repr(finger), "fw_query_time": fw_query_time})
