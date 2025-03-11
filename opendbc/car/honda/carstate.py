b26bed76ee3e766af1c19133dbdffa8c44619099
mvl-boston <manuel@alumni.virginia.edu>
2025-03-11 15:43:37 -0400

    force bus

--- opendbc/car/honda/carstate.py
+++ opendbc/car/honda/carstate.py
@@ -121,8 +121,8 @@ class CarState(CarStateBase):
     self.dash_speed_seen = False
 
   def update(self, can_parsers) -> structs.CarState:
-    cp = can_parsers[0]
-    cp_cam = can_parsers[2]
+    cp = can_parsers[Bus.rad]
+    cp_cam = can_parsers[Bus.cam]
     if self.CP.enableBsm:
       cp_body = can_parsers[Bus.body]
 
