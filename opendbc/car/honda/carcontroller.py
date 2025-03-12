8a4cec4f52cfe6b8afd4ec930419a90eaed44c4b
mvl-boston <manuel@alumni.virginia.edu>
2025-03-12 19:35:59 -0400

    tuning typos

--- opendbc/car/honda/carcontroller.py
+++ opendbc/car/honda/carcontroller.py
@@ -241,10 +241,10 @@ class CarController(CarControllerBase):
     # On Nidec, this controls longitudinal positive acceleration
     if self.frame % 10 == 0:
       gas = np.clip ( gas, 0, NIDEC_GAS_MAX )
-            hud = HUDData(int(gas), int(round(hud_v_cruise)), hud_control.leadVisible,
-            hud_control.lanesVisible, fcw_display, acc_alert, steer_required, hud_control.leadDistanceBars)
+      hud = HUDData(int(gas), int(round(hud_v_cruise)), hud_control.leadVisible,
+      hud_control.lanesVisible, fcw_display, acc_alert, steer_required, hud_control.leadDistanceBars)
       #      hud = HUDData(int(pcm_accel), int(round(hud_v_cruise)), hud_control.leadVisible,
-       #             hud_control.lanesVisible, fcw_display, acc_alert, steer_required, hud_control.leadDistanceBars)
+      #             hud_control.lanesVisible, fcw_display, acc_alert, steer_required, hud_control.leadDistanceBars)
 
       can_sends.extend(hondacan.create_ui_commands(self.packer, self.CAN, self.CP, CC.enabled, pcm_speed, hud, CS.is_metric, CS.acc_hud, CS.lkas_hud))
 
