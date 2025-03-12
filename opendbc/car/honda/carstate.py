7c50f4d3c2e2bf067c95f966dc063851b05bdf57
mvl-boston <manuel@alumni.virginia.edu>
2025-03-11 21:32:41 -0400

    temp remove steerstate until accuracy check

--- opendbc/car/honda/carstate.py
+++ opendbc/car/honda/carstate.py
@@ -280,7 +280,7 @@ class CarState(CarStateBase):
       ret.cruiseState.available = bool(cp.vl[self.main_on_sig_msg]["MAIN_ON"])
 
     # Adds low speed warning as some models disable cruise at various speeds, ignore warning under 3mph
-    ret.lowSpeedAlert = ret.cruiseState.enabled and ret.vEgo >= 4 * CV.MPH_TO_MS and cp.vl["STEER_STATUS"]["STEER_CONTROL_ACTIVE"] == 0 # fix for flip cars
+    # ret.lowSpeedAlert = ret.cruiseState.enabled and ret.vEgo >= 4 * CV.MPH_TO_MS and cp.vl["STEER_STATUS"]["STEER_CONTROL_ACTIVE"] == 0 # fix for flip cars - remove for now, not sure if serial board flag is any good
 
     # Gets rid of Pedal Grinding noise when brake is pressed at slow speeds for some models
     if self.CP.carFingerprint in (CAR.HONDA_PILOT, CAR.HONDA_RIDGELINE):
