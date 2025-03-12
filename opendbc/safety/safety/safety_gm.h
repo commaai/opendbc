02280159c022cedf19e85dbe1ea26c3d1366a436
Shane Smiskol <shane@smiskol.com>
2025-03-11 17:16:21 -0700

    cleaner

--- opendbc/safety/safety/safety_gm.h
+++ opendbc/safety/safety/safety_gm.h
@@ -241,13 +241,16 @@ static safety_config gm_init(uint16_t param) {
 #endif
   gm_pcm_cruise = (gm_hw == GM_CAM) && !gm_cam_long;
 
-  safety_config ret = BUILD_SAFETY_CFG(gm_rx_checks, GM_ASCM_TX_MSGS);
+  safety_config ret;
   if (gm_hw == GM_CAM) {
     // FIXME: cppcheck thinks that gm_cam_long is always false. This is not true
     // if ALLOW_DEBUG is defined but cppcheck is run without ALLOW_DEBUG
     // cppcheck-suppress knownConditionTrueFalse
     ret = gm_cam_long ? BUILD_SAFETY_CFG(gm_rx_checks, GM_CAM_LONG_TX_MSGS) : BUILD_SAFETY_CFG(gm_rx_checks, GM_CAM_TX_MSGS);
+  } else {
+    ret = BUILD_SAFETY_CFG(gm_rx_checks, GM_ASCM_TX_MSGS);
   }
+
   if (gm_ev) {
     SET_RX_CHECKS(gm_ev_rx_checks, ret);
   }
