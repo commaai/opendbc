be7c869bd0d20b30fd6e274a3d9f4c61c1347925
Hiromasa Ihara <iharahiromasa@gmail.com>
2025-03-15 00:18:43 +0900

    update nbox castate to change transmission-type

--- opendbc/car/honda/interface.py
+++ opendbc/car/honda/interface.py
@@ -66,8 +66,8 @@ class CarInterface(CarInterfaceBase):
     if any(0x33DA in f for f in fingerprint.values()):
       ret.flags |= HondaFlags.BOSCH_EXT_HUD.value
 
-    # Accord ICE 1.5T CVT has different gearbox message
-    if candidate == CAR.HONDA_ACCORD and 0x191 in fingerprint[CAN.pt]:
+    # N-Box and Accord ICE 1.5T CVT has different gearbox message
+    if candidate in (CAR.HONDA_ACCORD, CAR.HONDA_NBOX) and 0x191 in fingerprint[CAN.pt]:
       ret.transmissionType = TransmissionType.cvt
     # Civic Type R is missing 0x191 and 0x1A3
     elif candidate == CAR.HONDA_CIVIC_2022 and all(msg not in fingerprint[CAN.pt] for msg in (0x191, 0x1A3)):
