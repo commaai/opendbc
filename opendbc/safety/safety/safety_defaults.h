de96aaf51b60be02b4978b15fbf3b6dd0d829cfd
Shane Smiskol <shane@smiskol.com>
2025-03-13 21:31:15 -0700

    safety: start fwd hook config (#1997)
    * somple start
    
    * do it correctly
    
    * misra

--- opendbc/safety/safety/safety_defaults.h
+++ opendbc/safety/safety/safety_defaults.h
@@ -13,7 +13,7 @@ void default_rx_hook(const CANPacket_t *to_push) {
 
 static safety_config nooutput_init(uint16_t param) {
   UNUSED(param);
-  return (safety_config){NULL, 0, NULL, 0};
+  return (safety_config){NULL, 0, NULL, 0, true};
 }
 
 // GCOV_EXCL_START
@@ -24,30 +24,19 @@ static bool nooutput_tx_hook(const CANPacket_t *to_send) {
 }
 // GCOV_EXCL_STOP
 
-static bool default_fwd_hook(int bus_num, int addr) {
-  UNUSED(bus_num);
-  UNUSED(addr);
-  return true;
-}
-
 const safety_hooks nooutput_hooks = {
   .init = nooutput_init,
   .rx = default_rx_hook,
   .tx = nooutput_tx_hook,
-  .fwd = default_fwd_hook,
 };
 
 // *** all output safety mode ***
-
-// Enables passthrough mode where relay is open and bus 0 gets forwarded to bus 2 and vice versa
-static bool alloutput_passthrough = false;
-
 static safety_config alloutput_init(uint16_t param) {
   // Enables passthrough mode where relay is open and bus 0 gets forwarded to bus 2 and vice versa
   const uint16_t ALLOUTPUT_PARAM_PASSTHROUGH = 1;
   controls_allowed = true;
-  alloutput_passthrough = GET_FLAG(param, ALLOUTPUT_PARAM_PASSTHROUGH);
-  return (safety_config){NULL, 0, NULL, 0};
+  bool alloutput_passthrough = GET_FLAG(param, ALLOUTPUT_PARAM_PASSTHROUGH);
+  return (safety_config){NULL, 0, NULL, 0, !alloutput_passthrough};
 }
 
 static bool alloutput_tx_hook(const CANPacket_t *to_send) {
@@ -55,15 +44,8 @@ static bool alloutput_tx_hook(const CANPacket_t *to_send) {
   return true;
 }
 
-static bool alloutput_fwd_hook(int bus_num, int addr) {
-  UNUSED(bus_num);
-  UNUSED(addr);
-  return !alloutput_passthrough;
-}
-
 const safety_hooks alloutput_hooks = {
   .init = alloutput_init,
   .rx = default_rx_hook,
   .tx = alloutput_tx_hook,
-  .fwd = alloutput_fwd_hook,
 };
