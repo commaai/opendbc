140cc18b3a94d988b735e34741b76db2b0d353c4
mvl-boston <manuel@alumni.virginia.edu>
2025-03-15 20:15:11 -0400

    typo fixes

--- opendbc/car/honda/carstate.py
+++ opendbc/car/honda/carstate.py
@@ -107,7 +107,7 @@ class CarState(CarStateBase):
     self.brake_switch_active = False
     self.cruise_setting = 0
     self.v_cruise_pcm_prev = 0
-    
+
     # When available we use cp.vl["CAR_SPEED"]["ROUGH_CAR_SPEED_2"] to populate vEgoCluster
     # However, on cars without a digital speedometer this is not always present (HRV, FIT, CRV 2016, ILX and RDX)
     self.dash_speed_seen = False
@@ -252,7 +252,6 @@ class CarState(CarStateBase):
     ret.cruiseState.available = bool(cp.vl[self.main_on_sig_msg]["MAIN_ON"])
 
     # Adds low speed warning as some models disable cruise at various speeds, ignore warning under 3mph
-    
     if self.CP.carFingerprint in (CAR.HONDA_ODYSSEY_5G_MMR):
       ret.lowSpeedAlert = ret.cruiseState.enabled and ret.vEgo >= 4 * CV.MPH_TO_MS and cp.vl["STEER_STATUS"]["STEER_CONTROL_ACTIVE"] == 0
 
