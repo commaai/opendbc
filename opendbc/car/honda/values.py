a5e59897c70739db9fd1a044892b900ec75df1cb
mvl-boston <manuel@alumni.virginia.edu>
2025-03-16 22:42:46 -0400

    tuning

--- opendbc/car/honda/values.py
+++ opendbc/car/honda/values.py
@@ -183,7 +183,7 @@ class CAR(Platforms):
   )
   HONDA_ODYSSEY_5G_MMR = HondaBoschPlatformConfig(
     [HondaCarDocs("Honda Odyssey 2021-25", "All", min_steer_speed=3. * CV.MPH_TO_MS)],
-    CarSpecs(mass=4590 * CV.LB_TO_KG, wheelbase=3.00, steerRatio=19.5, centerToFrontRatio=0.54, tireStiffnessFactor=1.18),  # per spec + plotjuggler
+    CarSpecs(mass=4590 * CV.LB_TO_KG, wheelbase=3.00, steerRatio=19.4, centerToFrontRatio=0.54, tireStiffnessFactor=1.02),  # per spec + plotjuggler
     {Bus.pt: 'acura_rdx_2020_can_generated'},
     flags=HondaFlags.BOSCH_ALT_BRAKE,
   )
