fea694ac5b449cc1b6f750bd2c90a0f90d736507
mvl-boston <manuel@alumni.virginia.edu>
2025-03-19 18:49:23 -0400

    fix doorstatus

--- opendbc/car/honda/carstate.py
+++ opendbc/car/honda/carstate.py
@@ -143,6 +143,8 @@ class CarState(CarStateBase):
       ret.doorOpen = bool(cp.vl["SCM_FEEDBACK"]["DRIVERS_DOOR_OPEN"])
     elif self.CP.carFingerprint in (CAR.HONDA_ODYSSEY_CHN, CAR.HONDA_FREED, CAR.HONDA_HRV):
       ret.doorOpen = bool(cp.vl["SCM_BUTTONS"]["DRIVERS_DOOR_OPEN"])
+    elif self.CP.carFingerprint in (CAR.ACURA_MDX_3G_HYBRID):
+      ret.doorOpen = False
     else:
       ret.doorOpen = any([cp.vl["DOORS_STATUS"]["DOOR_OPEN_FL"], cp.vl["DOORS_STATUS"]["DOOR_OPEN_FR"],
                           cp.vl["DOORS_STATUS"]["DOOR_OPEN_RL"], cp.vl["DOORS_STATUS"]["DOOR_OPEN_RR"]])
