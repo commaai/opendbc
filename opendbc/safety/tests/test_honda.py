0dcc8faebfddbacb6d9d380544abeb2e4e1d34bd
mvl-boston <manuel@alumni.virginia.edu>
2025-03-19 10:25:27 -0400

    move steercontrol back to bus 0

--- opendbc/safety/tests/test_honda.py
+++ opendbc/safety/tests/test_honda.py
@@ -8,7 +8,8 @@ import opendbc.safety.tests.common as common
 from opendbc.car.structs import CarParams
 from opendbc.safety.tests.common import CANPackerPanda, MAX_WRONG_COUNTERS
 
-HONDA_N_COMMON_TX_MSGS = [[0xE4, 0], [0xE4, 2], [0x194, 0], [0x1FA, 0], [0x30C, 0], [0x33D, 0]]
+HONDA_N_COMMON_TX_MSGS = [[0xE4, 0], [0x194, 0], [0x1FA, 0], [0x30C, 0], [0x33D, 0]]
+// HONDA_N_COMMON_TX_MSGS = [[0xE4, 0], [0xE4, 2], [0x194, 0], [0x1FA, 0], [0x30C, 0], [0x33D, 0]] - version that allowed bus 2
 
 class Btn:
   NONE = 0
@@ -172,8 +173,8 @@ class HondaBase(common.PandaCarSafetyTest):
   STEER_BUS: int | None = None  # must be set when inherited
   BUTTONS_BUS: int | None = None  # must be set when inherited, tx on this bus, rx on PT_BUS
 
-  // RELAY_MALFUNCTION_ADDRS = {0: (0xE4, 0x194)}  # STEERING_CONTROL
-  RELAY_MALFUNCTION_ADDRS = {2: (0xE4)}  # STEERING_CONTROL
+  RELAY_MALFUNCTION_ADDRS = {0: (0xE4, 0x194)}  # STEERING_CONTROL
+  // version that allowed bus 2 - RELAY_MALFUNCTION_ADDRS = {2: (0xE4)}  # STEERING_CONTROL
 
   cnt_speed = 0
   cnt_button = 0
@@ -249,12 +250,12 @@ class HondaBase(common.PandaCarSafetyTest):
 
 class TestHondaNidecSafetyBase(HondaBase):
   TX_MSGS = HONDA_N_COMMON_TX_MSGS
-  // FWD_BLACKLISTED_ADDRS = {2: [0xE4, 0x194, 0x33D, 0x30C]}
-  FWD_BLACKLISTED_ADDRS = {2: [0x194, 0x33D, 0x30C]}
+  FWD_BLACKLISTED_ADDRS = {2: [0xE4, 0x194, 0x33D, 0x30C]}
+  // version that allowed bus 2 - FWD_BLACKLISTED_ADDRS = {2: [0x194, 0x33D, 0x30C]}
 
   PT_BUS = 0
-  // STEER_BUS = 0
-  STEER_BUS = 2
+  STEER_BUS = 0
+  // version that allowed bus 2: STEER_BUS = 2
   BUTTONS_BUS = 0
 
   MAX_GAS = 198
@@ -292,8 +293,8 @@ class TestHondaNidecSafetyBase(HondaBase):
     super().test_fwd_hook()
 
     # forwarding AEB brake signal
-    // self.FWD_BLACKLISTED_ADDRS = {2: [0xE4, 0x194, 0x33D, 0x30C]}
-    self.FWD_BLACKLISTED_ADDRS = {2: [0x194, 0x33D, 0x30C]}
+    self.FWD_BLACKLISTED_ADDRS = {2: [0xE4, 0x194, 0x33D, 0x30C]}
+    // version that allowed bus 2 - self.FWD_BLACKLISTED_ADDRS = {2: [0x194, 0x33D, 0x30C]}
     self.safety.set_honda_fwd_brake(True)
     super().test_fwd_hook()
 
