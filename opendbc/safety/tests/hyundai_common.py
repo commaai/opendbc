e670860afd704e684c58d7a62a7dfda67bb51e44
Jason Wen <haibin.wen3@gmail.com>
2025-03-19 08:52:23 -0400

    MADS: Steering Mode on Brake Pedal Press (#89)

--- opendbc/safety/tests/hyundai_common.py
+++ opendbc/safety/tests/hyundai_common.py
@@ -158,7 +158,7 @@ class HyundaiLongitudinalBase(common.LongitudinalAccelSafetyTest):
 
             # Test initial state
             self._mads_states_cleanup()
-            self.safety.set_mads_params(enable_mads, False)
+            self.safety.set_mads_params(enable_mads, False, False)
 
             self.assertFalse(self.safety.get_acc_main_on())
 
@@ -191,7 +191,7 @@ class HyundaiLongitudinalBase(common.LongitudinalAccelSafetyTest):
         self.safety.set_safety_hooks(default_safety_mode, default_safety_param)
 
         self._mads_states_cleanup()
-        self.safety.set_mads_params(enable_mads, False)
+        self.safety.set_mads_params(enable_mads, False, False)
 
         # Initial state
         self._rx(self._main_cruise_button_msg(0))
@@ -226,7 +226,7 @@ class HyundaiLongitudinalBase(common.LongitudinalAccelSafetyTest):
         self.safety.set_safety_hooks(default_safety_mode, default_safety_param)
 
         self._mads_states_cleanup()
-        self.safety.set_mads_params(enable_mads, False)
+        self.safety.set_mads_params(enable_mads, False, False)
         self.safety.set_controls_allowed_lat(True)
 
         # Start with matched states
@@ -269,7 +269,7 @@ class HyundaiLongitudinalBase(common.LongitudinalAccelSafetyTest):
         self.safety.set_safety_hooks(default_safety_mode, default_safety_param)
 
         self._mads_states_cleanup()
-        self.safety.set_mads_params(enable_mads, False)
+        self.safety.set_mads_params(enable_mads, False, False)
 
         # Create initial mismatch
         self._rx(self._main_cruise_button_msg(1))  # Press button
