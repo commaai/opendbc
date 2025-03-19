08617c6efdb5a1c25fd17c1fa71a98282bbdc1d8
mvl-boston <manuel@alumni.virginia.edu>
2025-03-19 16:09:04 -0400

    standardizing

--- opendbc/car/honda/interface.py
+++ opendbc/car/honda/interface.py
@@ -79,8 +79,8 @@ class CarInterface(CarInterfaceBase):
         ret.stopAccel = CarControllerParams.BOSCH_ACCEL_MIN  # stock uses -4.0 m/s^2 once stopped but limited by safety model
     else:
       # default longitudinal tuning for all hondas
-      ret.longitudinalTuning.kiBP = [0., 5., 35.] - let tuner figure this out
-      ret.longitudinalTuning.kiV = [1.2, 0.8, 0.5] - let tuner figure this out
+      ret.longitudinalTuning.kiBP = [0., 5., 35.]
+      ret.longitudinalTuning.kiV = [1.2, 0.8, 0.5]
 
     eps_modified = False
     for fw in car_fw:
