683f4cfead137d7f7205381802bf5ab0fb187809
royjr <royjr96@gmail.com>
2025-03-21 01:26:38 -0400

    these too

--- opendbc/car/hyundai/hyundaicanfd.py
+++ opendbc/car/hyundai/hyundaicanfd.py
@@ -146,9 +146,9 @@ def create_ccnc(packer, CAN, openpilotLongitudinalControl, enabled, hud, latacti
     "LFA_ICON": 2 if latactive else 3 if enabled else 1,
     "CENTERLINE": 1 if enabled else 0,
     "LANELINE_LEFT": (
-      1 if not hud.leftLaneVisible else 4 if hud.leftLaneDepart else 0 if not latactive else 2 if out.leftBlindspot or out.vEgo < 8.94 else 6),
+      1 if not hud.leftLaneVisible else 4 if hud.leftLaneDepart else 0 if not enabled else 2 if out.leftBlindspot or out.vEgo < 8.94 else 6),
     "LANELINE_RIGHT": (
-      1 if not hud.rightLaneVisible else 4 if hud.rightLaneDepart else 0 if not latactive else 2 if out.rightBlindspot or out.vEgo < 8.94 else 6),
+      1 if not hud.rightLaneVisible else 4 if hud.rightLaneDepart else 0 if not enabled else 2 if out.rightBlindspot or out.vEgo < 8.94 else 6),
     "LCA_LEFT_ARROW": 2 if leftBlinker else 0,
     "LCA_RIGHT_ARROW": 2 if rightBlinker else 0,
     "LANE_LEFT": 1 if leftBlinker else 0,
