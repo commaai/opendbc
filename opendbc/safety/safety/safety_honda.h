af7b80d44d20d9f8f36f6601fb9771479670d6c4
mvl-boston <manuel@alumni.virginia.edu>
2025-03-20 23:08:14 -0400

    hybrid cleanup

--- opendbc/safety/safety/safety_honda.h
+++ opendbc/safety/safety/safety_honda.h
@@ -151,12 +151,14 @@ static void honda_rx_hook(const CANPacket_t *to_push) {
   if (!(alternative_experience & ALT_EXP_DISABLE_STOCK_AEB)) {
     if ((bus == 2) && (addr == 0x1FA)) {
       bool honda_stock_aeb = GET_BIT(to_push, 29U);
+      int offset = 0
       if (honda_nidec_hybrid) {
-        int honda_stock_brake = (GET_BYTE(to_push, 6) << 2) | (GET_BYTE(to_push, 7) >> 6);
+        offset = 6;
       }
       else {
-        int honda_stock_brake = (GET_BYTE(to_push, 0) << 2) | (GET_BYTE(to_push, 1) >> 6);
+        offet = 0;
       }
+      int honda_stock_brake = (GET_BYTE(to_push, 0 + offset) << 2) | (GET_BYTE(to_push, 1 + offset) >> 6);
       
       // Forward AEB when stock braking is higher than openpilot braking
       // only stop forwarding when AEB event is over
