c10bc5bd85a66b61e9f917a56154b6e3e2414288
Jason Wen <haibin.wen3@gmail.com>
2025-03-21 01:23:28 -0400

    Controls: Neural Network Lateral Control (NNLC) (#85)
    * in structs
    
    * new structs

--- opendbc/car/structs.py
+++ opendbc/car/structs.py
@@ -64,6 +64,18 @@ class CarParamsSP:
   flags: int = auto_field()        # flags for car specific quirks
   safetyParam: int = auto_field()  # flags for custom safety flags
 
+  neuralNetworkLateralControl: 'CarParamsSP.NeuralNetworkLateralControl' = field(default_factory=lambda: CarParamsSP.NeuralNetworkLateralControl())
+
+  @auto_dataclass
+  class NeuralNetworkLateralControl:
+    model: 'CarParamsSP.NeuralNetworkLateralControl.Model' = field(default_factory=lambda: CarParamsSP.NeuralNetworkLateralControl.Model())
+    fuzzyFingerprint: bool = auto_field()
+
+    @auto_dataclass
+    class Model:
+      path: str = auto_field()
+      name: str = auto_field()
+
 
 @auto_dataclass
 class ModularAssistiveDrivingSystem:
