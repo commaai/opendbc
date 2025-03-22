1dce4f27502672c5ff23823363db98b118d4231f
mvl-boston <manuel@alumni.virginia.edu>
2025-03-22 10:05:23 -0400

    return to initial steering limits

--- opendbc/car/honda/interface.py
+++ opendbc/car/honda/interface.py
@@ -156,8 +156,7 @@ class CarInterface(CarInterfaceBase):
         ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.8], [0.24]]  # TODO: can probably use some tuning
 
     elif candidate == CAR.ACURA_MDX_3G_HYBRID:
-      # ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 239], [0, 239]]  # TODO: determine if there is a dead zone at the top end
-      ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 400], [0, 400]]  # TODO: determine if there is a dead zone at the top end
+      ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 239], [0, 239]]
       ret.lateralTuning.pid.kf = 0.000035
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.115], [0.052]]
 
