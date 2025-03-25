91715fd18af49f63e044b1bbead85613c8d56a7a
Shane Smiskol <shane@smiskol.com>
2025-03-24 21:47:45 -0700

    clean up

--- opendbc/car/rivian/interface.py
+++ opendbc/car/rivian/interface.py
@@ -1,28 +1,15 @@
-import numpy as np
-from opendbc.car import get_safety_config, structs, get_friction
-from opendbc.car.interfaces import CarInterfaceBase, TorqueFromLateralAccelCallbackType, FRICTION_THRESHOLD, LatControlInputs, NanoFFModel
+from opendbc.car import get_safety_config, structs
+from opendbc.car.interfaces import CarInterfaceBase
 from opendbc.car.rivian.carcontroller import CarController
 from opendbc.car.rivian.carstate import CarState
 from opendbc.car.rivian.radar_interface import RadarInterface
-from opendbc.car.rivian.values import CarControllerParams, RivianSafetyFlags
+from opendbc.car.rivian.values import RivianSafetyFlags
 
 
 class CarInterface(CarInterfaceBase):
   CarState = CarState
   CarController = CarController
   RadarInterface = RadarInterface
-
-  def get_steer_max(self, v_ego: float) -> int:
-    return round(np.interp(v_ego, CarControllerParams.STEER_MAXES[0], CarControllerParams.STEER_MAXES[1]))
-
-  # def torque_from_lateral_accel_rivian(self, latcontrol_inputs: LatControlInputs, torque_params: structs.CarParams.LateralTorqueTuning,
-  #                                      lateral_accel_error: float, lateral_accel_deadzone: float, friction_compensation: bool, gravity_adjusted: bool) -> float:
-  #   # The default is a linear relationship between torque and lateral acceleration (accounting for road roll and steering friction)
-  #   friction = get_friction(lateral_accel_error, lateral_accel_deadzone, FRICTION_THRESHOLD, torque_params, friction_compensation)
-  #   return (latcontrol_inputs.lateral_acceleration / float(torque_params.latAccelFactor)) + friction
-  #
-  # def torque_from_lateral_accel(self) -> TorqueFromLateralAccelCallbackType:
-  #   return self.torque_from_lateral_accel_rivian
 
   @staticmethod
   def _get_params(ret: structs.CarParams, candidate, fingerprint, car_fw, experimental_long, docs) -> structs.CarParams:
--- opendbc/car/rivian/values.py
+++ opendbc/car/rivian/values.py
@@ -107,13 +107,14 @@ GEAR_MAP = {
 
 class CarControllerParams:
   # The R1T 2023 and R1S 2023 we tested on achieves slightly more lateral acceleration going left vs. right
-  # and lateral acceleration falls linearly as speed decreases from ~40 mph to 20 mph. This value is set
+  # and lateral acceleration falls linearly as speed decreases from ~40 mph to 20 mph. these values are set
   # conservatively to reach a maximum of 3.0 m/s^2 turning left at 80 mph
-  # TODO: figure out why some users' cars are seeing almost half lateral accelerations at all speeds.
 
   # these refer to turning left (R1T and R1S both seen with more torque left vs. right)
   # 250 is ~2.8 m/s^2 above 17 m/s, then linearly ramps to ~1.6 m/s^2 from 17 m/s to 10 m/s
-  STEER_MAX = 350
+  # TODO: it is theorized older models have different steering racks and achieve down to half the
+  #  lateral acceleration referenced here at all speeds. detect this and ship a torque increase for those models
+  STEER_MAX = 250  # 350 is intended to maintain lateral accel, not increase it
   STEER_MAX_LOOKUP = [9, 17], [350, 250]
   STEER_STEP = 1
   STEER_DELTA_UP = 3  # torque increase per refresh
