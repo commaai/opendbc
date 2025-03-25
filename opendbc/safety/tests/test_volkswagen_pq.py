27a83865f5adbb30d3b2ba4f0db5d7257dc138a3
Jason Young <jyoung8607@gmail.com>
2025-03-25 14:57:27 -0400

    rename to vw_pq

--- opendbc/safety/tests/test_volkswagen_pq.py
+++ opendbc/safety/tests/test_volkswagen_pq.py
@@ -116,7 +116,7 @@ class TestVolkswagenPqStockSafety(TestVolkswagenPqSafetyBase):
   FWD_BLACKLISTED_ADDRS = {2: [MSG_HCA_1, MSG_LDW_1]}
 
   def setUp(self):
-    self.packer = CANPackerPanda("vw_pq35")
+    self.packer = CANPackerPanda("vw_pq")
     self.safety = libsafety_py.libsafety
     self.safety.set_safety_hooks(CarParams.SafetyModel.volkswagenPq, 0)
     self.safety.init_tests()
@@ -137,7 +137,7 @@ class TestVolkswagenPqLongSafety(TestVolkswagenPqSafetyBase, common.Longitudinal
   INACTIVE_ACCEL = 3.01
 
   def setUp(self):
-    self.packer = CANPackerPanda("vw_pq35")
+    self.packer = CANPackerPanda("vw_pq")
     self.safety = libsafety_py.libsafety
     self.safety.set_safety_hooks(CarParams.SafetyModel.volkswagenPq, VolkswagenSafetyFlags.LONG_CONTROL)
     self.safety.init_tests()
