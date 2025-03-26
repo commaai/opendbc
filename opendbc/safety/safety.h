7174470ca57d8d511c3c01a093c58b3a78b61116
Shane Smiskol <shane@smiskol.com>
2025-03-25 20:40:00 -0700

    and more!

--- opendbc/safety/safety.h
+++ opendbc/safety/safety.h
@@ -642,14 +642,6 @@ bool steer_torque_cmd_checks(int desired_torque, int steer_req, const TorqueStee
   bool violation = false;
   uint32_t ts = microsecond_timer_get();
 
-  // Some safety models support variable torque limit based on vehicle speed
-  int max_torque = limits.max_torque;
-  if (limits.dynamic_max_torque) {
-    const float fudged_speed = (vehicle_speed.min / VEHICLE_SPEED_FACTOR) - 1.;
-    max_torque = interpolate(limits.max_torque_lookup, fudged_speed) + 1;
-    max_torque = CLAMP(max_torque, -limits.max_torque, limits.max_torque);
-  }
-
   if (controls_allowed) {
     // Some safety models support variable torque limit based on vehicle speed
     int max_torque = limits.max_torque;
