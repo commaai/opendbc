5e2aa35a5d5a33b2eeb9c35a9e549e49a9764cef
mvl-boston <manuel@alumni.virginia.edu>
2025-03-30 21:40:57 -0400

    Update carcontroller.py

--- opendbc/car/honda/carcontroller.py
+++ opendbc/car/honda/carcontroller.py
@@ -150,7 +150,7 @@ class CarController(CarControllerBase):
 
     if CC.longActive:
       accel = float (np.clip ( actuators.accel, -100.0, np.interp (steerfactor, [ 0.0, 1.0], [-3.5, 3.5]) ) )
-      if accel > CS.out.vEgo:
+      if accel > CS.out.aEgo:
         accel = 10000.0
       gas, brake = compute_gas_brake(accel, CS.out.vEgo, self.CP.carFingerprint)
     else:
