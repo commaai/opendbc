45306554b452afa2f16984f8fa6f63a429c94f10
Aubrey Wahl <10409668+aubsw@users.noreply.github.com>
2025-04-07 21:05:36 -0400

    revert main.c

--- opendbc/safety/main.c
+++ opendbc/safety/main.c
@@ -4,10 +4,10 @@
 // this file is checked by cppcheck
 
 // Ignore misra-c2012-8.7 as these functions are only called from panda and libsafety
-bool heartbeat_engaged __attribute__((unused));
+UNUSED(heartbeat_engaged);
 
-bool safety_rx_hook(const CANPacket_t *to_push) UNUSED_FUNC;
-bool safety_tx_hook(CANPacket_t *to_send) UNUSED_FUNC;
-int safety_fwd_hook(int bus_num, int addr) UNUSED_FUNC;
-void safety_tick(const safety_config *cfg) UNUSED_FUNC;
-int set_safety_hooks(uint16_t mode, uint16_t param) UNUSED_FUNC;
+UNUSED(safety_rx_hook);
+UNUSED(safety_tx_hook);
+UNUSED(safety_fwd_hook);
+UNUSED(safety_tick);
+UNUSED(set_safety_hooks);
