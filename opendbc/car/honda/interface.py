a0acee7d3e20652f0579d6893ab441b5c03699f8
mvl-boston <manuel@alumni.virginia.edu>
2025-04-09 07:58:01 -0400

    typo fix

--- opendbc/car/honda/interface.py
+++ opendbc/car/honda/interface.py
@@ -5,7 +5,7 @@ from opendbc.car.common.conversions import Conversions as CV
 from opendbc.car.disable_ecu import disable_ecu
 from opendbc.car.honda.hondacan import CanBus
 from opendbc.car.honda.values import CarControllerParams, HondaFlags, CAR, HONDA_BOSCH, \
-                                                 HONDA_NIDEC_ALT_SCM_MESSAGES, HONDA_BOSCH_RADARLESS, HondaSafetyFlags \
+                                                 HONDA_NIDEC_ALT_SCM_MESSAGES, HONDA_BOSCH_RADARLESS, HondaSafetyFlags, \
                                                  HONDA_CANFD_CAR
 from opendbc.car.honda.carcontroller import CarController
 from opendbc.car.honda.carstate import CarState
