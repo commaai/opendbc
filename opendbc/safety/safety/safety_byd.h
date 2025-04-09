59fe43657f2cba1bdbfacf8e95e056c92136a96c
yysnet <yy@yy.yy>
2025-04-09 23:21:59 +0800

    Added checks and removed not tested models

--- opendbc/safety/safety/safety_byd.h
+++ opendbc/safety/safety/safety_byd.h
@@ -2,6 +2,9 @@
 
 #include "safety_declarations.h"
 
+//Todo: Support of other platforms to be verified and added later, placeholders for now
+//Currently been tested on: Han DM and EV 21 models
+
 #define BYD_CANADDR_IPB               0x1F0
 #define BYD_CANADDR_ACC_MPC_STATE     0x316
 #define BYD_CANADDR_ACC_EPS_STATE     0x318
@@ -15,6 +18,7 @@
 #define BYD_CANBUS_ESC  0
 #define BYD_CANBUS_MRR  1
 #define BYD_CANBUS_MPC  2
+
 
 static bool byd_eps_cruiseactivated = false;
 
@@ -63,44 +67,44 @@ static void byd_rx_hook(const CANPacket_t *to_push) {
 static bool byd_tx_hook(const CANPacket_t *to_send) {
   const TorqueSteeringLimits HAN_DMEV_STEERING_LIMITS = {
     .max_torque = 300,
-    .max_rate_up = 17,
-    .max_rate_down = 17,
+    .max_rate_up = 9,
+    .max_rate_down = 9,
     .max_torque_error = 80,
-    .max_rt_delta = 243,
+    .max_rt_delta = 113,
     .type = TorqueMotorLimited,
   };
-  const TorqueSteeringLimits TANG_DMI_STEERING_LIMITS = { //values to be check
-    .max_torque = 300,
-    .max_rate_up = 17,
-    .max_rate_down = 17,
-    .max_rt_delta = 243,
-    .max_torque_error = 80,
-    .type = TorqueMotorLimited,
-  };
-  const TorqueSteeringLimits SONG_STEERING_LIMITS = { //values to be check
-    .max_torque = 300,
-    .max_rate_up = 17,
-    .max_rate_down = 17,
-    .max_rt_delta = 243,
-    .max_torque_error = 80,
-    .type = TorqueMotorLimited,
-  };
-  const TorqueSteeringLimits QIN_STEERING_LIMITS = { //values to be check
-    .max_torque = 300,
-    .max_rate_up = 17,
-    .max_rate_down = 17,
-    .max_rt_delta = 243,
-    .max_torque_error = 80,
-    .type = TorqueMotorLimited,
-  };
-  const TorqueSteeringLimits YUAN_ATTO3_STEERING_LIMITS = { //values to be check
-    .max_torque = 300,
-    .max_rate_up = 17,
-    .max_rate_down = 17,
-    .max_rt_delta = 243,
-    .max_torque_error = 80,
-    .type = TorqueMotorLimited,
-  };
+  // const TorqueSteeringLimits TANG_DMI_STEERING_LIMITS = { //values to be check
+  //   .max_torque = 300,
+  //   .max_rate_up = 9,
+  //   .max_rate_down = 9,
+  //   .max_rt_delta = 113,
+  //   .max_torque_error = 80,
+  //   .type = TorqueMotorLimited,
+  // };
+  // const TorqueSteeringLimits SONG_STEERING_LIMITS = { //values to be check
+  //   .max_torque = 300,
+  //   .max_rate_up = 9,
+  //   .max_rate_down = 9,
+  //   .max_rt_delta = 113,
+  //   .max_torque_error = 80,
+  //   .type = TorqueMotorLimited,
+  // };
+  // const TorqueSteeringLimits QIN_STEERING_LIMITS = { //values to be check
+  //   .max_torque = 300,
+  //   .max_rate_up = 9,
+  //   .max_rate_down = 9,
+  //   .max_rt_delta = 113,
+  //   .max_torque_error = 80,
+  //   .type = TorqueMotorLimited,
+  // };
+  // const TorqueSteeringLimits YUAN_ATTO3_STEERING_LIMITS = { //values to be check
+  //   .max_torque = 300,
+  //   .max_rate_up = 9,
+  //   .max_rate_down = 9,
+  //   .max_rt_delta = 113,
+  //   .max_torque_error = 80,
+  //   .type = TorqueMotorLimited,
+  // };
 
   bool tx = true;
   int bus = GET_BUS(to_send);
@@ -112,11 +116,12 @@ static bool byd_tx_hook(const CANPacket_t *to_send) {
       bool steer_req = GET_BIT(to_send, 28U) || byd_eps_cruiseactivated; //LKAS_Active
       if ( desired_torque >= 1024 )
         desired_torque -= 2048;
-      const TorqueSteeringLimits limits = (byd_platform == HAN_TANG_DMEV) ? HAN_DMEV_STEERING_LIMITS :
-                                          (byd_platform == TANG_DMI) ? TANG_DMI_STEERING_LIMITS :
-                                          (byd_platform == SONG_PLUS_DMI) ? SONG_STEERING_LIMITS :
-                                          (byd_platform == QIN_PLUS_DMI) ? QIN_STEERING_LIMITS : YUAN_ATTO3_STEERING_LIMITS;
+      // const TorqueSteeringLimits limits = (byd_platform == HAN_TANG_DMEV) ? HAN_DMEV_STEERING_LIMITS :
+      //                                     (byd_platform == TANG_DMI) ? TANG_DMI_STEERING_LIMITS :
+      //                                     (byd_platform == SONG_PLUS_DMI) ? SONG_STEERING_LIMITS :
+      //                                     (byd_platform == QIN_PLUS_DMI) ? QIN_STEERING_LIMITS : YUAN_ATTO3_STEERING_LIMITS;
 
+      const TorqueSteeringLimits limits = HAN_DMEV_STEERING_LIMITS;
       if (steer_torque_cmd_checks(desired_torque, steer_req, limits)) {
         tx = false;
       }
@@ -141,12 +146,12 @@ static bool byd_fwd_hook(int bus, int addr) {
 }
 
 static safety_config byd_init(uint16_t param) {
-
-  const uint32_t FLAG_HAN_TANG_DMEV = 0x1U;
-  const uint32_t FLAG_TANG_DMI = 0x2U;
-  const uint32_t FLAG_SONG_PLUS_DMI = 0x4U;
-  const uint32_t FLAG_QIN_PLUS_DMI = 0x8U;
-  const uint32_t FLAG_YUAN_PLUS_DMI_ATTO3 = 0x10U;
+  UNUSED(param);
+  // const uint32_t FLAG_HAN_TANG_DMEV = 0x1U;
+  // const uint32_t FLAG_TANG_DMI = 0x2U;
+  // const uint32_t FLAG_SONG_PLUS_DMI = 0x4U;
+  // const uint32_t FLAG_QIN_PLUS_DMI = 0x8U;
+  // const uint32_t FLAG_YUAN_PLUS_DMI_ATTO3 = 0x10U;
 
   static const CanMsg BYD_HAN_DMEV_TX_MSGS[] = {
     {BYD_CANADDR_ACC_CMD,         BYD_CANBUS_ESC, 8, false},
@@ -154,53 +159,45 @@ static safety_config byd_init(uint16_t param) {
     {BYD_CANADDR_ACC_EPS_STATE,   BYD_CANBUS_MPC, 8, false},
   };
 
-  static const CanMsg BYD_YUANPLUS_ATTO3_TX_MSGS[] = {
-    {BYD_CANADDR_ACC_CMD,         BYD_CANBUS_ESC, 8, false},
-    {BYD_CANADDR_ACC_MPC_STATE,   BYD_CANBUS_ESC, 8, true},
-    {BYD_CANADDR_ACC_EPS_STATE,   BYD_CANBUS_MPC, 8, false},
-  };
+  // static const CanMsg BYD_YUANPLUS_ATTO3_TX_MSGS[] = {
+  //   {BYD_CANADDR_ACC_CMD,         BYD_CANBUS_ESC, 8, false},
+  //   {BYD_CANADDR_ACC_MPC_STATE,   BYD_CANBUS_ESC, 8, true},
+  //   {BYD_CANADDR_ACC_EPS_STATE,   BYD_CANBUS_MPC, 8, false},
+  // };
 
   static RxCheck byd_han_dmev_rx_checks[] = {
-    {.msg = {{BYD_CANADDR_ACC_EPS_STATE,    BYD_CANBUS_ESC, 8, .ignore_checksum = true, .ignore_counter = true, .frequency = 50U}, { 0 }, { 0 }}},
-    {.msg = {{BYD_CANADDR_CARSPEED,         BYD_CANBUS_ESC, 8, .ignore_checksum = true, .ignore_counter = true, .frequency = 50U}, { 0 }, { 0 }}},
-    {.msg = {{BYD_CANADDR_IPB,              BYD_CANBUS_ESC, 8, .ignore_checksum = true, .ignore_counter = true, .frequency = 50U}, { 0 }, { 0 }}},
-    {.msg = {{BYD_CANADDR_DRIVE_STATE,      BYD_CANBUS_ESC, 8, .ignore_checksum = true, .ignore_counter = true, .frequency = 50U}, { 0 }, { 0 }}},
-    {.msg = {{BYD_CANADDR_ACC_HUD_ADAS,     BYD_CANBUS_MPC, 8, .ignore_checksum = true, .ignore_counter = true, .frequency = 50U}, { 0 }, { 0 }}},
+    {.msg = {{BYD_CANADDR_PEDAL,         BYD_CANBUS_ESC, 8, .ignore_checksum = true, .ignore_counter = true, .frequency = 50U}, { 0 }, { 0 }}},
+    {.msg = {{BYD_CANADDR_CARSPEED,      BYD_CANBUS_ESC, 8, .ignore_checksum = true, .ignore_counter = true, .frequency = 50U}, { 0 }, { 0 }}},
+    {.msg = {{BYD_CANADDR_ACC_EPS_STATE, BYD_CANBUS_ESC, 8, .ignore_checksum = true, .ignore_counter = true, .frequency = 50U}, { 0 }, { 0 }}},
+    {.msg = {{BYD_CANADDR_ACC_HUD_ADAS,  BYD_CANBUS_MPC, 8, .ignore_checksum = true, .ignore_counter = true, .frequency = 50U}, { 0 }, { 0 }}},
   };
 
-  // static RxCheck byd_yuanplus_atto3_rx_checks[] = {
-  //   {.msg = {{BYD_CANADDR_ACC_EPS_STATE,    BYD_CANBUS_ESC, 8, .ignore_checksum = true, .ignore_counter = true, .frequency = 50U}, { 0 }, { 0 }}},
-  //   {.msg = {{BYD_CANADDR_IPB,              BYD_CANBUS_ESC, 8, .ignore_checksum = true, .ignore_counter = true, .frequency = 50U}, { 0 }, { 0 }}},
-  //   {.msg = {{BYD_CANADDR_DRIVE_STATE,      BYD_CANBUS_ESC, 8, .ignore_checksum = true, .ignore_counter = true, .frequency = 50U}, { 0 }, { 0 }}},
-  // };
+  //   static RxCheck byd_yuanplus_atto3_rx_checks[] = {
+  //     {.msg = {{BYD_CANADDR_PEDAL,         BYD_CANBUS_ESC, 8, .ignore_checksum = true, .ignore_counter = true, .frequency = 50U}, { 0 }, { 0 }}},
+  //     {.msg = {{BYD_CANADDR_CARSPEED,      BYD_CANBUS_ESC, 8, .ignore_checksum = true, .ignore_counter = true, .frequency = 50U}, { 0 }, { 0 }}},
+  //     {.msg = {{BYD_CANADDR_ACC_EPS_STATE, BYD_CANBUS_ESC, 8, .ignore_checksum = true, .ignore_counter = true, .frequency = 50U}, { 0 }, { 0 }}},
+  //     {.msg = {{BYD_CANADDR_ACC_HUD_ADAS,  BYD_CANBUS_MPC, 8, .ignore_checksum = true, .ignore_counter = true, .frequency = 50U}, { 0 }, { 0 }}},
+  //   };
 
   safety_config ret;
 
-  bool use_han_dm = GET_FLAG(param, FLAG_HAN_TANG_DMEV);
-  bool use_tang_dmi = GET_FLAG(param, FLAG_TANG_DMI);
-  bool use_song = GET_FLAG(param, FLAG_SONG_PLUS_DMI);
-  bool use_qin = GET_FLAG(param, FLAG_QIN_PLUS_DMI);
-  bool use_yuan = GET_FLAG(param, FLAG_YUAN_PLUS_DMI_ATTO3);
+  // bool use_han_dm = GET_FLAG(param, FLAG_HAN_TANG_DMEV);
+  // bool use_tang_dmi = GET_FLAG(param, FLAG_TANG_DMI);
+  // bool use_song = GET_FLAG(param, FLAG_SONG_PLUS_DMI);
+  // bool use_qin = GET_FLAG(param, FLAG_QIN_PLUS_DMI);
+  // bool use_yuan = GET_FLAG(param, FLAG_YUAN_PLUS_DMI_ATTO3);
 
-  if(use_han_dm) {
+  // if (use_tang_dmi || use_song || use_qin) {
+  //   byd_platform = TANG_DMI;
+  //   ret = BUILD_SAFETY_CFG(byd_han_dmev_rx_checks, BYD_HAN_DMEV_TX_MSGS);
+  // } else if (use_yuan) {
+  //   byd_platform = YUAN_PLUS_DMI_ATTO3;
+
+  //   ret = BUILD_SAFETY_CFG(byd_yuanplus_atto3_rx_checks, BYD_YUANPLUS_ATTO3_TX_MSGS);
+  // } else {
     byd_platform = HAN_TANG_DMEV;
     ret = BUILD_SAFETY_CFG(byd_han_dmev_rx_checks, BYD_HAN_DMEV_TX_MSGS);
-  } else if (use_tang_dmi || use_song || use_qin) {
-    byd_platform = TANG_DMI;
-    ret = BUILD_SAFETY_CFG(byd_han_dmev_rx_checks, BYD_HAN_DMEV_TX_MSGS);
-  } else if (use_yuan) {
-    byd_platform = YUAN_PLUS_DMI_ATTO3;
-    static RxCheck byd_yuanplus_atto3_rx_checks[] = {
-      {.msg = {{BYD_CANADDR_ACC_EPS_STATE,    BYD_CANBUS_ESC, 8, .ignore_checksum = true, .ignore_counter = true, .frequency = 50U}, { 0 }, { 0 }}},
-      {.msg = {{BYD_CANADDR_IPB,              BYD_CANBUS_ESC, 8, .ignore_checksum = true, .ignore_counter = true, .frequency = 50U}, { 0 }, { 0 }}},
-      {.msg = {{BYD_CANADDR_DRIVE_STATE,      BYD_CANBUS_ESC, 8, .ignore_checksum = true, .ignore_counter = true, .frequency = 50U}, { 0 }, { 0 }}},
-    }; //why should I write such an ugly code just to comply with misra?
-    ret = BUILD_SAFETY_CFG(byd_yuanplus_atto3_rx_checks, BYD_YUANPLUS_ATTO3_TX_MSGS);
-  } else {
-    //should not reach here
-    byd_platform = HAN_TANG_DMEV;
-    ret = BUILD_SAFETY_CFG(byd_han_dmev_rx_checks, BYD_HAN_DMEV_TX_MSGS);
-  }
+  // }
 
   return ret;
 }
--- opendbc/safety/tests/test_byd.py
+++ opendbc/safety/tests/test_byd.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import unittest
 
+from opendbc.car.byd.values import BydSafetyFlags
 from opendbc.car.structs import CarParams
 from opendbc.safety.tests.libsafety import libsafety_py
 import opendbc.safety.tests.common as common
@@ -16,11 +17,7 @@ BYD_CANADDR_DRIVE_STATE       = 0x242
 BYD_CANADDR_PEDAL             = 0x342
 BYD_CANADDR_CARSPEED          = 0x121
 
-class TestBydSafety(common.PandaCarSafetyTest, common.DriverTorqueSteeringSafetyTest):
-
-  #0x316 ACC_MPC_STATE
-  #0x318 ACC_EPS_STATE
-  #0x32E ACC_CMD
+class TestBydSafety(common.PandaCarSafetyTest, common.MotorTorqueSteeringSafetyTest):
   TX_MSGS = [[BYD_CANADDR_ACC_MPC_STATE, 0],
              [BYD_CANADDR_ACC_EPS_STATE, 2],
              [BYD_CANADDR_ACC_CMD, 0]]
@@ -31,19 +28,16 @@ class TestBydSafety(common.PandaCarSafetyTest, common.DriverTorqueSteeringSafety
 
   STANDSTILL_THRESHOLD = 0
 
-  MAX_RATE_UP = 17
-  MAX_RATE_DOWN = 17
+  MAX_RATE_UP = 9
+  MAX_RATE_DOWN = 9
   MAX_TORQUE_LOOKUP = [0], [300]
-
-  MAX_RT_DELTA = 243
-
-  DRIVER_TORQUE_ALLOWANCE = 68
-  DRIVER_TORQUE_FACTOR = 1
+  MAX_RT_DELTA = 113
+  MAX_TORQUE_ERROR = 80
 
   def setUp(self):
     self.packer = CANPackerPanda("byd_han_dmev_2020")
     self.safety = libsafety_py.libsafety
-    self.safety.set_safety_hooks(CarParams.SafetyModel.byd, 1)
+    self.safety.set_safety_hooks(CarParams.SafetyModel.byd, BydSafetyFlags.HAN_TANG_DMEV)
     self.safety.init_tests()
 
   def _torque_meas_msg(self, torque):
@@ -52,14 +46,14 @@ class TestBydSafety(common.PandaCarSafetyTest, common.DriverTorqueSteeringSafety
 
   def _torque_cmd_msg(self, torque, steer_req=1):
     values = {"LKAS_Output": torque, "LKAS_Active": steer_req}
-    return self.packer.make_can_msg_panda("ACC_MPC_STATE", 2, values)
+    return self.packer.make_can_msg_panda("ACC_MPC_STATE", 0, values)
 
   def _speed_msg(self, speed):
     values = {"CarDisplaySpeed": speed}
     return self.packer.make_can_msg_panda("CARSPEED", 0, values)
 
   def _user_brake_msg(self, brake):
-    values = {"BrakePedal": brake}
+    values = {"BrakePedal": 1 if brake else 0}
     return self.packer.make_can_msg_panda("PEDAL", 0, values)
 
   def _user_gas_msg(self, gas):
@@ -70,12 +64,6 @@ class TestBydSafety(common.PandaCarSafetyTest, common.DriverTorqueSteeringSafety
     values = {"AccState": 3 if enable else 0}
     return self.packer.make_can_msg_panda("ACC_HUD_ADAS", 2, values)
 
-  def _button_msg(self, resume=False, cancel=False):
-    values = {
-      "BTN_AccCancel": cancel,
-      "BTN_AccUpDown_Cmd": 3 if resume else 0,
-    }
-    return self.packer.make_can_msg_panda("PCM_BUTTONS", 0, values)
 
 if __name__ == "__main__":
   unittest.main()
