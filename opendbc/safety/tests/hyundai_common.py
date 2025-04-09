a2d2aed407d45fc423bf79b8c8ef5f9b181f8bdf
Shane Smiskol <shane@smiskol.com>
2025-04-08 21:25:50 -0700

    Revert "Move relay malfunction check to fwd hook (#2087)"
    This reverts commit 5a24ffee4549b857837df03a7faaf5f31236f787.

--- opendbc/safety/tests/hyundai_common.py
+++ opendbc/safety/tests/hyundai_common.py
@@ -2,6 +2,7 @@ import unittest
 
 import opendbc.safety.tests.common as common
 from opendbc.safety.tests.libsafety import libsafety_py
+from opendbc.safety.tests.common import make_msg
 
 
 class Buttons:
@@ -150,5 +151,5 @@ class HyundaiLongitudinalBase(common.LongitudinalAccelSafetyTest):
 
     addr, bus = self.DISABLED_ECU_ACTUATION_MSG
     self.assertFalse(self.safety.get_relay_malfunction())
-    self.safety.safety_fwd_hook(bus, addr)
+    self._rx(make_msg(bus, addr, 8))
     self.assertTrue(self.safety.get_relay_malfunction())
