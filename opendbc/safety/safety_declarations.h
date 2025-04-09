fbcf8d428c02a55b9f70e563361d1a72fafd99ac
Shane Smiskol <shane@smiskol.com>
2025-04-08 19:34:30 -0700

    whops

--- opendbc/safety/safety_declarations.h
+++ opendbc/safety/safety_declarations.h
@@ -193,7 +193,7 @@ void gen_crc_lookup_table_8(uint8_t poly, uint8_t crc_lut[]);
 #ifdef CANFD
 void gen_crc_lookup_table_16(uint16_t poly, uint16_t crc_lut[]);
 #endif
-static void generic_rx_checks();
+static void generic_rx_checks(void);
 static void stock_ecu_check(bool stock_ecu_detected);
 bool steer_torque_cmd_checks(int desired_torque, int steer_req, const TorqueSteeringLimits limits);
 bool steer_angle_cmd_checks(int desired_angle, bool steer_control_enabled, const AngleSteeringLimits limits);
