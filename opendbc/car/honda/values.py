3214a6aac7f8be7814daba82603bf088ea396c6c
vanillagorillaa <31773928+vanillagorillaa@users.noreply.github.com>
2025-04-12 09:21:57 -0400

    remove

--- opendbc/car/honda/values.py
+++ opendbc/car/honda/values.py
@@ -139,12 +139,6 @@ class CAR(Platforms):
     CarSpecs(mass=3279 * CV.LB_TO_KG, wheelbase=2.83, steerRatio=16.33, centerToFrontRatio=0.39, tireStiffnessFactor=0.8467),
     {Bus.pt: 'honda_accord_2018_can_generated'},
   )
-  HONDA_ACCORD_11G = HondaBoschPlatformConfig(
-    [HondaCarDocs("Honda Accord 2023", "All")],
-    CarSpecs(mass=3279 * CV.LB_TO_KG, wheelbase=2.83, steerRatio=16.33, centerToFrontRatio=0.39, tireStiffnessFactor=0.8467),
-    {Bus.pt: 'honda_pilot_2023_can_generated'},
-    flags=HondaFlags.CANFD_CAR,
-  )
   HONDA_CIVIC_BOSCH = HondaBoschPlatformConfig(
     [
       HondaCarDocs("Honda Civic 2019-21", "All", video_link="https://www.youtube.com/watch?v=4Iz1Mz5LGF8",
