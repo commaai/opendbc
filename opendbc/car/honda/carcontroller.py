5743e08af8ad3ce8f65578cb3031e3e04dbb3f07
mvl-boston <manuel@alumni.virginia.edu>
2025-04-14 21:10:10 -0400

    adding stop go support

--- opendbc/car/honda/carcontroller.py
+++ opendbc/car/honda/carcontroller.py
@@ -196,9 +196,9 @@ class CarController(CarControllerBase):
       if self.frame % 2 == 0 and self.CP.carFingerprint not in HONDA_BOSCH_RADARLESS:  # radarless cars don't have supplemental message
         can_sends.append(hondacan.create_bosch_supplemental_1(self.packer, self.CAN))
       # If using stock ACC, spam cancel command to kill gas when OP disengages.
-      if pcm_cancel_cmd and self.CP.carFingerprint not in HONDA_CANFD_CAR: # canFD buttons blocked awaiting PR 998
+      if pcm_cancel_cmd:
         can_sends.append(hondacan.spam_buttons_command(self.packer, self.CAN, CruiseButtons.CANCEL, self.CP.carFingerprint))
-      elif CC.cruiseControl.resume and self.CP.carFingerprint not in HONDA_CANFD_CAR: # canFD buttons blocked awaiting PR 998
+      elif CC.cruiseControl.resume:
         can_sends.append(hondacan.spam_buttons_command(self.packer, self.CAN, CruiseButtons.RES_ACCEL, self.CP.carFingerprint))
 
     else:
