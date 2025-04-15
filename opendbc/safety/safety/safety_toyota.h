c515573b43747085f1c9511b318272078df200b2
chrispypatt <christriplenickel@yahoo.com>
2025-04-14 21:55:28 -0500

    Update safety

--- opendbc/safety/safety/safety_toyota.h
+++ opendbc/safety/safety/safety_toyota.h
@@ -14,7 +14,7 @@
 #define TOYOTA_COMMON_SECOC_TX_MSGS \
   TOYOTA_BASE_TX_MSGS \
   {0x2E4, 0, 8, .check_relay = true}, {0x131, 0, 8, .check_relay = true}, \
-  {0x343, 0, 8, .check_relay = true},  /* ACC cancel cmd */  \
+  {0x343, 0, 8, .check_relay = false},  /* ACC cancel cmd */  \
 
 #define TOYOTA_COMMON_LONG_TX_MSGS \
   TOYOTA_COMMON_TX_MSGS \
