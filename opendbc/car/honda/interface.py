f658db390a569c687cba928586021e7310e62968
vanillagorillaa <31773928+vanillagorillaa@users.noreply.github.com>
2025-04-15 06:57:57 -0500

    remove for now

--- opendbc/car/honda/interface.py
+++ opendbc/car/honda/interface.py
@@ -217,7 +217,7 @@ class CarInterface(CarInterfaceBase):
     if ret.openpilotLongitudinalControl and candidate in HONDA_BOSCH:
       ret.safetyConfigs[0].safetyParam |= HondaSafetyFlags.BOSCH_LONG.value
 
-    if candidate in (HONDA_BOSCH_RADARLESS | HONDA_CANFD_CAR):
+    if candidate in HONDA_BOSCH_RADARLESS:
       ret.safetyConfigs[-1].safetyParam |= HondaSafetyFlags.RADARLESS.value
 
     # min speed to enable ACC. if car can do stop and go, then set enabling speed
