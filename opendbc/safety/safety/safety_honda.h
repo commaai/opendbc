288778a77c3a4f2fa67c78498c345da7d6c52222
vanillagorillaa <31773928+vanillagorillaa@users.noreply.github.com>
2025-04-25 21:09:34 -0500

    add

--- opendbc/safety/safety/safety_honda.h
+++ opendbc/safety/safety/safety_honda.h
@@ -389,6 +389,8 @@ static safety_config honda_bosch_init(uint16_t param) {
     SET_RX_CHECKS(honda_bosch_radarless_rx_checks, ret);
   } else if (honda_alt_brake_msg) {
     SET_RX_CHECKS(honda_bosch_alt_brake_rx_checks, ret);
+  } else if (honda_bosch_canfd) {
+    SET_RX_CHECKS(honda_bosch_canfd_rx_checks, ret);
   } else {
     SET_RX_CHECKS(honda_bosch_rx_checks, ret);
   }
