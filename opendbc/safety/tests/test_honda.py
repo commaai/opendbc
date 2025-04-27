d32cc77c62babc9882df0d06f6f6607a122dd7b8
vanillagorillaa <31773928+vanillagorillaa@users.noreply.github.com>
2025-04-26 22:35:18 -0500

    update tests

--- opendbc/safety/tests/test_honda.py
+++ opendbc/safety/tests/test_honda.py
@@ -571,9 +571,9 @@ class TestHondaBoschCANFDSafetyBase(TestHondaBoschSafetyBase):
   STEER_BUS = 0
   BUTTONS_BUS = 0
 
-  TX_MSGS = [[0xE4, 0], [0x296, 0], [0x33D, 0]]
-  FWD_BLACKLISTED_ADDRS = {2: [0xE4, 0x33D]}
-  RELAY_MALFUNCTION_ADDRS = {0: (0xE4, 0x33D)}  # STEERING_CONTROL
+  TX_MSGS = [[0xE4, 0], [0x296, 0], [0x33D, 0] , [0x33DB, 0]]
+  FWD_BLACKLISTED_ADDRS = {2: [0xE4, 0xE5, 0x33D]}
+  RELAY_MALFUNCTION_ADDRS = {0: (0xE4, 0xE5, 0x33D)}  # STEERING_CONTROL
 
   def setUp(self):
     self.packer = CANPackerPanda("honda_pilot_2023_can_generated")
