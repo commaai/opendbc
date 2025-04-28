e59e27ca32c630baaa3f8836a547f4ebe6cc8d63
mvl-boston <manuel@alumni.virginia.edu>
2025-04-28 17:23:17 -0400

    Update carstate.py

--- opendbc/car/honda/carstate.py
+++ opendbc/car/honda/carstate.py
@@ -55,8 +55,9 @@ def get_can_messages(CP, gearbox_msg):
   if CP.carFingerprint in (HONDA_BOSCH | {CAR.HONDA_CIVIC, CAR.HONDA_ODYSSEY, CAR.HONDA_ODYSSEY_CHN}):
     messages.append(("EPB_STATUS", 50))
 
-  if CP.carFingerprint in HONDA_BOSCH_CANFD and False: # find a signal
-    messages.append(("BRAKE_ERROR", 100))
+  if CP.carFingerprint in HONDA_BOSCH_CANFD:
+    messages.append(("BRAKE_ERROR", 0))
+    # messages.append(("BRAKE_ERROR", 100)) - find a signal for MDX
 
   if CP.carFingerprint in HONDA_BOSCH:
     # these messages are on camera bus on radarless cars
