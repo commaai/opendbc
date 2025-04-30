9400c664826c66c49f57d625c57aa9588ea0dd5d
Shane Smiskol <shane@smiskol.com>
2025-04-29 19:15:02 -0700

    Consolidate lateral ISO globals

--- opendbc/car/ford/carcontroller.py
+++ opendbc/car/ford/carcontroller.py
@@ -4,14 +4,12 @@ from opendbc.can.packer import CANPacker
 from opendbc.car import ACCELERATION_DUE_TO_GRAVITY, Bus, DT_CTRL, apply_std_steer_angle_limits, structs
 from opendbc.car.ford import fordcan
 from opendbc.car.ford.values import CarControllerParams, FordFlags
-from opendbc.car.interfaces import CarControllerBase, V_CRUISE_MAX
+from opendbc.car.interfaces import CarControllerBase, ISO_LATERAL_ACCEL, V_CRUISE_MAX
 
 LongCtrlState = structs.CarControl.Actuators.LongControlState
 VisualAlert = structs.CarControl.HUDControl.VisualAlert
 
-# ISO 11270
-ISO_LATERAL_ACCEL = 3.0  # m/s^2  # TODO: import from test lateral limits file?
-
+# CAN FD limits:
 # Limit to average banked road since safety doesn't have the roll
 AVERAGE_ROAD_ROLL = 0.06  # ~3.4 degrees, 6% superelevation
 MAX_LATERAL_ACCEL = ISO_LATERAL_ACCEL - (ACCELERATION_DUE_TO_GRAVITY * AVERAGE_ROAD_ROLL)  # ~2.4 m/s^2
