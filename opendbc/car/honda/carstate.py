276e79cacf3537562f02e7764bd1f242c4bd9008
mvl-boston <manuel@alumni.virginia.edu>
2025-05-01 01:46:34 -0400

    fixing copy/paste error

--- opendbc/car/honda/carstate.py
+++ opendbc/car/honda/carstate.py
@@ -246,7 +246,7 @@ class CarState(CarStateBase):
     ret.cruiseState.available = bool(cp.vl[self.main_on_sig_msg]["MAIN_ON"])
 
     # Adds low speed warning as some models disable cruise at various speeds, ignore warning under 3mph
-    if self.CP.carFingerprint == CAR.HONDA_ODYSSEY_5G_MMR:
+    if self.CP.carFingerprint == CAR.ACURA_RDX_3G_MMR:
       ret.lowSpeedAlert = ret.cruiseState.enabled and ret.vEgo >= 4 * CV.MPH_TO_MS and \
         cp.vl["STEER_STATUS"]["STEER_CONTROL_ACTIVE"] == 0 and not ret.steeringPressed
 
