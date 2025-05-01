9712ed3b210d6a4bc2ff6a912525897846bd675b
mvl-boston <manuel@alumni.virginia.edu>
2025-05-01 07:48:56 -0400

    new test

--- opendbc/car/honda/hondacan.py
+++ opendbc/car/honda/hondacan.py
@@ -81,8 +81,7 @@ def create_acc_commands(packer, CAN, enabled, active, accel, gas, stopping_count
   control_on = 5 if enabled else 0
   gas_command = gas if active and accel > min_gas_accel else -30000
   accel_command = accel if active else 0
-  braking = 1 if active and accel < min_gas_accel else 0
-  braking = 1 if active and accel < min_gas_accel else 0
+  braking = 1 if active and (accel + extra_brake) < min_gas_accel else 0
   standstill = 1 if active and stopping_counter > 0 else 0
   standstill_release = 1 if active and stopping_counter == 0 else 0
 
