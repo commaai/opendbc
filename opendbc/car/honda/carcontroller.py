7c69b76ee09e7e7c70410d1bf16505242e05ffeb
mvl-boston <manuel@alumni.virginia.edu>
2025-05-03 19:35:21 -0400

    Update carcontroller.py

--- opendbc/car/honda/carcontroller.py
+++ opendbc/car/honda/carcontroller.py
@@ -222,7 +222,7 @@ class CarController(CarControllerBase):
           stopping = actuators.longControlState == LongCtrlState.stopping
           self.stopping_counter = self.stopping_counter + 1 if stopping else 0
 
-          self.accel = float(np.clip(aTarget - stopping_counter * self.CP.stoppingDecelRate, self.params.BOSCH_ACCEL_MIN, self.params.BOSCH_ACCEL_MAX))
+          self.accel = float(np.clip(aTarget - self.stopping_counter * self.CP.stoppingDecelRate, self.params.BOSCH_ACCEL_MIN, self.params.BOSCH_ACCEL_MAX))
           self.gas = float(np.interp(accel + wind_brake_ms2 + hill_brake, self.params.BOSCH_GAS_LOOKUP_BP, self.params.BOSCH_GAS_LOOKUP_V))
 
           can_sends.extend(hondacan.create_acc_commands(self.packer, self.CAN, CC.enabled, CC.longActive, self.accel, self.gas,
