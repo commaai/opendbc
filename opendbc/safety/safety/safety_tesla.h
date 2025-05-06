961754aa9fd64a659a618d242e213b87f6e6131e
Shane Smiskol <shane@smiskol.com>
2025-05-05 19:55:16 -0700

    forward stock LKAS

--- opendbc/safety/safety/safety_tesla.h
+++ opendbc/safety/safety/safety_tesla.h
@@ -4,6 +4,12 @@
 
 static bool tesla_longitudinal = false;
 static bool tesla_stock_aeb = false;
+
+// Only rising edges while controls are not allowed are considered for these systems:
+// TODO: Only LKAS (non-emergency) is currently supported since we've only seen it
+static bool tesla_stock_lkas = false;
+static bool tesla_stock_lkas_prev = false;
+
 // Only Summon is currently supported due to Autopark not setting Autopark state properly
 static bool tesla_autopark = false;
 static bool tesla_autopark_prev = false;
@@ -76,9 +82,25 @@ static void tesla_rx_hook(const CANPacket_t *to_push) {
   }
 
   if (bus == 2) {
+    // DAS_control
     if (addr == 0x2b9) {
       // "AEB_ACTIVE"
       tesla_stock_aeb = (GET_BYTE(to_push, 2) & 0x03U) == 1U;
+    }
+
+    // DAS_steeringControl
+    if (addr == 0x488) {
+      const int steering_control_type = GET_BYTE(to_push, 2) >> 6;
+      const tesla_stock_lkas_now = steering_control_type == 2;  // "LANE_KEEP_ASSIST"
+
+      // Only consider rising edges while controls are not allowed
+      if (tesla_stock_lkas_now && !tesla_stock_lkas_prev && !controls_allowed) {
+        tesla_stock_lkas = true;
+      }
+      if (!tesla_stock_lkas_now) {
+        tesla_stock_lkas = false;
+      }
+      tesla_stock_lkas_prev = tesla_stock_lkas_now;
     }
   }
 }
@@ -121,6 +143,11 @@ static bool tesla_tx_hook(const CANPacket_t *to_send) {
     int steer_control_type = GET_BYTE(to_send, 2) >> 6;
     bool steer_control_enabled = (steer_control_type != 0) &&  // NONE
                                  (steer_control_type != 3);    // DISABLED
+
+    if (tesla_stock_lkas) {
+      // Don't allow any steering commands when stock LKAS is active
+      violation = true;
+    }
 
     if (steer_angle_cmd_checks(desired_angle, steer_control_enabled, TESLA_STEERING_LIMITS)) {
       violation = true;
@@ -178,8 +205,13 @@ static bool tesla_fwd_hook(int bus_num, int addr) {
 
   if (bus_num == 2) {
     if (!tesla_autopark) {
-      // DAS_steeringControl, APS_eacMonitor
-      if ((addr == 0x488) || (addr == 0x27d)) {
+      // APS_eacMonitor
+      if (addr == 0x27d) {
+        block_msg = true;
+      }
+
+      // DAS_steeringControl
+      if ((addr == 0x488) && !tesla_stock_lkas) {
         block_msg = true;
       }
 
@@ -214,6 +246,8 @@ static safety_config tesla_init(uint16_t param) {
 #endif
 
   tesla_stock_aeb = false;
+  tesla_stock_lkas = false;
+  tesla_stock_lkas_prev = false;
   // we need to assume Autopark/Summon on startup since DI_state is a low freq msg.
   // this is so that we don't fault if starting while these systems are active
   tesla_autopark = true;
@@ -221,6 +255,7 @@ static safety_config tesla_init(uint16_t param) {
 
   static RxCheck tesla_model3_y_rx_checks[] = {
     {.msg = {{0x2b9, 2, 8, .ignore_checksum = true, .ignore_counter = true, .frequency = 25U}, { 0 }, { 0 }}},   // DAS_control
+    {.msg = {{0x488, 2, 4, .ignore_checksum = true, .ignore_counter = true, .frequency = 50U}, { 0 }, { 0 }}},   // DAS_steeringControl
     {.msg = {{0x257, 0, 8, .ignore_checksum = true, .ignore_counter = true, .frequency = 50U}, { 0 }, { 0 }}},   // DI_speed (speed in kph)
     {.msg = {{0x370, 0, 8, .ignore_checksum = true, .ignore_counter = true, .frequency = 100U}, { 0 }, { 0 }}},  // EPAS3S_sysStatus (steering angle)
     {.msg = {{0x118, 0, 8, .ignore_checksum = true, .ignore_counter = true, .frequency = 100U}, { 0 }, { 0 }}},  // DI_systemStatus (gas pedal)
