fc229463628f535de2d445f4887064d77a0b6798
mvl-boston <manuel@alumni.virginia.edu>
2025-05-08 08:09:07 -0400

    removing speed control

--- opendbc/car/honda/hondacan.py
+++ opendbc/car/honda/hondacan.py
@@ -166,7 +166,7 @@ def create_bosch_supplemental_1(packer, CAN):
   return packer.make_can_msg("BOSCH_SUPPLEMENTAL_1", CAN.lkas, values)
 
 
-def create_ui_commands(packer, CAN, CP, enabled, pcm_speed, hud, is_metric, acc_hud, lkas_hud, speed_control):
+def create_ui_commands(packer, CAN, CP, enabled, pcm_speed, hud, is_metric, acc_hud, lkas_hud):
   commands = []
   radar_disabled = CP.carFingerprint in (HONDA_BOSCH - HONDA_BOSCH_RADARLESS) and CP.openpilotLongitudinalControl
 
