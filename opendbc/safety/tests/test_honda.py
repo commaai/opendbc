43029539679afcdbefdd6e973bd6d78172438600
mvl-boston <manuel@alumni.virginia.edu>
2025-05-08 08:16:10 -0400

    updating for 0x1c9

--- opendbc/safety/tests/test_honda.py
+++ opendbc/safety/tests/test_honda.py
@@ -548,7 +548,7 @@ class TestHondaBoschRadarlessLongSafety(common.LongitudinalAccelSafetyTest, Hond
   """
   TX_MSGS = [[0xE4, 0], [0x33D, 0], [0x1C8, 0], [0x30C, 0], [0x1C9, 0]]
   FWD_BLACKLISTED_ADDRS = {2: [0xE4, 0x33D, 0x1C8, 0x30C]}
-  RELAY_MALFUNCTION_ADDRS = {0: (0xE4, 0x1C8, 0x30C, 0x33D)}
+  RELAY_MALFUNCTION_ADDRS = {0: (0xE4, 0x1C8, 0x30C, 0x33D, 0x1C9)}
 
   def setUp(self):
     super().setUp()
