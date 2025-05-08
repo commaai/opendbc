856503a844fa4440cb0c39a8c45c1d24f5c4e477
mvl-boston <manuel@alumni.virginia.edu>
2025-05-08 08:24:18 -0400

    updating comment - to rerun CI checkers

--- opendbc/car/honda/carstate.py
+++ opendbc/car/honda/carstate.py
@@ -159,7 +159,7 @@ class CarState(CarStateBase):
     ret.steerFaultTemporary = steer_status not in ("NORMAL", "LOW_SPEED_LOCKOUT", "NO_TORQUE_ALERT_2")
 
     if self.CP.carFingerprint in HONDA_BOSCH_RADARLESS:
-      # ret.accFaulted = bool(cp.vl["CRUISE_FAULT_STATUS"]["CRUISE_FAULT"])
+      # ret.accFaulted = bool(cp.vl["CRUISE_FAULT_STATUS"]["CRUISE_FAULT"]) - temp remove for initial testing
       ret.accFaulted = False
     else:
       # On some cars, these two signals are always 1, this flag is masking a bug in release
