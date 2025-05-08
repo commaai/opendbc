fd3b8910afb6991d45188e5174ae338c00530faf
mvl-boston <manuel@alumni.virginia.edu>
2025-05-08 08:06:54 -0400

    typos

--- opendbc/car/honda/carstate.py
+++ opendbc/car/honda/carstate.py
@@ -280,12 +280,12 @@ class CarState(CarStateBase):
       self.lkas_hud = cp_cam.vl["LKAS_HUD"]
       self.voacc_camera = cp_cam.vl["VOACC_CAMERA"]
       # recalibrating lead spoofing inputs
-      if voacc_camera["LEAD_DISTANCE_OBSERVED"] > 0:
+      if self.voacc_camera["LEAD_DISTANCE_OBSERVED"] > 0:
         if ret.vEgo >= 10:
-          self.voacc_last_target_distance = voacc_camera["LEAD_DISTANCE_TARGET"]
+          self.voacc_last_target_distance = self.voacc_camera["LEAD_DISTANCE_TARGET"]
           self.voacc_last_vEgospeed = ret.vEgo
         if ret.aEgo <= -0.25:
-          self.voacc_last_target_diff = voacc_camera["LEAD_DISTANCE_OBSERVED"] - voacc_camera["LEAD_DISTANCE_TARGET"]
+          self.voacc_last_target_diff = self.voacc_camera["LEAD_DISTANCE_OBSERVED"] - self.voacc_camera["LEAD_DISTANCE_TARGET"]
           self.voacc_last_target_accel = ret.aEgo
 
     if self.CP.enableBsm:
