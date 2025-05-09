36461557c47176b16a081ea40195bc327a34d97f
Jason Wen <haibin.wen3@gmail.com>
2025-05-09 12:49:52 -0400

    Subaru: Remove Pre-Global from `dashcamOnly` (#150)

--- opendbc/car/subaru/interface.py
+++ opendbc/car/subaru/interface.py
@@ -99,6 +99,13 @@ class CarInterface(CarInterfaceBase):
     return ret
 
   @staticmethod
+  def _get_params_sp(stock_cp: structs.CarParams, ret: structs.CarParamsSP, candidate, fingerprint: dict[int, dict[int, int]],
+                     car_fw: list[structs.CarParams.CarFw], alpha_long: bool, docs: bool) -> structs.CarParamsSP:
+    stock_cp.dashcamOnly = bool(stock_cp.flags & (SubaruFlags.LKAS_ANGLE | SubaruFlags.HYBRID))
+
+    return ret
+
+  @staticmethod
   def init(CP, CP_SP, can_recv, can_send):
     if CP.flags & SubaruFlags.DISABLE_EYESIGHT:
       disable_ecu(can_recv, can_send, bus=2, addr=GLOBAL_ES_ADDR, com_cont_req=b'\x28\x03\x01')
