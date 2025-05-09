c9386d7eb1b5255567d73605353278322daf1419
alan-polk <56047663+alan-polk@users.noreply.github.com>
2025-05-09 11:41:08 -0500

    F150 Lighting Expedition Install Video

--- opendbc/car/ford/values.py
+++ opendbc/car/ford/values.py
@@ -81,7 +81,9 @@ class FordCarDocs(CarDocs):
 
     if harness == CarHarness.ford_q4:
       self.setup_video_link = "https://www.youtube.com/watch?v=uUGkH6C_EQU"
-
+      
+    if CP.carFingerprint in (CAR.FORD_F_150_MK14, CAR.FORD_F_150_LIGHTNING_MK1):
+      self.setup_video_link = "https://www.youtube.com/watch?v=MewJc9LYp9M"    
 
 @dataclass
 class FordPlatformConfig(PlatformConfig):
