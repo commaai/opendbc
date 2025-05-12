0c05e5cf027ff9220706ca75fb5c6a43e23c1a32
Shane Smiskol <shane@smiskol.com>
2025-05-12 16:50:58 -0700

    CANParser: support Tesla checksums (#2223)
    * fix
    
    * fix more
    
    * stash
    
    * sucks we have to do this
    
    * cons
    
    * fix
    
    * implement
    
    * ???
    
    * ugh
    
    * works
    
    * stash
    
    * clean up
    
    * can check that too

--- opendbc/can/common.h
+++ opendbc/can/common.h
@@ -33,6 +33,7 @@ unsigned int xor_checksum(uint32_t address, const Signal &sig, const std::vector
 unsigned int hkg_can_fd_checksum(uint32_t address, const Signal &sig, const std::vector<uint8_t> &d);
 unsigned int fca_giorgio_checksum(uint32_t address, const Signal &sig, const std::vector<uint8_t> &d);
 unsigned int pedal_checksum(uint32_t address, const Signal &sig, const std::vector<uint8_t> &d);
+unsigned int tesla_checksum(uint32_t address, const Signal &sig, const std::vector<uint8_t> &d);
 
 #define DBC_ASSERT(condition, message)                             \
   do {                                                             \
