76a6fd8fd1d0af53898ad963cb382299eebdd0a4
Shane Smiskol <shane@smiskol.com>
2025-05-12 16:54:14 -0700

    Tesla: let CANPacker set checksums (#2224)
    tesla: let CANPacker set checksums

--- opendbc/car/tesla/teslacan.py
+++ opendbc/car/tesla/teslacan.py
@@ -7,12 +7,6 @@ class TeslaCAN:
   def __init__(self, packer):
     self.packer = packer
 
-  @staticmethod
-  def checksum(msg_id, dat):
-    ret = (msg_id & 0xFF) + ((msg_id >> 8) & 0xFF)
-    ret += sum(dat)
-    return ret & 0xFF
-
   def create_steering_control(self, angle, enabled, counter):
     values = {
       "DAS_steeringAngleRequest": -angle,
@@ -21,8 +15,6 @@ class TeslaCAN:
       "DAS_steeringControlCounter": counter,
     }
 
-    data = self.packer.make_can_msg("DAS_steeringControl", CANBUS.party, values)[1]
-    values["DAS_steeringControlChecksum"] = self.checksum(0x488, data[:3])
     return self.packer.make_can_msg("DAS_steeringControl", CANBUS.party, values)
 
   def create_longitudinal_command(self, acc_state, accel, cntr, v_ego, active):
@@ -40,10 +32,7 @@ class TeslaCAN:
       "DAS_accelMin": accel,
       "DAS_accelMax": max(accel, 0),
       "DAS_controlCounter": cntr,
-      "DAS_controlChecksum": 0,
     }
-    data = self.packer.make_can_msg("DAS_control", CANBUS.party, values)[1]
-    values["DAS_controlChecksum"] = self.checksum(0x2b9, data[:7])
     return self.packer.make_can_msg("DAS_control", CANBUS.party, values)
 
   def create_steering_allowed(self, counter):
@@ -52,6 +41,4 @@ class TeslaCAN:
       "APS_eacMonitorCounter": counter,
     }
 
-    data = self.packer.make_can_msg("APS_eacMonitor", CANBUS.party, values)[1]
-    values["APS_eacMonitorChecksum"] = self.checksum(0x27d, data[:2])
     return self.packer.make_can_msg("APS_eacMonitor", CANBUS.party, values)
