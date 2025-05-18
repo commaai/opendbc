3a8b097b3dad97ddc2d721c037c980f29579293a
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2025-05-17 17:39:12 -0700

    include from root

--- opendbc/safety/tests/libsafety/safety.c
+++ opendbc/safety/tests/libsafety/safety.c
@@ -1,13 +1,13 @@
 #include <stdbool.h>
 
-#include "safety/board/fake_stm.h"
-#include "safety/board/can.h"
+#include "opendbc/safety/board/fake_stm.h"
+#include "opendbc/safety/board/can.h"
 
 //int safety_tx_hook(CANPacket_t *to_send) { return 1; }
 
-#include "safety/board/faults.h"
-#include "safety/safety.h"
-#include "safety/board/drivers/can_common.h"
+#include "opendbc/safety/board/faults.h"
+#include "opendbc/safety/safety.h"
+#include "opendbc/safety/board/drivers/can_common.h"
 
 // libsafety stuff
-#include "safety/tests/libsafety/safety_helpers.h"
+#include "opendbc/safety/tests/libsafety/safety_helpers.h"
