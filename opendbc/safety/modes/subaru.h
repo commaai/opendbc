ddfefba9fced5c54fe8aecdbe1f08a2fe77b39b0
Shane Smiskol <shane@smiskol.com>
2025-05-28 14:54:14 -0700

    safety: kph -> ms constant (#2291)
    * is this good?
    
    * function
    
    * function!
    
    * not now
    
    * Revert "function"
    
    This reverts commit 85f35e8663ef4f97e351a2217b3a337a3a9992d5.
    
    * use it
    
    * will split out into helpers soon

--- opendbc/safety/modes/subaru.h
+++ opendbc/safety/modes/subaru.h
@@ -123,7 +123,7 @@ static void subaru_rx_hook(const CANPacket_t *to_push) {
 
     vehicle_moving = (fr > 0U) || (rr > 0U) || (rl > 0U) || (fl > 0U);
 
-    UPDATE_VEHICLE_SPEED((fr + rr + rl + fl) / 4.0 * 0.057 / 3.6);
+    UPDATE_VEHICLE_SPEED((fr + rr + rl + fl) / 4.0 * 0.057 * KPH_TO_MS);
   }
 
   if ((addr == MSG_SUBARU_Brake_Status) && (bus == alt_main_bus)) {
