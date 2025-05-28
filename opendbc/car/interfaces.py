59554c3edf88d7c8a38b993fc3856b39ce153094
Shane Smiskol <shane@smiskol.com>
2025-05-28 15:21:47 -0700

    fixes

--- opendbc/car/interfaces.py
+++ opendbc/car/interfaces.py
@@ -135,7 +135,7 @@ class CarInterfaceBase(ABC):
     """
     Parameters essential to controlling the car may be incomplete or wrong without FW versions or fingerprints.
     """
-    return cls.get_params(candidate, gen_empty_fingerprint(), list(), False, False)
+    return cls.get_params(candidate, gen_empty_fingerprint(), list(), False, False, False)
 
   @classmethod
   def get_params(cls, candidate: str, fingerprint: dict[int, dict[int, int]], car_fw: list[structs.CarParams.CarFw],
