226f3728ff838a5ba601c125f053adc149f03090
Shane Smiskol <shane@smiskol.com>
2025-05-28 14:52:42 -0700

    will split out into helpers soon

--- opendbc/safety/safety_declarations.h
+++ opendbc/safety/safety_declarations.h
@@ -64,7 +64,8 @@ extern const int MAX_WRONG_COUNTERS;
 // used to represent floating point vehicle speed in a sample_t
 #define VEHICLE_SPEED_FACTOR 1000.0
 #define MAX_TORQUE_RT_INTERVAL 250000U
-//#define MS_TO_KPH 3.6
+
+// Conversions
 #define KPH_TO_MS (1.0 / 3.6)
 
 // Lateral constants
