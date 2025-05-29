2177e513e40fd5bca35569ff87e1c05691693f23
Shane Smiskol <shane@smiskol.com>
2025-05-28 17:16:43 -0700

    we know it works

--- opendbc/safety/tests/install_mull.sh
+++ opendbc/safety/tests/install_mull.sh
@@ -8,5 +8,4 @@ if ! command -v "mull-runner-17" > /dev/null 2>&1; then
   sudo apt-get update && sudo apt-get install -y curl clang-17
   curl -1sLf 'https://dl.cloudsmith.io/public/mull-project/mull-stable/setup.deb.sh' | sudo -E bash
   sudo apt-get update && sudo apt-get install -y mull-17
-  exit 1
 fi
