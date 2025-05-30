07c8f8512992bbee76c401e6d34bb031ba645eb9
Shane Smiskol <shane@smiskol.com>
2025-05-29 21:01:53 -0700

    safety: add frequency parameter to angle limit struct (#2334)
    * angle frequency param
    
    * misra
    
    * not yet

--- opendbc/safety/safety_declarations.h
+++ opendbc/safety/safety_declarations.h
@@ -136,6 +136,7 @@ typedef struct {
   const struct lookup_t angle_rate_down_lookup;
   const int max_angle_error;             // used to limit error between meas and cmd while enabled
   const float angle_error_min_speed;     // minimum speed to start limiting angle error
+  const int frequency;                   // Hz
 
   const bool angle_is_curvature;         // if true, we can apply max lateral acceleration limits
   const bool enforce_angle_error;        // enables max_angle_error check
