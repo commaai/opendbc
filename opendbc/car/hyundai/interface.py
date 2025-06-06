f2b2b9e50ded6c9024715cbb1f5aa7ef911c792d
Jason Wen <haibin.wen3@gmail.com>
2025-06-05 23:05:31 -0400

    Hyundai CAN: Remove `dashcamOnly` (#182)
    * Hyundai CAN: No dashcam for all
    
    * car list

--- opendbc/car/hyundai/interface.py
+++ opendbc/car/hyundai/interface.py
@@ -176,6 +176,8 @@ class CarInterface(CarInterfaceBase):
     if stock_cp.flags & (HyundaiFlags.CANFD_CAMERA_SCC | HyundaiFlags.CAMERA_SCC):
       stock_cp.radarUnavailable = False
 
+    stock_cp.dashcamOnly = False
+
     return ret
 
   @staticmethod
