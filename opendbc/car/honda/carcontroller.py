f07aa2668268010625fe4a5ea16961a4ffcc60ee
mvl-boston <manuel@alumni.virginia.edu>
2025-06-08 22:30:47 -0400

    exclude tuning from radarless cars

--- opendbc/car/honda/carcontroller.py
+++ opendbc/car/honda/carcontroller.py
@@ -228,10 +228,11 @@ class CarController(CarControllerBase):
             self.gasonly_pid.neg_limit = self.params.BOSCH_ACCEL_MIN
             self.gasonly_pid.pos_limit = self.params.BOSCH_ACCEL_MAX
             gas_pedal_force = self.gasonly_pid.update(gas_error, speed=CS.out.vEgo, feedforward=self.accel)
+            gas_pedal_force += wind_brake_ms2 + hill_brake
           else:
             gas_pedal_force = self.accel
             self.gasonly_pid.reset()
-          gas_pedal_force += wind_brake_ms2 + hill_brake
+            gas_pedal_force += wind_brake_ms2 + hill_brake
           self.gas = float(np.interp(gas_pedal_force, self.params.BOSCH_GAS_LOOKUP_BP, self.params.BOSCH_GAS_LOOKUP_V))
 
           stopping = actuators.longControlState == LongCtrlState.stopping
