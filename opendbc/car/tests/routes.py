c478157328e4fe3405ea94f61f9e156e7343e11f
vanillagorillaa <31773928+vanillagorillaa@users.noreply.github.com>
2025-06-17 20:50:59 -0500

    current test route

--- opendbc/car/tests/routes.py
+++ opendbc/car/tests/routes.py
@@ -109,7 +109,7 @@ routes = [
   CarTestRoute("1f032f5173c8ad99/00000006--573b3fcaf5", HONDA.HONDA_CIVIC_2022),  # Civic Type R with manual transmission
   CarTestRoute("b1c832ad56b6bc9d/00000010--debfcf5867", HONDA.HONDA_CIVIC_2022),  # 2025 Civic Hatch Hybrid with new eCVT transmission
   CarTestRoute("f9c43864cf057d05|2024-01-15--23-01-20", HONDA.HONDA_PILOT_4G),  # TODO: Replace with a newer route
-  CarTestRoute("56b2cf1dacdcd033/00000004--d861224fdb", HONDA.HONDA_CITY), # Brazilian model
+  CarTestRoute("56b2cf1dacdcd033/00000048--3267801001", HONDA.HONDA_CITY), # Brazilian model
 
   CarTestRoute("87d7f06ade479c2e|2023-09-11--23-30-11", HYUNDAI.HYUNDAI_AZERA_6TH_GEN),
   CarTestRoute("66189dd8ec7b50e6|2023-09-20--07-02-12", HYUNDAI.HYUNDAI_AZERA_HEV_6TH_GEN),
