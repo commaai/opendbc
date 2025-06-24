7100306673c790d273965c410a10e587afc0ac4e
mvl-boston <manuel@alumni.virginia.edu>
2025-06-23 20:50:14 -0400

    more cleanup

--- opendbc/car/honda/carcontroller.py
+++ opendbc/car/honda/carcontroller.py
@@ -153,10 +153,6 @@ class CarController(CarControllerBase):
     apply_torque = int(np.interp(-limited_torque * self.params.STEER_MAX,
                                  self.params.STEER_LOOKUP_BP, self.params.STEER_LOOKUP_V))
 
-    # alt_camera models fault if user and comma both apply torque
-    if CS.out.steeringPressed and self.CP.carFingerprint in (HONDA_BOSCH_ALT_CAMERA):
-      apply_torque = 0
-
     # Send CAN commands
     can_sends = []
 
