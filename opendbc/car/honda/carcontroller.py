534c2cb5d2f605df9a770f2b80319699a1daadc9
mvl-boston <manuel@alumni.virginia.edu>
2025-06-23 20:19:56 -0400

    change alt camera to alt radar

--- opendbc/car/honda/carcontroller.py
+++ opendbc/car/honda/carcontroller.py
@@ -5,7 +5,7 @@ from opendbc.can.packer import CANPacker
 from opendbc.car import Bus, DT_CTRL, rate_limit, make_tester_present_msg, structs
 from opendbc.car.honda import hondacan
 from opendbc.car.honda.values import CruiseButtons, VISUAL_HUD, HONDA_BOSCH, HONDA_BOSCH_RADARLESS, HONDA_NIDEC_ALT_PCM_ACCEL, \
-                                     CarControllerParams, HONDA_BOSCH_ALT_CAMERA
+                                     CarControllerParams, HONDA_BOSCH_ALT_RADAR
 from opendbc.car.interfaces import CarControllerBase
 
 VisualAlert = structs.CarControl.HUDControl.VisualAlert
