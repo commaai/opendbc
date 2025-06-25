4f381c204ea136077bdafdb1b27b01d677ce136a
mvl-boston <manuel@alumni.virginia.edu>
2025-06-25 00:35:15 -0400

    whitespace fix

--- opendbc/car/honda/interface.py
+++ opendbc/car/honda/interface.py
@@ -182,7 +182,7 @@ class CarInterface(CarInterfaceBase):
     elif candidate in (CAR.ACURA_MDX_4G):
       ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 4096], [0, 4096]]  # TODO: determine if there is a dead zone at the top end
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.38], [0.11]] # copied from Pilot
-    
+
     elif candidate in (CAR.HONDA_ODYSSEY, CAR.HONDA_ODYSSEY_CHN):
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.28], [0.08]]
       if candidate == CAR.HONDA_ODYSSEY_CHN:
