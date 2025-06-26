ab6f3ae173f2803196c0cabe25bb5de78915e29d
mvl-boston <manuel@alumni.virginia.edu>
2025-06-26 00:33:23 -0400

    fix merge

--- opendbc/car/honda/carcontroller.py
+++ opendbc/car/honda/carcontroller.py
@@ -117,15 +117,11 @@ class CarController(CarControllerBase):
     self.speed = 0.0
     self.gas = 0.0
     self.brake = 0.0
-<<<<<<< 25mdx-merge
-    self.last_torque = 0.0
+    self.last_torque = 0.0 # last eps torque
+    self.steeringTorque_last = 0.0 # last driver torque
     self.pitch = 0.0
     self.gas_pedal_force = 0.0
     self.last_gas = 0.0
-=======
-    self.last_torque = 0.0 # last eps torque
-    self.steeringTorque_last = 0.0 # last driver torque
->>>>>>> combined-honda
     self.gasonly_pid = PIDController (k_p=([0,], [0,]),
                                       k_i= ([0., 5., 35.], [1.2, 0.8, 0.5]),
                                       k_f=1, rate= 1 / DT_CTRL / 2)
@@ -239,17 +235,10 @@ class CarController(CarControllerBase):
         if self.CP.carFingerprint in HONDA_BOSCH:
           self.accel = float(np.clip(accel, self.params.BOSCH_ACCEL_MIN, self.params.BOSCH_ACCEL_MAX))
 
-<<<<<<< 25mdx-merge
-          stopping = (actuators.longControlState == LongCtrlState.stopping)
-          self.stopping_counter = self.stopping_counter + 1 if stopping else 0
 
-          # perform a gas-only pid
-          if (actuators.longControlState == LongCtrlState.pid):
-=======
           if self.CP.carFingerprint in HONDA_BOSCH_RADARLESS:
             gas_pedal_force = self.accel # radarless does not need a pid
           elif (actuators.longControlState == LongCtrlState.pid): # perform a gas-only pid
->>>>>>> combined-honda
             gas_error = self.accel - CS.out.aEgo
             self.gasonly_pid.neg_limit = self.params.BOSCH_ACCEL_MIN
             self.gasonly_pid.pos_limit = self.params.BOSCH_ACCEL_MAX
@@ -258,20 +247,12 @@ class CarController(CarControllerBase):
           else:
             gas_pedal_force = self.accel
             self.gasonly_pid.reset()
-<<<<<<< 25mdx-merge
-          gas_pedal_force += wind_brake_ms2 + hill_brake
-=======
             gas_pedal_force += wind_brake_ms2 + hill_brake
->>>>>>> combined-honda
           self.gas = float(np.interp(gas_pedal_force, self.params.BOSCH_GAS_LOOKUP_BP, self.params.BOSCH_GAS_LOOKUP_V))
           self.last_gas = self.gas
 
           can_sends.extend(hondacan.create_acc_commands(self.packer, self.CAN, CC.enabled, CC.longActive, self.accel, self.gas,
                                                         self.stopping_counter, self.CP.carFingerprint, gas_pedal_force, CS.out.vEgo))
-<<<<<<< 25mdx-merge
-
-=======
->>>>>>> combined-honda
         else:
           apply_brake = np.clip(self.brake_last - wind_brake, 0.0, 1.0)
           apply_brake = int(np.clip(apply_brake * self.params.NIDEC_BRAKE_MAX, 0, self.params.NIDEC_BRAKE_MAX - 1))
