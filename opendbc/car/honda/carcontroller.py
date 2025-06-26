62e30a17f7650bd55d5e24fb2810a7887df8aa32
mvl-boston <manuel@alumni.virginia.edu>
2025-06-26 00:50:21 -0400

    more merge cleanup

--- opendbc/car/honda/carcontroller.py
+++ opendbc/car/honda/carcontroller.py
@@ -275,7 +275,7 @@ class CarController(CarControllerBase):
 
     new_actuators = actuators.as_builder()
     new_actuators.speed = self.speed
-    new_actuators.accel = self.gas_pedal_force
+    new_actuators.accel = self.accel
     new_actuators.gas = self.gas
     new_actuators.brake = self.brake
     new_actuators.torque = self.last_torque
