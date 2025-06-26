4ef094aeaea961488f1740421a8b1d1ccc1c26e2
mvl-boston <manuel@alumni.virginia.edu>
2025-06-26 18:22:09 -0400

    typo fix

--- opendbc/car/honda/carcontroller.py
+++ opendbc/car/honda/carcontroller.py
@@ -120,8 +120,8 @@ class CarController(CarControllerBase):
     self.last_torque = 0.0 # last eps torque
     self.steeringTorque_last = 0.0 # last driver torque
     # try new Bosch pid
-    self.gasonly_pid = PIDController (k_p=([0,], [0.5,]),
-#                                     k_i= ([0,], [0,]),
+    self.gasonly_pid = PIDController (k_p=([0,], [0.5,]), 
+                                      k_i= ([0,], [0,]),
                                       k_f=1, rate= 1 / DT_CTRL / 2)
 #    self.gasonly_pid = PIDController (k_p=([0,], [0,]),
 #                                      k_i= ([0., 5., 35.], [1.2, 0.8, 0.5]),
