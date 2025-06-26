e165414d9b04c0a54530d3ad2698f109f118ac94
mvl-boston <manuel@alumni.virginia.edu>
2025-06-26 03:13:09 -0400

    whitespace fix

--- opendbc/car/honda/interface.py
+++ opendbc/car/honda/interface.py
@@ -165,7 +165,7 @@ class CarInterface(CarInterfaceBase):
 
     elif candidate in (CAR.HONDA_CRV_HYBRID_6G, CAR.HONDA_CRV_6G):
       if candidate == CAR.HONDA_CRV_HYBRID_6G:
-        ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 5000], [0, 5000]] 
+        ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 5000], [0, 5000]]
       else:
         ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 4096], [0, 4096]]  # TODO: determine if there is a dead zone at the top end
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.6], [0.18]]
