d800bf68dc0bca0076eb2525633a0e19ed12a6e6
Bruce Wayne <harald.the.engineer@gmail.com>
2025-07-01 20:38:45 -0700

    Bronco special

--- opendbc/car/ford/carcontroller.py
+++ opendbc/car/ford/carcontroller.py
@@ -3,7 +3,7 @@ import numpy as np
 from opendbc.can.packer import CANPacker
 from opendbc.car import ACCELERATION_DUE_TO_GRAVITY, Bus, DT_CTRL, apply_std_steer_angle_limits, structs
 from opendbc.car.ford import fordcan
-from opendbc.car.ford.values import CarControllerParams, FordFlags
+from opendbc.car.ford.values import CarControllerParams, FordFlags, CAR
 from opendbc.car.interfaces import CarControllerBase, ISO_LATERAL_ACCEL, V_CRUISE_MAX
 
 LongCtrlState = structs.CarControl.Actuators.LongControlState
@@ -13,6 +13,24 @@ VisualAlert = structs.CarControl.HUDControl.VisualAlert
 # Limit to average banked road since safety doesn't have the roll
 AVERAGE_ROAD_ROLL = 0.06  # ~3.4 degrees, 6% superelevation. higher actual roll raises lateral acceleration
 MAX_LATERAL_ACCEL = ISO_LATERAL_ACCEL - (ACCELERATION_DUE_TO_GRAVITY * AVERAGE_ROAD_ROLL)  # ~2.4 m/s^2
+
+
+def bronco_special(apply_curvature, apply_curvature_last, v_ego_raw):
+    diff = 0.15
+    tau = 1 # 1 second smooths over the overshoot
+    dt = DT_CTRL * CarControllerParams.STEER_STEP
+    alpha = 1 - np.exp(-dt/tau)
+
+    lataccel = apply_curvature * (v_ego_raw ** 2)
+    last_lataccel = apply_curvature_last * (v_ego_raw ** 2)
+
+    if lataccel > last_lataccel + diff:
+        last_lataccel = lataccel - diff
+    elif lataccel < last_lataccel - diff:
+        last_lataccel = lataccel + diff
+    last_lataccel = last_lataccel * alpha + lataccel * (1 - alpha)
+
+    return float(last_lataccel / (v_ego_raw ** 2 + 1e-6))
 
 
 def apply_ford_curvature_limits(apply_curvature, apply_curvature_last, current_curvature, v_ego_raw, steering_angle, lat_active, CP):
@@ -48,6 +66,7 @@ class CarController(CarControllerBase):
     self.CAN = fordcan.CanBus(CP)
 
     self.apply_curvature_last = 0
+    self.bronco_special_curvature_last = 0
     self.accel = 0.0
     self.gas = 0.0
     self.brake_request = False
@@ -82,9 +101,18 @@ class CarController(CarControllerBase):
     ### lateral control ###
     # send steer msg at 20Hz
     if (self.frame % CarControllerParams.STEER_STEP) == 0:
+      # Bronco and some other cars consistently overshoot curv requests
+      # Apply some deadzone + smoothing convergence to avoid oscillations
+      if self.CP.carFingerprint in [CAR.FORD_BRONCO_SPORT_MK1, CAR.FORD_F_150_MK14]:
+        self.bronco_special_curvature_last = bronco_special(actuators.curvature, self.bronco_special_curvature_last, CS.out.vEgoRaw)
+        request_curvature = self.bronco_special_curvature_last
+      else:
+        request_curvature = actuators.curvature
+
       # apply rate limits, curvature error limit, and clip to signal range
       current_curvature = -CS.out.yawRate / max(CS.out.vEgoRaw, 0.1)
-      self.apply_curvature_last = apply_ford_curvature_limits(actuators.curvature, self.apply_curvature_last, current_curvature,
+
+      self.apply_curvature_last = apply_ford_curvature_limits(request_curvature, self.apply_curvature_last, current_curvature,
                                                               CS.out.vEgoRaw, 0., CC.latActive, self.CP)
 
       if self.CP.flags & FordFlags.CANFD:
