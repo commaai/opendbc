e5e288f7c8f4aa55ce8e180be37e0a69fff4dd8d
Bruce Wayne <harald.the.engineer@gmail.com>
2025-07-02 15:10:11 -0700

    typo

--- opendbc/car/ford/carcontroller.py
+++ opendbc/car/ford/carcontroller.py
@@ -17,7 +17,7 @@ MAX_LATERAL_ACCEL = ISO_LATERAL_ACCEL - (ACCELERATION_DUE_TO_GRAVITY * AVERAGE_R
 
 def bronco_special(apply_curvature, apply_curvature_last, v_ego_raw):
     diff = 0.05
-    tau = 5 # 5d smooths over the overshoot
+    tau = 5 # 5 smooths over the overshoot
     dt = DT_CTRL * CarControllerParams.STEER_STEP
     alpha = 1 - np.exp(-dt/tau)
 
