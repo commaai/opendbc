c19fdfc247ddc779edba95c8e54fdb7c7b37b25f
mvl-boston <manuel@alumni.virginia.edu>
2025-07-04 14:39:59 -0400

    typo fix

--- opendbc/car/honda/carstate.py
+++ opendbc/car/honda/carstate.py
@@ -110,7 +110,7 @@ class CarState(CarStateBase):
     if CP.transmissionType != TransmissionType.manual:
       self.shifter_values = can_define.dv[self.gearbox_msg]["GEAR_SHIFTER"]
 
-    if CP.carFingerpring != CAR.ACURA_RLX_HYBRID:
+    if CP.carFingerprint != CAR.ACURA_RLX_HYBRID:
       self.steer_status_values = defaultdict(lambda: "UNKNOWN", can_define.dv["STEER_STATUS"]["STEER_STATUS"])
 
     self.brake_switch_prev = False
