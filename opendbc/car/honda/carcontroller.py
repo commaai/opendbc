ad1b16d07d197e25fd6f9fe1e5d9c76d709e4d45
mvl-boston <manuel@alumni.virginia.edu>
2025-07-05 23:27:06 -0400

    try letting planner limit braking

--- opendbc/car/honda/carcontroller.py
+++ opendbc/car/honda/carcontroller.py
@@ -154,7 +154,7 @@ class CarController(CarControllerBase):
     if CC.longActive:
       # accel = float (np.clip ( actuators.accel, -100.0, np.interp (steerfactor, [ 1.0, 4.0 ], [-3.5, 3.5]) ) )
       hill_brake = math.sin(self.pitch) * ACCELERATION_DUE_TO_GRAVITY
-      accel = float (np.clip ( actuators.accel, -3.5, 2) ) + hill_brake
+      accel = actuators.accel + hill_brake
       if accel > max ( 0, CS.out.aEgo) + 0.1:
         accel = 10000.0
       gas, brake = compute_gas_brake(accel, CS.out.vEgo, self.CP.carFingerprint)
