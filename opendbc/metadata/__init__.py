34c834d5be534cd9dc32b97352331241feb7a78f
ugtthis <dratunuman@gmail.com>
2025-07-14 20:07:17 -0400

    mv Hyundai and dont use init_make

--- opendbc/metadata/__init__.py
+++ opendbc/metadata/__init__.py
@@ -1,9 +1,11 @@
 from opendbc.metadata.tesla.attributes import METADATA as TESLA_METADATA
 from opendbc.metadata.rivian.attributes import METADATA as RIVIAN_METADATA
+from opendbc.metadata.hyundai.attributes import METADATA as HYUNDAI_METADATA
 
 METADATA = {
   **TESLA_METADATA,
   **RIVIAN_METADATA,
+  **HYUNDAI_METADATA,
 }
 
 def get_brand_metadata(platform):
