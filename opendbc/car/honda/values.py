10d73e1970dab8030f6881f64138e5e92b3408d8
mvl-boston <manuel@alumni.virginia.edu>
2025-07-15 23:57:54 -0400

    aligning with odysey values changes

--- opendbc/car/honda/values.py
+++ opendbc/car/honda/values.py
@@ -70,7 +70,7 @@ class HondaFlags(IntFlag):
   NIDEC_ALT_SCM_MESSAGES = 64
 
   BOSCH_CANFD = 128
-  BOSCH_ALT_RADAR = 256
+  BOSCH_ALT_RADAR = 512
 
   HAS_ALL_DOOR_STATES = 256  # Some Hondas have all door states, others only driver door
   HAS_EPB = 512
@@ -361,7 +361,6 @@ FW_QUERY_CONFIG = FwQueryConfig(
 STEER_THRESHOLD = {
   # default is 1200, overrides go here
   CAR.ACURA_RDX: 400,
-  CAR.ACURA_RDX_3G_MMR: 500,
   CAR.HONDA_CRV_EU: 400,
 }
 
