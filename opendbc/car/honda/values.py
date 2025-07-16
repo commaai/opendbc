748213bc09f34279d40ae7d7c96c3f4e8e5c3a2e
mvl-boston <manuel@alumni.virginia.edu>
2025-07-16 00:04:26 -0400

    update compatible years to only 2024

--- opendbc/car/honda/values.py
+++ opendbc/car/honda/values.py
@@ -206,7 +206,7 @@ class CAR(Platforms):
     flags=HondaFlags.BOSCH_ALT_BRAKE,
   )
   ACURA_RDX_3G_MMR = HondaBoschPlatformConfig(
-    [HondaCarDocs("Acura RDX 2022-25", "All", min_steer_speed=45. * CV.MPH_TO_MS)],
+    [HondaCarDocs("Acura RDX 2022-24", "All", min_steer_speed=45. * CV.MPH_TO_MS)],
     CarSpecs(mass=4079 * CV.LB_TO_KG, wheelbase=2.75, steerRatio=12.0, centerToFrontRatio=0.41, tireStiffnessFactor=0.677),  # as spec
     {Bus.pt: 'acura_rdx_2020_can_generated'},
     flags=HondaFlags.BOSCH_ALT_BRAKE | HondaFlags.BOSCH_ALT_RADAR,
