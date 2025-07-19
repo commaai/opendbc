1c44f0fa1b8d603115b44737be76510a18ce3b40
mvl-boston <manuel@alumni.virginia.edu>
2025-07-19 17:32:11 -0400

    whitespace fix

--- opendbc/car/honda/values.py
+++ opendbc/car/honda/values.py
@@ -225,7 +225,7 @@ class CAR(Platforms):
     [HondaCarDocs("Honda Odyssey 2021-25", "All", min_steer_speed=70. * CV.KPH_TO_MS)],
     CarSpecs(mass=4590 * CV.LB_TO_KG, wheelbase=3.00, steerRatio=14.35, centerToFrontRatio=0.41, tireStiffnessFactor=1.02),  # per spec
     {Bus.pt: 'acura_rdx_2020_can_generated'},
-    flags=HondaFlags.BOSCH_ALT_BRAKE | HondaFlags.BOSCH_ALT_RADAR, 
+    flags=HondaFlags.BOSCH_ALT_BRAKE | HondaFlags.BOSCH_ALT_RADAR,
   )
 
   # Nidec Cars
