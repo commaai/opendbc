eda960b60b5a5de1dff69ac966062cb7aed03b15
ugtthis <dratunuman@gmail.com>
2025-07-20 19:03:50 -0400

    fix buy link

--- opendbc/car/docs_definitions.py
+++ opendbc/car/docs_definitions.py
@@ -312,8 +312,8 @@ class CarDocs:
     # hardware column
     hardware_col = "None"
     if self.car_parts.parts:
-      model_years = self.model + (' ' + self.years if self.years else '')
-      buy_link = f'<a href="https://comma.ai/shop/comma-3x.html?make={self.make}&model={model_years}">Buy Here</a>'
+      car_info = f"{self.make} {self.model}{' ' + self.years if self.years else ''}".replace(' ', '%20')
+      buy_link = f'<a href="https://comma.ai/shop/comma-3x?harness={car_info}">Buy Here</a>'
 
       tools_docs = [part for part in self.car_parts.all_parts() if isinstance(part, Tool)]
       parts_docs = [part for part in self.car_parts.all_parts() if not isinstance(part, Tool)]
