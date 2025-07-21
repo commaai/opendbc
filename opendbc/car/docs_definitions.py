50e4b63ece53fe8af6bf435cc9f6c58ac0f49246
ugtthis <142481257+ugtthis@users.noreply.github.com>
2025-07-20 22:37:06 -0700

    Docs: Update buy link in CARS.md (#2498)
    * fix buy link
    
    * better idea
    
    Co-authored-by: Shane Smiskol <shane@smiskol.com>
    
    ---------
    
    Co-authored-by: Shane Smiskol <shane@smiskol.com>

--- opendbc/car/docs_definitions.py
+++ opendbc/car/docs_definitions.py
@@ -312,8 +312,7 @@ class CarDocs:
     # hardware column
     hardware_col = "None"
     if self.car_parts.parts:
-      model_years = self.model + (' ' + self.years if self.years else '')
-      buy_link = f'<a href="https://comma.ai/shop/comma-3x.html?make={self.make}&model={model_years}">Buy Here</a>'
+      buy_link = f'<a href="https://comma.ai/shop/comma-3x?harness={self.name}">Buy Here</a>'
 
       tools_docs = [part for part in self.car_parts.all_parts() if isinstance(part, Tool)]
       parts_docs = [part for part in self.car_parts.all_parts() if not isinstance(part, Tool)]
