76582f4994eac2a0bf2d67c9ef24777e6ff96dec
mvl-boston <manuel@alumni.virginia.edu>
2025-07-20 23:40:18 -0400

    update canfd long tx messages

--- opendbc/safety/tests/test_honda.py
+++ opendbc/safety/tests/test_honda.py
@@ -604,7 +604,7 @@ class TestHondaBoschCanfdLongSafety(TestHondaBoschLongSafety, TestHondaBoschCANF
   STEER_BUS = 0
   BUTTONS_BUS = 0
 
-  TX_MSGS = [[0xE4, 0], [0x296, 0], [0x33D, 0], [0x33DA, 0], [0x33DB, 0]]
+  TX_MSGS = [[0xE4, 0], [0x1DF, 0],  [0x1EF, 0], [0x30C, 0], [0x33D, 0], [0x18DAB0F1, 0]]]
   FWD_BLACKLISTED_ADDRS = {2: [0xE4, 0xE5, 0x33D, 0x33DA, 0x33DB]}
   RELAY_MALFUNCTION_ADDRS = {0: (0xE4, 0x33D)}  # STEERING_CONTROL / LKAS_HUD
 
