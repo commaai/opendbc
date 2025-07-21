3dfbd6bffcde7d1e0564b571214a50f33d8d5d46
Shane Smiskol <shane@smiskol.com>
2025-07-21 15:51:12 -0700

    do it here

--- opendbc/car/__init__.py
+++ opendbc/car/__init__.py
@@ -202,14 +202,13 @@ def rate_limit(new_value, last_value, dw_step, up_step):
 
 
 def get_friction(lateral_accel_error: float, lateral_accel_deadzone: float, friction_threshold: float,
-                 torque_params: structs.CarParams.LateralTorqueTuning, friction_compensation: bool) -> float:
+                 torque_params: structs.CarParams.LateralTorqueTuning) -> float:
   friction_interp = np.interp(
     apply_center_deadzone(lateral_accel_error, lateral_accel_deadzone),
     [-friction_threshold, friction_threshold],
     [-torque_params.friction, torque_params.friction]
   )
-  friction = float(friction_interp) if friction_compensation else 0.0
-  return friction
+  return float(friction_interp)
 
 
 def make_tester_present_msg(addr, bus, subaddr=None, suppress_response=False):
