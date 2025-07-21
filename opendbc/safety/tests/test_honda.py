ab6df0a45d6d7bd62317f1a8f4c92a70758a7c9c
mvl-boston <manuel@alumni.virginia.edu>
2025-07-20 22:28:52 -0400

    different order for CanFD

--- opendbc/safety/tests/test_honda.py
+++ opendbc/safety/tests/test_honda.py
@@ -595,12 +595,10 @@ class TestHondaBoschCANFDAltBrakeSafety(HondaPcmEnableBase, TestHondaBoschCANFDS
     self.safety.set_safety_hooks(CarParams.SafetyModel.hondaBosch, HondaSafetyFlags.BOSCH_CANFD | HondaSafetyFlags.ALT_BRAKE)
     self.safety.init_tests()
 
-class TestHondaBoschCanfdLongSafety(TestHondaBoschCANFDSafetyBase, TestHondaBoschLongSafety):
+class TestHondaBoschCanfdLongSafety(TestHondaBoschLongSafety, TestHondaBoschCANFDSafetyBase):
   """
     Covers the Honda Bosch CANFD safety mode with longitudinal control
   """
-
-  STEER_BUS = 0
 
   RELAY_MALFUNCTION_ADDRS = {0: (0xE4, 0x1DF, 0x33D)}  # STEERING_CONTROL, ACC_CONTROL
 
