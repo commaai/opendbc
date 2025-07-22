169dcd759f9c5c108aa244d48258e9c5be2aaf21
mvl-boston <manuel@alumni.virginia.edu>
2025-07-22 16:00:20 -0400

    Adding manual transmission flag to integra

--- opendbc/car/honda/values.py
+++ opendbc/car/honda/values.py
@@ -226,7 +226,7 @@ class CAR(Platforms):
     [HondaCarDocs("Acura Integra 2024-25", "All")],
     CarSpecs(mass=3338.8 * CV.LB_TO_KG, wheelbase=2.5, centerToFrontRatio=0.5, steerRatio=16.71, tireStiffnessFactor=0.82),
     {Bus.pt: 'honda_civic_ex_2022_can_generated'},
-    flags=HondaFlags.BOSCH_RADARLESS,
+    flags=HondaFlags.BOSCH_RADARLESS | HondaFlags.ALLOW_MANUAL_TRANS,
   )
 
   # Nidec Cars
