b00eec69be75d48418d901516c2b6664f2157959
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2025-07-23 11:02:39 -0700

    reindent

--- opendbc/can/__init__.py
+++ opendbc/can/__init__.py
@@ -1,8 +1,5 @@
 from opendbc.can.parser_pyx import CANParser, CANDefine
 assert CANParser, CANDefine
 
-try:
-  from opendbc.can.packer import CANPacker
-except Exception:  # fallback to compiled version if available
-  from opendbc.can.packer_pyx import CANPacker
+from opendbc.can.packer import CANPacker
 assert CANPacker
