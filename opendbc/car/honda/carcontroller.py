8a4dcff3e3a541d0b25a749a26443ce7a34bc4ac
mvl-boston <manuel@alumni.virginia.edu>
2025-07-22 21:26:11 -0400

    add import

--- opendbc/car/honda/carcontroller.py
+++ opendbc/car/honda/carcontroller.py
@@ -6,7 +6,7 @@ from opendbc.can.packer import CANPacker
 from opendbc.car import ACCELERATION_DUE_TO_GRAVITY, Bus, DT_CTRL, rate_limit, make_tester_present_msg, structs
 from opendbc.car.honda import hondacan
 from opendbc.car.honda.values import CruiseButtons, VISUAL_HUD, HONDA_BOSCH, HONDA_BOSCH_RADARLESS, HONDA_NIDEC_ALT_PCM_ACCEL, HONDA_BOSCH_CANFD, \
-                                                 CarControllerParams, HONDA_BOSCH_ALT_RADAR, HONDA_NIDEC_HYBRID
+                                                 CarControllerParams, HONDA_BOSCH_ALT_RADAR, HONDA_NIDEC_HYBRID, CAR
 from opendbc.car.interfaces import CarControllerBase
 from opendbc.car.common.pid import PIDController
 from opendbc.car.common.conversions import Conversions as CV
