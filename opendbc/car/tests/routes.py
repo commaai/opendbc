24d80d33a844236e9eb81230e80a16d369acf214
mvl-boston <manuel@alumni.virginia.edu>
2025-07-22 20:38:57 -0400

    adding nidec hybrid

--- opendbc/car/tests/routes.py
+++ opendbc/car/tests/routes.py
@@ -26,6 +26,8 @@ non_tested_cars = [
   HONDA.HONDA_ACCORD_11G, # TODO: get route
   HONDA.HONDA_CRV_HYBRID_6G, # TODO: get route
   HONDA.HONDA_CRV_6G, # TODO: get route
+  HONDA.ACURA_MDX_3G_HYBRID, # TODO: get route once steering fixed
+  HONDA.ACURA_RLX_HYBRID, # TODO: get route once tuning done
   VOLKSWAGEN.VOLKSWAGEN_CRAFTER_MK2,  # need a route from an ACC-equipped Crafter
   SUBARU.SUBARU_FORESTER_HYBRID,
 
