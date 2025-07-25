26c1277c9e7c26911cf5b9005f967b8467df92bd
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2025-07-24 17:44:44 -0700

    Revert "Slimming `carState`, part 2 (#2508)"
    This reverts commit 019038b3de8e3d7a2239f684061c1a31230f5c61.

--- opendbc/car/honda/carstate.py
+++ opendbc/car/honda/carstate.py
@@ -131,7 +131,9 @@ class CarState(CarStateBase):
 
     ret.gasPressed = cp.vl["POWERTRAIN_DATA"]["PEDAL_GAS"] > 0
 
-    ret.steeringPressed = abs(cp.vl["STEER_STATUS"]["STEER_TORQUE_SENSOR"]) > STEER_THRESHOLD.get(self.CP.carFingerprint, 1200)
+    ret.steeringTorque = cp.vl["STEER_STATUS"]["STEER_TORQUE_SENSOR"]
+    ret.steeringTorqueEps = cp.vl["STEER_MOTOR_TORQUE"]["MOTOR_TORQUE"]
+    ret.steeringPressed = abs(ret.steeringTorque) > STEER_THRESHOLD.get(self.CP.carFingerprint, 1200)
 
     if self.CP.carFingerprint in HONDA_BOSCH:
       # The PCM always manages its own cruise control state, but doesn't publish it
