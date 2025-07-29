b69700ba4e43dd065bc1b83d12ec3fd8267c149c
Shengming Yuan <me@theeditorstudio.com>
2025-07-29 03:21:50 -0400

    increase rivian test coverage

--- opendbc/safety/modes/rivian.h
+++ opendbc/safety/modes/rivian.h
@@ -4,20 +4,15 @@
 
 static uint8_t rivian_get_counter(const CANPacket_t *msg) {
   uint8_t cnt = 0;
-  if ((msg->addr == 0x208U) || (msg->addr == 0x150U)) {
-    // Signal: ESP_Status_Counter, VDM_PropStatus_Counter
-    cnt = msg->data[1] & 0xFU;
-  }
+  // Signal: ESP_Status_Counter, VDM_PropStatus_Counter
+  cnt = msg->data[1] & 0xFU;
   return cnt;
 }
 
 static uint32_t rivian_get_checksum(const CANPacket_t *msg) {
   uint8_t chksum = 0;
-  if ((msg->addr == 0x208U) || (msg->addr == 0x150U)) {
-    // Signal: ESP_Status_Checksum, VDM_PropStatus_Checksum
-    chksum = msg->data[0];
-  } else {
-  }
+  // Signal: ESP_Status_Checksum, VDM_PropStatus_Checksum
+  chksum = msg->data[0];
   return chksum;
 }
 
@@ -43,9 +38,9 @@ static uint32_t rivian_compute_checksum(const CANPacket_t *msg) {
   uint8_t chksum = 0;
   if (msg->addr == 0x208U) {
     chksum = _rivian_compute_checksum(msg, 0x1D, 0xB1);
-  } else if (msg->addr == 0x150U) {
-    chksum = _rivian_compute_checksum(msg, 0x1D, 0x9A);
   } else {
+    // msg->addr == 0x150U
+    chksum = _rivian_compute_checksum(msg, 0x1D, 0x9A);
   }
   return chksum;
 }
@@ -54,9 +49,9 @@ static bool rivian_get_quality_flag_valid(const CANPacket_t *msg) {
   bool valid = false;
   if (msg->addr == 0x208U) {
     valid = ((msg->data[3] >> 3) & 0x3U) == 0x1U;  // ESP_Vehicle_Speed_Q
-  } else if (msg->addr == 0x150U) {
-    valid = (msg->data[1] >> 6) == 0x1U;  // VDM_VehicleSpeedQ
   } else {
+    // msg->addr == 0x150U
+    valid = (msg->data[1] >> 6) == 0x1U;  // VDM_VehicleSpeedQ
   }
   return valid;
 }
@@ -94,10 +89,8 @@ static void rivian_rx_hook(const CANPacket_t *msg) {
 
   if (msg->bus == 2U) {
     // Cruise state
-    if (msg->addr == 0x100U) {
-      const int feature_status = msg->data[2] >> 5U;
-      pcm_cruise_check(feature_status == 1);
-    }
+    const int feature_status = msg->data[2] >> 5U;
+    pcm_cruise_check(feature_status == 1);
   }
 }
 
