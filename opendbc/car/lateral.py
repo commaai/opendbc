f84a32208a1a9ff2b42b541166326be230fb717a
DevTekVE <devtekve@gmail.com>
2025-07-31 08:32:59 +0200

    Leave rate_limit where it belongs

--- opendbc/car/lateral.py
+++ opendbc/car/lateral.py
@@ -107,10 +107,6 @@ def apply_center_deadzone(error, deadzone):
   return error
 
 
-def rate_limit(new_value, last_value, dw_step, up_step):
-  return float(np.clip(new_value, last_value + dw_step, last_value + up_step))
-
-
 def get_friction(lateral_accel_error: float, lateral_accel_deadzone: float, friction_threshold: float,
                  torque_params: structs.CarParams.LateralTorqueTuning) -> float:
   friction_interp = np.interp(
