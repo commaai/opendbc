73c50259a806411c8c37f189eeb1a6fc2753df28
mvl-boston <manuel@alumni.virginia.edu>
2025-08-01 11:49:45 -0400

    removing standardized canfd values

--- opendbc/car/honda/values.py
+++ opendbc/car/honda/values.py
@@ -167,8 +167,6 @@ class CAR(Platforms):
   HONDA_ACCORD_11G = HondaBoschPlatformConfig(
     [HondaCarDocs("Honda Accord 2023", "All")],
     CarSpecs(mass=3279 * CV.LB_TO_KG, wheelbase=2.83, steerRatio=16.33, centerToFrontRatio=0.39, tireStiffnessFactor=0.8467),
-    {Bus.pt: 'honda_pilot_2023_can_generated'},
-    flags=HondaFlags.BOSCH_CANFD,
   )
   HONDA_CIVIC_BOSCH = HondaBoschPlatformConfig(
     [
