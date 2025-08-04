aad1c6007880111d1354568bf8a10056e4038bd5
Jason Young <jyoung8607@gmail.com>
2025-08-04 14:22:46 -0400

    test lat accel control with rescaled actuator

--- opendbc/car/honda/interface.py
+++ opendbc/car/honda/interface.py
@@ -139,7 +139,7 @@ class CarInterface(CarInterfaceBase):
     # TODO: see if all Bosch CAN-FD can be handled in one candidate block
     elif candidate == CAR.HONDA_ACCORD_11G:
       ret.steerActuatorDelay = 0.22
-      ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 4096], [0, 4096]]
+      ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 2560], [0, 2560]]
       CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
       CarControllerParams.BOSCH_GAS_LOOKUP_V = [0, 1060]
 
