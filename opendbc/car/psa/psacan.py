6e704fd5992957a1a5483a9c2de0fea1d3af51e8
elkoled <elkoled@gmail.com>
2025-08-04 23:49:09 +0200

    clean psacan

--- opendbc/car/psa/psacan.py
+++ opendbc/car/psa/psacan.py
@@ -1,18 +1,11 @@
 def psa_checksum(address: int, sig, d: bytearray) -> int:
-  if address == 0x452:
-    chk_ini = 0x4
-  elif address == 0x38D:
-    chk_ini = 0x7
-  else:
-    chk_ini = 0xB
+  chk_ini = {0x452: 0x4, 0x38D: 0x7}.get(address, 0xB)
   checksum = sum((b >> 4) + (b & 0xF) for b in d)
   return (chk_ini - checksum) & 0xF
 
 def create_lka_steering(packer, frame: int, lat_active: bool, apply_angle: float):
   values = {
     'DRIVE': 1,
-    'COUNTER': frame % 0x10,
-    'CHECKSUM': 0,
     # Cycle STATUS 2->3->4->2.. this keeps control active. 0: UNAVAILABLE, 1: UNSELECTED, 2: READY, 3: AUTHORIZED, 4: ACTIVE
     'STATUS': (frame % 3) + 2 if lat_active else 0,
     'LXA_ACTIVATION': 1,
