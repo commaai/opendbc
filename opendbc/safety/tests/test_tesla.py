194b3dbcac61b7bbb5aa89978fa22c70570b6864
Shane Smiskol <shane@smiskol.com>
2025-08-06 14:46:57 -0700

    Clean up "Refactor steering angle limit functions and add common application logic" (#2633)
    * Revert "Refactor steering angle limit functions and add common application lo…"
    
    This reverts commit adb6001649c0cc022873739187652b0014483dd3.
    
    * move to lateral
    
    * make work
    
    * now we move
    
    * fixup imports
    
    * fixup safety test
    
    * clean up
    
    * cmt
    
    * ?
    
    * by default no limit
    
    * 0 is better
    
    * these are good

--- opendbc/safety/tests/test_tesla.py
+++ opendbc/safety/tests/test_tesla.py
@@ -3,8 +3,8 @@ import random
 import unittest
 import numpy as np
 
-from opendbc.car.tesla.values import TeslaSafetyFlags, CarControllerParams
 from opendbc.car.lateral import get_max_angle_delta_vm, get_max_angle_vm
+from opendbc.car.tesla.values import CarControllerParams, TeslaSafetyFlags
 from opendbc.car.tesla.carcontroller import get_safety_CP
 from opendbc.car.structs import CarParams
 from opendbc.car.vehicle_model import VehicleModel
@@ -36,11 +36,8 @@ class TestTeslaSafetyBase(common.PandaCarSafetyTest, common.AngleSteeringSafetyT
   # Angle control limits
   STEER_ANGLE_MAX = 360  # deg
   DEG_TO_CAN = 10
-  ANGLE_LIMITS = CarControllerParams.ANGLE_LIMITS
-  MAX_LATERAL_ACCEL = CarControllerParams.ANGLE_LIMITS.MAX_LATERAL_ACCEL
-  MAX_LATERAL_JERK = CarControllerParams.ANGLE_LIMITS.MAX_LATERAL_JERK
 
-  # Tesla uses get_max_angle_delta and get_max_angle for real lateral accel and jerk limits
+  # Tesla uses get_max_angle_delta_vm and get_max_angle_vm for real lateral accel and jerk limits
   # TODO: integrate this into AngleSteeringSafetyTest
   ANGLE_RATE_BP = None
   ANGLE_RATE_UP = None
@@ -60,7 +57,7 @@ class TestTeslaSafetyBase(common.PandaCarSafetyTest, common.AngleSteeringSafetyT
   packer: CANPackerPanda
 
   def _get_steer_cmd_angle_max(self, speed):
-    return get_max_angle_vm(max(speed, 1), self.VM, self.MAX_LATERAL_ACCEL)
+    return get_max_angle_vm(max(speed, 1), self.VM, CarControllerParams)
 
   def setUp(self):
     self.VM = VehicleModel(get_safety_CP())
@@ -301,14 +298,14 @@ class TestTeslaSafetyBase(common.PandaCarSafetyTest, common.AngleSteeringSafetyT
         angle_unit_offset = -1 if sign == -1 else 0
 
         # at limit (safety tolerance adds 1)
-        max_angle = round_angle(get_max_angle_vm(speed, self.VM, self.MAX_LATERAL_ACCEL), angle_unit_offset + 1) * sign
+        max_angle = round_angle(get_max_angle_vm(speed, self.VM, CarControllerParams), angle_unit_offset + 1) * sign
         max_angle = np.clip(max_angle, -self.STEER_ANGLE_MAX, self.STEER_ANGLE_MAX)
         self.safety.set_desired_angle_last(round(max_angle * self.DEG_TO_CAN))
 
         self.assertTrue(self._tx(self._angle_cmd_msg(max_angle, True)))
 
         # 1 unit above limit
-        max_angle_raw = round_angle(get_max_angle_vm(speed, self.VM, self.MAX_LATERAL_ACCEL), angle_unit_offset + 2) * sign
+        max_angle_raw = round_angle(get_max_angle_vm(speed, self.VM, CarControllerParams), angle_unit_offset + 2) * sign
         max_angle = np.clip(max_angle_raw, -self.STEER_ANGLE_MAX, self.STEER_ANGLE_MAX)
         self._tx(self._angle_cmd_msg(max_angle, True))
 
@@ -331,8 +328,7 @@ class TestTeslaSafetyBase(common.PandaCarSafetyTest, common.AngleSteeringSafetyT
 
         # Stay within limits
         # Up
-        max_angle_delta = get_max_angle_delta_vm(speed, CarControllerParams.STEER_STEP, self.VM, self.MAX_LATERAL_JERK)
-        max_angle_delta = round_angle(max_angle_delta, angle_unit_offset) * sign
+        max_angle_delta = round_angle(get_max_angle_delta_vm(speed, self.VM, CarControllerParams), angle_unit_offset) * sign
         self.assertTrue(self._tx(self._angle_cmd_msg(max_angle_delta, True)))
 
         # Don't change
@@ -343,8 +339,7 @@ class TestTeslaSafetyBase(common.PandaCarSafetyTest, common.AngleSteeringSafetyT
 
         # Inject too high rates
         # Up
-        max_angle_delta = get_max_angle_delta_vm(speed, CarControllerParams.STEER_STEP, self.VM, self.MAX_LATERAL_JERK)
-        max_angle_delta = round_angle(max_angle_delta, angle_unit_offset + 1) * sign
+        max_angle_delta = round_angle(get_max_angle_delta_vm(speed, self.VM, CarControllerParams), angle_unit_offset + 1) * sign
         self.assertFalse(self._tx(self._angle_cmd_msg(max_angle_delta, True)))
 
         # Don't change
