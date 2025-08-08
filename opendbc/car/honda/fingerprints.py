5ce0dec1e8500cb3a358a46b1d9b9e94ca0ead36
mvl-boston <manuel@alumni.virginia.edu>
2025-08-07 23:56:33 -0400

    Adding 23 ICE fingerprints (Taiwan market)

--- opendbc/car/honda/fingerprints.py
+++ opendbc/car/honda/fingerprints.py
@@ -978,9 +978,11 @@ FW_VERSIONS = {
   CAR.HONDA_CRV_6G: {
     (Ecu.fwdRadar, 0x18dab0f1, None): [
       b'8S302-3A0-A060\x00\x00',
+      b'8S302-3C0-Q050\x00\x00',      
     ],
     (Ecu.fwdCamera, 0x18dab5f1, None): [
       b'8S102-3A0-A090\x00\x00',
+      b'8S102-3C0-Q060\x00\x00',
     ],
   },
 }
