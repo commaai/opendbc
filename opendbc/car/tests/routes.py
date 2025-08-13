1a9fdbfec2914867330bedbf157933ac3fc66501
Jason Young <jyoung8607@gmail.com>
2025-08-12 23:24:27 -0400

    Revert "Updating Pilot to new test route"
    This reverts commit 84adcc449e7f45ca1ba2d7108f6a3ece11aa517f.

--- opendbc/car/tests/routes.py
+++ opendbc/car/tests/routes.py
@@ -108,7 +108,7 @@ routes = [
   CarTestRoute("f44aa96ace22f34a/2021-12-22--06-22-31", HONDA.HONDA_CIVIC_2022),
   CarTestRoute("1f032f5173c8ad99/00000006--573b3fcaf5", HONDA.HONDA_CIVIC_2022),  # Civic Type R with manual transmission
   CarTestRoute("b1c832ad56b6bc9d/00000010--debfcf5867", HONDA.HONDA_CIVIC_2022),  # 2025 Civic Hatch Hybrid with new eCVT transmission
-  CarTestRoute("ea1fc707f10a580a/00000002--5d8a903b09", HONDA.HONDA_PILOT_4G),
+  CarTestRoute("f9c43864cf057d05/2024-01-15--23-01-20", HONDA.HONDA_PILOT_4G),  # TODO: Replace with a newer route
   CarTestRoute("f39cf149898833ff/0000002b--54f3fae045", HONDA.HONDA_ACCORD_11G),
 
   CarTestRoute("87d7f06ade479c2e/2023-09-11--23-30-11", HYUNDAI.HYUNDAI_AZERA_6TH_GEN),
