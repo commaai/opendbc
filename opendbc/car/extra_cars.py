bdaf5bff0a2ca3f1c826dc4257580822f95baf1b
mvl-boston <manuel@alumni.virginia.edu>
2025-08-13 11:52:06 -0400

    adding 2023 to community docs

--- opendbc/car/extra_cars.py
+++ opendbc/car/extra_cars.py
@@ -49,6 +49,8 @@ class CAR(Platforms):
       CommunityCarDocs("Honda Accord 2016-17", "Honda Sensing"),
       CommunityCarDocs("Honda Accord 2024-25", "All"),
       CommunityCarDocs("Honda Clarity 2018-21", "All"),
+      CommunityCarDocs("Honda CR-V 2023", "All"),
+      CommunityCarDocs("Honda CR-V Hybrid 2023", "All"),
       CommunityCarDocs("Honda Odyssey 2021-25", "All"),
       CommunityCarDocs("Honda Passport 2026", "All"),
       GMSecurityCarDocs("Honda Prologue 2024-25", "All"),
