c466a77de356508be8089a169aac77e9dcde0e73
Jason Young <jyoung8607@gmail.com>
2025-08-13 09:46:57 -0400

    auto_fingerprint 63568e3e2f56c8ad/00000001--2c87c755e1

--- opendbc/car/honda/fingerprints.py
+++ opendbc/car/honda/fingerprints.py
@@ -975,9 +975,11 @@ FW_VERSIONS = {
   },
   CAR.HONDA_CRV_6G: {
     (Ecu.fwdRadar, 0x18dab0f1, None): [
+      b'8S302-3C0-Q050\x00\x00',
       b'8S302-3D4-A050\x00\x00',
     ],
     (Ecu.fwdCamera, 0x18dab5f1, None): [
+      b'8S102-3C0-Q060\x00\x00',
       b'8S102-3D4-A070\x00\x00',
       b'8S102-3D4-A080\x00\x00',
       b'8S102-3D4-A090\x00\x00',
