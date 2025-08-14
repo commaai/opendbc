fa210c3fd81273a06691b2c355814560804a072e
mvl-boston <manuel@alumni.virginia.edu>
2025-08-14 10:16:26 -0400

    Honda: CRV 2023-2025 (canfd) (#2475)
    * init CANFD CRV
    
    * init CANFD CRV Hybrid
    
    * remove
    
    * remove
    
    * remove import for now
    
    * remove import for now
    
    * shane fixes
    
    * rebase fixes
    
    * fix
    
    * update
    
    * remove whitespace
    
    * new test route
    
    * remove CRV Hybrid
    
    * 2025
    
    * rename honda pilot dbc to canfd common
    
    * rename canfd common dbc to canfd common can dbc
    
    * renaming dbc to honda_canfd_common
    
    * updating car specs
    
    * separate steer interface and increase to 5000 per testers
    
    * adding brake_error commands for canFD
    
    * autocalculate alt brake for canfd
    
    * adding canfd safety value
    
    * adding bosch canfd safety value
    
    * canfd required safety changes from pr 2129
    
    * canfd required safety changes from pr2129
    
    * renaming dbc to honda_canfd_common
    
    * typo fix
    
    * fix coding style per CI
    
    * move canfd altbrake to autodetection
    
    * clearer diff, for now
    
    * Adding 24 CRV Hybrid fingerprint
    
    * switch to torque controller
    
    * updating hybrid test route
    
    * applying log-determined values for CRV 6G Hybrid
    
    * updating values from additional log
    
    * updating steer threshold based on log
    
    * udpating values based on log
    
    * found higher numbers in CRV log
    
    * repoint ICE to Hybrid
    
    * typo fix
    
    * typo fix
    
    * remove CRV from car template
    
    * remove CRV from extra cars
    
    * Adding 23 ICE fingerprints (Taiwan market)
    
    * Adding CRV ICE test route
    
    * whitespace fix
    
    * collapse CRV_HYBRID_6G into CRV_6G
    
    * verified max of 2560 from dashcam route
    
    * cleanup
    
    * not seeing data to elevate this above other CAN-FD
    
    * swag after actuator rescale
    
    * it wants a little more SR
    
    * same steerActuatorDelay as the others
    
    * prepare to recreate FW lists
    
    * auto_fingerprint 864613393836f643/00000002--3cef07213f
    
    * auto_fingerprint 51dea302d9d61e22/00000006--f3732a4905
    
    * auto_fingerprint 0a84f6dd8372aef6/00000005--aff7debbbe
    
    * auto_fingerprint ea1fc707f10a580a/00000016--a6e25f0867
    
    * auto_fingerprint c6dc5864bedfeaa9/00000000--b11ca96af3
    
    * auto_fingerprint 63568e3e2f56c8ad/00000001--2c87c755e1
    
    * don't have a verified dongle with MY2023 yet
    
    * adding 2023 to community docs
    
    * auto_fingerprint 472a018ff8fd479a/0000006f--72d838b24d
    
    * Revert "adding 2023 to community docs"
    
    This reverts commit bdaf5bff0a2ca3f1c826dc4257580822f95baf1b.
    
    * Revert "don't have a verified dongle with MY2023 yet"
    
    This reverts commit 0e8b8e300168ab3404cc74980a182bec1d3f50e0.
    
    ---------
    
    Co-authored-by: vanillagorillaa <31773928+vanillagorillaa@users.noreply.github.com>
    Co-authored-by: Jason Young <jyoung8607@gmail.com>

--- opendbc/car/honda/fingerprints.py
+++ opendbc/car/honda/fingerprints.py
@@ -961,4 +961,17 @@ FW_VERSIONS = {
       b'8S102-30A-A060\x00\x00',
     ],
   },
+  CAR.HONDA_CRV_6G: {
+    (Ecu.fwdRadar, 0x18dab0f1, None): [
+      b'8S302-3C0-Q050\x00\x00',
+      b'8S302-3D4-A050\x00\x00',
+    ],
+    (Ecu.fwdCamera, 0x18dab5f1, None): [
+      b'8S102-3C0-Q060\x00\x00',
+      b'8S102-3D4-A060\x00\x00',
+      b'8S102-3D4-A070\x00\x00',
+      b'8S102-3D4-A080\x00\x00',
+      b'8S102-3D4-A090\x00\x00',
+    ],
+  },
 }
