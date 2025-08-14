08570411ffd2e3a9f7368108a8dea2da841a71ad
Jason Wen <haibin.wen3@gmail.com>
2025-08-14 15:09:22 -0400

    Reapply "Hyundai CAN: Non-SCC HEV/PHEV platform support (#222)" (#234) (#235)
    This reverts commit c91973e361a73b140c836d0ce885ad7941dad2d2.

--- opendbc/sunnypilot/car/hyundai/fingerprints_ext.py
+++ opendbc/sunnypilot/car/hyundai/fingerprints_ext.py
@@ -10,4 +10,12 @@ FW_VERSIONS_EXT = {
       b'\xf1\x00DE  MDPS C 1.00 1.05 56310Q4200\x00 4DEEC105',
     ],
   },
+  CAR.KIA_CEED_PHEV_2022_NON_SCC: {
+    (Ecu.eps, 0x7D4, None): [
+      b'\xf1\x00CD  MDPS C 1.00 1.01 56310-XX000 4CPHC101',
+    ],
+    (Ecu.fwdCamera, 0x7C4, None): [
+      b'\xf1\x00CDH LKAS AT EUR LHD 1.00 1.01 99211-CR700 931',
+    ],
+  },
 }
