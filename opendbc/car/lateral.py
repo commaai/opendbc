4e50498a177f9cf0af5eb50613ba59bc865ca217
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2025-08-18 19:43:30 -0700

    Revert "Torque controller: refactor calculations to be in accel space (#2528)"
    This reverts commit 74bfaa2c750f2eca584dd6b58c1b862ba59d29d8.

--- opendbc/car/lateral.py
+++ opendbc/car/lateral.py
@@ -160,10 +160,9 @@ def apply_center_deadzone(error, deadzone):
 
 def get_friction(lateral_accel_error: float, lateral_accel_deadzone: float, friction_threshold: float,
                  torque_params: structs.CarParams.LateralTorqueTuning) -> float:
-  # TODO torque params' friction should be in lataxel space, not torque space
   friction_interp = np.interp(
     apply_center_deadzone(lateral_accel_error, lateral_accel_deadzone),
     [-friction_threshold, friction_threshold],
-    [-torque_params.friction * torque_params.latAccelFactor, torque_params.friction * torque_params.latAccelFactor]
+    [-torque_params.friction, torque_params.friction]
   )
   return float(friction_interp)
