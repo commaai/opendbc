4143c4a9d93c54f0bb1e9eb63c1c69f4e4c5ca21
royjr <royjr96@gmail.com>
2025-08-24 17:33:56 -0400

    remove HYUNDAI_KONA_2ND_GEN

--- opendbc/car/hyundai/fingerprints.py
+++ opendbc/car/hyundai/fingerprints.py
@@ -568,16 +568,6 @@ FW_VERSIONS = {
       b'\xf1\x00OS9 LKAS AT USA LHD 1.00 1.00 95740-J9300 g21',
     ],
   },
-  CAR.HYUNDAI_KONA_2ND_GEN: {
-    (Ecu.fwdCamera, 0x7c4, None): [
-      b'\xf1\x00SX2 MFC  AT USA LHD 1.00 1.03 99211-BE000 230517',
-      b'\xf1\x00SX2 MFC  AT USA LHD 1.00 1.07 99211-BE000 240611',
-    ],
-    (Ecu.fwdRadar, 0x7d0, None): [
-      b'\xf1\x00SX2_ RDR -----      1.00 1.02 99110-BE000         ',
-      b'\xf1\x00SX2_ RDR -----      1.00 1.02 99110-BE500         ',
-    ],
-  },
   CAR.KIA_CEED: {
     (Ecu.fwdRadar, 0x7d0, None): [
       b'\xf1\x00CD__ SCC F-CUP      1.00 1.00 99110-J7500         ',
