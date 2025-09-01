d3b8250f62812b08612c04f2921563aaebbf81d6
Sathans <hans5438@gmail.com>
2025-09-01 10:42:47 +0200

    Add car_docs reference for NISSAN_LEAF_IC
    Updated NISSAN_LEAF_IC configuration to include platform "description".
    
    Currently comma uses fingerprints.py to include the fingerprints, also for platforms with same "description". This works for a manually written file.
    
    In Sunnypilot vehicle selector rewrite the files are autogenerated and put inside "car_list.json". Here the generator does not include a copy of Nissan Leaf IC because they have the same description.

--- opendbc/car/nissan/values.py
+++ opendbc/car/nissan/values.py
@@ -66,7 +66,7 @@ class CAR(Platforms):
   )
   # Leaf with ADAS ECU found behind instrument cluster instead of glovebox
   # Currently the only known difference between them is the inverted seatbelt signal.
-  NISSAN_LEAF_IC = NISSAN_LEAF.override(car_docs=[])
+  NISSAN_LEAF_IC = NISSAN_LEAF.override(car_docs=["Nissan Leaf 2018-23 Instrument Cluster"])
   NISSAN_ROGUE = NissanPlatformConfig(
     [NissanCarDocs("Nissan Rogue 2018-20")],
     NissanCarSpecs(mass=1610, wheelbase=2.705)
