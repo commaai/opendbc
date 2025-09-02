49d5e5fc9ec195f0773ead9ce723b059db931494
Jason Young <jyoung8607@gmail.com>
2025-09-02 19:52:56 -0400

    activate minSteerSpeed support

--- opendbc/car/honda/values.py
+++ opendbc/car/honda/values.py
@@ -268,8 +268,8 @@ class CAR(Platforms):
     CarSpecs(mass=4544 * CV.LB_TO_KG, wheelbase=2.89, centerToFrontRatio=0.428, steerRatio=16.2),
   )
   HONDA_ODYSSEY_5G_MMR = HondaBoschPlatformConfig(
-    [HondaCarDocs("Honda Odyssey 2021-25", "All", min_steer_speed=70. * CV.KPH_TO_MS)],
-    CarSpecs(mass=4590 * CV.LB_TO_KG, wheelbase=3.00, steerRatio=18.4, centerToFrontRatio=0.41),
+    [HondaCarDocs("Honda Odyssey 2021-25", "All")],
+    CarSpecs(mass=4590 * CV.LB_TO_KG, wheelbase=3.00, steerRatio=18.4, centerToFrontRatio=0.41, minSteerSpeed=70. * CV.KPH_TO_MS),
     {Bus.pt: 'acura_rdx_2020_can_generated'},
     flags=HondaFlags.BOSCH_ALT_BRAKE | HondaFlags.BOSCH_ALT_RADAR,
   )
