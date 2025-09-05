eb7b74c703ce73c9d1c3a2ca5c918c5ded07fbae
Karan <karanlokchandani@protonmail.com>
2025-09-05 08:58:47 +0530

    tests: defer global imports and remove pytest parametrize decorators

--- opendbc/car/tests/test_routes.py
+++ opendbc/car/tests/test_routes.py
@@ -1,11 +1,10 @@
-import pytest
+def test_test_route_present(subtests):
+  from opendbc.car.values import PLATFORMS
+  from opendbc.car.tests.routes import non_tested_cars, routes
 
-from opendbc.car.values import PLATFORMS
-from opendbc.car.tests.routes import non_tested_cars, routes
-
-
-@pytest.mark.parametrize("platform", PLATFORMS.keys())
-def test_test_route_present(platform):
   tested_platforms = [r.car_model for r in routes]
-  assert platform in set(tested_platforms) | set(non_tested_cars), \
-    f"Missing test route for {platform}. Add a route to opendbc/car/tests/routes.py"
+  tested_platforms_set = set(tested_platforms) | set(non_tested_cars)
+
+  for platform in PLATFORMS.keys():
+    with subtests.test(msg=f"test for {platform}"):
+      assert platform in tested_platforms_set, f"Missing test route for {platform}. Add a route to opendbc/car/tests/routes.py"
