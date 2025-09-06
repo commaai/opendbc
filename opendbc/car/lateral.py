612219dbbcc9c9469cb51e2bfc8cb967221fd52a
DevTekVE <devtekve@gmail.com>
2025-09-06 08:19:55 +0200

    Better?

--- opendbc/car/lateral.py
+++ opendbc/car/lateral.py
@@ -120,12 +120,12 @@ def apply_steer_angle_limits_vm(apply_angle: float, apply_angle_last: float, v_e
   max_angle = get_max_angle_vm(v_ego_raw, VM, limits)
   new_apply_angle = np.clip(new_apply_angle, -max_angle, max_angle)
 
-  # angle is current angle when inactive, but still limited to max lat accel to prevent faults.
+  # angle is current angle when inactive
   if not lat_active:
-    new_apply_angle = np.clip(steering_angle, -max_angle, max_angle)
+    new_apply_angle = steering_angle
 
-  # prevent fault
-  return float(np.clip(new_apply_angle, -limits.ANGLE_LIMITS.STEER_ANGLE_MAX, limits.ANGLE_LIMITS.STEER_ANGLE_MAX))
+  # prevent fault (we always limit to lat accel)
+  return float(np.clip(new_apply_angle, -max_angle, max_angle))
 
 
 def common_fault_avoidance(fault_condition: bool, request: bool, above_limit_frames: int,
