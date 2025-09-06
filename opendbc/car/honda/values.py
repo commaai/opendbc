69df2bacc1a8e75d8de07f4eb84a02cf6ace7c9a
mvl-boston <manuel@alumni.virginia.edu>
2025-09-06 07:39:50 -0400

    Updating to learned steer ratio

--- opendbc/car/honda/values.py
+++ opendbc/car/honda/values.py
@@ -268,7 +268,7 @@ class CAR(Platforms):
   )
   ACURA_INTEGRA = HondaBoschPlatformConfig(
     [HondaCarDocs("Acura Integra 2023-25", "All")],
-    CarSpecs(mass=3338.8 * CV.LB_TO_KG, wheelbase=2.5, centerToFrontRatio=0.5, steerRatio=16.71, tireStiffnessFactor=0.82),
+    CarSpecs(mass=3338.8 * CV.LB_TO_KG, wheelbase=2.5, centerToFrontRatio=0.5, steerRatio=15.5,
     {Bus.pt: 'honda_bosch_radarless_generated'},
     flags=HondaFlags.BOSCH_RADARLESS | HondaFlags.ALLOW_MANUAL_TRANS,
   )
