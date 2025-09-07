daa0378b03c78185c60a987711fcf985c5b90d9d
mvl-boston <manuel@alumni.virginia.edu>
2025-09-07 13:28:02 -0400

    whitespace fix

--- opendbc/car/honda/interface.py
+++ opendbc/car/honda/interface.py
@@ -68,7 +68,7 @@ class CarInterface(CarInterfaceBase):
 
     if (ret.flags & HondaFlags.NIDEC) and (ret.flags & HondaFlags.HYBRID) and (0x223 in fingerprint[CAN.pt]):
       ret.flags |= HondaFlags.HYBRID_ALT_BRAKEHOLD.value
-      
+
     if ret.flags & HondaFlags.ALLOW_MANUAL_TRANS and all(msg not in fingerprint[CAN.pt] for msg in (0x191, 0x1A3)):
       # Manual transmission support for allowlisted cars only, to prevent silent fall-through on auto-detection failures
       ret.transmissionType = TransmissionType.manual
