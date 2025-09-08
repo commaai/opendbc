7c3179f774f483752fcb3c868d6178f459296a8a
Shane Smiskol <shane@smiskol.com>
2025-09-08 11:49:24 -0700

    Export fingerprints

--- opendbc/car/honda/fingerprints.py
+++ opendbc/car/honda/fingerprints.py
@@ -581,6 +581,7 @@ FW_VERSIONS = {
       b'28101-5EZ-A600\x00\x00',
       b'28101-5EZ-A700\x00\x00',
       b'28103-5EY-A110\x00\x00',
+      b'28103-5EZ-A010\x00\x00',
     ],
     (Ecu.gateway, 0x18daeff1, None): [
       b'38897-TG7-A030\x00\x00',
