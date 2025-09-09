d3e6d65972387f98935351c6a6e250caa07783e8
Jason Young <jyoung8607@gmail.com>
2025-09-09 18:29:32 -0400

    you will respect my authoritah

--- opendbc/car/honda/interface.py
+++ opendbc/car/honda/interface.py
@@ -178,9 +178,11 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.6], [0.18]] # TODO: can probably use some tuning
 
     elif candidate == CAR.HONDA_ODYSSEY_5G_MMR:
-      ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 2560], [0, 2560]]  # clipped by radar
-      ret.steerActuatorDelay = 0.14
-      CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
+      # Stock camera sends up to 2560 during LKA operation and up to 3840 during RDM operation
+      # Steer motor torque does rise a little above 2560, but not linearly, RDM also applies one-sided brake drag
+      ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 2560, 3072], [0, 2560, 3840]]
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[1.0], [0.10]]
+      ret.steerActuatorDelay = 0.15
       CarControllerParams.BOSCH_GAS_LOOKUP_V = [0, 2000]
       if not ret.openpilotLongitudinalControl:
         # When using stock ACC, the radar intercepts and filters steering commands the EPS would otherwise accept
