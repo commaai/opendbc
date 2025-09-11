4170d7d876a87904dab4b351aa8139ec3d400430
Hiromasa Ihara <iharahiromasa@gmail.com>
2025-09-11 19:54:14 +0900

    Honda: Honda N-Box 2018 (#1079)
    * feat: add honda n-box 2018 support (dbc file)
    
    * feat: add honda n-box 2018 support (car porting)
    
    * re-generate nbox dbc
    
    * fix for following latest upstream master
    
    * add test route
    
    * refactor gearbox configration condition
    
    * fix typo
    
    * update nbox castate to change transmission-type
    
    * fix lint; change judgement condition of CVT
    
    * generated DBCs don't live in the repo anymore
    
    * place in approximately sorted order
    
    * add generation to CAR name
    
    * follow STANDSTILL message refactor
    
    * forgot to actually remove the dupe
    
    * use a similar DBC
    
    * cleanup
    
    * confirmed VSA not available w/o comma power
    
    * rescale actuator, convert to lat accel torque control
    
    * almost forgot this
    
    * Revert "rescale actuator, convert to lat accel torque control"
    
    This reverts commit bfaf5cc33ab699aa48840692e29615dd90e08124.
    
    * manual lat accel figures should still move over here
    
    ---------
    
    Co-authored-by: Jason Young <jyoung8607@gmail.com>

--- opendbc/car/honda/values.py
+++ opendbc/car/honda/values.py
@@ -153,6 +153,13 @@ def radar_dbc_dict(pt_dict):
 
 class CAR(Platforms):
   # Bosch Cars
+  HONDA_NBOX_2G = HondaBoschPlatformConfig(
+    [
+      HondaCarDocs("Honda N-Box 2018", "All", min_steer_speed=5.),
+    ],
+    CarSpecs(mass=890., wheelbase=2.520, steerRatio=18.64),
+    {Bus.pt: 'acura_rdx_2020_can_generated'},
+  )
   HONDA_ACCORD = HondaBoschPlatformConfig(
     [
       HondaCarDocs("Honda Accord 2018-22", "All", video="https://www.youtube.com/watch?v=mrUwlj3Mi58", min_steer_speed=3. * CV.MPH_TO_MS),
@@ -357,6 +364,7 @@ STEER_THRESHOLD = {
   CAR.ACURA_MDX_4G_MMR: 600,
   CAR.HONDA_CRV_6G: 600,
   CAR.HONDA_CITY_7G: 600,
+  CAR.HONDA_NBOX_2G: 600,
 }
 
 
@@ -403,7 +411,7 @@ FW_QUERY_CONFIG = FwQueryConfig(
   non_essential_ecus={
     Ecu.eps: [CAR.ACURA_RDX_3G, CAR.HONDA_ACCORD, CAR.HONDA_E, *HONDA_BOSCH_ALT_RADAR, *HONDA_BOSCH_RADARLESS, *HONDA_BOSCH_CANFD],
     Ecu.vsa: [CAR.ACURA_RDX_3G, CAR.HONDA_ACCORD, CAR.HONDA_CIVIC, CAR.HONDA_CIVIC_BOSCH, CAR.HONDA_CRV_5G, CAR.HONDA_CRV_HYBRID,
-              CAR.HONDA_E, CAR.HONDA_INSIGHT, *HONDA_BOSCH_ALT_RADAR, *HONDA_BOSCH_RADARLESS, *HONDA_BOSCH_CANFD],
+              CAR.HONDA_E, CAR.HONDA_INSIGHT, CAR.HONDA_NBOX_2G, *HONDA_BOSCH_ALT_RADAR, *HONDA_BOSCH_RADARLESS, *HONDA_BOSCH_CANFD],
   },
   extra_ecus=[
     (Ecu.combinationMeter, 0x18da60f1, None),
