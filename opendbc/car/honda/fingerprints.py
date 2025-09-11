60a609f3249e25f5e6b8a5a231fe2198dcde1a60
Jason Young <jyoung8607@gmail.com>
2025-09-11 05:01:56 -0400

    auto_fingerprint f39cf149898833ff/00000090--32f3a73844

--- opendbc/car/honda/fingerprints.py
+++ opendbc/car/honda/fingerprints.py
@@ -559,34 +559,33 @@ FW_VERSIONS = {
       b'54008-THR-A020\x00\x00',
     ],
   },
-  # mid-model refresh
   CAR.HONDA_ODYSSEY_5G_MMR: {
     (Ecu.vsa, 0x18da28f1, None): [
-      b'PLACEHOLDER',
+      b'57114-THR-A240\x00\x00',
     ],
     (Ecu.fwdRadar, 0x18dab0f1, None): [
-      b'PLACEHOLDER',
+      b'36802-THR-A220\x00\x00',
     ],
     (Ecu.fwdCamera, 0x18dab5f1, None): [
-      b'PLACEHOLDER',
+      b'36161-THR-A230\x00\x00',
     ],
     (Ecu.shiftByWire, 0x18da0bf1, None): [
-      b'PLACEHOLDER',
+      b'54008-THR-A310\x00\x00',
     ],
     (Ecu.transmission, 0x18da1ef1, None): [
-      b'PLACEHOLDER',
+      b'28102-5MX-A200\x00\x00',
     ],
     (Ecu.srs, 0x18da53f1, None): [
-      b'PLACEHOLDER',
+      b'77959-THR-A230\x00\x00',
     ],
     (Ecu.electricBrakeBooster, 0x18da2bf1, None): [
-      b'PLACEHOLDER',
+      b'46114-THR-A540\x00\x00',
     ],
     (Ecu.gateway, 0x18daeff1, None): [
-      b'PLACEHOLDER',
+      b'38897-THR-A410\x00\x00',
     ],
     (Ecu.eps, 0x18da30f1, None): [
-      b'PLACEHOLDER',
+      b'39990-THR-A050\x00\x00',
     ],
   },
   CAR.HONDA_PILOT: {
