2a48d0118c22c362bb705ace19ff48fb7460b706
Gavin Canon-Phratsachack <37220586+gncnpk@users.noreply.github.com>
2025-09-11 02:50:15 -0500

    Increase nissan leaf steer ratio

--- opendbc/car/nissan/values.py
+++ opendbc/car/nissan/values.py
@@ -61,7 +61,7 @@ class CAR(Platforms):
   )
   NISSAN_LEAF = NissanPlatformConfig(
     [NissanCarDocs("Nissan Leaf 2018-23", video="https://youtu.be/vaMbtAh_0cY")],
-    NissanCarSpecs(mass=1610, wheelbase=2.700),
+    NissanCarSpecs(mass=1610, wheelbase=2.700, steer_ratio=17.68),
     {Bus.pt: 'nissan_leaf_2018_generated'},
   )
   # Leaf with ADAS ECU found behind instrument cluster instead of glovebox
