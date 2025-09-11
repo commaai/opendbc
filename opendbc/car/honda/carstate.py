534e66918a5916f3b87235365aaa8d4fc6123e75
mvl-boston <manuel@alumni.virginia.edu>
2025-09-11 06:58:37 -0400

    aligning with odyssey carstate changes

--- opendbc/car/honda/carstate.py
+++ opendbc/car/honda/carstate.py
@@ -44,7 +44,6 @@ class CarState(CarStateBase):
     self.dynamic_v_cruise_units = self.CP.carFingerprint in (HONDA_BOSCH_RADARLESS | HONDA_BOSCH_ALT_RADAR | HONDA_BOSCH_CANFD)
     self.cruise_setting = 0
     self.v_cruise_pcm_prev = 0
-    self.steering_blocked = False
 
     # When available we use cp.vl["CAR_SPEED"]["ROUGH_CAR_SPEED_2"] to populate vEgoCluster
     # However, on cars without a digital speedometer this is not always present (HRV, FIT, CRV 2016, ILX and RDX)
@@ -187,9 +186,6 @@ class CarState(CarStateBase):
     ret.brake = cp.vl["VSA_STATUS"]["USER_BRAKE"]
     ret.cruiseState.enabled = cp.vl["POWERTRAIN_DATA"]["ACC_STATUS"] != 0
     ret.cruiseState.available = bool(cp.vl[self.car_state_scm_msg]["MAIN_ON"])
-
-    if self.CP.carFingerprint in HONDA_BOSCH_ALT_RADAR:
-      self.steering_blocked = cp.vl["STEER_STATUS"]["STEER_CONTROL_ACTIVE"] != 1
 
     # Gets rid of Pedal Grinding noise when brake is pressed at slow speeds for some models
     if self.CP.carFingerprint in (CAR.HONDA_PILOT, CAR.HONDA_RIDGELINE):
