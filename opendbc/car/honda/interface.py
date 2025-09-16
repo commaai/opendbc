644a75a43cd1b74eed64ad4990b8abc244768807
mvl-boston <manuel@alumni.virginia.edu>
2025-09-15 23:34:56 -0400

    typo fix

--- opendbc/car/honda/interface.py
+++ opendbc/car/honda/interface.py
@@ -51,7 +51,7 @@ class CarInterface(CarInterfaceBase):
       ret.openpilotLongitudinalControl = alpha_long
       ret.pcmCruise = not ret.openpilotLongitudinalControl
     else:
-      cfgs =  = [get_safety_config(structs.CarParams.SafetyModel.hondaNidec)]
+      cfgs = [get_safety_config(structs.CarParams.SafetyModel.hondaNidec)]
       if candidate == CAR.ACURA_RLX:
         cfgs.insert(1, get_safety_config(structs.CarParams.SafetyModel.noOutput)
       ret.safetyConfigs = cfgs
