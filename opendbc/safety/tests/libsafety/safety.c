5e73beb8e52e14fff9a3b202b000f7f30ce3e283
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2025-09-27 15:17:04 -0700

    rm drivers

--- opendbc/safety/tests/libsafety/safety.c
+++ opendbc/safety/tests/libsafety/safety.c
@@ -6,7 +6,6 @@
 //int safety_tx_hook(CANPacket_t *msg) { return 1; }
 
 #include "opendbc/safety/safety.h"
-#include "opendbc/safety/board/drivers/can_common.h"
 
 // libsafety stuff
 #include "opendbc/safety/tests/libsafety/safety_helpers.h"
