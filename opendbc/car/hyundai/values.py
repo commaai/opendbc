aa4ed60a4ba463074a2e498ad31cc22b3aa59fd0
shurtadohwnew <shurtado.hw.new@gmail.com>
2025-09-27 11:51:31 -0400

    Instead of a new 2024 Kia forte Add to exisiting 21-23
    Kia Forte 2024 is same as 21-23, Same Harness and Can Addresses,

--- opendbc/car/hyundai/values.py
+++ opendbc/car/hyundai/values.py
@@ -382,8 +382,7 @@ class CAR(Platforms):
   KIA_FORTE = HyundaiPlatformConfig(
     [
       HyundaiCarDocs("Kia Forte 2019-21", min_enable_speed=6 * CV.MPH_TO_MS, car_parts=CarParts.common([CarHarness.hyundai_g])),
-      HyundaiCarDocs("Kia Forte 2022-23", car_parts=CarParts.common([CarHarness.hyundai_e])),
-      HyundaiCarDocs("Kia Forte 2024", car_parts=CarParts.common([CarHarness.hyundai_e])),
+      HyundaiCarDocs("Kia Forte 2022-24", car_parts=CarParts.common([CarHarness.hyundai_e])),
     ],
     CarSpecs(mass=2878 * CV.LB_TO_KG, wheelbase=2.8, steerRatio=13.75, tireStiffnessFactor=0.5)
   )
@@ -763,7 +762,7 @@ FW_QUERY_CONFIG = FwQueryConfig(
   non_essential_ecus={
     Ecu.abs: [CAR.HYUNDAI_PALISADE, CAR.HYUNDAI_SONATA, CAR.HYUNDAI_SANTA_FE_2022, CAR.KIA_K5_2021, CAR.HYUNDAI_ELANTRA_2021,
               CAR.HYUNDAI_SANTA_FE, CAR.HYUNDAI_KONA_EV_2022, CAR.HYUNDAI_KONA_EV, CAR.HYUNDAI_CUSTIN_1ST_GEN, CAR.KIA_SORENTO,
-              CAR.KIA_CEED, CAR.KIA_SELTOS, CAR.KIA_FORTE],
+              CAR.KIA_CEED, CAR.KIA_SELTOS],
   },
   extra_ecus=[
     (Ecu.adas, 0x730, None),              # ADAS Driving ECU on platforms with LKA steering
