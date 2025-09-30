33f1966dbf30bfc174334b0778aff2623967bf1a
zhou jielei <zhou-jielei@gmo.jp>
2025-09-30 09:39:09 +0900

    change test route

--- opendbc/car/tests/routes.py
+++ opendbc/car/tests/routes.py
@@ -111,7 +111,7 @@ routes = [
   CarTestRoute("f44aa96ace22f34a/2021-12-22--06-22-31", HONDA.HONDA_CIVIC_2022),
   CarTestRoute("1f032f5173c8ad99/00000006--573b3fcaf5", HONDA.HONDA_CIVIC_2022),  # Civic Type R with manual transmission
   CarTestRoute("b1c832ad56b6bc9d/00000010--debfcf5867", HONDA.HONDA_CIVIC_2022),  # 2025 Civic Hatch Hybrid with new eCVT transmission
-  CarTestRoute("a3fa73a8f1cd8e8d/0000000b--777636dc85", HONDA.HONDA_ODYSSEY_RC5),
+  CarTestRoute("a3fa73a8f1cd8e8d/0000000c--439df2ef4a", HONDA.HONDA_ODYSSEY_RC5),
   CarTestRoute("f9c43864cf057d05/2024-01-15--23-01-20", HONDA.HONDA_PILOT_4G),  # TODO: Replace with a newer route
   CarTestRoute("f39cf149898833ff/0000002b--54f3fae045", HONDA.HONDA_ACCORD_11G),
   CarTestRoute("ad9840558640c31d/0000001a--d6cd4871c2", HONDA.ACURA_MDX_4G_MMR),  # 2025 MDX
