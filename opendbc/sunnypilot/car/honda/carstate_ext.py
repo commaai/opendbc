bf1e82a91d8587c27ea2ed261144697f605ed251
MVL <manuel@alumni.virginia.edu>
2025-10-05 22:29:47 -0400

    Honda: Update Clarity brake to renamed DBC message name (#282)
    Co-authored-by: Jason Wen <haibin.wen3@gmail.com>

--- opendbc/sunnypilot/car/honda/carstate_ext.py
+++ opendbc/sunnypilot/car/honda/carstate_ext.py
@@ -22,7 +22,7 @@ class CarStateExt:
 
     if self.CP_SP.flags & HondaFlagsSP.CLARITY:
       ret.accFaulted = bool(cp.vl["HYBRID_BRAKE_ERROR"]["BRAKE_ERROR_1"] or cp.vl["HYBRID_BRAKE_ERROR"]["BRAKE_ERROR_2"])
-      ret.stockAeb = bool(cp_cam.vl["BRAKE_COMMAND"]["AEB_REQ_1"] and cp_cam.vl["BRAKE_COMMAND"]["COMPUTER_BRAKE_ALT"] > 1e-5)
+      ret.stockAeb = bool(cp_cam.vl["BRAKE_COMMAND"]["AEB_REQ_1"] and cp_cam.vl["BRAKE_COMMAND"]["COMPUTER_BRAKE_HYBRID"] > 1e-5)
 
     if self.CP_SP.enableGasInterceptor:
       # Same threshold as panda, equivalent to 1e-5 with previous DBC scaling
