b0e575c622388fc4266ed5694fbe6808a107dbd8
Bruce Wayne <harald.the.engineer@gmail.com>
2025-10-20 13:04:28 -0700

    deprecate unused gains

--- opendbc/car/interfaces.py
+++ opendbc/car/interfaces.py
@@ -225,10 +225,6 @@ class CarInterfaceBase(ABC):
     params = get_torque_params()[candidate]
 
     tune.init('torque')
-    tune.torque.kf = 1.0
-    tune.torque.kp = 1.0
-    tune.torque.ki = 0.3
-    tune.torque.kd = 0.0
     tune.torque.friction = params['FRICTION']
     tune.torque.latAccelFactor = params['LAT_ACCEL_FACTOR']
     tune.torque.latAccelOffset = 0.0
