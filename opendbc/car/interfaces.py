387e6e48d5d855649b71c306f64eb78f365930c0
felsager <d.felsager@gmail.com>
2025-10-20 15:08:50 -0700

    remove long kf field assignment that makes hitl test fail

--- opendbc/car/interfaces.py
+++ opendbc/car/interfaces.py
@@ -210,7 +210,6 @@ class CarInterfaceBase(ABC):
     ret.stoppingDecelRate = 0.8 # brake_travel/s while trying to stop
     ret.vEgoStopping = 0.5
     ret.vEgoStarting = 0.5
-    ret.longitudinalTuning.kf = 1.
     ret.longitudinalTuning.kpBP = [0.]
     ret.longitudinalTuning.kpV = [0.]
     ret.longitudinalTuning.kiBP = [0.]
