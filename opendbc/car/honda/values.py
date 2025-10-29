7b48e97f99934664e2a8f16aeaf1325c859c89fa
MVL <manuel@alumni.virginia.edu>
2025-10-29 19:14:45 -0400

    try no radar on RLX

--- opendbc/car/honda/values.py
+++ opendbc/car/honda/values.py
@@ -269,7 +269,7 @@ class CAR(Platforms):
   ACURA_RLX_HYBRID = HondaNidecPlatformConfig(
     [], # 2017 RLX Hybrid, don't add to cardocs since custom panda
     CarSpecs(mass=4359 * CV.LB_TO_KG, wheelbase=2.85, centerToFrontRatio=0.39, steerRatio=13.9, tireStiffnessFactor=0.8467),  #spec, stiff/ctf from Accord
-    radar_dbc_dict('acura_rlx'),
+    {Bus.pt: 'acura_rlx'}, #try no radar
     flags=HondaFlags.NIDEC_ALT_SCM_MESSAGES | HondaFlags.NIDEC_HYBRID,
   )
   HONDA_ODYSSEY = HondaNidecPlatformConfig(
