22b7c50957e17eea2ac339cae9367ba3c7a6348f
MVL <manuel@alumni.virginia.edu>
2025-11-01 23:24:27 -0400

    clean up safety errors

--- opendbc/safety/modes/honda.h
+++ opendbc/safety/modes/honda.h
@@ -288,9 +288,9 @@ static safety_config honda_nidec_init(uint16_t param) {
   // 0xE4 is steering on all cars except CRV and RDX, 0x194 for CRV and RDX,
   // 0x1FA is brake control, 0x30C is acc hud, 0x33D is lkas hud
   static CanMsg HONDA_N_TX_MSGS[] = {{0xE4, 0, 5, .check_relay = true},
-                                     {0x194, 0, 4, .check_relay = false}, {0x194, 4, 4, .check_relay = false},
+                                     {0x194, 0, 4, .check_relay = true},
                                      {0x1FA, 0, 8, .check_relay = false}, {0x30C, 0, 8, .check_relay = true},
-                                     {0x33D, 0, 5, .check_relay = false}, {0x33D, 4, 5, .check_relay = false}};
+                                     {0x33D, 0, 5, .check_relay = false}};
 
   const uint16_t HONDA_PARAM_NIDEC_ALT = 4;
   const uint16_t HONDA_PARAM_NIDEC_HYBRID = 32;
