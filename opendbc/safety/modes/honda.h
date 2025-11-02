fb71f4d0c1853bf5d445355d095b96eaed179b49
MVL <manuel@alumni.virginia.edu>
2025-11-01 23:30:02 -0400

    remove safety errors

--- opendbc/safety/modes/honda.h
+++ opendbc/safety/modes/honda.h
@@ -290,7 +290,7 @@ static safety_config honda_nidec_init(uint16_t param) {
   static CanMsg HONDA_N_TX_MSGS[] = {{0xE4, 0, 5, .check_relay = true},
                                      {0x194, 0, 4, .check_relay = true},
                                      {0x1FA, 0, 8, .check_relay = false}, {0x30C, 0, 8, .check_relay = true},
-                                     {0x33D, 0, 5, .check_relay = false}};
+                                     {0x33D, 0, 5, .check_relay = true}};
 
   const uint16_t HONDA_PARAM_NIDEC_ALT = 4;
   const uint16_t HONDA_PARAM_NIDEC_HYBRID = 32;
