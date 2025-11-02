5833640751cb6121430453f77dca6be31459dda1
MVL <manuel@alumni.virginia.edu>
2025-11-02 00:39:16 -0400

    switch to pump_on

--- opendbc/car/honda/carcontroller.py
+++ opendbc/car/honda/carcontroller.py
@@ -236,8 +236,7 @@ class CarController(CarControllerBase):
           pump_on, self.last_pump_ts = brake_pump_hysteresis(apply_brake, self.apply_brake_last, self.last_pump_ts, ts)
 
           pcm_override = True
-          pump_send = (apply_brake > 0) if self.CP.carFingerprint in HONDA_NIDEC_HYBRID else pump_on
-          can_sends.append(hondacan.create_brake_command(self.packer, self.CAN, apply_brake, pump_send,
+          can_sends.append(hondacan.create_brake_command(self.packer, self.CAN, apply_brake, pump_on,
                                                          pcm_override, pcm_cancel_cmd, fcw_display,
                                                          self.CP.carFingerprint, CS.stock_brake, self.CP))
           self.apply_brake_last = apply_brake
