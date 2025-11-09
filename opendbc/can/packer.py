03eaa3a713b29d47ab36a2a07b019476d48ac3f2
Kevin Turcios <turcioskevinr@gmail.com>
2025-11-08 23:40:33 -0800

    Optimize set_value with mask caching and combined operations

--- opendbc/can/packer.py
+++ opendbc/can/packer.py
@@ -54,18 +54,21 @@ class CANPacker:
       return 0, b'', bus
     return addr, bytes(dat), bus
 
+_MASKS: list[int] = [(1 << k) - 1 for k in range(9)]
 
 def set_value(msg: bytearray, sig: Signal, ival: int) -> None:
   i = sig.lsb // 8
   bits = sig.size
   if sig.size < 64:
     ival &= (1 << sig.size) - 1
+
   while 0 <= i < len(msg) and bits > 0:
     shift = sig.lsb % 8 if (sig.lsb // 8) == i else 0
     size = min(bits, 8 - shift)
-    mask = ((1 << size) - 1) << shift
-    msg[i] &= ~mask
-    msg[i] |= (ival & ((1 << size) - 1)) << shift
+    size_mask = _MASKS[size] if size <= 8 else (1 << size) - 1
+    mask = size_mask << shift
+    msgval = msg[i]
+    msg[i] = (msgval & ~mask) | ((ival & size_mask) << shift)
     bits -= size
     ival >>= size
     i = i + 1 if sig.is_little_endian else i - 1
