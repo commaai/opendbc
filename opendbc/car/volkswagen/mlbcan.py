953e4652e2af99fd55490f22931873c4d118d5dd
Jason Young <jyoung8607@gmail.com>
2025-11-09 22:45:45 -0500

    consolidate create_lka_hud_control, unpretty but works

--- opendbc/car/volkswagen/mlbcan.py
+++ opendbc/car/volkswagen/mlbcan.py
@@ -1,4 +1,5 @@
-from opendbc.car.volkswagen.mqbcan import volkswagen_mqb_meb_checksum, xor_checksum
+from opendbc.car.volkswagen.mqbcan import (volkswagen_mqb_meb_checksum, xor_checksum,
+                                           create_lka_hud_control as mqb_create_lka_hud_control)
 
 # TODO: Parameterize the hca control type (5 vs 7) and consolidate with MQB (and PQ?)
 def create_steering_control(packer, bus, apply_steer, lkas_enabled):
@@ -14,16 +15,7 @@ def create_steering_control(packer, bus, apply_steer, lkas_enabled):
 
 
 def create_lka_hud_control(packer, bus, ldw_stock_values, enabled, steering_pressed, hud_alert, hud_control):
-  values = ldw_stock_values.copy()
-
-  values.update({
-    "LDW_Status_LED_gelb": 1 if enabled and steering_pressed else 0,
-    "LDW_Status_LED_gruen": 1 if enabled and not steering_pressed else 0,
-    "LDW_Lernmodus_links": 3 if hud_control.leftLaneDepart else 1 + hud_control.leftLaneVisible,
-    "LDW_Lernmodus_rechts": 3 if hud_control.rightLaneDepart else 1 + hud_control.rightLaneVisible,
-    "LDW_Texte": hud_alert,
-  })
-  return packer.make_can_msg("LDW_02", bus, values)
+  return mqb_create_lka_hud_control(packer, bus, ldw_stock_values, enabled, steering_pressed, hud_alert, hud_control)
 
 
 def create_acc_buttons_control(packer, bus, gra_stock_values, cancel=False, resume=False):
