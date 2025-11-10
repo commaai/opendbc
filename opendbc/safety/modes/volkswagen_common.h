dd629a7e9cdc01a6a4cf0a83b0b16e116134b57c
Jason Young <46612682+jyoung8607@users.noreply.github.com>
2025-11-09 21:24:36 -0800

    VW MLB: Porsche Macan (dashcam mode) (#2846)
    * add DBC
    
    * add platform definition data
    
    * carstate checkpoint
    
    * checkpoint carcontroller/mlbcan/interface
    
    * checkpoint safety and safety test
    
    * PEBCAK
    
    * checkpoint various fixes
    
    * fixes
    
    * more fixes
    
    * Blinkmodi_01 is genuinely on-demand only, add a fixme
    
    * get turn signal state from a less annoying source
    
    * add @Bentley's route for dev/test only
    
    * gearshift position
    
    * cleanup
    
    * auto_fingerprint e8dac44d3a3458a5/00000000--9b83a227ce
    
    * smooth move, ex-lax
    
    * Kombi_01 COUNTER isn't real, at least not universal
    
    * use lat accel torque controller
    
    * checkpoint refactor
    
    * Revert "always CANFD"
    
    This reverts commit ac6122e272e0dc040d5abf3bde6fca4e034a7ef7.
    
    * auto_fingerprint 3a4f7c559c2f1215/00000001--5dc72af277
    
    * add the VIN just to be sure
    
    * auto_fingerprint a3bd1cb95d7c2f97/00000082--dbb4d37c0d
    
    * she's a quick one
    
    * more torque
    
    * use different turn state signal
    
    * slight reduction in ramp-up rate for jerk limit safety
    
    * fix HCA status handling
    
    * FINE
    
    * add seatbelt signal
    
    * enable door states
    
    * Revert "use different turn state signal"
    
    This reverts commit fb334567544d1adbeb71289310356e3ddcba17c6.
    
    * use turn stalk helper to handle one-touch lane changes
    
    * derp
    
    * more friction
    
    * a little more SR
    
    * cleanup test routes
    
    * reduce whitespace diff
    
    * unnecessary for now
    
    * dashcam
    
    * cleanup
    
    * Reapply "always CANFD"
    
    This reverts commit 38e304f898107f16d963c976b3ef79978eb49e1d.
    
    * VW: Safety code cleanup
    
    * not yet
    
    * consolidate ESP_05 for MQB
    
    * retry CI after pinning pycapnp
    
    * DRY not worth it here
    
    * a little more DRY
    
    * convert another single-bit read to GET_BIT
    
    * separate MLB checksum handling, add XOR support
    
    * XOR checksum support for LS_01
    
    * shared message handling for LH_EPS_03 and LWI_01
    
    * selective signal pass-through for LS_01
    
    * add note for later consolidation
    
    * consolidate create_lka_hud_control, unpretty but works
    
    * XOR checksum support for TSK_05
    
    * add TODO
    
    * try it out
    
    * good enough for who it's for

--- opendbc/safety/modes/volkswagen_common.h
+++ opendbc/safety/modes/volkswagen_common.h
@@ -32,6 +32,13 @@ bool volkswagen_brake_pressure_detected = false;
 #define MSG_LDW_02           0x397U   // TX by OP, Lane line recognition and text alerts
 #define MSG_MOTOR_14         0x3BEU   // RX from ECU, for brake switch status
 
+// MLB only messages
+#define MSG_ESP_03      0x103U   // RX from ABS, for wheel speeds
+#define MSG_LS_01       0x10BU   // TX by OP, ACC control buttons for cancel/resume
+#define MSG_MOTOR_03    0x105U   // RX from ECU, for driver throttle input and brake switch status
+#define MSG_TSK_02      0x10CU   // RX from ECU, for ACC status from drivetrain coordinator
+#define MSG_ACC_05      0x10DU   // RX from radar, for ACC status
+
 static void volkswagen_common_init(void) {
   volkswagen_set_button_prev = false;
   volkswagen_resume_button_prev = false;
