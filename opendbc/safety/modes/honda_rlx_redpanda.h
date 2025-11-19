fc7eeadb2890752ab83ca025d63e34d98cb5f48f
MVL <manuel@alumni.virginia.edu>
2025-11-19 16:32:15 -0500

    Update honda_rlx_redpanda.h

--- opendbc/safety/modes/honda_rlx_redpanda.h
+++ opendbc/safety/modes/honda_rlx_redpanda.h
@@ -48,13 +48,6 @@ static void rlx_redpanda_rx_hook(const CANPacket_t *msg) {
   (void) msg; // ignore msg
 }
 
-static bool rlx_redpanda_tx_hook(const CANPacket_t *msg) {
-
-  bool tx = true;
-
-  return tx;
-}
-
 static uint32_t honda_get_rlxpanda_checksum(const CANPacket_t *msg) {
   int checksum_byte = GET_LEN(msg) - 1U;
   return (uint8_t)(msg->data[checksum_byte]) & 0xFU;
@@ -86,7 +79,6 @@ static uint8_t honda_get_rlxpanda_counter(const CANPacket_t *msg) {
 const safety_hooks honda_rlx_redpanda_hooks = {
   .init = rlx_redpanda_init,
   .rx = rlx_redpanda_rx_hook,
-  .tx = rlx_redpanda_tx_hook,
   .fwd = rlx_redpanda_fwd_hook,
   .get_counter = honda_get_rlxpanda_counter,
   .get_checksum =  honda_get_rlxpanda_checksum,
