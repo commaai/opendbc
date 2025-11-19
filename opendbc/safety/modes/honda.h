83736eb2ac249dc26df0d0f497f35ebb06e552f3
MVL <manuel@alumni.virginia.edu>
2025-11-18 22:52:47 -0500

    Remove unused variable 'honda_bosch_canfd'
    Remove unused variable 'honda_bosch_canfd' from honda.h

--- opendbc/safety/modes/honda.h
+++ opendbc/safety/modes/honda.h
@@ -13,7 +13,6 @@ static bool honda_alt_brake_msg = false;
 static bool honda_fwd_brake = false;
 static bool honda_bosch_long = false;
 static bool honda_bosch_radarless = false;
-static bool honda_bosch_canfd = false;
 typedef enum {HONDA_NIDEC, HONDA_BOSCH} HondaHw;
 static HondaHw honda_hw = HONDA_NIDEC;
 
