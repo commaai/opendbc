3a7042306b7123942eb81d9297df67236f76eedb
MVL <manuel@alumni.virginia.edu>
2025-11-20 20:36:11 -0500

    updating steer strength

--- opendbc/car/honda/interface.py
+++ opendbc/car/honda/interface.py
@@ -156,9 +156,8 @@ class CarInterface(CarInterfaceBase):
       else:
         ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.8], [0.24]]  # TODO: can probably use some tuning
 
-    elif candidate == CAR.ACURA_RLX_HYBRID: # copying MDX
-      ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 239], [0, 239]]
-      # ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 2047], [0, 2047]]
+    elif candidate == CAR.ACURA_RLX_HYBRID:
+      ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 2047], [0, 2047]]
       ret.lateralTuning.pid.kf = 0.000035
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.115], [0.052]]
 
