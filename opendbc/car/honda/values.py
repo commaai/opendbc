142ebc42e62b19797d3b16221e4818bf3568491f
MVL <manuel@alumni.virginia.edu>
2025-11-26 22:00:20 -0500

    update steer ratio and centerToFront

--- opendbc/car/honda/values.py
+++ opendbc/car/honda/values.py
@@ -321,8 +321,8 @@ class CAR(Platforms):
   )
   ACURA_RLX_HYBRID = HondaNidecPlatformConfig(
     [], # 2017 RLX Hybrid, don't add to cardocs since custom panda
-    CarSpecs(mass=4359 * CV.LB_TO_KG, wheelbase=2.85, centerToFrontRatio=0.39, steerRatio=13.9, tireStiffnessFactor=0.8467),  #spec, stiff/ctf from Accord
-    {Bus.pt: 'acura_rlx_2017_can_generated', Bus.radar: 'acura_ilx_2016_nidec', Bus.alt: 'acura_rlx_2017_can_generated'},
+    CarSpecs(mass=4359 * CV.LB_TO_KG, wheelbase=2.85, centerToFrontRatio=0.43, steerRatio=18.3),
+    radar_dbc_dict('acura_rlx_2017_can_generated'),
     flags=HondaFlags.NIDEC_ALT_SCM_MESSAGES,
   )
   HONDA_ODYSSEY = HondaNidecPlatformConfig(
