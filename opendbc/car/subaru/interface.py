6fe0c03ee738cc179987e6b2aa198f65707303e2
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2025-12-10 10:31:12 -0800

    lil cleanup

--- opendbc/car/subaru/interface.py
+++ opendbc/car/subaru/interface.py
@@ -43,6 +43,7 @@ class CarInterface(CarInterfaceBase):
       CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
 
     if ret.flags & SubaruFlags.LKAS_ANGLE:
+      ret.steerActuatorDelay = 0.3
       ret.safetyConfigs[0].safetyParam |= SubaruSafetyFlags.LKAS_ANGLE.value
 
     if candidate in (CAR.SUBARU_ASCENT, CAR.SUBARU_ASCENT_2023):
@@ -67,10 +68,6 @@ class CarInterface(CarInterfaceBase):
 
     elif candidate == CAR.SUBARU_CROSSTREK_HYBRID:
       ret.steerActuatorDelay = 0.1
-
-    elif candidate == CAR.SUBARU_CROSSTREK_2025:
-      ret.dashcamOnly = is_release
-      ret.steerActuatorDelay = 0.3
 
     elif candidate in (CAR.SUBARU_FORESTER, CAR.SUBARU_FORESTER_2022, CAR.SUBARU_FORESTER_HYBRID):
       ret.lateralTuning.init('pid')
