c7c2263ca4fa69e5a256962be980e2c1e02f70c7
elkoled <elkoled@gmail.com>
2025-12-15 15:07:23 +0100

    add more fingerprints

--- opendbc/car/psa/fingerprints.py
+++ opendbc/car/psa/fingerprints.py
@@ -6,17 +6,17 @@ Ecu = CarParams.Ecu
 
 FW_VERSIONS = {
   CAR.PSA_PEUGEOT_208: {
-    # ABRASR - ABS/ESP
-    (Ecu.abs, 0x6AD, None): [
-        b'085095700857210527', # Peugeot 208 ACC
-        b'285160381930060025B',# Peugeot 2008 ACC
+    (Ecu.abs, 0x6ad, None): [
+      b'085095700857210527',
+      b'085095706198220818',
+      b'085135705863191103',
+      b'285160381930060025B',
     ],
   },
   CAR.PSA_PEUGEOT_508: {
-    # ABRASR - ABS/ESP
-    (Ecu.abs, 0x6AD, None): [
-        b'085095308910190312', # Citroen C5 Aircross CC-only
-        b'085065315928191130', # Peugeot 508 Hybrid, Citroen Berlingo VP K9
+    (Ecu.abs, 0x6ad, None): [
+      b'085065315928191130',
+      b'085095308910190312',
     ],
   },
 }
