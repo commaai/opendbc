c9081379a7cc7066a6b7b8d35194713f0edd51e7
Matt Purnell <matt.purnell98@gmail.com>
2025-12-17 02:17:50 -0600

    safety: remove redundant checks in Rivian
    - Remove redundant address checks in rivian_get_counter and rivian_get_checksum as these functions are securely guarded by the safety whitelist.
    - Simplify rivian_compute_checksum and rivian_get_quality_flag_valid to remove unreachable branches.

--- opendbc/safety/modes/rivian.h
+++ opendbc/safety/modes/rivian.h
@@ -3,22 +3,13 @@
 #include "opendbc/safety/declarations.h"
 
 static uint8_t rivian_get_counter(const CANPacket_t *msg) {
-  uint8_t cnt = 0;
-  if ((msg->addr == 0x208U) || (msg->addr == 0x150U)) {
-    // Signal: ESP_Status_Counter, VDM_PropStatus_Counter
-    cnt = msg->data[1] & 0xFU;
-  }
-  return cnt;
+  // Signal: ESP_Status_Counter, VDM_PropStatus_Counter
+  return msg->data[1] & 0xFU;
 }
 
 static uint32_t rivian_get_checksum(const CANPacket_t *msg) {
-  uint8_t chksum = 0;
-  if ((msg->addr == 0x208U) || (msg->addr == 0x150U)) {
-    // Signal: ESP_Status_Checksum, VDM_PropStatus_Checksum
-    chksum = msg->data[0];
-  } else {
-  }
-  return chksum;
+  // Signal: ESP_Status_Checksum, VDM_PropStatus_Checksum
+  return msg->data[0];
 }
 
 static uint8_t _rivian_compute_checksum(const CANPacket_t *msg, uint8_t poly, uint8_t xor_output) {
@@ -43,9 +34,9 @@ static uint32_t rivian_compute_checksum(const CANPacket_t *msg) {
   uint8_t chksum = 0;
   if (msg->addr == 0x208U) {
     chksum = _rivian_compute_checksum(msg, 0x1D, 0xB1);
-  } else if (msg->addr == 0x150U) {
+  }
+  if (msg->addr == 0x150U) {
     chksum = _rivian_compute_checksum(msg, 0x1D, 0x9A);
-  } else {
   }
   return chksum;
 }
@@ -54,9 +45,9 @@ static bool rivian_get_quality_flag_valid(const CANPacket_t *msg) {
   bool valid = false;
   if (msg->addr == 0x208U) {
     valid = ((msg->data[3] >> 3) & 0x3U) == 0x1U;  // ESP_Vehicle_Speed_Q
-  } else if (msg->addr == 0x150U) {
+  }
+  if (msg->addr == 0x150U) {
     valid = (msg->data[1] >> 6) == 0x1U;  // VDM_VehicleSpeedQ
-  } else {
   }
   return valid;
 }
