4aa7ca972290af6c6bbd314d1975eab46a888b6f
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2025-12-20 15:18:07 -0800

    Add `CarInterface.DRIVABLE_GEARS` attribute (#2978)
    * Add CarInterface.DRIVABLE_GEARS gears attribute
    
    * lil less

--- opendbc/car/interfaces.py
+++ opendbc/car/interfaces.py
@@ -96,6 +96,8 @@ class CarInterfaceBase(ABC):
   CarController: 'CarControllerBase'
   RadarInterface: 'RadarInterfaceBase' = RadarInterfaceBase
 
+  DRIVABLE_GEARS: tuple[structs.CarState.GearShifter, ...] = ()
+
   def __init__(self, CP: structs.CarParams):
     self.CP = CP
 
