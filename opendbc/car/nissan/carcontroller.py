20383025ec1e9e1128cefffae13b52f00ff09199
downquark7 <mail2emn@gmail.com>
2025-12-22 09:26:14 -0600

    Nissan: use MADS enabled status for LKAS HUD logic (#371)
    * Update Nissan LKAS HUD logic to use latActive instead of enabled
    
    display the green steering-wheel icon on the hud whenever lat is active instead of when cc is active
    this fixes the issue where there is no hud icon when MADS is enabled when cc is disabled
    
    * switch CC.latActive to CC_SP.mads.enabled
    
    (cherry picked from commit 48773fd781be9a8bd3a5ddb66f91a00c7a5156a4)
    
    * Fix formatting of lkas_hud_msg parameters
    
    ---------
    
    Co-authored-by: Jason Wen <haibin.wen3@gmail.com>

--- opendbc/car/nissan/carcontroller.py
+++ opendbc/car/nissan/carcontroller.py
@@ -61,8 +61,8 @@ class CarController(CarControllerBase):
     # Below are the HUD messages. We copy the stock message and modify
     if self.CP.carFingerprint != CAR.NISSAN_ALTIMA:
       if self.frame % 2 == 0:
-        can_sends.append(nissancan.create_lkas_hud_msg(self.packer, CS.lkas_hud_msg, CC.enabled, hud_control.leftLaneVisible, hud_control.rightLaneVisible,
-                                                       hud_control.leftLaneDepart, hud_control.rightLaneDepart))
+        can_sends.append(nissancan.create_lkas_hud_msg(self.packer, CS.lkas_hud_msg, CC_SP.mads.enabled, hud_control.leftLaneVisible,
+                                                       hud_control.rightLaneVisible, hud_control.leftLaneDepart, hud_control.rightLaneDepart))
 
       if self.frame % 50 == 0:
         can_sends.append(nissancan.create_lkas_hud_info_msg(
