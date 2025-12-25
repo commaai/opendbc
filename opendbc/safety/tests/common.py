04cbe273a78a7f5792f70c1f85e04167f5533f89
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2025-12-24 16:23:36 -0800

    not none

--- opendbc/safety/tests/common.py
+++ opendbc/safety/tests/common.py
@@ -808,7 +808,7 @@ class AngleSteeringSafetyTest(VehicleSpeedSafetyTest):
 
 
 class SafetyTest(SafetyTestBase):
-  TX_MSGS: list[list[int]] | None = None
+  TX_MSGS: list[list[int]]
   SCANNED_ADDRS = [*range(0x800),                      # Entire 11-bit CAN address space
                    *range(0x18DA00F1, 0x18DB00F1, 0x100),   # 29-bit UDS physical addressing
                    *range(0x18DB00F1, 0x18DC00F1, 0x100),   # 29-bit UDS functional addressing
@@ -845,7 +845,7 @@ class SafetyTest(SafetyTestBase):
   def test_spam_can_buses(self):
     for bus in range(4):
       for addr in self.SCANNED_ADDRS:
-        if self.TX_MSGS is None or [addr, bus] not in self.TX_MSGS:
+        if [addr, bus] not in self.TX_MSGS:
           self.assertFalse(self._tx(make_msg(bus, addr, 8)), f"allowed TX {addr=} {bus=}")
 
   def test_default_controls_not_allowed(self):
@@ -932,7 +932,7 @@ class SafetyTest(SafetyTestBase):
         msg = make_msg(bus, addr)
         self.safety.set_controls_allowed(1)
         # TODO: this should be blocked
-        if current_test in ["TestNissanSafety", "TestNissanSafetyAltEpsBus", "TestNissanLeafSafety"] and self.TX_MSGS is not None and [addr, bus] in self.TX_MSGS:
+        if current_test in ["TestNissanSafety", "TestNissanSafetyAltEpsBus", "TestNissanLeafSafety"] and [addr, bus] in self.TX_MSGS:
           continue
         self.assertFalse(self._tx(msg), f"transmit of {addr=:#x} {bus=} from {test_name} during {current_test} was allowed")
 
