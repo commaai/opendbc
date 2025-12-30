40a30f27dcf112df47161b6c5d9e234e85bafe2e
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2025-12-29 16:04:22 -0800

    who doesn't have curl

--- setup.sh
+++ setup.sh
@@ -9,9 +9,8 @@ export PYTHONPATH=$BASEDIR
 # *** dependencies install ***
 if [ "$(uname -s)" = "Linux" ]; then
   if ! command -v "mull-runner-17" > /dev/null 2>&1; then
-    sudo apt-get update && sudo apt-get install -y curl clang-17
     curl -1sLf 'https://dl.cloudsmith.io/public/mull-project/mull-stable/setup.deb.sh' | sudo -E bash
-    sudo apt-get update && sudo apt-get install -y mull-17
+    sudo apt-get update && sudo apt-get install -y clang-17 mull-17
   fi
 elif [ "$(uname -s)" = "Darwin" ]; then
   if ! brew list llvm@17 &>/dev/null; then
