9623c5c8bacd1fba19a667ce250250611ac98020
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2025-12-29 16:13:12 -0800

    uv sourcing

--- setup.sh
+++ setup.sh
@@ -31,6 +31,7 @@ fi
 if ! command -v uv &>/dev/null; then
   echo "'uv' is not installed. Installing 'uv'..."
   curl -LsSf https://astral.sh/uv/install.sh | sh
+  source $HOME/.local/bin/env
 fi
 
 export UV_PROJECT_ENVIRONMENT="$BASEDIR/.venv"
