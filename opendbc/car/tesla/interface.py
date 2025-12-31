444e9eb662462bece6a26d16db17abb968ea04c6
Shane Smiskol <shane@smiskol.com>
2025-12-31 13:24:23 -0800

    add diffs

--- opendbc/car/tesla/interface.py
+++ opendbc/car/tesla/interface.py
@@ -13,6 +13,20 @@ class CarInterface(CarInterfaceBase):
   def _get_params(ret: structs.CarParams, candidate, fingerprint, car_fw, alpha_long, is_release, docs) -> structs.CarParams:
     ret.brand = "tesla"
 
+    """
+    Comparing:
+    - FSD 13: 2c912ca5de3b1ee9/000002f7--e9a4e26504
+    - FSD 14: 0f79c454f812791a/000000b6--8c7866d3bf
+    Differences between FSD 13 and 14:
+      - 0x51: 13 is 3 bytes, 14 is 8 bytes
+      - 0x20E: doesn't exist on 13
+      - 0x248: doesn't exist on 13
+      - 0x3E9: doesn't exist on 13
+      - 0x40C: doesn't exist on 13
+      - 0x489: doesn't exist on 13
+      - EPAS3S_sysStatus: bit 52 is 0 on 13, 1 on 14
+    """
+
     ret.safetyConfigs = [get_safety_config(structs.CarParams.SafetyModel.tesla)]
 
     ret.steerLimitTimer = 0.4
