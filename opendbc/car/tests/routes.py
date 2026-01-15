4c96e611140f33f5d5482bc148a6ba4c2a11bf0a
MVL <manuel@alumni.virginia.edu>
2026-01-15 14:35:09 -0500

    adding 25 TLX initial testroute

--- opendbc/car/tests/routes.py
+++ opendbc/car/tests/routes.py
@@ -29,7 +29,6 @@ non_tested_cars = [
   VOLKSWAGEN.PORSCHE_MACAN_MK1,
   # Honda/Acura test routes below expired, replace when CI bucket sync is fixed
   HONDA.ACURA_TLX_2G,
-  HONDA.ACURA_TLX_2G_MMR, # temporary until testdrive
   HONDA.HONDA_NBOX_2G,
   HONDA.ACURA_MDX_4G_MMR,
   HONDA.HONDA_CITY_7G,
@@ -128,6 +127,7 @@ routes = [
   # CarTestRoute("56b2cf1dacdcd033/00000017--d24ffdb376", HONDA.HONDA_CITY_7G),  # Brazilian model
   CarTestRoute("2dc4489d7e1410ca/00000001--bbec3f5117", HONDA.HONDA_CRV_6G),
   CarTestRoute("a703d058f4e05aeb/00000008--f169423024", HONDA.HONDA_PASSPORT_4G),
+  CarTestRoute("ad9840558640c31d/000001ce--095dc79881", HONDA.HONDA.ACURA_TLX_2G_MMR),
 
   CarTestRoute("87d7f06ade479c2e/2023-09-11--23-30-11", HYUNDAI.HYUNDAI_AZERA_6TH_GEN),
   CarTestRoute("66189dd8ec7b50e6/2023-09-20--07-02-12", HYUNDAI.HYUNDAI_AZERA_HEV_6TH_GEN),
