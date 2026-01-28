c79e6758aa8e63b4b793c56c9482ea7e5a8277cd
Shane Smiskol <shane@smiskol.com>
2026-01-27 22:51:45 -0800

    honda: init class variables in __init__

--- opendbc/car/honda/carstate.py
+++ opendbc/car/honda/carstate.py
@@ -48,6 +48,8 @@ class CarState(CarStateBase):
     # When available we use cp.vl["CAR_SPEED"]["ROUGH_CAR_SPEED_2"] to populate vEgoCluster
     # However, on cars without a digital speedometer this is not always present (HRV, FIT, CRV 2016, ILX and RDX)
     self.dash_speed_seen = False
+    self.is_metric = False
+    self.v_cruise_factor = 1.
 
   def update(self, can_parsers) -> structs.CarState:
     cp = can_parsers[Bus.pt]
