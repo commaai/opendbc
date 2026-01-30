fb8e3ae6453811829ce91d39a5082a3131baee00
MVL <manuel@alumni.virginia.edu>
2026-01-29 23:11:10 -0500

    Update to latest community car support

--- opendbc/car/extra_cars.py
+++ opendbc/car/extra_cars.py
@@ -37,9 +37,11 @@ class CAR(Platforms):
 
   EXTRA_HONDA = ExtraPlatformConfig(
     [
+      CommunityCarDocs("Acura ADX 2025-26"),
       CommunityCarDocs("Acura Integra 2023-25"),
       CommunityCarDocs("Acura MDX 2015-16", "Advance Package"),
       CommunityCarDocs("Acura MDX 2017-20"),
+      CommunityCarDocs("Acura MDX Hybrid 2017-20"),
       CommunityCarDocs("Acura MDX 2022-24"),
       CommunityCarDocs("Acura RDX 2022-25"),
       CommunityCarDocs("Acura RLX 2017", "Advance Package or Technology Package"),
@@ -48,6 +50,7 @@ class CAR(Platforms):
       CommunityCarDocs("Acura TLX 2022-23"),
       GMSecurityCarDocs("Acura ZDX 2024"),
       CommunityCarDocs("Honda Accord 2016-17", "Honda Sensing"),
+      CommunityCarDocs("Honda Accord Hybrid 2017"),
       CommunityCarDocs("Honda Clarity 2018-21"),
       GMSecurityCarDocs("Honda Prologue 2024-25"),
     ],
