875d959f1692efb060fbbeaf00d94e5701d3898a
MVL <manuel@alumni.virginia.edu>
2026-02-05 23:12:04 -0500

    Adding Fit 4G, pulling centerToFront from Nidec Fit

--- opendbc/car/honda/values.py
+++ opendbc/car/honda/values.py
@@ -283,6 +283,13 @@ class CAR(Platforms):
     [HondaCarDocs("Acura TLX 2025", "All")],
     CarSpecs(mass=3990 * CV.LB_TO_KG, wheelbase=2.87, centerToFrontRatio=0.43, steerRatio=13.7),
   )
+  HONDA_FIT_4G = HondaBoschPlatformConfig(
+    [HondaCarDocs("Honda Fit (Taiwan) 2025", "All")],
+    CarSpecs(mass=1229, wheelbase=2.53, steerRatio=15.2, centerToFrontRatio=0.39),
+    {Bus.pt: 'honda_bosch_radarless_generated'},
+    flags=HondaFlags.BOSCH_RADARLESS,
+  )
+
 
   # Nidec Cars
   ACURA_ILX = HondaNidecPlatformConfig(
