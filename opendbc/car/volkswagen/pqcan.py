5ccd19a11c7d65fe2981de5c2190445750e3fb79
T3vl <80123588+tealtwo@users.noreply.github.com>
2026-02-16 16:04:05 -0600

    VW PQ: update override

--- opendbc/car/volkswagen/pqcan.py
+++ opendbc/car/volkswagen/pqcan.py
@@ -49,29 +49,32 @@ def create_acc_buttons_control(packer, bus, gra_stock_values, cancel=False, resu
   return packer.make_can_msg("GRA_Neu", bus, values)
 
 
-def acc_control_value(main_switch_on, acc_faulted, long_active):
-  if long_active:
+def acc_control_value(main_switch_on, long_active, cruiseOverride, acc_faulted):
+  if long_active or cruiseOverride:
     acc_control = 1
   elif main_switch_on:
     acc_control = 2
+  elif acc_faulted:
+    acc_control = 3
   else:
     acc_control = 0
 
   return acc_control
 
 
-def acc_hud_status_value(main_switch_on, acc_faulted, long_active):
-  if acc_faulted:
-    hud_status = 6
-  elif long_active:
+def acc_hud_status_value(main_switch_on, acc_faulted, longActive, longOverride):
+  if longOverride:
+    hud_status = 4
+  elif not longOverride and longActive:
     hud_status = 3
+  elif acc_faulted:
+    hud_status = 6
   elif main_switch_on:
     hud_status = 2
   else:
     hud_status = 0
 
   return hud_status
-
 
 def create_acc_accel_control(packer, bus, acc_type, acc_enabled, accel, acc_control, stopping, starting, esp_hold):
   commands = []
