41a1bc2d8a0bec5c29593ede1edde0c11a8f242d
halfbase <halfbase@gmail.com>
2026-02-26 08:06:58 +0800

    Add flags and min_enable_speed to ALPHARD_H_2016 config
    NO_DSU flag and the -1 speed limit

--- opendbc/car/toyota/values.py
+++ opendbc/car/toyota/values.py
@@ -131,6 +131,8 @@ class CAR(Platforms):
     [ToyotaCarDocs("Toyota Alphard Hybrid 2016")],
     CarSpecs(mass=2136., wheelbase=3.00, steerRatio=15.6),
     dbc_dict('toyota_nodsu_pt_generated', 'toyota_adas'),
+    flags=ToyotaFlags.NO_DSU,
+    min_enable_speed=-1,
   )
   TOYOTA_ALPHARD_TSS2 = ToyotaTSS2PlatformConfig(
     [
