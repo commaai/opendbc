73ec8a03b5965e43b592813e575c480d9417a3ef
MVL <manuel@alumni.virginia.edu>
2026-03-02 23:48:23 -0500

    Autodetect reverse_light_msg

--- opendbc/car/honda/carstate.py
+++ opendbc/car/honda/carstate.py
@@ -145,10 +145,8 @@ class CarState(CarStateBase):
     ret.parkingBrake = bool(cp.vl[self.car_state_scm_msg]["PARKING_BRAKE_ON"])
 
     if self.CP.transmissionType == TransmissionType.manual:
-      if self.CP.carFingerprint == CAR.HONDA_FIT:
-        ret.gearShifter = GearShifter.drive # temp override until message found for Nidec
-      else:
-        ret.gearShifter = GearShifter.reverse if bool(cp.vl["SCM_FEEDBACK"]["REVERSE_LIGHT"]) else GearShifter.drive
+      reverse_light_msg = cp.vl["SCM_FEEDBACK"] if "REVERSE_LIGHT" in cp.vl["SCM_FEEDBACK"] else cp.vl["SCM_BUTTONS"]
+      ret.gearShifter = GearShifter.reverse if bool(reverse_light_msg["REVERSE_LIGHT"]) else GearShifter.drive
     else:
       gear_position = self.shifter_values.get(cp.vl[self.gearbox_msg]["GEAR_SHIFTER"], None)
       ret.gearShifter = self.parse_gear_shifter(gear_position)
