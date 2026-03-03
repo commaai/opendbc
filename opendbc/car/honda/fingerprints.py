bd4c9852a3285f606e689f3ad551ab30d00a4284
MVL <manuel@alumni.virginia.edu>
2026-03-03 17:56:22 -0500

    Add 2022 Acura TLX fingerprint

--- opendbc/car/honda/fingerprints.py
+++ opendbc/car/honda/fingerprints.py
@@ -1092,11 +1092,13 @@ FW_VERSIONS = {
     ],
     (Ecu.fwdCamera, 0x18dab5f1, None): [
       b'36161-TGV-A030\x00\x00',
+      b'36161-TGV-A040\x00\x00',
     ],
   },
   CAR.ACURA_TLX_2G_MMR: {
     (Ecu.fwdRadar, 0x18dab0f1, None): [
       b'8S302-TGV-A030\x00\x00',
+      
     ],
     (Ecu.fwdCamera, 0x18dab5f1, None): [
       b'8S102-TGV-A030\x00\x00',
