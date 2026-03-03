d6e830e690367c363aa9cb5132f32cc0ef91706a
MVL <manuel@alumni.virginia.edu>
2026-03-03 01:05:23 -0500

    switch to fingerprint-based reverse light detection due to canbus timing

--- opendbc/car/honda/carstate.py
+++ opendbc/car/honda/carstate.py
@@ -145,7 +145,7 @@ class CarState(CarStateBase):
     ret.parkingBrake = bool(cp.vl[self.car_state_scm_msg]["PARKING_BRAKE_ON"])
 
     if self.CP.transmissionType == TransmissionType.manual:
-      reverse_light_msg = cp.vl["SCM_BUTTONS"] if "REVERSE_LIGHT" in cp.vl["SCM_BUTTONS"] else cp.vl["SCM_FEEDBACK"] 
+      reverse_light_msg = cp.vl["SCM_FEEDBACK"] if self.CP.carFingerprint in HONDA_BOSCH else cp.vl["SCM_BUTTONS"]
       ret.gearShifter = GearShifter.reverse if bool(reverse_light_msg["REVERSE_LIGHT"]) else GearShifter.drive
     else:
       gear_position = self.shifter_values.get(cp.vl[self.gearbox_msg]["GEAR_SHIFTER"], None)
