d8317e120ee2c51c052692111aa698b6a7d71265
MVL <manuel@alumni.virginia.edu>
2026-03-07 16:22:53 -0500

    Add 2023 CRV Fingerprint

--- opendbc/car/honda/fingerprints.py
+++ opendbc/car/honda/fingerprints.py
@@ -1048,6 +1048,7 @@ FW_VERSIONS = {
     ],
     (Ecu.fwdCamera, 0x18dab5f1, None): [
       b'8S102-3A0-A070\x00\x00',
+      b'8S102-3A0-A080\x00\x00',
       b'8S102-3A0-A090\x00\x00',
       b'8S102-3A0-A110\x00\x00',
       b'8S102-3A0-A230\x00\x00',
