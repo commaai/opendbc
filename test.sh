d0273f19255ac36425dc44191ff44db093ccf1b9
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-03-14 18:04:18 -0400

    fix: apply solution for issue #1834

--- test.sh
+++ test.sh
@@ -2,10 +2,11 @@
 set -e
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
-cd $DIR
-
-source ./setup.sh
-
+pip3 install -e .[testing,docs]  # install dependencies
+scons -j8                        # build with 8 cores
+pytest .                         # run the tests
+pytest opendbc/can_common.py     # run specific tests for can_common
+lefthook run lint                # run the linter
 # *** uv lockfile check ***
 uv lock --check
 
