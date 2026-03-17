a1c84bce30bc67171c351a377221fe44aa7bec7e
stefpi <19478336+stefpi@users.noreply.github.com>
2026-03-17 16:25:35 -0700

    pid from cereal messages

--- opendbc/car/body/carcontroller.py
+++ opendbc/car/body/carcontroller.py
@@ -6,22 +6,31 @@ from opendbc.car.common.pid import PIDController
 from opendbc.car.body import bodycan
 from opendbc.car.body.values import SPEED_FROM_RPM
 from opendbc.car.interfaces import CarControllerBase
+from openpilot.common.params import Params
 
-MAX_TORQUE = 500
-MAX_TORQUE_RATE = 50
+MAX_TORQUE = 700
+MAX_TORQUE_RATE = 80
 MAX_ANGLE_ERROR = np.radians(7)
 MAX_POS_INTEGRATOR = 0.2   # meters
 MAX_TURN_INTEGRATOR = 0.1  # meters
 
 
 class CarController(CarControllerBase):
+  PARAM_DEFAULTS = {
+    'BodySpeedPidKp': 110.0,
+    'BodySpeedPidKi': 11.5,
+    'BodyTurnPidKp': 150.0,
+    'BodyTurnPidKi': 15.0,
+  }
+
   def __init__(self, dbc_names, CP):
     super().__init__(dbc_names, CP)
     self.packer = CANPacker(dbc_names[Bus.main])
+    self.params = Params()
 
     # PIDs
-    self.turn_pid = PIDController(110, k_i=11.5, rate=1 / DT_CTRL)
-    self.wheeled_speed_pid = PIDController(110, k_i=11.5, rate=1 / DT_CTRL)
+    self.turn_pid = PIDController(self.PARAM_DEFAULTS['BodyTurnPidKp'], k_i=self.PARAM_DEFAULTS['BodyTurnPidKi'], rate=1 / DT_CTRL)
+    self.wheeled_speed_pid = PIDController(self.PARAM_DEFAULTS['BodySpeedPidKp'], k_i=self.PARAM_DEFAULTS['BodySpeedPidKi'], rate=1 / DT_CTRL)
 
     self.torque_r_filtered = 0.
     self.torque_l_filtered = 0.
@@ -43,7 +52,7 @@ class CarController(CarControllerBase):
       # Read these from the joystick
       # TODO: this isn't acceleration, okay?
       speed_desired = CC.actuators.accel / 5.
-      speed_diff_desired = -CC.actuators.torque / 2.
+      speed_diff_desired = -CC.actuators.torque / 1.5
 
       speed_measured = SPEED_FROM_RPM * (CS.out.wheelSpeeds.fl + CS.out.wheelSpeeds.fr) / 2.
       speed_error = speed_desired - speed_measured
@@ -77,6 +86,21 @@ class CarController(CarControllerBase):
     new_actuators.accel = torque_l
     new_actuators.torque = torque_r
     new_actuators.torqueOutputCan = torque_r
+
+    # Read PID tuning params every ~1s
+    if self.frame % 100 == 0:
+      try:
+        for key, pid, attr in [
+          ('BodySpeedPidKp', self.wheeled_speed_pid, '_k_p'),
+          ('BodySpeedPidKi', self.wheeled_speed_pid, '_k_i'),
+          ('BodyTurnPidKp', self.turn_pid, '_k_p'),
+          ('BodyTurnPidKi', self.turn_pid, '_k_i'),
+        ]:
+          val = self.params.get(key, return_default=True)
+          if val is not None:
+            setattr(pid, attr, [[0], [float(val)]])
+      except Exception:
+        pass
 
     self.frame += 1
     return new_actuators, can_sends
