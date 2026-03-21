b725108b4ee8fe5bce53a762a0d7964ac4f0cf85
MVL <manuel@alumni.virginia.edu>
2026-03-20 21:41:28 -0400

    correct testroute exception

--- opendbc/car/tests/routes.py
+++ opendbc/car/tests/routes.py
@@ -28,7 +28,7 @@ non_tested_cars = [
   SUBARU.SUBARU_FORESTER_HYBRID,
   VOLKSWAGEN.PORSCHE_MACAN_MK1,
   HONDA.ACURA_TLX_2G,
-  HONDA.PILOT_4G_MMR,
+  HONDA.HONDA_PILOT_4G_MMR,
 
   # These had their DSUs unplugged, need new routes
   # TOYOTA.LEXUS_ES  # hybrid
