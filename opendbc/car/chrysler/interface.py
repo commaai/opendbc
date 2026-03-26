7ceb57a9f5c1230265e2161842695d4a535315a2
Jason Young <jyoung8607@gmail.com>
2026-03-26 13:38:38 -0400

    touchup

--- opendbc/car/chrysler/interface.py
+++ opendbc/car/chrysler/interface.py
@@ -18,7 +18,7 @@ class CarInterface(CarInterfaceBase):
   def _get_params(ret: structs.CarParams, candidate, fingerprint, car_fw, alpha_long, is_release, docs) -> structs.CarParams:
     ret.brand = "chrysler"
 
-    # Chrysler CUSW in dashcam pending comma safety validation and a fix for LKAS fault on disengage
+    # TODO: Chrysler CUSW in dashcam pending comma safety validation and a fix for LKAS fault on disengage
     ret.dashcamOnly = candidate in (RAM_HD, CUSW_CARS)
 
     # radar parsing needs some work, see https://github.com/commaai/openpilot/issues/26842
