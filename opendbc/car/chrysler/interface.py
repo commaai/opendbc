cdef0f64290872d39a1f3648d4234299d7237bf5
Jason Young <jyoung8607@gmail.com>
2026-03-26 13:37:51 -0400

    try lat accel controller again

--- opendbc/car/chrysler/interface.py
+++ opendbc/car/chrysler/interface.py
@@ -54,10 +54,6 @@ class CarInterface(CarInterfaceBase):
     # Jeep
     elif candidate == CAR.JEEP_CHEROKEE_5TH_GEN:
       ret.steerActuatorDelay = 0.15
-      ret.lateralTuning.init('pid')
-      ret.lateralTuning.pid.kpBP, ret.lateralTuning.pid.kiBP = [[9., 20.], [9., 20.]]
-      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.15, 0.30], [0.03, 0.05]]
-      ret.lateralTuning.pid.kf = 0.0002
 
     elif candidate in (CAR.JEEP_GRAND_CHEROKEE, CAR.JEEP_GRAND_CHEROKEE_2019):
       ret.steerActuatorDelay = 0.2
