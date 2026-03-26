6ef881b487919bfdd145b2bf5e23016894657bf1
Robbie <robbiethewadley@gmail.com>
2026-03-26 17:09:37 -0600

    fix failing mutation test

--- opendbc/safety/tests/common.py
+++ opendbc/safety/tests/common.py
@@ -177,14 +177,19 @@ class LongitudinalAccelSafetyTest(SafetyTestBase, abc.ABC):
       for accel in np.concatenate((np.arange(min_accel - 1, max_accel + 1, 0.05), [0, self.INACTIVE_ACCEL])):
         accel = round(accel, 2)  # floats might not hit exact boundary conditions without rounding
         for controls_allowed in [True, False]:
-          self.safety.set_controls_allowed(controls_allowed)
-          self.safety.set_alternative_experience(alternative_experience)
-          if self.LONGITUDINAL:
-            should_tx = controls_allowed and min_accel <= accel <= max_accel
-            should_tx = should_tx or accel == self.INACTIVE_ACCEL
-          else:
-            should_tx = False
-          self.assertEqual(should_tx, self._tx(self._accel_msg(accel)))
+          for gas_pressed in [True, False]:
+            self.safety.set_controls_allowed(controls_allowed)
+            self.safety.set_gas_pressed_prev(gas_pressed)
+            self.safety.set_alternative_experience(alternative_experience)
+            if self.LONGITUDINAL:
+              gas_allowed = controls_allowed
+              brake_allowed = controls_allowed and not gas_pressed
+              longitudinal_allowed = gas_allowed if accel >= 0 else brake_allowed
+              should_tx = longitudinal_allowed and min_accel <= accel <= max_accel
+              should_tx = should_tx or accel == self.INACTIVE_ACCEL
+            else:
+              should_tx = False
+            self.assertEqual(should_tx, self._tx(self._accel_msg(accel)))
 
 
 class LongitudinalGasBrakeSafetyTest(SafetyTestBase, abc.ABC):
