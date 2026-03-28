3d58d8ca70c7c0be26947d54d25ae4d60dd9e05e
Alex <alx_sterling@yahoo.com>
2026-03-27 21:30:39 -0600

    Update values.py
    Add Jeep Grand Cherokee SRT Specs to values.py

--- opendbc/car/chrysler/values.py
+++ opendbc/car/chrysler/values.py
@@ -82,6 +82,12 @@ class CAR(Platforms):
     JEEP_GRAND_CHEROKEE.specs,
   )
 
+  JEEP_GRAND_CHEROKEE_SRT_2016 = ChryslerPlatformConfig(  # 
+    [ChryslerCarDocs("Jeep Grand Cherokee 2016-18", video="https://www.youtube.com/watch?v=eLR9o2JkuRk")],
+    ChryslerCarSpecs(mass=1836.0, wheelbase=2.70, steerRatio=13.0),
+  )
+
+
   # Ram
   RAM_1500_5TH_GEN = ChryslerPlatformConfig(
     [ChryslerCarDocs("Ram 1500 2019-24", car_parts=CarParts.common([CarHarness.ram]))],
