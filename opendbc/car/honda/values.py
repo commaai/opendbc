81e544684803407e14eeaafbc0cc2093ee78c694
Jason Young <jyoung8607@gmail.com>
2026-03-27 22:51:09 -0400

    diff reduction

--- opendbc/car/honda/values.py
+++ opendbc/car/honda/values.py
@@ -308,7 +308,7 @@ class CAR(Platforms):
     [HondaCarDocs("Honda Fit 2018-20", min_steer_speed=12. * CV.MPH_TO_MS)],
     CarSpecs(mass=2644 * CV.LB_TO_KG, wheelbase=2.53, steerRatio=13.06, centerToFrontRatio=0.39, tireStiffnessFactor=0.75),
     radar_dbc_dict('acura_ilx_2016_can_generated'),
-    flags=HondaFlags.NIDEC_ALT_SCM_MESSAGES
+    flags=HondaFlags.NIDEC_ALT_SCM_MESSAGES,
   )
   HONDA_FREED = HondaNidecPlatformConfig(
     [HondaCarDocs("Honda Freed 2020", min_steer_speed=12. * CV.MPH_TO_MS)],
