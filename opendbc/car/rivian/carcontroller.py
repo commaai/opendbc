e3a0c5a7487767d17d11eef1f34a0f2479134642
lukasloetkolben <61192133+lukasloetkolben@users.noreply.github.com>
2026-03-27 20:36:47 -0700

    remove fault avoidance and merge it as a standalone
    Co-authored-by: elkoled <elkoled@gmail.com>

--- opendbc/car/rivian/carcontroller.py
+++ opendbc/car/rivian/carcontroller.py
@@ -1,15 +1,10 @@
 import numpy as np
 from opendbc.can import CANPacker
 from opendbc.car import Bus
-from opendbc.car.lateral import apply_driver_steer_torque_limits, common_fault_avoidance
+from opendbc.car.lateral import apply_driver_steer_torque_limits
 from opendbc.car.interfaces import CarControllerBase
 from opendbc.car.rivian.riviancan import create_lka_steering, create_longitudinal, create_wheel_touch, create_adas_status
 from opendbc.car.rivian.values import CarControllerParams, CAR
-
-# EPS faults if you apply torque while the steering angle is above 90 degrees for more than 1 second
-MAX_ANGLE = 90
-MAX_ANGLE_FRAMES = 89
-MAX_ANGLE_CONSECUTIVE_FRAMES = 2
 
 
 class CarController(CarControllerBase):
@@ -17,7 +12,6 @@ class CarController(CarControllerBase):
     super().__init__(dbc_names, CP)
     self.apply_torque_last = 0
     self.packer = CANPacker(dbc_names[Bus.pt])
-    self.angle_limit_counter = 0
 
     self.cancel_frames = 0
 
@@ -33,17 +27,9 @@ class CarController(CarControllerBase):
       apply_torque = apply_driver_steer_torque_limits(new_torque, self.apply_torque_last,
                                                       CS.out.steeringTorque, CarControllerParams, steer_max)
 
-    # >90 degree steering fault prevention
-    self.angle_limit_counter, apply_steer_req = common_fault_avoidance(abs(CS.out.steeringAngleDeg) >= MAX_ANGLE, CC.latActive,
-                                                                       self.angle_limit_counter, MAX_ANGLE_FRAMES,
-                                                                       MAX_ANGLE_CONSECUTIVE_FRAMES)
-    torque_fault = CC.latActive and not apply_steer_req
-    if not CC.latActive or torque_fault:
-      apply_torque = 0
-
     # send steering command
     self.apply_torque_last = apply_torque
-    can_sends.append(create_lka_steering(self.packer, self.frame, CS.acm_lka_hba_cmd, apply_torque, CC.enabled, apply_steer_req))
+    can_sends.append(create_lka_steering(self.packer, self.frame, CS.acm_lka_hba_cmd, apply_torque, CC.enabled, CC.latActive))
 
     if self.frame % 5 == 0 and self.CP.carFingerprint == CAR.RIVIAN_R1_GEN1:
       can_sends.append(create_wheel_touch(self.packer, CS.sccm_wheel_touch, CC.enabled))
