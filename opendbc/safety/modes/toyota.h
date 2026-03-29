8b28ac6b18df4178fd0352a31b26997d7a62b6bc
Devansh Varshney <varshney.devansh614@gmail.com>
2026-03-29 18:27:48 +0530

    cppcheck: replace bool 0 assignment with false in toyota safety

--- opendbc/safety/modes/toyota.h
+++ opendbc/safety/modes/toyota.h
@@ -331,7 +331,7 @@ static bool toyota_tx_hook(const CANPacket_t *msg) {
     // this address is sub-addressed. only allow tester present to radar (0xF)
     bool invalid_uds_msg = (GET_BYTES(msg, 0, 4) != 0x003E020FU) || (GET_BYTES(msg, 4, 4) != 0x0U);
     if (invalid_uds_msg) {
-      tx = 0;
+      tx = false;
     }
   }
 
