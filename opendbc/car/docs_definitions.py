44a09ce24ab5c1920eb52ec591c853831bb63d2f
Shane Smiskol <shane@smiskol.com>
2026-04-03 15:42:13 -0700

    capital

--- opendbc/car/docs_definitions.py
+++ opendbc/car/docs_definitions.py
@@ -140,7 +140,7 @@ class CarHarness(EnumBase):
   ford_q3 = BaseCarHarness("Ford Q3 connector")
   ford_q4 = BaseCarHarness("Ford Q4 connector", parts=[Accessory.harness_box, Accessory.comma_power, Cable.long_obdc_cable])
   rivian_a = BaseCarHarness("Rivian A connector", parts=[Accessory.harness_box, Accessory.comma_power, Cable.long_obdc_cable])
-  rivian_b = BaseCarHarness("Rivian b connector", parts=[Accessory.harness_box, Accessory.comma_power, Cable.long_obdc_cable])
+  rivian_b = BaseCarHarness("Rivian B connector", parts=[Accessory.harness_box, Accessory.comma_power, Cable.long_obdc_cable])
   tesla_a = BaseCarHarness("Tesla A connector", parts=[Accessory.harness_box, Cable.long_obdc_cable])
   tesla_b = BaseCarHarness("Tesla B connector", parts=[Accessory.harness_box, Cable.long_obdc_cable])
   psa_a = BaseCarHarness("PSA A connector", parts=[Accessory.harness_box, Cable.long_obdc_cable])
