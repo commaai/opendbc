c0c3c7be69ab349e6d2b5e04ccd1bdd6ce2c5c10
Shane Smiskol <shane@smiskol.com>
2026-04-03 15:44:21 -0700

    we don't have a driving video?!

--- opendbc/car/rivian/values.py
+++ opendbc/car/rivian/values.py
@@ -28,7 +28,6 @@ class ModelYear(StrEnum):
 @dataclass
 class RivianCarDocs(CarDocs):
   package: str = "All"
-  setup_video: str = "https://youtu.be/uaISd1j7Z4U"
 
 
 @dataclass
@@ -50,9 +49,9 @@ class RivianSafetyFlags(IntFlag):
 class CAR(Platforms):
   RIVIAN_R1 = RivianPlatformConfig(
     [
-      RivianCarDocs("Rivian R1S 2022-24", car_parts=CarParts.common([CarHarness.rivian_a])),
+      RivianCarDocs("Rivian R1S 2022-24", setup_video="https://youtu.be/uaISd1j7Z4U", car_parts=CarParts.common([CarHarness.rivian_a])),
       RivianCarDocs("Rivian R1S 2025", car_parts=CarParts.common([CarHarness.rivian_b])),
-      RivianCarDocs("Rivian R1T 2022-24", car_parts=CarParts.common([CarHarness.rivian_a])),
+      RivianCarDocs("Rivian R1T 2022-24", setup_video="https://youtu.be/uaISd1j7Z4U", car_parts=CarParts.common([CarHarness.rivian_a])),
       RivianCarDocs("Rivian R1T 2025", car_parts=CarParts.common([CarHarness.rivian_b])),
     ],
     CarSpecs(mass=3206., wheelbase=3.08, steerRatio=15.2),
