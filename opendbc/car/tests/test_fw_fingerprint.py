2c0b932064d5bee85e469049b593c03579b10869
stefpi <19478336+stefpi@users.noreply.github.com>
2026-04-07 15:19:20 -0700

    increase timout on test_fw_query_timing for body

--- opendbc/car/tests/test_fw_fingerprint.py
+++ opendbc/car/tests/test_fw_fingerprint.py
@@ -262,10 +262,10 @@ class TestFwFingerprintTiming(unittest.TestCase):
         print(f'get_vin {name} case, query time={self.total_time / self.N} seconds')
 
   def test_fw_query_timing(self):
-    total_ref_time = 7.4
+    total_ref_time = 7.5
     brand_ref_times = {
       'gm': 1.0,
-      'body': 0.1,
+      'body': 0.2,
       'chrysler': 0.3,
       'ford': 1.5,
       'honda': 0.45,
