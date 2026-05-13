b82518388317b81ef1691c8d710fcd01986865c6
elkoled <elkoled@gmail.com>
2026-05-12 19:46:01 -0700

    clean tests

--- opendbc/safety/tests/test_ignition.py
+++ opendbc/safety/tests/test_ignition.py
@@ -14,70 +14,75 @@ class TestIgnitionHook(unittest.TestCase):
     self.rivian_packer = CANPackerSafety("rivian_primary_actuator")
     self.tesla_packer = CANPackerSafety("tesla_model3_party")
 
+  def _gm_msg(self, mode):
+    return self.gm_packer.make_can_msg_safety("BCMGeneralPlatformStatus", 0,
+                                              {"SystemPowerMode": mode})
+
+  def _rivian_msg(self, counter, mode):
+    return self.rivian_packer.make_can_msg_safety("VDM_OutputSignals", 0,
+                                                  {"VDM_OutputSigs_Counter": counter,
+                                                   "VDM_EpasPowerMode": mode})
+
+  def _tesla_msg(self, counter, state):
+    return self.tesla_packer.make_can_msg_safety("VCFRONT_LVPowerState", 0,
+                                                 {"VCFRONT_LVPowerStateCounter": counter,
+                                                  "VCFRONT_vehiclePowerState": state})
+
+  def _mazda_msg(self, byte0):
+    return make_msg(0, 0x9E, dat=bytes([byte0]) + b"\x00" * 7)
+
   # GM: SystemPowerMode 2=Run, 3=Crank Request
   def test_gm_ignition_on(self):
-    self.safety.ignition_can_hook(self.gm_packer.make_can_msg_safety("BCMGeneralPlatformStatus", 0, {"SystemPowerMode": 2}))
+    self.safety.ignition_can_hook(self._gm_msg(2))
     self.assertTrue(self.safety.get_ignition_can())
 
   def test_gm_ignition_off(self):
-    self.safety.ignition_can_hook(self.gm_packer.make_can_msg_safety("BCMGeneralPlatformStatus", 0, {"SystemPowerMode": 2}))
+    self.safety.ignition_can_hook(self._gm_msg(2))
     self.assertTrue(self.safety.get_ignition_can())
-    self.safety.ignition_can_hook(self.gm_packer.make_can_msg_safety("BCMGeneralPlatformStatus", 0, {"SystemPowerMode": 0}))
+    self.safety.ignition_can_hook(self._gm_msg(0))
     self.assertFalse(self.safety.get_ignition_can())
 
-  # Rivian: VDM_EpasPowerMode_Drive_On=1
+  # Rivian: VDM_EpasPowerMode_Drive_On=1 (counter-gated)
   def test_rivian_ignition_on(self):
     for i in range(15):
       self.safety.init_tests()
-      self.safety.ignition_can_hook(self.rivian_packer.make_can_msg_safety("VDM_OutputSignals", 0,
-                                                                           {"VDM_OutputSigs_Counter": i, "VDM_EpasPowerMode": 1}))
+      self.safety.ignition_can_hook(self._rivian_msg(i, 1))
       self.assertFalse(self.safety.get_ignition_can())
-      self.safety.ignition_can_hook(self.rivian_packer.make_can_msg_safety("VDM_OutputSignals", 0,
-                                                                           {"VDM_OutputSigs_Counter": (i + 1) % 15, "VDM_EpasPowerMode": 1}))
+      self.safety.ignition_can_hook(self._rivian_msg((i + 1) % 15, 1))
       self.assertTrue(self.safety.get_ignition_can())
 
   def test_rivian_ignition_off(self):
-    self.safety.ignition_can_hook(self.rivian_packer.make_can_msg_safety("VDM_OutputSignals", 0,
-                                                                         {"VDM_OutputSigs_Counter": 0, "VDM_EpasPowerMode": 1}))
-    self.safety.ignition_can_hook(self.rivian_packer.make_can_msg_safety("VDM_OutputSignals", 0,
-                                                                         {"VDM_OutputSigs_Counter": 1, "VDM_EpasPowerMode": 1}))
+    self.safety.ignition_can_hook(self._rivian_msg(0, 1))
+    self.safety.ignition_can_hook(self._rivian_msg(1, 1))
     self.assertTrue(self.safety.get_ignition_can())
-    self.safety.ignition_can_hook(self.rivian_packer.make_can_msg_safety("VDM_OutputSignals", 0,
-                                                                         {"VDM_OutputSigs_Counter": 2, "VDM_EpasPowerMode": 0}))
-    self.safety.ignition_can_hook(self.rivian_packer.make_can_msg_safety("VDM_OutputSignals", 0,
-                                                                         {"VDM_OutputSigs_Counter": 3, "VDM_EpasPowerMode": 0}))
+    self.safety.ignition_can_hook(self._rivian_msg(2, 0))
+    self.safety.ignition_can_hook(self._rivian_msg(3, 0))
     self.assertFalse(self.safety.get_ignition_can())
 
-  # Tesla: VEHICLE_POWER_STATE_DRIVE=3
+  # Tesla: VEHICLE_POWER_STATE_DRIVE=3 (counter-gated)
   def test_tesla_ignition_on(self):
-    self.safety.ignition_can_hook(self.tesla_packer.make_can_msg_safety("VCFRONT_LVPowerState", 0,
-                                                                        {"VCFRONT_LVPowerStateCounter": 0, "VCFRONT_vehiclePowerState": 3}))
+    self.safety.ignition_can_hook(self._tesla_msg(0, 3))
     self.assertFalse(self.safety.get_ignition_can())
-    self.safety.ignition_can_hook(self.tesla_packer.make_can_msg_safety("VCFRONT_LVPowerState", 0,
-                                                                        {"VCFRONT_LVPowerStateCounter": 1, "VCFRONT_vehiclePowerState": 3}))
+    self.safety.ignition_can_hook(self._tesla_msg(1, 3))
     self.assertTrue(self.safety.get_ignition_can())
 
   def test_tesla_ignition_off(self):
-    self.safety.ignition_can_hook(self.tesla_packer.make_can_msg_safety("VCFRONT_LVPowerState", 0,
-                                                                        {"VCFRONT_LVPowerStateCounter": 0, "VCFRONT_vehiclePowerState": 3}))
-    self.safety.ignition_can_hook(self.tesla_packer.make_can_msg_safety("VCFRONT_LVPowerState", 0,
-                                                                        {"VCFRONT_LVPowerStateCounter": 1, "VCFRONT_vehiclePowerState": 3}))
+    self.safety.ignition_can_hook(self._tesla_msg(0, 3))
+    self.safety.ignition_can_hook(self._tesla_msg(1, 3))
     self.assertTrue(self.safety.get_ignition_can())
-    self.safety.ignition_can_hook(self.tesla_packer.make_can_msg_safety("VCFRONT_LVPowerState", 0,
-                                                                        {"VCFRONT_LVPowerStateCounter": 2, "VCFRONT_vehiclePowerState": 2}))
-    self.safety.ignition_can_hook(self.tesla_packer.make_can_msg_safety("VCFRONT_LVPowerState", 0,
-                                                                        {"VCFRONT_LVPowerStateCounter": 3, "VCFRONT_vehiclePowerState": 2}))
+    self.safety.ignition_can_hook(self._tesla_msg(2, 2))
+    self.safety.ignition_can_hook(self._tesla_msg(3, 2))
     self.assertFalse(self.safety.get_ignition_can())
 
-  # Mazda: 0x9E byte 0 high 3 bits == 6
+  # Mazda: 0x9E byte 0 high 3 bits == 6 (0xC0)
   def test_mazda_ignition_on(self):
-    self.safety.ignition_can_hook(make_msg(0, 0x9E, dat=b"\xC0" + b"\x00" * 7))
+    self.safety.ignition_can_hook(self._mazda_msg(0xC0))
     self.assertTrue(self.safety.get_ignition_can())
 
   def test_mazda_ignition_off(self):
-    self.safety.ignition_can_hook(make_msg(0, 0x9E, dat=b"\xC0" + b"\x00" * 7))
+    self.safety.ignition_can_hook(self._mazda_msg(0xC0))
     self.assertTrue(self.safety.get_ignition_can())
-    self.safety.ignition_can_hook(make_msg(0, 0x9E, dat=b"\x20" + b"\x00" * 7))
+    self.safety.ignition_can_hook(self._mazda_msg(0x20))
     self.assertFalse(self.safety.get_ignition_can())
 
   def test_wrong_bus_ignored(self):
