567ec80414531cdba9633a8d056610c57f4e7fbd
elkoled <elkoled@gmail.com>
2026-05-29 01:02:21 -0700

    no ford

--- opendbc/car/ford/carcontroller.py
+++ opendbc/car/ford/carcontroller.py
@@ -2,13 +2,17 @@ import math
 import numpy as np
 from opendbc.can import CANPacker
 from opendbc.car import ACCELERATION_DUE_TO_GRAVITY, Bus, DT_CTRL, apply_hysteresis, structs
-from opendbc.car.lateral import MAX_LATERAL_ACCEL, apply_std_steer_angle_limits
+from opendbc.car.lateral import ISO_LATERAL_ACCEL, AVERAGE_ROAD_ROLL, apply_std_steer_angle_limits
 from opendbc.car.ford import fordcan
 from opendbc.car.ford.values import CarControllerParams, FordFlags, CAR
 from opendbc.car.interfaces import CarControllerBase, V_CRUISE_MAX
 
 LongCtrlState = structs.CarControl.Actuators.LongControlState
 VisualAlert = structs.CarControl.HUDControl.VisualAlert
+
+# CAN FD limits:
+# Limit to average banked road since safety doesn't have the roll
+MAX_LATERAL_ACCEL = ISO_LATERAL_ACCEL - (ACCELERATION_DUE_TO_GRAVITY * AVERAGE_ROAD_ROLL)  # ~2.4 m/s^2
 
 
 def anti_overshoot(apply_curvature, apply_curvature_last, v_ego):
