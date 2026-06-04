99f66e15a7f8351c8a4cf16901b976b4003bb8cd
John Belmonte <john@neggie.net>
2026-06-03 22:40:21 -0700

    move conditions for belowEngageSpeed into opendbc
    Previously belowEngageSpeed conditions where calculated by car_specific.py.
    Added CarState.belowEngageSpeed and moved conditions into carstate logic.

--- opendbc/car/volkswagen/carstate.py
+++ opendbc/car/volkswagen/carstate.py
@@ -140,6 +140,7 @@ class CarState(CarStateBase):
     ret.buttonEvents = self.create_button_events(pt_cp, self.CCP.BUTTONS)
 
     ret.lowSpeedAlert = self.update_low_speed_alert(ret.vEgo)
+    ret.belowEngageSpeed = self.CP.openpilotLongitudinalControl and ret.vEgo < self.CP.minEnableSpeed + 0.5
 
     self.frame += 1
     return ret
@@ -230,6 +231,7 @@ class CarState(CarStateBase):
     ret.espDisabled = bool(pt_cp.vl["Bremse_1"]["BR1_ESPASR_passive"])
 
     ret.lowSpeedAlert = self.update_low_speed_alert(ret.vEgo)
+    ret.belowEngageSpeed = self.CP.openpilotLongitudinalControl and ret.vEgo < self.CP.minEnableSpeed + 0.5
 
     self.frame += 1
     return ret
@@ -369,6 +371,8 @@ class CarState(CarStateBase):
 
     ret.cruiseState.standstill = self.CP.pcmCruise and self.esp_hold_confirmation
     ret.standstill = ret.vEgoRaw == 0
+
+    ret.belowEngageSpeed = self.CP.openpilotLongitudinalControl and ret.vEgo < self.CP.minEnableSpeed + 0.5
 
     self.frame += 1
     return ret
