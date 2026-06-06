5290721d4dad07ec0efd3c78420c28e184d71fe1
elkoled <elkoled@gmail.com>
2026-06-06 10:07:11 -0700

    test

--- opendbc/car/volkswagen/interface.py
+++ opendbc/car/volkswagen/interface.py
@@ -128,13 +128,9 @@ class CarInterface(CarInterfaceBase):
       ret.steerActuatorDelay = 0.07
 
     ret.pcmCruise = not ret.openpilotLongitudinalControl
-    if ret.flags & VolkswagenFlags.MEB:
-      ret.startingState = True
-      ret.startAccel = 0.8
-    else:
-      ret.stopAccel = -0.55
-      ret.vEgoStarting = 0.1
-      ret.vEgoStopping = 0.5
+    ret.stopAccel = -0.55
+    ret.vEgoStarting = 0.1
+    ret.vEgoStopping = 0.5
     ret.autoResumeSng = ret.minEnableSpeed == -1
 
     CAN = CanBus(fingerprint=fingerprint)
