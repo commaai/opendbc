1f8b48c08f22cb2deebffccf9d3abb6f85e2a81f
autodev-botbit <hutianci20@outlook.com>
2026-06-15 01:23:57 +0800

    fix: [$10k bounty] Ford F-150 2026 (TRON) support (closes #3426)

---
+++ opendbc/car/ford/__init__.py
@@ -0,0 +1,8 @@
+from opendbc.car.ford.values import CAR, PLATFORMS
+from opendbc.car.ford.carcontroller import CarController
+from opendbc.car.ford.carstate import CarState
+from opendbc.car.ford.interface import CarInterface
+
+
+def get_car_interface():
+  return CarInterface
