abc5c7647a4b72aed8d9e47226a8dd021918ae64
T3vl <80123588+tealtwo@users.noreply.github.com>
2026-07-14 08:31:49 -0500

    VW MQB: use torque controller instead of PID controller

--- opendbc/car/volkswagen/interface.py
+++ opendbc/car/volkswagen/interface.py
@@ -104,11 +104,7 @@ class CarInterface(CarInterfaceBase):
       ret.steerActuatorDelay = 0.3
     else:
       ret.steerActuatorDelay = 0.1
-      ret.lateralTuning.pid.kpBP = [0.]
-      ret.lateralTuning.pid.kiBP = [0.]
-      ret.lateralTuning.pid.kf = 0.00006
-      ret.lateralTuning.pid.kpV = [0.6]
-      ret.lateralTuning.pid.kiV = [0.2]
+      CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
 
     # Global longitudinal tuning defaults, can be overridden per-vehicle
 
