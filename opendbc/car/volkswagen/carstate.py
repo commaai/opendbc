193d7f68bc1c4e5ede7e5f756e7cec35abfc4a29
Shane Smiskol <shane@smiskol.com>
2026-08-03 16:25:15 -0700

    VW MEB: don't check camera ACC fault (#3636)
    we only added this to catch a fault when radar is covered up, but now op long will be default, and radar interface catches it

--- opendbc/car/volkswagen/carstate.py
+++ opendbc/car/volkswagen/carstate.py
@@ -298,9 +298,7 @@ class CarState(CarStateBase):
     tsk_faulted = pt_cp.vl["Motor_51"]["TSK_Status"] in (6, 7)
     engine_off = pt_cp.vl["Motor_54"]["Engine_On"] == 0
     long_control_inhibit = pt_cp.vl["VMM_02"]["Long_Control_Inhibit"] == 2
-    # TODO: check permanent camera fault, it happens too often right now
-    ret.accFaulted = (self.update_acc_fault(tsk_faulted, engine_off, long_control_inhibit) or
-                      ext_cp.vl["ACC_18"]["ACC_Status_ACC"] == 6)  # reversible fault in ACC system
+    ret.accFaulted = self.update_acc_fault(tsk_faulted, engine_off, long_control_inhibit)
 
     # TSK winds braking down through brake_only after driver brakes at low speeds. Requesting drive-off in this
     # state can fault TSK, and stock refuses to engage here as well, so block entry until it clears.
