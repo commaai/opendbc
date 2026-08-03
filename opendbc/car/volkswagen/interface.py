dda00632a20421c5b7e4f4a9cff1fffbc189cc90
Shane Smiskol <shane@smiskol.com>
2026-08-03 01:45:27 -0700

    fix

--- opendbc/car/volkswagen/interface.py
+++ opendbc/car/volkswagen/interface.py
@@ -117,7 +117,7 @@ class CarInterface(CarInterfaceBase):
 
     if ret.flags & VolkswagenFlags.MEB:
       ret.openpilotLongitudinalControl = True
-      ret.longitudinalActuatorDelay = 0.5
+      ret.longitudinalActuatorDelay = 0.3
       ret.longitudinalTuning.kiBP = [0., 30.]
       ret.longitudinalTuning.kiV = [0.4, 0.]
     else:
