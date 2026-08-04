9bad7e5dbd0c4ff8ac4123d10589aaa0eea015e2
Shane Smiskol <shane@smiskol.com>
2026-08-03 18:57:52 -0700

    VW MEB: update longitudinal actuator delay (#3641)
    meas from routes

--- opendbc/car/volkswagen/interface.py
+++ opendbc/car/volkswagen/interface.py
@@ -116,7 +116,7 @@ class CarInterface(CarInterfaceBase):
     # Global longitudinal tuning defaults, can be overridden per-vehicle
 
     if ret.flags & VolkswagenFlags.MEB:
-      ret.longitudinalActuatorDelay = 0.5
+      ret.longitudinalActuatorDelay = 0.3
       ret.longitudinalTuning.kiBP = [0., 30.]
       ret.longitudinalTuning.kiV = [0.4, 0.]
 
