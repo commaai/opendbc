f72f81d69cd42556f20fbfe6241a8c7f87ba9729
Upendra Singh Chauhan <upendrachauhanrbt@gmail.com>
2026-08-22 17:52:07 +0530

    Agent: Automated fix for issue #3695

---
+++ solution.py
@@ -0,0 +1,8 @@
+from openpilot.selfdrive.car.toyota.values import CAR, TOYOTA_STT
+
+CAR.TOYOTA_TSS3 = [
+  CAR.PRIUS_TSS3,
+]
+
+# Add any necessary definitions to support TSS3 vehicles like the Prius
+TOYOTA_STT.add(CAR.PRIUS_TSS3)
