9a3f69c2c7ecfbec976a5cbaa35efe8fd0a7a6b0
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2026-09-16 16:30:48 -0700

    rm that

--- opendbc/car/byd/values.py
+++ opendbc/car/byd/values.py
@@ -1,13 +1,9 @@
 from dataclasses import dataclass, field
-from enum import IntFlag, StrEnum
 
-from opendbc.car import ACCELERATION_DUE_TO_GRAVITY, Bus, CarSpecs, DbcDict, PlatformConfig, Platforms, structs
+from opendbc.car import ACCELERATION_DUE_TO_GRAVITY, Bus, CarSpecs, DbcDict, PlatformConfig, Platforms
 from opendbc.car.lateral import AngleSteeringLimitsVM, ISO_LATERAL_ACCEL
 from opendbc.car.docs_definitions import CarDocs, CarHarness, CarParts
 from opendbc.car.fw_query_definitions import FwQueryConfig
-from opendbc.car.vin import Vin
-
-Ecu = structs.CarParams.Ecu
 
 
 # Add extra tolerance for average banked road since safety doesn't have the roll
@@ -35,21 +31,6 @@ class CarControllerParams:
   STEER_DRIVER_DISENGAGE = 30  # EPS torque threshold for hard disengage
 
 
-class BydSafetyFlags(IntFlag):
-  LONG_CONTROL = 1
-
-
-class WMI(StrEnum):
-  BYD_AUTO = "LGX"  # BYD Auto Co., Ltd. (Shenzhen)
-
-
-class ModelYear(StrEnum):
-  N_2022 = "N"
-  P_2023 = "P"
-  R_2024 = "R"
-  S_2025 = "S"
-
-
 @dataclass
 class BydCarDocs(CarDocs):
   package: str = "All"
@@ -61,37 +42,18 @@ class BydPlatformConfig(PlatformConfig):
   dbc_dict: DbcDict = field(default_factory=lambda: {
     Bus.pt: 'byd_atto3',
   })
-  wmis: set[WMI] = field(default_factory=set)
-  years: set[ModelYear] = field(default_factory=set)
 
 
 class CAR(Platforms):
   BYD_ATTO_3 = BydPlatformConfig(
     [BydCarDocs("BYD Atto 3 2022-25")],
     CarSpecs(mass=1750, wheelbase=2.72, steerRatio=14.8),
-    wmis={WMI.BYD_AUTO},
-    years={ModelYear.N_2022, ModelYear.P_2023, ModelYear.R_2024, ModelYear.S_2025},
   )
-
-
-def match_fw_to_car_fuzzy(live_fw_versions, vin, offline_fw_versions) -> set[str]:
-  # BYD Atto 3 VIN: LGX (WMI) + <VDS> + <year><plant><seq> (VIS).
-  # TODO: currently we only match on WMI + model year
-  vin_obj = Vin(vin)
-  year = vin_obj.vis[:1]
-
-  candidates = set()
-  for platform in CAR:
-    if vin_obj.wmi in platform.config.wmis and year in platform.config.years:
-      candidates.add(platform)
-
-  return {str(c) for c in candidates}
 
 
 FW_QUERY_CONFIG = FwQueryConfig(
   requests=[],
   fw_version_regex=b"",
-  match_fw_to_car_fuzzy=match_fw_to_car_fuzzy,
 )
 
 
