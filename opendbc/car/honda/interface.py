a477f06c0f096b06d2525f87beb09f24224f2971
Kristian Feldsam <feldsam@gmail.com>
2026-09-16 05:19:16 +0200

    Honda: add Prelude 2026 (HONDA_PRELUDE_6G) (#3624)

--- opendbc/car/honda/interface.py
+++ opendbc/car/honda/interface.py
@@ -102,7 +102,7 @@ class CarInterface(CarInterfaceBase):
       if candidate == CAR.HONDA_CIVIC_BOSCH:
           CarControllerParams.BOSCH_GAS_LOOKUP_V = [0, 750]
 
-    elif candidate == CAR.HONDA_CIVIC_2022:
+    elif candidate in (CAR.HONDA_CIVIC_2022, CAR.HONDA_PRELUDE_6G):
       ret.lateralTuning.pid.kpBP, ret.lateralTuning.pid.kpV = [[0, 10], [0.05, 0.5]]
       ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kiV = [[0, 10], [0.0125, 0.125]]
 
